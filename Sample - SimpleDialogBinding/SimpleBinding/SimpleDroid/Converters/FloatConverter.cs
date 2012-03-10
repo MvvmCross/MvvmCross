29108146b66199f05d658b337f340568ae2a9fbd
Stuart <lodge.stuart@gmail.com>
2012-03-10 11:49:09 +0000

    formatting

--- Sample - SimpleDialogBinding/SimpleBinding/SimpleDroid/Converters/FloatConverter.cs
+++ Sample - SimpleDialogBinding/SimpleBinding/SimpleDroid/Converters/FloatConverter.cs
@@ -5,13 +5,19 @@ namespace SimpleDroid.Converters
     public class FloatConverter
         : MvxBaseValueConverter
     {
-        public override object Convert(object value, System.Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        public override object Convert(object value, 
+            System.Type targetType, 
+            object parameter, 
+            System.Globalization.CultureInfo culture)
         {
-            var strValue = (float)value;
-            return strValue.ToString(System.Globalization.CultureInfo.CurrentUICulture);
+            var floatValue = (float)value;
+            return floatValue.ToString(System.Globalization.CultureInfo.CurrentUICulture);
         }
 
-        public override object ConvertBack(object value, System.Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        public override object ConvertBack(object value, 
+            System.Type targetType, 
+            object parameter, 
+            System.Globalization.CultureInfo culture)
         {
             var strValue = (string)value;
             return float.Parse(strValue, System.Globalization.CultureInfo.CurrentUICulture);
