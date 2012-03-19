bbef21dbe16b2670d99237892c720fb80590ad1e
Stuart <lodge.stuart@gmail.com>
2012-03-19 13:52:12 +0000

    Console example

--- Sample - CustomerManagement/CustomerManagement/CustomerManagement.Console/Program.cs
+++ Sample - CustomerManagement/CustomerManagement/CustomerManagement.Console/Program.cs
@@ -1,46 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Runtime.Remoting.Contexts;
 using System.Text;
-using Cirrious.MvvmCross.Application;
 using Cirrious.MvvmCross.Console.Interfaces;
-using Cirrious.MvvmCross.Console.Platform;
 using Cirrious.MvvmCross.ExtensionMethods;
 using Cirrious.MvvmCross.Interfaces.ServiceProvider;
 using Cirrious.MvvmCross.Interfaces.ViewModels;
 using CustomerManagement;
-using CustomerManagement.Console.Views;
-using CustomerManagement.Core.ViewModels;
 
 namespace CustomerManagement.Console
 {
-    public class Setup
-        : MvxBaseConsoleSetup
-        , IMvxServiceProducer<IMvxStartNavigation>
-    {
-        #region Overrides of MvxBaseSetup
-
-        protected override MvxApplication CreateApp()
-        {
-            var app = new CustomerManagement.Core.App();
-            return app;
-        }
-
-        protected override IDictionary<Type, Type> GetViewModelViewLookup()
-        {
-            return new Dictionary<Type, Type>()
-                       {
-                            { typeof(CustomerListViewModel), typeof(CustomerListView)},
-                            { typeof(DetailsCustomerViewModel), typeof(CustomerView)},
-                            { typeof(EditCustomerViewModel), typeof(CustomerEditView)},
-                            { typeof(NewCustomerViewModel), typeof(CustomerNewView)},
-                       };
-        }
-
-        #endregion
-    }    
-
     class Program
         : IMvxServiceConsumer<IMvxStartNavigation>
         , IMvxServiceConsumer<IMvxMessagePump>
