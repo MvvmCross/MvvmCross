b2c1d1192a9f1a2684d4cb67819ed78011f8b794
Stuart <lodge.stuart@gmail.com>
2012-05-04 07:56:44 +0100

    Added a generic simple binding activity

--- Cirrious/Cirrious.MvvmCross.Binding/Android/Simple/MvxSimpleBindingActivity.cs
+++ Cirrious/Cirrious.MvvmCross.Binding/Android/Simple/MvxSimpleBindingActivity.cs
@@ -29,4 +29,20 @@ namespace Cirrious.MvvmCross.Binding.Android.Simple
             // ignored  here
         }
     }
+
+    public class MvxSimpleBindingActivity<TViewModel>
+        : MvxBindingActivityView<MvxNullViewModel>
+    {
+        public new TViewModel ViewModel { get; set; }
+
+        public override object DefaultBindingSource
+        {
+            get { return ViewModel; }
+        }
+
+        protected sealed override void OnViewModelSet()
+        {
+            // ignored  here
+        }
+    }
 }
