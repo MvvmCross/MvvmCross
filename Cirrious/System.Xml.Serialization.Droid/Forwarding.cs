611556cc2ef3846af03ffe882b3d0b857aefc8d7
Stuart <lodge.stuart@gmail.com>
2012-05-07 11:14:58 +0100

    Force build to work with another forwarding dll - this time System.Xml.Serialization

---
+++ Cirrious/System.Xml.Serialization.Droid/Forwarding.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.ServiceModel;
+using System.Text;
+using System.Xml.Serialization;
+
+
+[assembly: TypeForwardedTo(typeof(XmlAnyElementAttributes))]
+[assembly: TypeForwardedTo(typeof(XmlArrayItemAttributes))]
+[assembly: TypeForwardedTo(typeof(XmlAttributeOverrides))]
+[assembly: TypeForwardedTo(typeof(XmlAttributes))]
+[assembly: TypeForwardedTo(typeof(XmlElementAttributes))]
+[assembly: TypeForwardedTo(typeof(XmlMapping))]
+[assembly: TypeForwardedTo(typeof(XmlMappingAccess))]
+[assembly: TypeForwardedTo(typeof(XmlSerializer))]
+[assembly: TypeForwardedTo(typeof(XmlSerializerNamespaces))]
+[assembly: TypeForwardedTo(typeof(XmlTypeMapping))]
+
+[assembly: TypeForwardedTo(typeof(XmlSerializerFormatAttribute))]
