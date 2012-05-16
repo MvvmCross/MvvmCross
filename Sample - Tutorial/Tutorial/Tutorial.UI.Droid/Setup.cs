249873afeeed43950f02490c45d561a83cc60a22
Stuart <lodge.stuart@gmail.com>
2012-05-16 11:55:02 +0100

    Samples all now use the new Setup creation scheme - improved lifecycle.

--- Sample - Tutorial/Tutorial/Tutorial.UI.Droid/Setup.cs
+++ Sample - Tutorial/Tutorial/Tutorial.UI.Droid/Setup.cs
@@ -1,18 +1,11 @@
 using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Reflection;
+using Android.App;
 using Android.Content;
-using Cirrious.MvvmCross.Android.Platform;
 using Cirrious.MvvmCross.Application;
 using Cirrious.MvvmCross.Binding.Android;
-using Cirrious.MvvmCross.Binding.Binders;
 using Tutorial.Core;
 using Tutorial.Core.Converters;
-using Tutorial.Core.ViewModels;
-using Tutorial.Core.ViewModels.Lessons;
-using Tutorial.UI.Droid.Views;
-using Tutorial.UI.Droid.Views.Lessons;
 
 namespace Tutorial.UI.Droid
 {
--- Sample - Tutorial/Tutorial/Tutorial.UI.Droid/SplashScreenActivity.cs
+++ Sample - Tutorial/Tutorial/Tutorial.UI.Droid/SplashScreenActivity.cs
@@ -1,13 +1,17 @@
 ﻿
 using Android.App;
 using Android.OS;
+using Android.Views;
 using Cirrious.MvvmCross.Android.Platform;
 using Cirrious.MvvmCross.Android.Views;
+using Cirrious.MvvmCross.Interfaces.ServiceProvider;
+using Cirrious.MvvmCross.Interfaces.ViewModels;
+using Cirrious.MvvmCross.ViewModels;
 
 namespace Tutorial.UI.Droid
 {
     [Activity(Label = "Tutorial.UI.Droid", MainLauncher = true, NoHistory = true, Icon = "@drawable/icon")]
-    public class SplashScreenActivity 
+    public class SplashScreenActivity
         : MvxBaseSplashScreenActivity
     {
         public SplashScreenActivity()
@@ -15,9 +19,9 @@ namespace Tutorial.UI.Droid
         {
         }
 
-        protected override MvxBaseAndroidSetup CreateSetup()
+        protected override void OnViewModelSet()
         {
-            return new Setup(ApplicationContext);
+            // ignored
         }
     }
 }
