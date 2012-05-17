0de9690fb84dc005aa987ea1c63f25671f677f91
Stuart <lodge.stuart@gmail.com>
2012-05-17 20:17:23 +0100

    Attempt to get simple binding working under new setup code

--- Sample - SimpleDialogBinding/SimpleBinding/DroidAutoComplete/Setup.cs
+++ Sample - SimpleDialogBinding/SimpleBinding/DroidAutoComplete/Setup.cs
@@ -6,17 +6,7 @@ namespace DroidAutoComplete
     public class Setup 
         : MvxSimpleAndroidBindingSetup
     {
-        private static Setup _singleton;
-
-        public static void EnsureInitialised(Context applicationContext)
-        {
-            if (_singleton != null)
-                return;
-            _singleton = new Setup(applicationContext);
-            _singleton.Initialize();
-        }
-
-        private Setup(Context applicationContext)
+        public Setup(Context applicationContext)
             : base(applicationContext)
         {
         }
--- Sample - SimpleDialogBinding/SimpleBinding/SimpleDroid/Setup.cs
+++ Sample - SimpleDialogBinding/SimpleBinding/SimpleDroid/Setup.cs
@@ -7,17 +7,7 @@ namespace SimpleDroid
     public class Setup 
         : MvxSimpleAndroidBindingSetup
     {
-        private static Setup _singleton;
-
-        public static void EnsureInitialised(Context applicationContext)
-        {
-            if (_singleton != null)
-                return;
-            _singleton = new Setup(applicationContext);
-            _singleton.Initialize();
-        }
-
-        private Setup(Context applicationContext)
+        public Setup(Context applicationContext)
             : base(applicationContext, typeof(Converters.Converters))
         {
         }
