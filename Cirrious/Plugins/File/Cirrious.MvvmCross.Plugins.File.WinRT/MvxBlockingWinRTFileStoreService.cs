289b64d76ddae30178f64df5709670b48846fa20
Stuart <me@slodge.com>
2012-05-19 11:50:05 -0700

    File Plugin added for WinRT

---
+++ Cirrious/Plugins/File/Cirrious.MvvmCross.Plugins.File.WinRT/MvxBlockingWinRTFileStoreService.cs
@@ -0,0 +1,192 @@
+﻿#warning Copyright needed
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Cirrious.MvvmCross.ExtensionMethods;
+using Cirrious.MvvmCross.Platform.Diagnostics;
+using Windows.Storage;
+
+namespace Cirrious.MvvmCross.Plugins.File.WinRT
+{
+    public class MvxBlockingWinRTFileStoreService : IMvxSimpleFileStoreService
+    {
+        #region IMvxSimpleFileStoreService Members
+
+        public bool TryReadTextFile(string path, out string contents)
+        {
+            string result = null;
+            var toReturn = TryReadFileCommon(path, (stream) =>
+            {
+                using (var streamReader = new StreamReader(stream))
+                {
+                    result = streamReader.ReadToEnd();
+                }
+                return true;
+            });
+            contents = result;
+            return toReturn;
+        }
+
+        public bool TryReadBinaryFile(string path, out byte[] contents)
+        {
+            Byte[] result = null;
+            var toReturn = TryReadFileCommon(path, (stream) =>
+            {
+                using (var binaryReader = new BinaryReader(stream))
+                {
+                    var memoryBuffer = new byte[stream.Length];
+                    if (binaryReader.Read(memoryBuffer, 0, memoryBuffer.Length) != memoryBuffer.Length)
+                        return false;
+
+                    result = memoryBuffer;
+                    return true;
+                }
+            });
+            contents = result;
+            return toReturn;
+        }
+
+        public bool TryReadBinaryFile(string path, Func<System.IO.Stream, bool> readMethod)
+        {
+            var toReturn = TryReadFileCommon(path, readMethod);
+            return toReturn;
+        }
+
+        public void WriteFile(string path, string contents)
+        {
+            WriteFileCommon(path, (stream) =>
+            {
+                using (var sw = new StreamWriter(stream))
+                {
+                    sw.Write(contents);
+                    sw.Flush();
+                }
+            });
+        }
+
+        public void WriteFile(string path, IEnumerable<byte> contents)
+        {
+            WriteFileCommon(path, (stream) =>
+            {
+                using (var binaryWriter = new BinaryWriter(stream))
+                {
+                    binaryWriter.Write(contents.ToArray());
+                    binaryWriter.Flush();
+                }
+            });
+        }
+
+        public void WriteFile(string path, Action<System.IO.Stream> writeMethod)
+        {
+            WriteFileCommon(path, writeMethod);
+        }
+
+        public bool TryMove(string from, string to, bool deleteExistingTo)
+        {
+            var fromFile = StorageFileFromRelativePath(from);
+            var toFile = StorageFileFromRelativePath(to);
+
+            fromFile.CopyAndReplaceAsync(toFile).DoSync();
+
+#warning What should TryMove return? Consider renaming to a void?
+            return true;
+        }
+
+        public bool Exists(string path)
+        {
+            try
+            {
+                var storageFile = StorageFileFromRelativePath(path);
+                return storageFile != null;
+            }
+            catch (FileNotFoundException)
+            {
+                return false;
+            }
+        }
+
+        public string PathCombine(string items0, string items1)
+        {
+            return Path.Combine(items0, items1);
+        }
+
+        public void EnsureFolderExists(string folderPath)
+        {
+#warning TODO - not sure how to do this in WinRT?
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> GetFilesIn(string folderPath)
+        {
+#warning TODO - not sure how to do this in WinRT?
+            var folder = StorageFolder.GetFolderFromPathAsync(ToFullPath(folderPath)).DoSync();
+            var files = folder.GetFilesAsync().DoSync();
+            return files.Select(x => x.Name);
+        }
+
+        public void DeleteFile(string path)
+        {
+            var file = StorageFileFromRelativePath(path);
+            file.DeleteAsync().DoSync();
+        }
+
+        #endregion
+
+        private static void WriteFileCommon(string path, Action<Stream> streamAction)
+        {
+            try
+            {
+                var storageFile = CreateStorageFileFromRelativePath(path);
+                var streamWithContentType = storageFile.OpenAsync(FileAccessMode.ReadWrite).DoSync();
+                var stream = streamWithContentType.AsStreamForWrite();
+                streamAction(stream);
+            }
+            catch (Exception exception)
+            {
+                MvxTrace.Trace("Error during file save {0} : {1}", path, exception.ToLongString());
+                throw;
+            }
+        }
+
+        private static bool TryReadFileCommon(string path, Func<Stream, bool> streamAction)
+        {
+            try
+            {
+                var storageFile = StorageFileFromRelativePath(path);
+                var streamWithContentType = storageFile.OpenReadAsync().DoSync();
+                var stream = streamWithContentType.AsStreamForRead();
+                return streamAction(stream);
+            }
+            catch (Exception exception)
+            {
+                MvxTrace.Trace("Error during file load {0} : {1}", path, exception.ToLongString());
+                return false;
+            }
+        }
+
+        private static StorageFile StorageFileFromRelativePath(string path)
+        {
+            var fullPath = ToFullPath(path);
+            var storageFile = StorageFile.GetFileFromPathAsync(fullPath).DoSync();
+            return storageFile;
+        }
+
+        private static StorageFile CreateStorageFileFromRelativePath(string path)
+        {
+            var fullPath = ToFullPath(path);
+            var directory = Path.GetDirectoryName(fullPath);
+            var fileName = Path.GetFileName(fullPath);
+            var storageFolder = StorageFolder.GetFolderFromPathAsync(directory).DoSync();            
+            var storageFile = storageFolder.CreateFileAsync(fileName).DoSync();
+            return storageFile;
+        }
+
+        private static string ToFullPath(string path)
+        {
+            var localFolderPath = Windows.Storage.ApplicationData.Current.LocalFolder.Path;
+            return System.IO.Path.Combine(localFolderPath, path);
+        }
+    }
+}
---
+++ Cirrious/Plugins/File/Cirrious.MvvmCross.Plugins.File.WinRT/WinRTExtensionMethods.cs
@@ -0,0 +1,34 @@
+#warning Copyright needed
+using System;
+using Windows.Foundation;
+
+namespace Cirrious.MvvmCross.Plugins.File.WinRT
+{
+#warning THis should go to a shared dll?
+    public static class WinRTExtensionMethods
+    {
+        public static void DoSync(this IAsyncAction operation)
+        {
+            var task = operation.AsTask();
+            task.Wait();
+            if (task.Exception != null)
+            {
+                // TODO - is this correct?
+                throw task.Exception.InnerException;
+            }
+        }
+
+        public static TResult DoSync<TResult>(this IAsyncOperation<TResult> operation)
+        {
+            var task = operation.AsTask<TResult>();
+            task.Wait();
+            if (task.Exception != null)
+            {
+                // TODO - is this correct?
+                throw task.Exception.InnerException;
+            }
+
+            return task.Result;
+        }
+    }
+}
