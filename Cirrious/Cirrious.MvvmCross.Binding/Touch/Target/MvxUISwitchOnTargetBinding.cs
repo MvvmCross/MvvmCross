b50951e450ecaa90c050ac6c984d76f875e70ffc
Stuart <lodge.stuart@gmail.com>
2012-07-18 14:08:59 +0100

    Copyright only change (plenty more to do - must run R#er again)

--- Cirrious/Cirrious.MvvmCross.Binding/Touch/Target/MvxUISwitchOnTargetBinding.cs
+++ Cirrious/Cirrious.MvvmCross.Binding/Touch/Target/MvxUISwitchOnTargetBinding.cs
@@ -1,3 +1,14 @@
+#region Copyright
+// <copyright file="MvxUISwitchOnTargetBinding.cs" company="Cirrious">
+// (c) Copyright Cirrious. http://www.cirrious.com
+// This source is subject to the Microsoft Public License (Ms-PL)
+// Please see license.txt on http://opensource.org/licenses/ms-pl.html
+// All other rights reserved.
+// </copyright>
+// 
+// Project Lead - Stuart Lodge, Cirrious. http://www.cirrious.com
+#endregion
+
 using System.Reflection;
 using Cirrious.MvvmCross.Binding.Bindings.Target;
 using Cirrious.MvvmCross.Binding.Interfaces;
