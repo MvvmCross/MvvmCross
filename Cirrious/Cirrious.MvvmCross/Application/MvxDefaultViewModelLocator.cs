8b882ecd5e6948c476f57c7f8f9c7cbbf25a99b5
Matthew Leibowitz <mattleibowmail@gmail.com>
2012-08-04 15:03:45 +0200

    Fixed default viewmodel locator
    Now corrdctly chooses the appropriate constructor

--- Cirrious/Cirrious.MvvmCross/Application/MvxDefaultViewModelLocator.cs
+++ Cirrious/Cirrious.MvvmCross/Application/MvxDefaultViewModelLocator.cs
@@ -26,28 +26,52 @@ namespace Cirrious.MvvmCross.Application
         public bool TryLoad(Type viewModelType, IDictionary<string, string> parameters, out IMvxViewModel model)
         {
             model = null;
-            var constructor = viewModelType
+            var specifiedParamCount = parameters == null ? 0 : parameters.Count;
+            var constructors = viewModelType
 #if NETFX_CORE
                 .GetTypeInfo().DeclaredConstructors
 #else
                 .GetConstructors()
 #endif
-                .FirstOrDefault(c => c.GetParameters().All(p=> p.ParameterType == typeof(string)));
+                // all the parameters must be strings or there must be no 
+                // parameters at all
+                .Where(c => c.GetParameters().All(p => p.ParameterType == typeof(string)) || !c.GetParameters().Any())
+                .Select(c => new { Constructor = c, Params = c.GetParameters(), ParamCount = c.GetParameters().Length })
+                // no use looking at a constructor that has fewer arguments 
+                // than was specified
+                .Where(c => c.ParamCount >= specifiedParamCount)
+                .ToArray();
 
-            if (constructor == null)
+            if (!constructors.Any())
                 return false;
 
             var invokeWith = new List<object>();
-            foreach (var parameter in constructor.GetParameters())
+
+            // we have specified no parameters and there is a parameterless 
+            // constructor
+            if (specifiedParamCount == 0 && constructors.Any(c => c.ParamCount == 0))
             {
-                string parameterValue = null;
-                if (parameters == null ||
-                    !parameters.TryGetValue(parameter.Name, out parameterValue))
+                // use the parameterless constructor
+            }
+            else
+            {
+                foreach (var constructor in constructors)
                 {
-                    MvxTrace.Trace("Missing parameter in call to {0} - missing parameter {1} - asssuming null", viewModelType,
-                                   parameter.Name);
+                    // try and load a value out of the specifed parameters that
+                    // match the constructor parameter name
+                    foreach (var parameter in constructor.Params)
+                    {
+                        string parameterValue = null;
+                        if (parameters == null || !parameters.TryGetValue(parameter.Name, out parameterValue))
+                        {
+                            MvxTrace.Trace(
+                                "Missing parameter in call to {0} - missing parameter {1} - asssuming null",
+                                viewModelType,
+                                parameter.Name);
+                        }
+                        invokeWith.Add(parameterValue);
+                    }
                 }
-                invokeWith.Add(parameterValue);
             }
 
             model = Activator.CreateInstance(viewModelType, invokeWith.ToArray()) as IMvxViewModel;
