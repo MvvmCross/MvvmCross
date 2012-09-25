f5fd5e17c11f407052dc6735b581cfdad53c4234
Stuart <lodge.stuart@gmail.com>
2012-09-25 20:13:46 +0100

    Attempt to plug the latest leak inside #17 - all about nulling bound listviews - this version is mostly tested... but there's potentially more still to do!

---
+++ Cirrious/Cirrious.MvvmCross.Binding/Attributes/MvxSetToNullAfterBindingAttribute.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Cirrious.MvvmCross.Binding.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class MvxSetToNullAfterBindingAttribute : Attribute
+    {
+    }
+}
