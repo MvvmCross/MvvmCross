1f5e5f5860a67e345082047993851fa3b0a33094
Stuart <lodge.stuart@gmail.com>
2012-10-12 10:36:35 +0100

    A sqlite example for droid

---
+++ Sample - SimpleDialogBinding/SimpleDroidSql.Core/ListViewModel.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Input;
+using Cirrious.MvvmCross.Commands;
+using Cirrious.MvvmCross.ExtensionMethods;
+using Cirrious.MvvmCross.Interfaces.ServiceProvider;
+using Cirrious.MvvmCross.Plugins.Sqlite;
+
+namespace SimpleDroidSql
+{
+    public class ListViewModel
+        : INotifyPropertyChanged
+          , IMvxServiceConsumer<ISQLiteConnectionFactory>
+    {
+        private readonly DatabaseBackedObservableCollection<ListItem, int> _items;
+        private string _textToAdd;
+
+        public IList Items { get { return _items; } }
+
+        public string TextToAdd
+        {
+            get { return _textToAdd; }
+            set { _textToAdd = value; RaisePropertyChanged("TextToAdd"); }
+        }
+
+        public ICommand AddCommand
+        {
+            get { return new MvxRelayCommand(DoAdd); }
+        }
+
+        private void DoAdd()
+        {
+            if (string.IsNullOrWhiteSpace(TextToAdd))
+            {
+                // Todo - signal the error... although really UI should not send this!
+                return;
+            }
+
+            _items.Add(new ListItem() { Name = TextToAdd, WhenCreated = DateTime.Now.ToString("HH:mm:ss ddd MMM yyyy") });
+            TextToAdd = string.Empty;
+        }
+
+        public ListViewModel()
+        {
+            Cirrious.MvvmCross.Plugins.Sqlite.PluginLoader.Instance.EnsureLoaded();
+            var factory = this.GetService<ISQLiteConnectionFactory>();
+            var connection = factory.Create("SimpleList");
+            connection.CreateTable<ListItem>();
+            _items = new DatabaseBackedObservableCollection<ListItem, int>(connection, listItem => -listItem.Id);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
