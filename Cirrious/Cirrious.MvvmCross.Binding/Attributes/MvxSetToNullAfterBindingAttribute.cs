d76b374fe939bc2f101592128b8c9e4d97e31f2d
Stuart <lodge.stuart@gmail.com>
2012-10-12 08:04:19 +0100

    Fixes and optimisations to the SetToNull binding attribute code

--- Cirrious/Cirrious.MvvmCross.Binding/Attributes/MvxSetToNullAfterBindingAttribute.cs
+++ Cirrious/Cirrious.MvvmCross.Binding/Attributes/MvxSetToNullAfterBindingAttribute.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Cirrious.MvvmCross.Binding.Attributes
 {
-    [AttributeUsage(AttributeTargets.Property)]
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
     public class MvxSetToNullAfterBindingAttribute : Attribute
     {
     }
