4c97f130ce09e5365000e82f0666120b39307413
Stuart <lodge.stuart@gmail.com>
2012-10-12 10:36:35 +0100

    A sqlite example for droid

---
+++ Sample - SimpleDialogBinding/SimpleDroidSql/Activity1.cs
@@ -0,0 +1,30 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Cirrious.MvvmCross.Binding.Droid.Simple;
+
+namespace SimpleDroidSql
+{
+    [Activity(Label = "SimpleDroidSql", MainLauncher = true, Icon = "@drawable/icon")]
+    public sealed class MainActivity : MvxSimpleBindingActivity<ListViewModel>
+    {
+        public MainActivity()
+        {
+        }
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            if (ViewModel == null)
+                ViewModel = new ListViewModel();
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
---
+++ Sample - SimpleDialogBinding/SimpleDroidSql/Setup.cs
@@ -0,0 +1,14 @@
+using Android.Content;
+using Cirrious.MvvmCross.Binding.Droid.Simple;
+
+namespace SimpleDroidSql
+{
+    public class Setup
+        : MvxSimpleAndroidBindingSetup
+    {
+        public Setup(Context applicationContext)
+            : base(applicationContext)
+        {
+        }
+    }
+}
