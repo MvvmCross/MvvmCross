6ad6c364a927dd4ec5e3b4e734a522b824963a38
Stuart <lodge.stuart@gmail.com>
2012-10-14 20:00:53 +0100

    WP7 Template added too

---
+++ Templates/MyApplication.UI.WP7/Setup.cs
@@ -0,0 +1,40 @@
+﻿using Cirrious.MvvmCross.Application;
+using Cirrious.MvvmCross.Platform;
+using Cirrious.MvvmCross.WindowsPhone.Platform;
+using Microsoft.Phone.Controls;
+
+namespace MyApplication.UI.WP7
+{
+    public class Setup
+        : MvxBaseWindowsPhoneSetup
+    {
+        public Setup(PhoneApplicationFrame rootFrame) 
+            : base(rootFrame)
+        {
+        }
+
+        protected override MvxApplication CreateApp()
+        {
+            return new Core.App();
+        }
+
+        protected override void InitializeDefaultTextSerializer()
+        {
+            Cirrious.MvvmCross.Plugins.Json.PluginLoader.Instance.EnsureLoaded(true);
+        }
+
+        protected override void AddPluginsLoaders(MvxLoaderPluginRegistry registry)
+        {
+            // TODO - Initialise any required plugins here:
+            // e.g. registry.AddConventionalPlugin<Cirrious.MvvmCross.Plugins.Visibility.WindowsPhone.Plugin>();
+            base.AddPluginsLoaders(registry);
+        }
+
+        protected override void InitializeLastChance()
+        {
+            var errorDisplayer = new ErrorDisplayer();
+
+            base.InitializeLastChance();
+        } 
+    }
+}
