02f7ccd973135e77d28c27015f604be9f538a402
Stuart <lodge.stuart@gmail.com>
2012-10-21 19:36:06 +0100

    One way binding working for new/edit

--- Sample - CustomerManagement/CustomerManagement/CustomerManagement.Droid/Views/BaseCustomerEditView.cs
+++ Sample - CustomerManagement/CustomerManagement/CustomerManagement.Droid/Views/BaseCustomerEditView.cs
@@ -3,7 +3,7 @@ using CustomerManagement.Core.ViewModels;
 
 namespace CustomerManagement.Droid.Views
 {
-    public class BaseCustomerEditView<TViewModel> : BaseView<TViewModel> 
+    public class BaseCustomerEditView<TViewModel> : BaseDialogView<TViewModel> 
         where TViewModel : BaseEditCustomerViewModel
     {
         private readonly int _whichMenu;
@@ -13,10 +13,101 @@ namespace CustomerManagement.Droid.Views
 			_whichMenu = whichMenu;
 		}
 
-        protected override void OnViewModelSet()
+        protected override string JsonText
         {
-            SetContentView(Resource.Layout.Page_EditCustomerView);
+            get
+            {
+                return _jsonText;
+            }
         }
+
+        private const string _jsonText = @"
+{
+    'Key':'Root',
+    'Properties':{
+        'Caption':'TestRootElement'
+    },
+    'Sections':[
+        {
+            'Properties':{
+                'Header':'Customer Info'
+             },
+            'Elements':[
+                {
+                    'Key':'String',
+                    'Properties':{
+                        'Caption':'ID',
+                        'Value':'@MvxBind:{\'Path\':\'Customer.ID\'}'
+                    }
+                },
+                {
+                    'Key':'Entry',
+                    'Properties':{
+                        'Caption':'Name',
+                        'Value':'@MvxBind:{\'Path\':\'Customer.Name\'}'
+                    }
+                },
+                {
+                    'Key':'Entry',
+                    'Properties':{
+                        'Caption':'Website',
+                        'Value':'@MvxBind:{\'Path\':\'Customer.Website\'}'
+                    }
+                },
+                {
+                    'Key':'Entry',
+                    'Properties':{
+                        'Caption':'Phone',
+                        'Value':'@MvxBind:{\'Path\':\'Customer.PrimaryPhone\'}'
+                    }
+                }
+            ]
+        },
+        {
+            'Properties':{
+                'Header':'Primary Address'
+             },
+            'Elements':[
+                {
+                    'Key':'Entry',
+                    'Properties':{
+                        'Caption':'Address',
+                        'Value':'@MvxBind:{\'Path\':\'Customer.PrimaryAddress.Street1\'}'
+                    }
+                },
+                {
+                    'Key':'Entry',
+                    'Properties':{
+                        'Caption':'Address1',
+                        'Value':'@MvxBind:{\'Path\':\'Customer.PrimaryAddress.Street2\'}'
+                    }
+                },
+                {
+                    'Key':'Entry',
+                    'Properties':{
+                        'Caption':'City',
+                        'Value':'@MvxBind:{\'Path\':\'Customer.PrimaryAddress.City\'}'
+                    }
+                },
+                {
+                    'Key':'Entry',
+                    'Properties':{
+                        'Caption':'State',
+                        'Value':'@MvxBind:{\'Path\':\'Customer.PrimaryAddress.State\'}'
+                    }
+                },
+                {
+                    'Key':'Entry',
+                    'Properties':{
+                        'Caption':'Zip',
+                        'Value':'@MvxBind:{\'Path\':\'Customer.PrimaryAddress.Zip\'}'
+                    }
+                }
+            ]
+        },
+    ]
+}
+";
 
         public override bool OnCreateOptionsMenu(IMenu menu)
         {
