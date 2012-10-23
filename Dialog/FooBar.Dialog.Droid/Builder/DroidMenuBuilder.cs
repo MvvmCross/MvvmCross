e93801042ef3dc4d9f9b3554da2818949f1298d0
Stuart <lodge.stuart@gmail.com>
2012-10-23 10:01:08 +0100

    First menus added

---
+++ Dialog/FooBar.Dialog.Droid/Builder/DroidMenuBuilder.cs
@@ -0,0 +1,23 @@
+using Foobar.Dialog.Core.Builder;
+
+namespace FooBar.Dialog.Droid.Builder
+{
+    public class DroidMenuBuilder : MenuBuilder
+    {
+        public const string DroidPlatformName = "Menu";
+
+        public DroidMenuBuilder(string platformName = DroidConstants.PlatformName, bool registerDefaultElements = true)
+            : base(platformName)
+        {
+            if (registerDefaultElements)
+            {
+                RegisterDefaultElements();
+            }
+        }
+
+        public void RegisterDefaultElements()
+        {
+            RegisterConventionalKeys(typeof(DroidResources).Assembly);
+        }
+    }
+}
---
+++ Dialog/FooBar.Dialog.Droid/Menus/RootMenu.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Foobar.Dialog.Core.Elements;
+using Foobar.Dialog.Core.Menus;
+using IMenu = Foobar.Dialog.Core.Menus.IMenu;
+
+namespace FooBar.Dialog.Droid.Menus
+{
+    class RootMenu : IParentMenu
+    {
+        public List<IMenu> Children { get; private set; }
+
+        public RootMenu()
+        {
+            Children = new List<IMenu>();
+        }
+    }
+}
