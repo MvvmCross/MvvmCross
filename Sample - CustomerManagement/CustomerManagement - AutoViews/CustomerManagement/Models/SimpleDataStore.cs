51642f4952014f7ee2c61136b4e84c8a4508ffa8
Stuart <lodge.stuart@gmail.com>
2012-11-19 09:57:50 +0000

    Simple Build Fix - auto views sample working on Droid and Touch :)

--- Sample - CustomerManagement/CustomerManagement - AutoViews/CustomerManagement/Models/SimpleDataStore.cs
+++ Sample - CustomerManagement/CustomerManagement - AutoViews/CustomerManagement/Models/SimpleDataStore.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Xml.Linq;
+using System.Xml.Serialization;
 using Cirrious.MvvmCross.ExtensionMethods;
 using Cirrious.MvvmCross.Interfaces.ServiceProvider;
 using Cirrious.MvvmCross.Plugins.File;
