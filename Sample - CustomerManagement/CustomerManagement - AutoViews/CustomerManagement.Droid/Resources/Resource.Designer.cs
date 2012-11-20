95068195487d42cf3c9966ab45e5bbad8d988cdc
Stuart <lodge.stuart@gmail.com>
2012-11-20 17:59:11 +0000

    Auto gen file fixed

--- Sample - CustomerManagement/CustomerManagement - AutoViews/CustomerManagement.Droid/Resources/Resource.Designer.cs
+++ Sample - CustomerManagement/CustomerManagement - AutoViews/CustomerManagement.Droid/Resources/Resource.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace CustomerManagement.Droid
+namespace CustomerManagement.AutoViews.Droid
 {
 	
 	
