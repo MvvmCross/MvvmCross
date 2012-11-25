f559030c775db4070f520631af87b260606af299
Stuart <lodge.stuart@gmail.com>
2012-11-25 20:43:00 +0000

    Fix for testability issues in IoC (vNext)

--- Cirrious/Cirrious.MvvmCross/IoC/MvxSimpleIoCServiceProvider.cs
+++ Cirrious/Cirrious.MvvmCross/IoC/MvxSimpleIoCServiceProvider.cs
@@ -5,40 +5,45 @@ namespace Cirrious.MvvmCross.IoC
 {
     public class MvxSimpleIoCServiceProvider : IMvxIoCProvider
     {
+        public MvxSimpleIoCServiceProvider()
+        {
+            MvxSimpleIoCContainer.Initialise();
+        }
+
         #region IMvxIoCProvider Members
 
         public bool SupportsService<T>() where T : class
         {
-            return MvxSimpleIoCContainer.Current.CanResolve<T>();
+            return MvxSimpleIoCContainer.Instance.CanResolve<T>();
         }
 
         public T GetService<T>() where T : class
         {
-            return MvxSimpleIoCContainer.Current.Resolve<T>();
+            return MvxSimpleIoCContainer.Instance.Resolve<T>();
         }
 
         public bool TryGetService<T>(out T service) where T : class
         {
-            return MvxSimpleIoCContainer.Current.TryResolve<T>(out service);
+            return MvxSimpleIoCContainer.Instance.TryResolve<T>(out service);
         }
 
         public void RegisterServiceType<TFrom, TTo>() 
             where TFrom : class 
             where TTo : class 
         {
-            MvxSimpleIoCContainer.Current.RegisterServiceType<TFrom, TTo>();
+            MvxSimpleIoCContainer.Instance.RegisterServiceType<TFrom, TTo>();
         }
 
         public void RegisterServiceInstance<TInterface>(TInterface theObject)
             where TInterface : class
         {
-            MvxSimpleIoCContainer.Current.RegisterServiceInstance(theObject);
+            MvxSimpleIoCContainer.Instance.RegisterServiceInstance(theObject);
         }
 
 		public void RegisterServiceInstance<TInterface>(Func<TInterface> theConstructor)
 			where TInterface : class
 		{
-			MvxSimpleIoCContainer.Current.RegisterServiceInstance(theConstructor);
+            MvxSimpleIoCContainer.Instance.RegisterServiceInstance(theConstructor);
 		}
 
         #endregion
