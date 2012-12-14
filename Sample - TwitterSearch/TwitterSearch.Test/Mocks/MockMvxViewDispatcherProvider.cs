35168464a7a224be6d6441d8576ee26251a88ecb
Stuart <lodge.stuart@gmail.com>
2012-12-14 22:23:51 +0000

    Twittersearch est updated - but not fully integrated (yet) - fixes #75

---
+++ Sample - TwitterSearch/TwitterSearch.Test/Mocks/MockMvxViewDispatcherProvider.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="MockMvxViewDispatcherProvider.cs" company="Cirrious">
+// (c) Copyright Cirrious. http://www.cirrious.com
+// This source is subject to the Microsoft Public License (Ms-PL)
+// Please see license.txt on http://opensource.org/licenses/ms-pl.html
+// All other rights reserved.
+// </copyright>
+//  
+// Project Lead - Stuart Lodge, Cirrious. http://www.cirrious.com - Hire me - I'm worth it!
+
+using Cirrious.MvvmCross.Interfaces.Views;
+
+namespace TwitterSearch.Test.Mocks
+{
+    public class MockMvxViewDispatcherProvider : IMvxViewDispatcherProvider
+    {
+        #region IMvxViewDispatcherProvider implementation
+
+        public IMvxViewDispatcher Dispatcher { get; set; }
+
+        #endregion
+    }
+}
