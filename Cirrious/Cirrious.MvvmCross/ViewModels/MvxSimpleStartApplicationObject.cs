82ce046d9a2a22b1b872ea6452b170b29d0f3960
Stuart <lodge.stuart@gmail.com>
2012-12-31 20:48:01 +0000

    Simple Start ApplicationObject - fixes #83

---
+++ Cirrious/Cirrious.MvvmCross/ViewModels/MvxSimpleStartApplicationObject.cs
@@ -0,0 +1,29 @@
+// <copyright file="MvxSimpleStartApplicationObject.cs" company="Cirrious">
+// (c) Copyright Cirrious. http://www.cirrious.com
+// This source is subject to the Microsoft Public License (Ms-PL)
+// Please see license.txt on http://opensource.org/licenses/ms-pl.html
+// All other rights reserved.
+// </copyright>
+// 
+// Project Lead - Stuart Lodge, Cirrious. http://www.cirrious.com
+
+using Cirrious.MvvmCross.Interfaces.ViewModels;
+
+namespace Cirrious.MvvmCross.ViewModels
+{
+    public class MvxSimpleStartApplicationObject<TViewModel>
+        : MvxApplicationObject
+        , IMvxStartNavigation
+        where TViewModel : IMvxViewModel
+    {
+        public void Start()
+        {
+            RequestNavigate<TViewModel>();
+        }
+
+        public bool ApplicationCanOpenBookmarks
+        {
+            get { return false; }
+        }
+    }
+}
