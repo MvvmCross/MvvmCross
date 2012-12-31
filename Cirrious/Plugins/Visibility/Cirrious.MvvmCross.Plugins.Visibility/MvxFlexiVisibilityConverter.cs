f125e2bdbf1ffecb470fe5cb872934592010f742
Stuart <lodge.stuart@gmail.com>
2012-12-31 09:41:07 +0000

    Correction on flexi override (removed)

--- Cirrious/Plugins/Visibility/Cirrious.MvvmCross.Plugins.Visibility/MvxFlexiVisibilityConverter.cs
+++ Cirrious/Plugins/Visibility/Cirrious.MvvmCross.Plugins.Visibility/MvxFlexiVisibilityConverter.cs
@@ -15,43 +15,6 @@ namespace Cirrious.MvvmCross.Plugins.Visibility
 {
     public class MvxFlexiVisibilityConverter : MvxBaseFlexiVisibilityConverter
     {
-        public static bool IsATrueValue(object value, object parameter, bool defaultValue)
-        {
-            if (value == null)
-            {
-                return false;
-            }
-
-            if (value is bool)
-            {
-                return (bool)value;
-            }
-
-            if (value is int)
-            {
-                if (parameter == null)
-                {
-                    return (int)value > 0;
-                }
-                else
-                {
-                    return (int)value > int.Parse(parameter.ToString());
-                }
-            }
-
-            if (value is double)
-            {
-                return (double)value > 0;
-            }
-
-            if (value is string)
-            {
-                return string.IsNullOrWhiteSpace(value as string);
-            }
-
-            return defaultValue;
-        }
-
         public override MvxVisibility ConvertToMvxVisibility(object value, object parameter, CultureInfo culture)
         {
             var visibility = IsATrueValue(value, parameter, true);
