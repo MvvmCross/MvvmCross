04d6775aaec01b5b2884179e52af0335629ce374
Stuart <lodge.stuart@gmail.com>
2013-01-20 22:12:04 +0000

    Whitespace (I think)

--- Cirrious/Cirrious.MvvmCross.Binding.Touch/Views/MvxBindingTouchCollectionViewController.cs
+++ Cirrious/Cirrious.MvvmCross.Binding.Touch/Views/MvxBindingTouchCollectionViewController.cs
@@ -17,49 +17,49 @@ using MonoTouch.UIKit;
 
 namespace Cirrious.MvvmCross.Binding.Touch.Views
 {
-	public class MvxBindingTouchCollectionViewController<TViewModel>
-		: MvxTouchCollectionViewController<TViewModel>
-			, IMvxBindingTouchView
-			where TViewModel : class, IMvxViewModel
-	{
-		protected MvxBindingTouchCollectionViewController(MvxShowViewModelRequest request, UICollectionViewLayout layout)
-			: base(request, layout)
-		{
-		}
-		
-		#region Shared area needed by all binding controllers
-		
-		private readonly List<IMvxUpdateableBinding> _bindings = new List<IMvxUpdateableBinding>();
-		
-		public List<IMvxUpdateableBinding> Bindings
-		{
-			get { return _bindings; }
-		}
-		
-		public virtual object DefaultBindingSource
-		{
-			get { return ViewModel; }
-		}
-		
-		protected override void Dispose(bool disposing)
-		{
-			if (disposing)
-			{
-				this.ClearBindings();
-			}
-			
-			base.Dispose(disposing);
-		}
-		
+    public class MvxBindingTouchCollectionViewController<TViewModel>
+        : MvxTouchCollectionViewController<TViewModel>
+            , IMvxBindingTouchView
+            where TViewModel : class, IMvxViewModel
+    {
+        protected MvxBindingTouchCollectionViewController(MvxShowViewModelRequest request, UICollectionViewLayout layout)
+            : base(request, layout)
+        {
+        }
+        
+        #region Shared area needed by all binding controllers
+        
+        private readonly List<IMvxUpdateableBinding> _bindings = new List<IMvxUpdateableBinding>();
+        
+        public List<IMvxUpdateableBinding> Bindings
+        {
+            get { return _bindings; }
+        }
+        
+        public virtual object DefaultBindingSource
+        {
+            get { return ViewModel; }
+        }
+        
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                this.ClearBindings();
+            }
+            
+            base.Dispose(disposing);
+        }
+        
 #warning really need to think about how to handle ios6 once ViewDidUnload has been removed
-		[Obsolete]
-		public override void ViewDidUnload()
-		{
-			this.ClearBindings();
-			base.ViewDidUnload();
-		}
-		
+        [Obsolete]
+        public override void ViewDidUnload()
+        {
+            this.ClearBindings();
+            base.ViewDidUnload();
+        }
+        
 #endregion
-	}
+    }
     
 }
