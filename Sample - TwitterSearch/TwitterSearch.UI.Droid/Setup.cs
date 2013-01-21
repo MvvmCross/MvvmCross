d69caed0a43dd5f1522efa350813874e76457c35
Stuart <lodge.stuart@gmail.com>
2013-01-21 14:26:44 +0000

    Switch TwitterSearch to Swiss binding prototype

--- Sample - TwitterSearch/TwitterSearch.UI.Droid/Setup.cs
+++ Sample - TwitterSearch/TwitterSearch.UI.Droid/Setup.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using Android.Content;
 using Cirrious.MvvmCross.Application;
 using Cirrious.MvvmCross.Binding.Droid;
+using Cirrious.MvvmCross.Binding.Interfaces.Parse;
+using Cirrious.MvvmCross.Binding.Parse.Binding.Swiss;
+using Cirrious.MvvmCross.ExtensionMethods;
 using TwitterSearch.Core;
 using TwitterSearch.Core.Converters;
 
@@ -14,6 +17,12 @@ namespace TwitterSearch.UI.Droid
         public Setup(Context applicationContext)
             : base(applicationContext)
         {
+        }
+
+        protected override void InitializeFirstChance()
+        {
+            this.RegisterServiceType<IMvxBindingParser, MvxSwissBindingParser>();
+            base.InitializeFirstChance();
         }
 
         protected override MvxApplication CreateApp()
