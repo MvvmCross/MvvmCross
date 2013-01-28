90ab4ba64c0a80f5dbc827e6e69698e1dd4ec372
Stuart <lodge.stuart@gmail.com>
2013-01-28 09:31:49 +0000

    Renaming of HttpImageView to ImageView

--- Cirrious/Cirrious.MvvmCross.Binding.Droid/Views/MvxHttpImageView.cs
+++ Cirrious/Cirrious.MvvmCross.Binding.Droid/Views/MvxHttpImageView.cs
@@ -7,57 +7,18 @@
 
 using System;
 using Android.Content;
-using Android.Graphics;
 using Android.Runtime;
 using Android.Util;
-using Android.Widget;
-using Cirrious.MvvmCross.Platform;
-using Cirrious.MvvmCross.Plugins.DownloadCache;
 
 namespace Cirrious.MvvmCross.Binding.Droid.Views
 {
+    [Obsolete("Use MvxImageView instead")]
     public class MvxHttpImageView
-        : ImageView
+        : MvxImageView
     {
-        static MvxHttpImageView()
-        {
-            Cirrious.MvvmCross.Plugins.DownloadCache.PluginLoader.Instance.EnsureLoaded();
-        }
-
-        private readonly MvxDynamicImageHelper<Bitmap> _imageHelper;
-
         public MvxHttpImageView(Context context, IAttributeSet attrs)
             : base(context, attrs)
         {
-            _imageHelper = new MvxDynamicImageHelper<Bitmap>();
-            _imageHelper.ImageChanged += ImageHelperOnImageChanged;
-            var typedArray = context.ObtainStyledAttributes(attrs,
-                                                            MvxAndroidBindingResource.Instance
-                                                                                     .HttpImageViewStylableGroupId);
-
-            int numStyles = typedArray.IndexCount;
-            for (var i = 0; i < numStyles; ++i)
-            {
-                int attributeId = typedArray.GetIndex(i);
-                if (attributeId == MvxAndroidBindingResource.Instance.HttpSourceBindId)
-                {
-                    ImageUrl = typedArray.GetString(attributeId);
-                }
-            }
-            typedArray.Recycle();
-        }
-
-        public string ImageUrl
-        {
-            get { return Image.ImageUrl; }
-            set { Image.ImageUrl = value; }
-        }
-
-        [Obsolete("Use ImageUrl instead")]
-        public string HttpImageUrl
-        {
-            get { return Image.HttpImageUrl; }
-            set { Image.HttpImageUrl = value; }
         }
 
         public MvxHttpImageView(Context context)
@@ -68,16 +29,6 @@ namespace Cirrious.MvvmCross.Binding.Droid.Views
         protected MvxHttpImageView(IntPtr javaReference, JniHandleOwnership transfer)
             : base(javaReference, transfer)
         {
-        }
-
-        public MvxDynamicImageHelper<Bitmap> Image
-        {
-            get { return _imageHelper; }
-        }
-
-        private void ImageHelperOnImageChanged(object sender, MvxValueEventArgs<Bitmap> mvxValueEventArgs)
-        {
-            SetImageBitmap(mvxValueEventArgs.Value);
         }
     }
 }
