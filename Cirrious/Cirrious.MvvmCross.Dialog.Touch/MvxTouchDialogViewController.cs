5371ab42aafc9d4ff62f6e83f696a7471163e442
Stuart <lodge.stuart@gmail.com>
2013-02-03 23:50:35 +0000

    Get Dialog up to speed with #heizenbugs changes

--- Cirrious/Cirrious.MvvmCross.Dialog.Touch/MvxTouchDialogViewController.cs
+++ Cirrious/Cirrious.MvvmCross.Dialog.Touch/MvxTouchDialogViewController.cs
@@ -18,9 +18,125 @@ using Cirrious.MvvmCross.Views;
 using CrossUI.Touch.Dialog;
 using CrossUI.Touch.Dialog.Elements;
 using MonoTouch.UIKit;
+using Cirrious.MvvmCross.Interfaces.Views;
+using Cirrious.MvvmCross.Touch.Views;
+using Cirrious.MvvmCross.Binding.Touch.Views;
 
 namespace Cirrious.MvvmCross.Dialog.Touch
 {
+	public class EventSourceDialogViewController
+		: DialogViewController
+		, IViewControllerEventSource
+	{
+		protected EventSourceDialogViewController (UITableViewStyle style = UITableViewStyle.Grouped, 
+		                                           RootElement root = null,
+		                                           bool pushing = false)
+			: base(style, root, pushing)
+		{			
+		}
+
+		public override void ViewWillDisappear (bool animated)
+		{
+			base.ViewWillDisappear (animated);
+			ViewWillDisappearCalled.Raise(this, animated);
+		}
+		
+		public override void ViewDidAppear (bool animated)
+		{
+			base.ViewDidAppear (animated);
+			ViewDidDisappearCalled.Raise(this, animated);
+		}
+		
+		public override void ViewWillAppear (bool animated)
+		{
+			base.ViewWillAppear (animated);
+			ViewWillAppearCalled.Raise(this, animated);
+		}
+		
+		public override void ViewDidDisappear (bool animated)
+		{
+			base.ViewDidDisappear (animated);
+			ViewDidAppearCalled.Raise(this, animated);
+		}
+		
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			ViewDidLoadCalled.Raise(this);
+		}
+		
+		protected override void Dispose (bool disposing)
+		{
+			if (disposing) {
+				IsDisposingDisposeCalled.Raise(this);
+			}
+			base.Dispose (disposing);
+		}
+		
+		public event EventHandler ViewDidLoadCalled;
+		public event EventHandler<TypedEventArgs<bool>> ViewWillAppearCalled;
+		public event EventHandler<TypedEventArgs<bool>> ViewDidAppearCalled;
+		public event EventHandler<TypedEventArgs<bool>> ViewDidDisappearCalled;
+		public event EventHandler<TypedEventArgs<bool>> ViewWillDisappearCalled;
+		public event EventHandler IsDisposingDisposeCalled;
+	}
+
+	public class MvxDialogViewController
+		: EventSourceDialogViewController
+		, IMvxTouchView
+	{
+		protected MvxDialogViewController (UITableViewStyle style = UITableViewStyle.Grouped, 
+		                                           RootElement root = null,
+		                                           bool pushing = false)
+			: base(style, root, pushing)
+		{			
+			var adapter = new MvxViewControllerAdapter(this);	
+		}
+
+		public IMvxViewModel ViewModel { get;set; }
+		
+		public bool IsVisible
+		{
+			get { return this.IsVisible(); }
+		}
+		
+		public MvxShowViewModelRequest ShowRequest { get; set; }
+	}
+
+	public class MvxBindingDialogViewController
+		: MvxDialogViewController
+		, IMvxBindingTouchView
+	{
+		protected MvxBindingDialogViewController (UITableViewStyle style = UITableViewStyle.Grouped, 
+		                                   RootElement root = null,
+		                                   bool pushing = false)
+			: base(style, root, pushing)
+		{			
+			var adapter = new MvxBindingViewControllerAdapter(this);	
+		}
+		
+		private readonly List<IMvxUpdateableBinding> _bindings = new List<IMvxUpdateableBinding>();
+		
+		public List<IMvxUpdateableBinding> Bindings
+		{
+			get { return _bindings; }
+		}
+
+		#region Extra Binding helpers just for Elements
+		
+		protected T Bind<T>(T element, string bindingDescription)
+		{
+			return element.Bind(this, bindingDescription);
+		}
+		
+		protected T Bind<T>(T element, IEnumerable<MvxBindingDescription> bindingDescription)
+		{
+			return element.Bind(this, bindingDescription);
+		}
+		
+		#endregion
+	}
+
     public class MvxTouchDialogViewController<TViewModel>
         : DialogViewController
           , IMvxTouchView<TViewModel>
@@ -41,30 +157,39 @@ namespace Cirrious.MvvmCross.Dialog.Touch
         }
 
         #region Shared code across all Touch ViewControllers
+       
+		private IMvxViewModel _viewModel;
+		
+		public Type ViewModelType
+		{
+			get { return typeof (TViewModel); }
+		}
+		
+		public TViewModel ViewModel
+		{
+			get { return (TViewModel)((IMvxView)this).ViewModel; }
+			set
+			{
+				((IMvxView)this).ViewModel = value;
+			}
+		}
+		
+		IMvxViewModel IMvxView.ViewModel
+		{
+			get { return _viewModel; }
+			set
+			{
+				_viewModel = (TViewModel)value;
+				OnViewModelChanged();
+			}
+		}
+		
+		public bool IsVisible
+		{
+			get { return this.IsVisible(); }
+		}
 
-        private TViewModel _viewModel;
-
-        public Type ViewModelType
-        {
-            get { return typeof (TViewModel); }
-        }
-
-        public bool IsVisible
-        {
-            get { return this.IsVisible(); }
-        }
-
-        public TViewModel ViewModel
-        {
-            get { return _viewModel; }
-            set
-            {
-                _viewModel = value;
-                OnViewModelChanged();
-            }
-        }
-
-        public MvxShowViewModelRequest ShowRequest { get; private set; }
+        public MvxShowViewModelRequest ShowRequest { get; set; }
 
         protected virtual void OnViewModelChanged()
         {
@@ -95,11 +220,6 @@ namespace Cirrious.MvvmCross.Dialog.Touch
         public List<IMvxUpdateableBinding> Bindings
         {
             get { return _bindings; }
-        }
-
-        public virtual object DefaultBindingSource
-        {
-            get { return ViewModel; }
         }
 
         protected override void Dispose(bool disposing)
