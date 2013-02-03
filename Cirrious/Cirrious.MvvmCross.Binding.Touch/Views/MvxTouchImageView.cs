48436dc23e8f7265fa0fa328bb243b4e1e69a1c9
Stuart <lodge.stuart@gmail.com>
2013-02-03 23:50:08 +0000

    Simplify name on TouchImageView

--- Cirrious/Cirrious.MvvmCross.Binding.Touch/Views/MvxTouchImageView.cs
+++ Cirrious/Cirrious.MvvmCross.Binding.Touch/Views/MvxTouchImageView.cs
@@ -5,69 +5,101 @@
 // 
 // Project Lead - Stuart Lodge, @slodge, me@slodge.com
 
+using System;
 using System.Drawing;
 using Cirrious.MvvmCross.Platform;
 using Cirrious.MvvmCross.Plugins.DownloadCache;
 using MonoTouch.UIKit;
+using MonoTouch.Foundation;
 
 namespace Cirrious.MvvmCross.Binding.Touch.Views
 {
+	[Register("MvxImageView")]
+	public class MvxImageView
+		: UIImageView
+	{
+		private MvxDynamicImageHelper<UIImage> _imageHelper;
+		
+		public string ImageUrl
+		{
+			get { return _imageHelper.ImageUrl; }
+			set { _imageHelper.ImageUrl = value; }
+		}
+		
+		public string DefaultImagePath
+		{
+			get { return _imageHelper.DefaultImagePath; }
+			set { _imageHelper.DefaultImagePath = value; }
+		}
+		
+		public string ErrorImagePath
+		{
+			get { return _imageHelper.ErrorImagePath; }
+			set { _imageHelper.ErrorImagePath = value; }
+		}
+		
+		#region constructors
+		
+		public MvxImageView()
+		{
+			InitialiseImageHelper();
+		}
+		
+		public MvxImageView(IntPtr handle)
+			: base(handle)
+		{
+			InitialiseImageHelper();
+		}
+		
+		public MvxImageView(RectangleF frame)
+			: base(frame)
+		{
+			InitialiseImageHelper();
+		}
+		
+#endregion
+		
+		private void InitialiseImageHelper()
+		{
+			_imageHelper = new MvxDynamicImageHelper<UIImage>();
+			_imageHelper.ImageChanged += ImageHelperOnImageChanged;
+		}
+		
+		private void ImageHelperOnImageChanged(object sender, MvxValueEventArgs<UIImage> mvxValueEventArgs)
+		{
+			Image = mvxValueEventArgs.Value;
+		}
+		
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				_imageHelper.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+	}
+
+	[Obsolete("Use MvxImageView")]
     public class MvxTouchImageView
-        : UIImageView
+        : MvxImageView
     {
-        private MvxDynamicImageHelper<UIImage> _imageHelper;
-
-        public string ImageUrl
-        {
-            get { return _imageHelper.ImageUrl; }
-            set { _imageHelper.ImageUrl = value; }
-        }
-
-        public string DefaultImagePath
-        {
-            get { return _imageHelper.DefaultImagePath; }
-            set { _imageHelper.DefaultImagePath = value; }
-        }
-
-        public string ErrorImagePath
-        {
-            get { return _imageHelper.ErrorImagePath; }
-            set { _imageHelper.ErrorImagePath = value; }
-        }
-
         #region constructors
 
         public MvxTouchImageView()
         {
-            InitialiseImageHelper();
         }
+
+		public MvxTouchImageView(IntPtr handle)
+			: base(handle)
+		{
+		}
 
         public MvxTouchImageView(RectangleF frame)
             : base(frame)
         {
-            InitialiseImageHelper();
         }
 
         #endregion
-
-        private void InitialiseImageHelper()
-        {
-            _imageHelper = new MvxDynamicImageHelper<UIImage>();
-            _imageHelper.ImageChanged += ImageHelperOnImageChanged;
-        }
-
-        private void ImageHelperOnImageChanged(object sender, MvxValueEventArgs<UIImage> mvxValueEventArgs)
-        {
-            Image = mvxValueEventArgs.Value;
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                _imageHelper.Dispose();
-            }
-            base.Dispose(disposing);
-        }
     }
 }
