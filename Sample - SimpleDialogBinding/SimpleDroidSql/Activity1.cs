feaaadfe980b884937d03c84b5c7185f3949d253
Stuart <lodge.stuart@gmail.com>
2013-02-10 19:41:46 +0000

    Simple SQL sample up and running too - move DataContext to after setup completion...

--- Sample - SimpleDialogBinding/SimpleDroidSql/Activity1.cs
+++ Sample - SimpleDialogBinding/SimpleDroidSql/Activity1.cs
@@ -1,5 +1,6 @@
 ﻿using Android.App;
 using Android.Content;
+using Android.Content.PM;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
@@ -8,18 +9,19 @@ using Cirrious.MvvmCross.Binding.Droid.Simple;
 
 namespace SimpleDroidSql
 {
-    [Activity(Label = "SimpleDroidSql", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity(Label = "SimpleDroidSql", MainLauncher = true, Icon = "@drawable/icon", ScreenOrientation = ScreenOrientation.Portrait)]
     public sealed class MainActivity 
         : MvxSimpleBindingActivity
     {
         public MainActivity()
         {
-            DataContext = new ListViewModel();
         }
 
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
+
+            DataContext = new ListViewModel();
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
