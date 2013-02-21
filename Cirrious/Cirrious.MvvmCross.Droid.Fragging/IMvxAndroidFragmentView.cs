25f038f7a868e9f48997cefd2b38431255a04a27
Stuart <lodge.stuart@gmail.com>
2013-02-21 22:24:39 +0000

    Fragging

---
+++ Cirrious/Cirrious.MvvmCross.Droid.Fragging/IMvxAndroidFragmentView.cs
@@ -0,0 +1,9 @@
+using Cirrious.CrossCore.Interfaces.Core;
+
+namespace Cirrious.MvvmCross.Droid.Fragging
+{
+    public interface IMvxAndroidFragmentView
+        : IMvxDataConsumer
+    {
+    }
+}
--- Cirrious/Cirrious.MvvmCross.Droid.Fragging/MvxFragment.cs
+++ Cirrious/Cirrious.MvvmCross.Droid.Fragging/MvxFragment.cs
@@ -1,78 +1,52 @@
 using System;
-using Android.Content;
 using Android.OS;
 using Android.Support.V4.App;
 using Android.Views;
+using Cirrious.MvvmCross.Binding.Droid.Interfaces.Views;
+using Cirrious.MvvmCross.Binding.Droid.Views;
 using Cirrious.MvvmCross.Droid.ExtensionMethods;
 using Cirrious.MvvmCross.Droid.Interfaces;
 using Cirrious.MvvmCross.Interfaces.ViewModels;
-
-/*
- * This is not finished code - in fact it's hardly even started code!
- * 
- * Things to consider:
- * 1. Would be nice to remove the lifecycle thing that doesn't really work anyway!
- * 2. Would be lovely to avoice the way that the GetActivityForResult works - how does AzureMoileServices do that?
- * 3. Would be cool to improve memory use - e.g. when bindings get torn down 
- * 4. Essential that the fragment stuff makes sense
- * 5. Take a look at the Android Google IO conference sample from Google - treat that as best practice?
- * 6. Maybe just try porting the conference sample across? 
- */ 
-    
+   
 namespace Cirrious.MvvmCross.Droid.Fragging
 {
-#warning This code is twinkle in Stuart's eye code only!
-    public abstract class MvxFragment<TViewModel>
+    public abstract class MvxFragment
         : Fragment
-        , IMvxAndroidFragmentView<TViewModel>
-        where TViewModel : class, IMvxViewModel
+        , IMvxAndroidFragmentView
     {
-        protected MvxFragment()
-        {
-        }
+        public IMvxBindingOwnerHelper BindingOwnerHelper { get; private set; }
 
-        #region Common code across all android views - one case for multiple inheritance?
+        public object DataContext { get; set; }
 
-        private TViewModel _viewModel;
-
-        public Type ViewModelType
+        public IMvxViewModel ViewModel
         {
-            get { return typeof (TViewModel); }
-        }
-        
-        public TViewModel ViewModel
-        {
-            get { return _viewModel; }
+            get { return DataContext as IMvxViewModel; }
             set
             {
-                _viewModel = value;
+                DataContext = value;
                 OnViewModelSet();
             }
         }
 
-        protected abstract void OnViewModelSet();
-
-        public override void OnCreate(Bundle p0)
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
-            // TODO - consider telling the ViewModel here
-            // ViewModel.
-            // probably via an extension method
-            base.OnCreate(p0);
+            BindingOwnerHelper = new MvxFragmentBindingOwnerHelper(Activity, Activity, this);
+            return base.OnCreateView(inflater, container, savedInstanceState);
         }
 
-        public override void OnDestroy()
+        protected virtual void OnViewModelSet()
         {
-            // TODO - consider telling the ViewModel here
-            // ViewModel.
-            // probably via an extension method
-            base.OnDestroy();
         }
 
-        #endregion
-    }
+        public override void OnDestroyView()
+        {
+            BindingOwnerHelper.ClearAllBindings();
+            base.OnDestroyView();
+        }
 
-    public interface IMvxAndroidFragmentView<TViewModel>
-        where TViewModel : class, IMvxViewModel
-    {
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
     }
 }
