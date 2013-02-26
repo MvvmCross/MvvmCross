e5b87468bb8089d055e97ae6e508552ee0e6d621
Stuart <lodge.stuart@gmail.com>
2013-02-26 23:47:45 +0000

    Big leak fix copied across from vnext

--- Plugins/Cirrious/DownloadCache/Cirrious.MvvmCross.Plugins.DownloadCache.Droid/MvxAndroidLocalFileImageLoader.cs
+++ Plugins/Cirrious/DownloadCache/Cirrious.MvvmCross.Plugins.DownloadCache.Droid/MvxAndroidLocalFileImageLoader.cs
@@ -30,7 +30,10 @@ namespace Cirrious.MvvmCross.Plugins.DownloadCache.Droid
             if (!fileStore.TryReadBinaryFile(localPath, out contents))
                 return null;
 
-            var image = BitmapFactory.DecodeByteArray(contents, 0, contents.Length);
+			// the InPurgeable option is very important for Droid memory management.
+			// see 
+			var options = new BitmapFactory.Options () { InPurgeable = true };
+            var image = BitmapFactory.DecodeByteArray(contents, 0, contents.Length, options);
             return image;
         }
 
