21b0a53b9b7e79821875d7fe582786ac3a0df660
sbou <sebastien.boury@cgi.com>
2013-03-05 12:42:14 +0100

    SQLite WPF plugin
    Add SQLite plugin for WPF

---
+++ Cirrious/Plugins/Sqlite/Cirrious.MvvmCross.Plugins.Sqlite.Wpf/MvxWpfSQLiteConnectionFactory.cs
@@ -0,0 +1,28 @@
+﻿#region Copyright
+
+// <copyright file="MvxWpfSQLiteConnectionFactory.cs" company="Ironshod">
+// (c) Copyright Ironshod. http://www.ironshod.co.nz
+// This source is subject to the Microsoft Public License (Ms-PL)
+// Please see license.txt on http://opensource.org/licenses/ms-pl.html
+// All other rights reserved.
+// </copyright>
+//  
+// Developer - Dave Leaver, Ironshod. http://www.ironshod.co.nz
+
+#endregion
+
+using SQLite;
+using System;
+using System.IO;
+
+namespace Cirrious.MvvmCross.Plugins.Sqlite.Wpf
+{
+	public class MvxWpfSQLiteConnectionFactory : ISQLiteConnectionFactory
+	{
+		public ISQLiteConnection Create(string address)
+		{
+            var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), address);
+			return new SQLiteConnection(path, SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.Create);
+		}
+	}
+}
---
+++ Cirrious/Plugins/Sqlite/Cirrious.MvvmCross.Plugins.Sqlite.Wpf/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de 
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("Cirrious.MvvmCross.Plugins.Sqlite.Wpf")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Cirrious.MvvmCross.Plugins.Sqlite.Wpf")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly 
+// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de 
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("e39482a2-a1fe-4338-9160-a95928923dcf")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire 
+//      Numéro de build
+//      Révision
+//
+// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
+// en utilisant '*', comme indiqué ci-dessous :
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
