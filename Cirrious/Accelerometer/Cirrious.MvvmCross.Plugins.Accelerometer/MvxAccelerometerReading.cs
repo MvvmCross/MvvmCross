2cf362c198a6edb18559bd00814b065a7ebe9b26
Stuart <lodge.stuart@gmail.com>
2013-03-09 20:31:38 +0000

    Accelerometer added - fixes #97

---
+++ Cirrious/Accelerometer/Cirrious.MvvmCross.Plugins.Accelerometer/MvxAccelerometerReading.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="MvxAccelerometerReading.cs" company="Cirrious">
+// (c) Copyright Cirrious. http://www.cirrious.com
+// This source is subject to the Microsoft Public License (Ms-PL)
+// Please see license.txt on http://opensource.org/licenses/ms-pl.html
+// All other rights reserved.
+// </copyright>
+//  
+// Project Lead - Stuart Lodge, Cirrious. http://www.cirrious.com - Hire me - I'm worth it!
+
+namespace Cirrious.MvvmCross.Plugins.Accelerometer
+{
+    public class MvxAccelerometerReading
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+
+        public MvxAccelerometerReading Clone()
+        {
+            return new MvxAccelerometerReading {X = X, Y = Y, Z = Z};
+        }
+    }
+}
