82cb8bfc71bd8c27dfd55c5d6e8ad15d2c600a7a
Stuart <lodge.stuart@gmail.com>
2013-03-09 19:04:56 +0000

    IoC additions - for #149

--- CrossCore/Cirrious.CrossCore/Interfaces/IoC/Mvx.cs
+++ CrossCore/Cirrious.CrossCore/Interfaces/IoC/Mvx.cs
@@ -30,6 +30,20 @@ namespace Cirrious.CrossCore.Interfaces.IoC
             return ioc.TryResolve(out service);
         }
 
+        public static T Create<T>()
+            where T : class
+        {
+            var ioc = MvxSingleton<IMvxIoCProvider>.Instance;
+            return ioc.Create<T>();
+        }
+
+        public static T GetSingleton<T>()
+            where T : class
+        {
+            var ioc = MvxSingleton<IMvxIoCProvider>.Instance;
+            return ioc.GetSingleton<T>();
+        }
+
         public static void RegisterSingleton<TInterface>(Func<TInterface> serviceConstructor)
             where TInterface : class
         {
