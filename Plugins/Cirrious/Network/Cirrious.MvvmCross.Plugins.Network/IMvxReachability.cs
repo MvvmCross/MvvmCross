1954d6cdf396c23f3bd4f0c04c9af75c953b540b
Stuart <lodge.stuart@gmail.com>
2013-03-10 15:37:21 +0000

    Reachability changes -> plugin

--- Plugins/Cirrious/Network/Cirrious.MvvmCross.Plugins.Network/IMvxReachability.cs
+++ Plugins/Cirrious/Network/Cirrious.MvvmCross.Plugins.Network/IMvxReachability.cs
@@ -1,17 +1,14 @@
-// MvxNetworkStatus.cs
+// IMvxReachability.cs
 // (c) Copyright Cirrious Ltd. http://www.cirrious.com
 // MvvmCross is licensed using Microsoft Public License (Ms-PL)
 // Contributions and inspirations noted in readme.md and license.txt
 // 
 // Project Lead - Stuart Lodge, @slodge, me@slodge.com
 
-namespace Cirrious.MvvmCross.Interfaces.Platform
+namespace Cirrious.MvvmCross.Plugins.Network
 {
-#warning Move to Plugin (or lose!)
-    public enum MvxNetworkStatus
+    public interface IMvxReachability
     {
-        NotReachable,
-        ReachableViaCarrierDataNetwork,
-        ReachableViaWiFiNetwork
+        MvxReachabilityStatus IsHostReachable(string host);
     }
 }
---
+++ Plugins/Cirrious/Network/Cirrious.MvvmCross.Plugins.Network/PluginLoader.cs
@@ -0,0 +1,28 @@
+﻿// PluginLoader.cs
+// (c) Copyright Cirrious Ltd. http://www.cirrious.com
+// MvvmCross is licensed using Microsoft Public License (Ms-PL)
+// Contributions and inspirations noted in readme.md and license.txt
+// 
+// Project Lead - Stuart Lodge, @slodge, me@slodge.com
+
+using Cirrious.CrossCore.Interfaces.IoC;
+using Cirrious.CrossCore.Interfaces.Plugins;
+
+namespace Cirrious.MvvmCross.Plugins.Network
+{
+    public class PluginLoader
+        : IMvxPluginLoader          
+    {
+        public static readonly PluginLoader Instance = new PluginLoader();
+
+        #region Implementation of IMvxPluginLoader
+
+        public void EnsureLoaded()
+        {
+            var manager = Mvx.Resolve<IMvxPluginManager>();
+            manager.EnsureLoaded<PluginLoader>();
+        }
+
+        #endregion
+    }
+}
