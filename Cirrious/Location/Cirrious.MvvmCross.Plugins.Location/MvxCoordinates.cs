049a67ce125e025802f641b6cbc4b3ad6ad74860
Stuart <lodge.stuart@gmail.com>
2013-03-20 08:15:50 +0000

    Location changes - heading and accuracy and nullables

--- Cirrious/Location/Cirrious.MvvmCross.Plugins.Location/MvxCoordinates.cs
+++ Cirrious/Location/Cirrious.MvvmCross.Plugins.Location/MvxCoordinates.cs
@@ -11,10 +11,14 @@ namespace Cirrious.MvvmCross.Plugins.Location
     {
         public double Latitude { get; set; }
         public double Longitude { get; set; }
-        public double Altitude { get; set; }
-        public double Accuracy { get; set; }
-        public double AltitudeAccuracy { get; set; }
-        public double Heading { get; set; }
-        public double Speed { get; set; }
+		public double? Accuracy { get; set; }
+
+		public double? Altitude { get; set; }
+        public double? AltitudeAccuracy { get; set; }
+
+        public double? Heading { get; set; }
+		public double? HeadingAccuracy { get; set; }
+
+		public double? Speed { get; set; }
     }
 }
