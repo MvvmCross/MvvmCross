25563e23e57c108d5f3a9b78a4277dba6e0988da
Stuart <lodge.stuart@gmail.com>
2013-03-20 14:59:40 +0000

    Get Templates out of the main project

--- Templates/MyApplication.Core/Interfaces/Errors/IErrorSource.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace MyApplication.Core.Interfaces.Errors
-{
-    public interface IErrorSource
-    {
-        event EventHandler<ErrorEventArgs> ErrorReported;
-    }
-}
--- Templates/MyApplication.UI.WinRT/NativeConverters/TimeAgoConverter.cs
+++
@@ -1,8 +0,0 @@
-using Cirrious.MvvmCross.WinRT.Platform.Converters;
-
-namespace MyApplication.UI.WinRT.NativeConverters
-{
-    public class TimeAgoConverter : MvxNativeValueConverter<Core.Converters.TimeAgoConverter>
-    {
-    }
-}
