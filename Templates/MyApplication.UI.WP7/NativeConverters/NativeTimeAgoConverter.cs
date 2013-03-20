a95f9d806d2c8d51663ac8ad58c5f87d8d6cfcc0
Stuart <lodge.stuart@gmail.com>
2013-03-20 08:53:01 +0000

    Templates removed

--- Templates/MyApplication.UI.WP7/NativeConverters/NativeTimeAgoConverter.cs
+++
@@ -1,9 +0,0 @@
-﻿using Cirrious.CrossCore.WindowsPhone.Converters;
-using MyApplication.Core.Converters;
-
-namespace MyApplication.UI.WP7.NativeConverters
-{
-    public class NativeTimeAgoConverter : MvxNativeValueConverter<TimeAgoConverter>
-    {
-    }
-}
