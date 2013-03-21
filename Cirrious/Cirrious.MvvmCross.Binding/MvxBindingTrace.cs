8000834271cb23020a9ab7c77511b2aaf6beaf02
Stuart <lodge.stuart@gmail.com>
2013-03-21 08:38:06 +0000

    Warning and Error in BindingTrace too

--- Cirrious/Cirrious.MvvmCross.Binding/MvxBindingTrace.cs
+++ Cirrious/Cirrious.MvvmCross.Binding/MvxBindingTrace.cs
@@ -22,5 +22,20 @@ namespace Cirrious.MvvmCross.Binding
 
             MvxTrace.TaggedTrace(level, Tag, message, args);
         }
-    }
+
+		public static void Trace(string message, params object[] args)
+		{
+			Trace(MvxTraceLevel.Diagnostic, Tag, message, args);
+		}
+
+		public static void Warning(string message, params object[] args)
+		{
+			Trace(MvxTraceLevel.Warning, Tag, message, args);
+		}
+
+		public static void Error(string message, params object[] args)
+		{
+			Trace(MvxTraceLevel.Error, Tag, message, args);
+		}
+	}
 }
