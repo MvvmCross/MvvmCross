f9d215323b3e5cbd44bdefa6075d7ea5bf784b83
Stuart <me@slodge.com>
2013-03-22 18:05:20 +0000

    Messenger api tweaked - MvxReference enumeration is more descriptive than true/false

--- Sample - CirriousConference/Cirrious.Conference.Core/ViewModels/BaseViewModel.cs
+++ Sample - CirriousConference/Cirrious.Conference.Core/ViewModels/BaseViewModel.cs
@@ -27,7 +27,7 @@ namespace Cirrious.Conference.Core.ViewModels
 		protected MvxSubscriptionToken Subscribe<TMessage> (Action<TMessage> action)
 			where TMessage : MvxMessage
 		{
-			return MvxMessenger.Subscribe<TMessage>(action, false /* weak reference */);
+		    return MvxMessenger.Subscribe<TMessage>(action, MvxReference.Weak);
 		}
 
 		protected void Unsubscribe<TMessage> (MvxSubscriptionToken id)
