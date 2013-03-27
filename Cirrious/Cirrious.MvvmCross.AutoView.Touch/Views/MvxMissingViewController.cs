985cbd3b8cbd0cb97a47e2d4e70f9adfddae9165
Stuart <lodge.stuart@gmail.com>
2013-03-27 11:28:17 +0000

    Code building on iOS :)

--- Cirrious/Cirrious.MvvmCross.AutoView.Touch/Views/MvxMissingViewController.cs
+++ Cirrious/Cirrious.MvvmCross.AutoView.Touch/Views/MvxMissingViewController.cs
@@ -11,6 +11,7 @@ using Cirrious.MvvmCross.Dialog.Touch;
 using Cirrious.MvvmCross.ViewModels;
 using Cirrious.MvvmCross.Views;
 using CrossUI.Touch.Dialog.Elements;
+using Cirrious.CrossCore.IoC;
 
 namespace Cirrious.MvvmCross.AutoView.Touch.Views
 {
