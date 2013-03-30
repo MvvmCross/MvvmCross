6324a157feb32b966847f924453d340f6aabc1ac
Stuart <lodge.stuart@gmail.com>
2013-03-30 13:19:19 +0000

    Comments only

--- Cirrious/Cirrious.MvvmCross/ViewModels/MvxPropertyChangedListener.cs
+++ Cirrious/Cirrious.MvvmCross/ViewModels/MvxPropertyChangedListener.cs
@@ -1,5 +1,4 @@
-﻿#region Copyright
-// <copyright file="MvxPropertyChangedListener.cs" company="Cirrious">
+﻿// <copyright file="MvxPropertyChangedListener.cs" company="Cirrious">
 // (c) Copyright Cirrious. http://www.cirrious.com
 // This source is subject to the Microsoft Public License (Ms-PL)
 // Please see license.txt on http://opensource.org/licenses/ms-pl.html
@@ -7,7 +6,6 @@
 // </copyright>
 // 
 // Project Lead - Stuart Lodge, Cirrious. http://www.cirrious.com
-#endregion
 
 using System;
 using System.ComponentModel;
