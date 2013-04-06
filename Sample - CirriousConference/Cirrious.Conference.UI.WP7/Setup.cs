f3d0547c31f2bc3b1938c84d0bfdef69e1446951
Stuart <lodge.stuart@gmail.com>
2013-04-06 18:45:26 +0100

    Conf sample using bootstrap plugin startup

--- Sample - CirriousConference/Cirrious.Conference.UI.WP7/Setup.cs
+++ Sample - CirriousConference/Cirrious.Conference.UI.WP7/Setup.cs
@@ -1,20 +1,5 @@
-﻿using System;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Ink;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
-using Cirrious.Conference.Core;
-using Cirrious.CrossCore.IoC;
-using Cirrious.CrossCore.Platform;
-using Cirrious.CrossCore.Plugins;
-using Cirrious.MvvmCross.Platform;
+﻿using Cirrious.Conference.Core;
 using Cirrious.MvvmCross.ViewModels;
-using Cirrious.MvvmCross.Views;
 using Cirrious.MvvmCross.WindowsPhone.Platform;
 using Microsoft.Phone.Controls;
 
