80cdc41620b5c1ff4be7eb281318ce40bb70024d
Stuart <lodge.stuart@gmail.com>
2013-04-07 22:27:26 +0100

    Build path fixes

---
+++ nuspec/BootstrapContent/ReflectionExPluginBootstrap.cs
@@ -0,0 +1,9 @@
+using Cirrious.CrossCore.Plugins;
+
+namespace Bootstrap
+{
+    public class ReflectionExPluginBootstrap
+        : MvxPluginBootstrapAction<Cirrious.MvvmCross.Plugins.ReflectionEx.PluginLoader>
+    {
+    }
+}
---
+++ nuspec/TouchBootstrapContent/ReflectionExPluginBootstrap.cs
@@ -0,0 +1,9 @@
+using Cirrious.CrossCore.Plugins;
+
+namespace Bootstrap
+{
+    public class ReflectionExPluginBootstrap
+        : MvxLoaderPluginBootstrapAction<Cirrious.MvvmCross.Plugins.ReflectionEx.PluginLoader, Cirrious.MvvmCross.Plugins.ReflectionEx.Touch.Plugin>
+    {
+    }
+}
