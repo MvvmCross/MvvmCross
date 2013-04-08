0ce6ac9eb75ee4b1596b17b00c3a7c282417d914
Stuart <lodge.stuart@gmail.com>
2013-04-08 10:12:57 +0100

    Bootstrap-tastic in the conference sample

---
+++ Sample - CirriousConference/Cirrious.Conference.UI.Touch/Bootstrap/DownloadCachePluginBootstrap.cs
@@ -0,0 +1,9 @@
+using Cirrious.CrossCore.Plugins;
+
+namespace Bootstrap
+{
+    public class DownloadCachePluginBootstrap
+        : MvxLoaderPluginBootstrapAction<Cirrious.MvvmCross.Plugins.DownloadCache.PluginLoader, Cirrious.MvvmCross.Plugins.DownloadCache.Touch.Plugin>
+    {
+    }
+}
