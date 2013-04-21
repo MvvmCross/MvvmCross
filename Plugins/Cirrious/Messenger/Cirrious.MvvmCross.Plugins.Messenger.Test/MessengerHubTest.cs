506b12c685a230995c2783e68ee6bff972d6126c
Stuart <lodge.stuart@gmail.com>
2013-04-21 21:08:55 +0100

    Messenger has HasSubscriptionsFor<T> plus it sends MvxSubscriberChangeMessage messages

--- Plugins/Cirrious/Messenger/Cirrious.MvvmCross.Plugins.Messenger.Test/MessengerHubTest.cs
+++ Plugins/Cirrious/Messenger/Cirrious.MvvmCross.Plugins.Messenger.Test/MessengerHubTest.cs
@@ -125,7 +125,6 @@ namespace Cirrious.MvvmCross.Plugins.Messenger.Test
             messenger.Subscribe<TestMessage>(null);
         }
 
-
         [Test]
         public void UnknownUnsubscribeDoesNotCauseException()
         {
@@ -146,7 +145,77 @@ namespace Cirrious.MvvmCross.Plugins.Messenger.Test
             messenger.Publish<TestMessage>(null);
         }
 
-#warning TODO - weak references need testing here really
+        [Test]
+        public void HasSubscriptionsForIsCorrect()
+        {
+            var messenger = new MvxMessengerHub();
+            Assert.AreEqual(false, messenger.HasSubscriptionsFor<MvxSubscriberChangeMessage>());
+            Assert.AreEqual(false, messenger.HasSubscriptionsFor<TestMessage>());
+            var changeToken = messenger.Subscribe<MvxSubscriberChangeMessage>(message => { });
+            Assert.AreEqual(true, messenger.HasSubscriptionsFor<MvxSubscriberChangeMessage>());
+            Assert.AreEqual(false, messenger.HasSubscriptionsFor<TestMessage>());
+            var token = messenger.Subscribe<TestMessage>(m =>
+            {
+                // stuff
+            });
+            Assert.AreEqual(true, messenger.HasSubscriptionsFor<MvxSubscriberChangeMessage>());
+            Assert.AreEqual(true, messenger.HasSubscriptionsFor<TestMessage>());
+            messenger.Unsubscribe<TestMessage>(token);
+            Assert.AreEqual(true, messenger.HasSubscriptionsFor<MvxSubscriberChangeMessage>());
+            Assert.AreEqual(false, messenger.HasSubscriptionsFor<TestMessage>());
+        }
+
+        [Test]
+        public void SubscribeAndUnsubscribeCauseChangeMessages()
+        {
+            var messenger = new MvxMessengerHub();
+            MvxSubscriberChangeMessage subscriberChangeMessage = null;
+            var changeToken = messenger.Subscribe<MvxSubscriberChangeMessage>(message => subscriberChangeMessage = message);
+            var token = messenger.Subscribe<TestMessage>(m =>
+            {
+                // stuff
+            });
+            Assert.NotNull(subscriberChangeMessage);
+            Assert.AreEqual(1, subscriberChangeMessage.SubscriberCount);
+            Assert.AreEqual(typeof(TestMessage), subscriberChangeMessage.MessageType);
+            subscriberChangeMessage = null;
+            messenger.Unsubscribe<TestMessage>(token);
+            Assert.NotNull(subscriberChangeMessage);
+            Assert.AreEqual(0, subscriberChangeMessage.SubscriberCount);
+            Assert.AreEqual(typeof(TestMessage), subscriberChangeMessage.MessageType);
+        }
+
+        [Test]
+        public void PurgeCausesChangeMessage()
+        {
+            var messenger = new MvxMessengerHub();
+            MvxSubscriberChangeMessage subscriberChangeMessage = null;
+            var changeToken = messenger.Subscribe<MvxSubscriberChangeMessage>(message => subscriberChangeMessage = message);
+            CreateShortLivedSubscription(messenger);
+            Assert.NotNull(subscriberChangeMessage);
+            Assert.AreEqual(1, subscriberChangeMessage.SubscriberCount);
+            Assert.AreEqual(typeof(TestMessage), subscriberChangeMessage.MessageType);
+            subscriberChangeMessage = null;
+            System.Threading.Thread.Sleep(100);
+            GC.Collect();
+            GC.WaitForFullGCComplete();
+            messenger.Publish(new TestMessage(this));
+            System.Threading.Thread.Sleep(100);
+            Assert.NotNull(subscriberChangeMessage);
+            Assert.AreEqual(0, subscriberChangeMessage.SubscriberCount);
+            Assert.AreEqual(typeof(TestMessage), subscriberChangeMessage.MessageType);
+        }
+
+        private void CreateShortLivedSubscription(MvxMessengerHub messenger)
+        {
+            // put a large byte array in place - this encourages the garbage collector to collect
+            var b = new byte[100000];
+            var action = new Action<TestMessage>((message) => { b[0] = 0; });
+            messenger.Subscribe<TestMessage>(action, MvxReference.Weak);
+            action = null;
+        }
+
+#warning TODO - weak references need more testing here really
 #warning TODO - async and ui threading need testing here really
     }
 }
