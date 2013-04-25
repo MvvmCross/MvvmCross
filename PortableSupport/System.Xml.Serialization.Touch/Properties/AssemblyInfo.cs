d36610cca1088d2a78f0b5e27db47bfadbbd81ee
Stuart <lodge.stuart@gmail.com>
2013-04-25 17:01:14 +0100

    Is this all really building on the PC now? Yikes that took a long time!

--- PortableSupport/System.Xml.Serialization.Touch/Properties/AssemblyInfo.cs
+++ PortableSupport/System.Xml.Serialization.Touch/Properties/AssemblyInfo.cs
@@ -1,24 +1,26 @@
-// AssemblyInfo.cs
-// (c) Copyright Cirrious Ltd. http://www.cirrious.com
-// MvvmCross is licensed using Microsoft Public License (Ms-PL)
-// Contributions and inspirations noted in readme.md and license.txt
-// 
-// Project Lead - Stuart Lodge, @slodge, me@slodge.com
-
-using System.Reflection;
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
-[assembly: AssemblyTitle("System.Windows")]
+[assembly: AssemblyTitle("System.Xml.Serialization")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("System.Windows")]
-[assembly: AssemblyCopyright("Copyright �  2012")]
+[assembly: AssemblyProduct("System.Xml.Serialization")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("283a7af0-4345-417a-ad4e-d920d36f7194")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -30,6 +32,5 @@ using System.Reflection;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-
 [assembly: AssemblyVersion("2.0.5.0")]
 [assembly: AssemblyFileVersion("2.0.5.0")]
