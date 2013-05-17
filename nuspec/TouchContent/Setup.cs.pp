c219ec3bf6c35fefaa21b6ac17f541eb46fb05d6
Stuart <lodge.stuart@gmail.com>
2013-05-17 18:35:29 +0100

    Fixes #262 (.pp file tidy)

--- nuspec/TouchContent/Setup.cs.pp
+++ nuspec/TouchContent/Setup.cs.pp
@@ -1,8 +1,5 @@
-using MonoTouch.Foundation;
 using MonoTouch.UIKit;
 using Cirrious.MvvmCross.Touch.Platform;
-using Cirrious.MvvmCross.Touch.Views.Presenters;
-using Cirrious.MvvmCross.ViewModels;
 
 namespace $rootnamespace$
 {
