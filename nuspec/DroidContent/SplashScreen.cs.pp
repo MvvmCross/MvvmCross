5096de6aa0fd3a9fc875bff91f2f5fe9deee8b3b
Stuart <lodge.stuart@gmail.com>
2013-05-20 14:20:04 +0100

    theme added to droid template (nicer startup)

--- nuspec/DroidContent/SplashScreen.cs.pp
+++ nuspec/DroidContent/SplashScreen.cs.pp
@@ -3,7 +3,12 @@ using Cirrious.MvvmCross.Droid.Views;
 
 namespace $rootnamespace$
 {
-    [Activity(Label = "$rootnamespace$", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity(
+		Label = "$rootnamespace$"
+		, MainLauncher = true
+		, Icon = "@drawable/icon"
+		, Theme = "@style/Theme.Splash"
+		, ScreenOrientation = ScreenOrientation.Portrait)]
     public class SplashScreen : MvxSplashScreenActivity
     {
         public SplashScreen()
