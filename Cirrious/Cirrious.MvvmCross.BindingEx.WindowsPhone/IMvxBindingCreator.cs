91c02c737e7c44aadd802b0a6e07b81963bfc001
Stuart <lodge.stuart@gmail.com>
2013-05-25 19:52:26 +0100

    Adding BindingEx projects - first steps towards #283

---
+++ Cirrious/Cirrious.MvvmCross.BindingEx.WindowsPhone/IMvxBindingCreator.cs
@@ -0,0 +1,22 @@
+﻿// IMvxBindingCreator.cs
+// (c) Copyright Cirrious Ltd. http://www.cirrious.com
+// MvvmCross is licensed using Microsoft Public License (Ms-PL)
+// Contributions and inspirations noted in readme.md and license.txt
+// 
+// Project Lead - Stuart Lodge, @slodge, me@slodge.com
+
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using Cirrious.MvvmCross.Binding.Binders;
+
+namespace Cirrious.MvvmCross.BindingEx.WindowsPhone
+{
+    public interface IMvxBindingCreator
+    {
+        void CreateBindings(
+            object sender, 
+            DependencyPropertyChangedEventArgs args,
+            Func<string, IEnumerable<MvxBindingDescription>> parseBindingDescriptions);
+    }
+}
