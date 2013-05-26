2ec401a01285fd88cf823cda2debb659636240fa
Stuart <lodge.stuart@gmail.com>
2013-05-26 16:31:32 +0100

    A couple of expression based methods for getting the language binder from the source

--- Cirrious/Cirrious.MvvmCross.Binding/BindingContext/MvxBindingContextOwnerExtensions.Language.cs
+++ Cirrious/Cirrious.MvvmCross.Binding/BindingContext/MvxBindingContextOwnerExtensions.Language.cs
@@ -9,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using Cirrious.MvvmCross.Binding.Binders;
+using Cirrious.MvvmCross.Localization;
 
 namespace Cirrious.MvvmCross.Binding.BindingContext
 {
@@ -25,6 +26,17 @@ namespace Cirrious.MvvmCross.Binding.BindingContext
             owner.BindLanguage(target, targetPath, sourceKey);
         }
 
+        public static void BindLanguage<TTarget, TViewModel>(this IMvxBindingContextOwner owner
+                                        , TTarget target
+                                        , string sourceKey
+                                        , Expression<Func<TViewModel, IMvxTextProvider>> textProvider)
+        {
+            var parser = PropertyExpressionParser;
+            var targetPath = MvxBindingSingletonCache.Instance.DefaultBindingNameLookup.DefaultFor(typeof(TTarget));
+            var sourcePath = parser.Parse(textProvider).Print();
+            owner.BindLanguage(target, targetPath, sourceKey, sourcePath);
+        }
+
         public static void BindLanguage<TTarget>(this IMvxBindingContextOwner owner
                                         , TTarget target
                                         , Expression<Func<TTarget, object>> targetPropertyExpression
@@ -36,6 +48,22 @@ namespace Cirrious.MvvmCross.Binding.BindingContext
             var parser = PropertyExpressionParser;
             var parsedTargetPath = parser.Parse(targetPropertyExpression);
             var parsedTargetPathText = parsedTargetPath.Print();
+            owner.BindLanguage(target, parsedTargetPathText, sourceKey, sourcePropertyName, fallbackValue, converterName);
+        }
+
+        public static void BindLanguage<TTarget, TViewModel>(this IMvxBindingContextOwner owner
+                                        , TTarget target
+                                        , Expression<Func<TTarget, object>> targetPropertyExpression
+                                        , string sourceKey
+                                        , Expression<Func<TViewModel,IMvxLanguageBinder>> sourcePropertyExpression
+                                        , string fallbackValue = null
+                                        , string converterName = null)
+        {
+            var parser = PropertyExpressionParser;
+            var parsedTargetPath = parser.Parse(targetPropertyExpression);
+            var parsedTargetPathText = parsedTargetPath.Print();
+            var parsedSourcePath = parser.Parse(sourcePropertyExpression);
+            var sourcePropertyName = parsedSourcePath.Print();
             owner.BindLanguage(target, parsedTargetPathText, sourceKey, sourcePropertyName, fallbackValue, converterName);
         }
 
