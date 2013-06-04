07d396ab46ada938017592891853aecd8e242be3
Stuart <lodge.stuart@gmail.com>
2013-06-04 10:28:02 +0100

    First attempt at moving Dialog Bindings down to the dialog project

---
+++ Cirrious/Cirrious.MvvmCross.Binding/BindingContext/MvxTypedBindingTarget.cs
@@ -0,0 +1,19 @@
+// MvxTypedBindingTarget.cs
+// (c) Copyright Cirrious Ltd. http://www.cirrious.com
+// MvvmCross is licensed using Microsoft Public License (Ms-PL)
+// Contributions and inspirations noted in readme.md and license.txt
+// 
+// Project Lead - Stuart Lodge, @slodge, me@slodge.com
+
+namespace Cirrious.MvvmCross.Binding.BindingContext
+{
+    public class MvxTypedBindingTarget<TViewModel>
+    {
+        public MvxTypedBindingTarget(IMvxBindingContextOwner bindingContextOwner)
+        {
+            BindingContextOwner = bindingContextOwner;
+        }
+
+        public IMvxBindingContextOwner BindingContextOwner { get; private set; }
+    }
+}
