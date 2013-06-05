96aad4f7c8a4c4078de94de82c5f4c5f77bcd8f4
csteeg <chris@chrisvandesteeg.nl>
2013-06-05 14:53:08 +0200

    Allow DateTimeElements to contain null values

--- CrossUI/CrossUI.Droid/Dialog/Elements/DateTimeElement.cs
+++ CrossUI/CrossUI.Droid/Dialog/Elements/DateTimeElement.cs
@@ -12,27 +12,22 @@ using CrossUI.Core;
 
 namespace CrossUI.Droid.Dialog.Elements
 {
-    public class DateTimeElement : StringDisplayingValueElement<DateTime>
+    public class DateTimeElement : StringDisplayingValueElement<DateTime?>
     {
         public int MinuteInterval { get; set; }
 
         public DateTimeElement(string caption = null, DateTime? date = null, string layoutName = null)
-            : this(caption, date ?? DateTime.UtcNow, layoutName)
-        {
-        }
-
-        public DateTimeElement(string caption, DateTime date, string layoutName = null)
             : base(caption, date, layoutName ?? "dialog_multiline_labelfieldbelow")
         {
             Click = delegate { EditDate(); };
             DateTimeFormat = "G";
-            if (Value.Kind != DateTimeKind.Utc)
+            if (Value.HasValue && Value.Value.Kind != DateTimeKind.Utc)
                 DialogTrace.WriteLine("Warning - non Utc datetmie used within DateTimeElement - can lead to unpredictable results");
         }
 
-        protected override string Format(DateTime dt)
+        protected override string Format(DateTime? dt)
         {
-            return dt.ToString(DateTimeFormat);
+            return dt.HasValue ? dt.Value.ToString(DateTimeFormat) : string.Empty;
         }
 
         public string DateTimeFormat { get; set; }
@@ -45,7 +40,7 @@ namespace CrossUI.Droid.Dialog.Elements
                 Android.Util.Log.Warn("DateElement", "No Context for Edit");
                 return;
             }
-            var val = Value;
+            var val = Value.HasValue ? Value.Value : DateTime.Now;
             new DatePickerDialog(context, DateCallback ?? OnDateTimeSet, val.Year, val.Month - 1, val.Day).Show();
         }
 
@@ -57,21 +52,21 @@ namespace CrossUI.Droid.Dialog.Elements
                 Android.Util.Log.Warn("TimeElement", "No Context for Edit");
                 return;
             }
-            DateTime val = Value;
+            var val = Value.HasValue ? Value.Value : DateTime.Now;
             new TimePickerDialog(context, OnTimeSet, val.Hour, val.Minute, DateFormat.Is24HourFormat(context)).Show();
         }
 
         // the event received when the user "sets" the date in the dialog
         protected void OnDateSet(object sender, DatePickerDialog.DateSetEventArgs e)
         {
-            DateTime current = Value;
+            DateTime current = Value.HasValue ? Value.Value : DateTime.Now;
             OnUserValueChanged(new DateTime(e.Date.Year, e.Date.Month, e.Date.Day, current.Hour, current.Minute, 0, DateTimeKind.Utc));
         }
 
         // the event received when the user "sets" the date in the dialog
         protected void OnDateTimeSet(object sender, DatePickerDialog.DateSetEventArgs e)
         {
-            DateTime current = Value;
+            DateTime current = Value.HasValue ? Value.Value : DateTime.Now;
             OnUserValueChanged(new DateTime(e.Date.Year, e.Date.Month, e.Date.Day, current.Hour, current.Minute, 0, DateTimeKind.Utc));
             EditTime();
         }
@@ -79,7 +74,7 @@ namespace CrossUI.Droid.Dialog.Elements
         // the event received when the user "sets" the time in the dialog
         protected void OnTimeSet(object sender, TimePickerDialog.TimeSetEventArgs e)
         {
-            DateTime current = Value;
+            DateTime current = Value.HasValue ? Value.Value : DateTime.Now;
             OnUserValueChanged(new DateTime(current.Year, current.Month, current.Day, e.HourOfDay, e.Minute, 0, DateTimeKind.Utc));
         }
 
