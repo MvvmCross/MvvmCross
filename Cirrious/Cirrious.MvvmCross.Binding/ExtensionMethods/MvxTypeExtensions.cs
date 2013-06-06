dc4009f907733f59af4d2ff130ea41c0451f9ae2
admin <unknown-user@192.168.253.130>
2013-06-06 15:44:07 +0200

    if value is null and type is value type (eg int? nullable), return default value

--- Cirrious/Cirrious.MvvmCross.Binding/ExtensionMethods/MvxTypeExtensions.cs
+++ Cirrious/Cirrious.MvvmCross.Binding/ExtensionMethods/MvxTypeExtensions.cs
@@ -14,15 +14,23 @@ namespace Cirrious.MvvmCross.Binding.ExtensionMethods
     {
         public static object MakeSafeValue(this Type propertyType, object value)
         {
-            if (value != null)
-            {
-                var autoConverter = MvxBindingSingletonCache.Instance.AutoValueConverters.Find(value.GetType(),
-                                                                                               propertyType);
-                if (autoConverter != null)
-                {
-                    return autoConverter.Convert(value, propertyType, null, CultureInfo.CurrentUICulture);
-                }
-            }
+			if (value != null) 
+			{
+				var autoConverter = MvxBindingSingletonCache.Instance.AutoValueConverters.Find (value.GetType(),
+				                                                                                propertyType);
+				if (autoConverter != null) {
+					return autoConverter.Convert (value, propertyType, null, CultureInfo.CurrentUICulture);
+				}
+			} 
+			else 
+			{
+				if(propertyType.IsValueType)
+				{
+					return Activator.CreateInstance(propertyType);
+				}
+				return null;
+			}
+
 
             var safeValue = value;
             if (!propertyType.IsInstanceOfType(value))
