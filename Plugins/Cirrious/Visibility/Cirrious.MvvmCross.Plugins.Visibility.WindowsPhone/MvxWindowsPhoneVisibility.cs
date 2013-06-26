f68b61e00ec4a173c9a92b469822a59e6c045ec4
Stuart <lodge.stuart@gmail.com>
2013-06-26 09:47:31 +0100

    Windows Phone Design Time Visibility Helper - part of #323

--- Plugins/Cirrious/Visibility/Cirrious.MvvmCross.Plugins.Visibility.WindowsPhone/MvxWindowsPhoneVisibility.cs
+++ Plugins/Cirrious/Visibility/Cirrious.MvvmCross.Plugins.Visibility.WindowsPhone/MvxWindowsPhoneVisibility.cs
@@ -11,15 +11,11 @@ namespace Cirrious.MvvmCross.Plugins.Visibility.WindowsPhone
 {
     public class MvxWindowsPhoneVisibility : IMvxNativeVisibility
     {
-        #region Implementation of IMvxNativeVisibility
-
         public object ToNative(MvxVisibility visibility)
         {
             return visibility == MvxVisibility.Visible
                        ? System.Windows.Visibility.Visible
                        : System.Windows.Visibility.Collapsed;
         }
-
-        #endregion
     }
 }
