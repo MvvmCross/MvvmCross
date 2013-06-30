0054721773e4ff13d98097a51a87702febb77b5b
Stuart <lodge.stuart@gmail.com>
2013-06-30 23:32:41 +0100

    Method Binding plugin - first attempt

---
+++ nuspec/TouchBootstrapContent/MethodBindingPluginBootstrap.cs.pp
@@ -0,0 +1,9 @@
+using Cirrious.CrossCore.Plugins;
+
+namespace $rootnamespace$.Bootstrap
+{
+    public class MethodBindingPluginBootstrap
+        : MvxPluginBootstrapAction<Cirrious.MvvmCross.Plugins.MethodBinding.PluginLoader>
+    {
+    }
+}
