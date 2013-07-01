114b98313a669b6f5ff52684fad4088d3681e4dc
Stuart <lodge.stuart@gmail.com>
2013-07-01 21:45:34 +0100

    Field binding plugin

---
+++ Cirrious/FieldBinding/Cirrious.MvvmCross.FieldBinding/NC.cs
@@ -0,0 +1,28 @@
+﻿// NC.cs
+// (c) Copyright Cirrious Ltd. http://www.cirrious.com
+// MvvmCross is licensed using Microsoft Public License (Ms-PL)
+// Contributions and inspirations noted in readme.md and license.txt
+// 
+// Project Lead - Stuart Lodge, @slodge, me@slodge.com
+
+using System;
+
+namespace Cirrious.MvvmCross.FieldBinding
+{
+    public class NC<T> : NotifyChange<T>, INC<T>
+    {
+        public NC()
+        {
+        }
+
+        public NC(T value)
+            : base(value)
+        {
+        }
+
+        public NC(T value, Action<T> valueChanged)
+            : base(value, valueChanged)
+        {
+        }
+    }
+}
---
+++ Cirrious/FieldBinding/Cirrious.MvvmCross.Plugins.FieldBinding/MvxLeafNotifyChangeFieldSourceBinding.cs
@@ -0,0 +1,43 @@
+﻿// MvxLeafNotifyChangeFieldSourceBinding.cs
+// (c) Copyright Cirrious Ltd. http://www.cirrious.com
+// MvvmCross is licensed using Microsoft Public License (Ms-PL)
+// Contributions and inspirations noted in readme.md and license.txt
+// 
+// Project Lead - Stuart Lodge, @slodge, me@slodge.com
+
+using System;
+using Cirrious.MvvmCross.Binding.Bindings.Source;
+using Cirrious.MvvmCross.FieldBinding;
+
+namespace Cirrious.MvvmCross.Plugins.FieldBinding
+{
+    public class MvxLeafNotifyChangeFieldSourceBinding
+        : MvxNotifyChangeFieldSourceBinding
+    {
+        public MvxLeafNotifyChangeFieldSourceBinding(object source, INotifyChange notifyChange)
+            : base(source, notifyChange)
+        {
+        }
+
+        protected override void NotifyChangeOnChanged(object sender, EventArgs eventArgs)
+        {
+            base.FireChanged(new MvxSourcePropertyBindingEventArgs(true, NotifyChange.Value));
+        }
+
+        public override void SetValue(object value)
+        {
+            NotifyChange.Value = value;
+        }
+
+        public override Type SourceType
+        {
+            get { return NotifyChange.ValueType; }
+        }
+
+        public override bool TryGetValue(out object value)
+        {
+            value = NotifyChange.Value;
+            return true;
+        }
+    }
+}
