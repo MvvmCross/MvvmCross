6fd3947efd58062bf4d5de21ee7b8d2fa37be437
Stuart <lodge.stuart@gmail.com>
2013-07-03 09:51:52 +0100

    Console->Wpf tidy up on the Sqlite plugin

--- Plugins/Cirrious/Sqlite/Cirrious.MvvmCross.Plugins.Sqlite.Wpf/MvxWpfSqLiteConnectionFactory.cs
+++ Plugins/Cirrious/Sqlite/Cirrious.MvvmCross.Plugins.Sqlite.Wpf/MvxWpfSqLiteConnectionFactory.cs
@@ -1,17 +1,16 @@
-﻿// MvxConsoleSQLiteConnectionFactory.cs
+﻿// MvxWpfSqLiteConnectionFactory.cs
 // (c) Copyright Cirrious Ltd. http://www.cirrious.com
 // MvvmCross is licensed using Microsoft Public License (Ms-PL)
 // Contributions and inspirations noted in readme.md and license.txt
 // 
 // Project Lead - Stuart Lodge, @slodge, me@slodge.com
 
-
 using System.IO;
 using SQLite;
 
-namespace Cirrious.MvvmCross.Plugins.Sqlite.Console
+namespace Cirrious.MvvmCross.Plugins.Sqlite.Wpf
 {
-	public class MvxConsoleSQLiteConnectionFactory : ISQLiteConnectionFactory
+	public class MvxWpfSqLiteConnectionFactory : ISQLiteConnectionFactory
 	{
 		public ISQLiteConnection Create(string address)
 		{
