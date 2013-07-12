517fcc2dbf5f2b055a3708769379051e5e09e012
Stuart <lodge.stuart@gmail.com>
2013-07-12 10:08:49 +0100

    Different error message (it is better? who knows?)

--- Cirrious/Cirrious.MvvmCross/ViewModels/MvxViewModelLoader.cs
+++ Cirrious/Cirrious.MvvmCross/ViewModels/MvxViewModelLoader.cs
@@ -44,7 +44,7 @@ namespace Cirrious.MvvmCross.ViewModels
             if (!viewModelLocator.TryLoad(request.ViewModelType, parameterValues, savedState, out viewModel))
             {
                 throw new MvxException(
-                    "Failed to load ViewModel for type {0} from locator {1}",
+                    "Failed to construct and initialise ViewModel for type {0} from locator {1} - check MvxTrace for more information",
                     request.ViewModelType, viewModelLocator.GetType().Name);
             }
 
