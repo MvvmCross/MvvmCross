b226a5ed7a268c12aa557b59d3900365a38cdbf1
Stuart <lodge.stuart@gmail.com>
2013-07-18 21:15:16 +0100

    Obsolete file - linked to #359

--- Cirrious/Cirrious.MvvmCross.Wpf/Views/MvxWpfSimpleViewPresenter.cs
+++ Cirrious/Cirrious.MvvmCross.Wpf/Views/MvxWpfSimpleViewPresenter.cs
@@ -5,10 +5,13 @@
 // 
 // Project Lead - Stuart Lodge, @slodge, me@slodge.com
 
+using System;
 using System.Windows;
 
 namespace Cirrious.MvvmCross.Wpf.Views
 {
+#warning Delete this class - see https://github.com/slodge/MvvmCross/issues/359
+    [Obsolete("Please use MvxSimpleWpfViewPresenter instead - this class will be deleted in future versions")]
     public class MvxWpfSimpleViewPresenter
         : MvxWpfViewPresenter
     {
