765107f0a8d9eb0fa9d5ad2ed6edc7382e849835
Stuart <lodge.stuart@gmail.com>
2013-07-23 13:42:43 +0100

    Possible fix for issue #353 - but really this syntax is so horrid that I'm not sure... could maybe consider adding some simple NDict and NColl interfaces and classes?

--- Cirrious/FieldBinding/Cirrious.MvvmCross.FieldBinding/NC.cs
+++ Cirrious/FieldBinding/Cirrious.MvvmCross.FieldBinding/NC.cs
@@ -6,10 +6,51 @@
 // Project Lead - Stuart Lodge, @slodge, me@slodge.com
 
 using System;
+using System.Collections.Generic;
 
 namespace Cirrious.MvvmCross.FieldBinding
 {
     public class NC<T> : NotifyChange<T>, INC<T>
+    {
+        public NC()
+        {
+        }
+
+        public NC(T value)
+            : base(value)
+        {
+        }
+
+        public NC(T value, Action<T> valueChanged)
+            : base(value, valueChanged)
+        {
+        }
+    }
+
+    public class NC<T, TValue>
+        : NotifyChange<T, TValue>
+        , INC<T, TValue>
+        where T : IList<TValue>
+    {
+        public NC()
+        {
+        }
+
+        public NC(T value)
+            : base(value)
+        {
+        }
+
+        public NC(T value, Action<T> valueChanged)
+            : base(value, valueChanged)
+        {
+        }
+    }
+
+    public class NC<T, TKey, TValue>
+        : NotifyChange<T, TKey, TValue>
+        , INC<T, TKey, TValue>
+        where T : IDictionary<TKey, TValue>
     {
         public NC()
         {
