30e14c6f4d7d0140b221116a5a90b389c6baa2f1
Stuart <lodge.stuart@gmail.com>
2013-07-23 13:42:43 +0100

    Possible fix for issue #353 - but really this syntax is so horrid that I'm not sure... could maybe consider adding some simple NDict and NColl interfaces and classes?

--- Plugins/Cirrious/FieldBinding/Cirrious.MvvmCross.FieldBinding/INotifyChange.cs
+++ Plugins/Cirrious/FieldBinding/Cirrious.MvvmCross.FieldBinding/INotifyChange.cs
@@ -6,6 +6,7 @@
 // Project Lead - Stuart Lodge, @slodge, me@slodge.com
 
 using System;
+using System.Collections.Generic;
 
 namespace Cirrious.MvvmCross.FieldBinding
 {
@@ -19,5 +20,19 @@ namespace Cirrious.MvvmCross.FieldBinding
     public interface INotifyChange<T> : INotifyChange
     {
         new T Value { get; set; }
+    }
+
+    public interface INotifyChange<T, TValue>
+        : INotifyChange<T>
+        where T : IList<TValue>
+    {
+        TValue this[int key] { get; set; }
+    }
+
+    public interface INotifyChange<T, TKey, TValue>
+        : INotifyChange<T>
+        where T : IDictionary<TKey,TValue>
+    {
+        TValue this[TKey key] { get; set; }
     }
 }
