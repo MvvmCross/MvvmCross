99f0fa2f38969aef420ba18d890d719ed9a47be2
iamahern <michael.i.ahern@gmail.com>
2013-08-15 09:22:06 +0200

    Add Console file plugin

---
+++ Plugins/Cirrious/File/Cirrious.MvvmCross.Plugins.File.Console/MvxConsoleFileStore.cs
@@ -0,0 +1,26 @@
+﻿// MvxAndroidFileStore.cs
+// (c) Copyright Cirrious Ltd. http://www.cirrious.com
+// MvvmCross is licensed using Microsoft Public License (Ms-PL)
+// Contributions and inspirations noted in readme.md and license.txt
+// 
+// Project Lead - Stuart Lodge, @slodge, me@slodge.com
+
+#region using
+
+using System.IO;
+using Cirrious.CrossCore;
+
+#endregion
+
+namespace Cirrious.MvvmCross.Plugins.File.Console
+{
+    public class MvxConsoleFileStore
+        : MvxFileStore
+          
+    {
+        protected override string FullPath(string path)
+        {
+            return Path.Combine(System.Environment.CurrentDirectory, path);
+        }
+    }
+}
