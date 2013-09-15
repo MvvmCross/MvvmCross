175d858ed9261a173766b5e239fa524fa2cc9953
Stuart <lodge.stuart@gmail.com>
2013-09-15 17:43:37 +0100

    Core binding changes - especially to allow for non-property info text fields to have some type conversion applied for numeric input (still doesn't allow commas for thousand separators though)

--- Cirrious/Cirrious.MvvmCross.Binding/ExtensionMethods/MvxBindingExtensions.cs
+++ Cirrious/Cirrious.MvvmCross.Binding/ExtensionMethods/MvxBindingExtensions.cs
@@ -1,4 +1,4 @@
-// MvxTypeExtensions.cs
+// MvxBindingExtensions.cs
 // (c) Copyright Cirrious Ltd. http://www.cirrious.com
 // MvvmCross is licensed using Microsoft Public License (Ms-PL)
 // Contributions and inspirations noted in readme.md and license.txt
@@ -11,8 +11,40 @@ using Cirrious.CrossCore.IoC;
 
 namespace Cirrious.MvvmCross.Binding.ExtensionMethods
 {
-    public static class MvxTypeExtensions
+    public static class MvxBindingExtensions
     {
+        public static bool ShouldSkipSetValueAsHaveNearlyIdenticalNumericText(this IMvxEditableTextView mvxEditableTextView, object target, object value)
+        {
+            if (value == null)
+                return false;
+
+            // specifically for double, float and decimal we do some special comparisons
+            // to prevent the user losing trailing periods, leading minus signs and trailing zeros
+            var valueType = value.GetType();
+            if (valueType == typeof(double) ||
+                valueType == typeof(float) ||
+                valueType == typeof(decimal))
+            {
+                var currentValue = mvxEditableTextView.CurrentText;
+                if (currentValue == null)
+                    return false;
+
+                try
+                {
+                    var equivalentCurrentValue = valueType.MakeSafeValue(currentValue);
+                    if (equivalentCurrentValue.Equals(value))
+                        return true;
+                }
+                catch (FormatException)
+                {
+                    // format problem - so they are definitely not equivalent
+                    return false;
+                }
+            }
+
+            return false;
+        }
+
         public static bool ConvertToBoolean(this object result)
         {
             if (result == null)
