6bd2902c1018ef259246052d8307d0fad6ee8504
E. Timothy Uy <tim@loqu8.com>
2013-09-23 16:22:17 -0700

    Added mac content

---
+++ nuspec/MacContent/Setup.cs.pp
@@ -0,0 +1,25 @@
+using MonoTouch.UIKit;
+using Cirrious.CrossCore.Platform;
+using Cirrious.MvvmCross.ViewModels;
+using Cirrious.MvvmCross.Touch.Platform;
+
+namespace $rootnamespace$
+{
+	public class Setup : MvxTouchSetup
+	{
+		public Setup(MvxApplicationDelegate applicationDelegate, UIWindow window)
+            : base(applicationDelegate, window)
+		{
+		}
+
+		protected override IMvxApplication CreateApp ()
+		{
+			return new Core.App();
+		}
+		
+        protected override IMvxTrace CreateDebugTrace()
+        {
+            return new DebugTrace();
+        }
+	}
+}
