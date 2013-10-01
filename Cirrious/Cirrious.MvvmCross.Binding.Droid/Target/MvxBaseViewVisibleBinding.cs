9b290edf736c9962b6d6a6cde36694000809f9fd
Stuart <lodge.stuart@gmail.com>
2013-10-01 11:06:14 +0100

    Add Hidden binding to Droid

--- Cirrious/Cirrious.MvvmCross.Binding.Droid/Target/MvxBaseViewVisibleBinding.cs
+++ Cirrious/Cirrious.MvvmCross.Binding.Droid/Target/MvxBaseViewVisibleBinding.cs
@@ -1,26 +1,31 @@
-// MvxViewVisibleBinding.cs
+// MvxBaseViewVisibleBinding.cs
 // (c) Copyright Cirrious Ltd. http://www.cirrious.com
 // MvvmCross is licensed using Microsoft Public License (Ms-PL)
 // Contributions and inspirations noted in readme.md and license.txt
 // 
 // Project Lead - Stuart Lodge, @slodge, me@slodge.com
 
+using System;
 using Android.Views;
-using Cirrious.MvvmCross.Binding.ExtensionMethods;
 
 namespace Cirrious.MvvmCross.Binding.Droid.Target
 {
-    public class MvxViewVisibleBinding
-        : MvxBaseViewVisibleBinding
+    public abstract class MvxBaseViewVisibleBinding
+        : MvxAndroidTargetBinding
     {
-        public MvxViewVisibleBinding(object target)
+        protected View View
+        {
+            get { return (View)Target; }
+        }
+
+        protected MvxBaseViewVisibleBinding(object target)
             : base(target)
         {
         }
 
-        protected override void SetValueImpl(object target, object value)
+        public override Type TargetType
         {
-            ((View)target).Visibility =  value.ConvertToBoolean() ? ViewStates.Visible : ViewStates.Gone;
+            get { return typeof(bool); }
         }
     }
 }
