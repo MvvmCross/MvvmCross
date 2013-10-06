49cf0e91084a7de391e5fe35331ad6d637d85c84
Stuart <lodge.stuart@gmail.com>
2013-10-06 15:09:15 +0100

    Debug text in color should say argb, not rgba

--- CrossCore/Cirrious.CrossCore/UI/MvxColor.cs
+++ CrossCore/Cirrious.CrossCore/UI/MvxColor.cs
@@ -76,7 +76,7 @@ namespace Cirrious.CrossCore.UI
 
         public override string ToString()
         {
-            return string.Format("rgba: #{0:X2}{1:X2}{2:X2}{3:X2}", 
+            return string.Format("argb: #{0:X2}{1:X2}{2:X2}{3:X2}", 
                 R, G, B, A);
         }
     }
