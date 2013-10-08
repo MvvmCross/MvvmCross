072c0bbf0545592dbfe955bee31457bdfbcd25f2
Stuart <lodge.stuart@gmail.com>
2013-10-08 19:20:09 +0100

    Fixes https://github.com/MvvmCross/MvvmCross/pull/453 and

--- CrossCore/Cirrious.CrossCore/WeakSubscription/MvxNotifyCollectionChangedEventSubscription.cs
+++ CrossCore/Cirrious.CrossCore/WeakSubscription/MvxNotifyCollectionChangedEventSubscription.cs
@@ -16,14 +16,11 @@ namespace Cirrious.CrossCore.WeakSubscription
     {
         private static readonly EventInfo EventInfo = typeof (INotifyCollectionChanged).GetEvent("CollectionChanged");
 
-        static MvxNotifyCollectionChangedEventSubscription()
+        // This code ensures the CollectionChanged event is not stripped by Xamarin linker
+        // see https://github.com/MvvmCross/MvvmCross/pull/453
+        public static void LinkerPleaseInclude(INotifyCollectionChanged collection)
         {
-            // This code ensures the CollectionChanged event is not stripped by Xamarin linker
-            INotifyCollectionChanged collection = null;
-            if (collection != null)
-            {
-                collection.CollectionChanged += (sender, e) => { };
-            }
+            collection.CollectionChanged += (sender, e) => { };
         }
 
         public MvxNotifyCollectionChangedEventSubscription(INotifyCollectionChanged source,
