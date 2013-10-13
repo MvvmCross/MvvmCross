699dc071ff11c9dabe5b3e8a45bc0f6f2e86f268
ShawInnes <shaw@immortal.net.au>
2013-10-13 17:21:21 +1000

    Fix AssemblyInfo.cs

--- Cirrious/ThreadUtils/Cirrious.MvvmCross.Plugins.ThreadUtils.Wpf/Properties/AssemblyInfo.cs
+++ Cirrious/ThreadUtils/Cirrious.MvvmCross.Plugins.ThreadUtils.Wpf/Properties/AssemblyInfo.cs
@@ -1,10 +1,17 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿// AssemblyInfo.cs
+// (c) Copyright Cirrious Ltd. http://www.cirrious.com
+// MvvmCross is licensed using Microsoft Public License (Ms-PL)
+// Contributions and inspirations noted in readme.md and license.txt
+// 
+// Project Lead - Stuart Lodge, @slodge, me@slodge.com
+
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+
 [assembly: AssemblyTitle("Cirrious.MvvmCross.Plugins.ThreadUtils.Wpf")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -13,14 +20,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("beb48dbf-e252-4462-9a04-8e2ae05f04f0")]
 
 // Version information for an assembly consists of the following four values:
 //
