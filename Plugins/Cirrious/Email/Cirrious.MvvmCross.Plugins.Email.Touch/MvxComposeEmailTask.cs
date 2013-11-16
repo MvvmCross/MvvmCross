4e91ff63dd2f8a1280a05c22ba570e5a99f91a22
Stuart <lodge.stuart@gmail.com>
2013-11-16 17:08:42 +0000

    Refactoring... not sure I like this refactoring... (forgotten how hard cross-platform apis are!)

--- Plugins/Cirrious/Email/Cirrious.MvvmCross.Plugins.Email.Touch/MvxComposeEmailTask.cs
+++ Plugins/Cirrious/Email/Cirrious.MvvmCross.Plugins.Email.Touch/MvxComposeEmailTask.cs
@@ -7,8 +7,9 @@
 
 using System;
 using System.Collections.Generic;
-using System.IO;
+using System.Linq;
 using Cirrious.CrossCore;
+using Cirrious.CrossCore.Exceptions;
 using Cirrious.CrossCore.Touch.Platform;
 using Cirrious.CrossCore.Touch.Views;
 using MonoTouch.MessageUI;
@@ -19,7 +20,7 @@ namespace Cirrious.MvvmCross.Plugins.Email.Touch
 {
     public class MvxComposeEmailTask
         : MvxTouchTask
-          , IMvxComposeEmailTask         
+        , IMvxComposeEmailTaskEx
     {
         private readonly IMvxTouchModalHost _modalHost;
         private MFMailComposeViewController _mail;
@@ -31,39 +32,59 @@ namespace Cirrious.MvvmCross.Plugins.Email.Touch
 
         public void ComposeEmail(string to, string cc, string subject, string body, bool isHtml)
         {
+            var toArray = to == null ? null : new[] { to };
+            var ccArray = cc == null ? null : new[] { cc };
+            ComposeEmail(
+                toArray,
+                ccArray,
+                subject,
+                body,
+                isHtml,
+                null);
+        }
+
+        public void ComposeEmail(
+            IEnumerable<string> to,
+            IEnumerable<string> cc, string subject,
+            string body, bool isHtml,
+            IEnumerable<EmailAttachment> attachments)
+        {
             if (!MFMailComposeViewController.CanSendMail)
-                return;
+                throw new MvxException("This device cannot send mail");
 
             _mail = new MFMailComposeViewController();
             _mail.SetMessageBody(body ?? string.Empty, isHtml);
             _mail.SetSubject(subject ?? string.Empty);
-            _mail.SetCcRecipients(new[] {cc ?? string.Empty});
-            _mail.SetToRecipients(new[] {to ?? string.Empty});
+            _mail.SetCcRecipients(cc == null ? new[] { string.Empty } : cc.ToArray());
+            _mail.SetToRecipients(to == null ? new[] { string.Empty } : to.ToArray());
+            if (attachments != null)
+            {
+                foreach (var a in attachments)
+                {
+                    _mail.AddAttachmentData(NSData.FromStream(a.Content), a.ContentType, a.FileName);
+                }
+            }
             _mail.Finished += HandleMailFinished;
 
             _modalHost.PresentModalViewController(_mail, true);
         }
 
-		public void ComposeEmail(
-			string[] to, string[] cc, string subject, 
-			string body, bool isHtml, 
-			List<EmailAttachment> attachments)
-		{
-			_mail = new MFMailComposeViewController();
-            _mail.SetMessageBody(body ?? string.Empty, isHtml);
-            _mail.SetSubject(subject ?? string.Empty);
-            _mail.SetCcRecipients(cc ?? new[] {string.Empty});
-            _mail.SetToRecipients(to ?? new[] {string.Empty});
-			attachments.ForEach (a => _mail.AddAttachmentData(NSData.FromStream(a.Content), a.ContentType, a.FileName));
-			_mail.Finished += HandleMailFinished;
+        public bool CanSendEmail
+        {
+            get
+            {
+                return MFMailComposeViewController.CanSendMail;
+            }
+        }
 
-			_modalHost.PresentModalViewController(_mail, true);
-		}
-
-		public bool CanSendEmail()
-		{
-			return MFMailComposeViewController.CanSendMail;
-		}
+        public bool CanSendAttachments
+        {
+            get
+            {
+                // if we can send email, then we can send attachments
+                return CanSendEmail;
+            }
+        }
 
         private void HandleMailFinished(object sender, MFComposeResultEventArgs e)
         {
