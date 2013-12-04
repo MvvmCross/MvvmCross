8defd93d0bcc9c495dac3e987889271ab996c4fe
E. Timothy Uy <tim@loqu8.com>
2013-12-04 11:22:58 -0800

    Add additional MacContent for StarterPack

---
+++ nuspec/MacContent/AppDelegate.cs.txt.pp
@@ -0,0 +1,34 @@
+using System;
+using System.Drawing;
+using MonoMac.Foundation;
+using MonoMac.AppKit;
+using MonoMac.ObjCRuntime;
+using Cirrious.MvvmCross.Mac.Views.Presenters;
+using Cirrious.CrossCore;
+using Cirrious.MvvmCross.ViewModels;
+using Cirrious.MvvmCross.Mac.Platform;
+
+namespace $rootnamespace$
+{
+	[Register ("AppDelegate")]
+	public partial class AppDelegate : MvxApplicationDelegate
+	{
+		NSWindow _window;
+
+		public override void FinishedLaunching (NSObject notification)
+		{
+			_window = new NSWindow (new RectangleF(200,200,400,700), NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Titled,
+			                        NSBackingStore.Buffered, false, NSScreen.MainScreen);
+
+			var setup = new Setup(this, _window);
+			setup.Initialize();
+
+			var startup = Mvx.Resolve<IMvxAppStart>();
+			startup.Start();
+
+			_window.MakeKeyAndVisible ();
+			
+			return true;
+		}
+	}
+}
