40df4f0e16097763f4508161789da014085bdd6d
Stephan van Stekelenburg <stephan.van.stekelenburg@gmail.com>
2014-01-20 14:12:15 +0100

    Added Target Bindings for UIView BackgroundColor and UIView Layer BorderWidth. Resolves #557

---
+++ Cirrious/Cirrious.MvvmCross.Binding.Touch/Target/MvxUIViewLayerBorderWidthTargetBinding.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Cirrious.MvvmCross.Binding.Bindings.Target;
+using MonoTouch.UIKit;
+
+namespace Cirrious.MvvmCross.Binding.Touch.Target
+{
+    public class MvxUIViewLayerBorderWidthTargetBinding
+        : MvxConvertingTargetBinding
+    {
+        public MvxUIViewLayerBorderWidthTargetBinding(object target) 
+            : base(target)
+        {
+        }
+
+        public override Type TargetType
+        {
+            get { return typeof (UIView); }
+        }
+
+        protected override void SetValueImpl(object target, object value)
+        {
+            var view = (UIView)target;
+
+            if (view == null || value == null)
+                return;
+
+            view.Layer.BorderWidth = (float)value;
+        }
+    }
+}
