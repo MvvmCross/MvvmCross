9d6dff5a7b02d1b2373ac210347daab9b0a21872
Stuart <lodge.stuart@gmail.com>
2014-01-26 20:09:26 +0000

    Merge of LayerBorderWidth code from @Stephanvs - fixes #559

---
+++ Cirrious/Cirrious.MvvmCross.Binding.Touch/Target/MvxUIViewLayerBorderWidthTargetBinding.cs
@@ -0,0 +1,40 @@
+// MvxUIViewLayerBorderWidthTargetBinding.cs
+// (c) Copyright Cirrious Ltd. http://www.cirrious.com
+// MvvmCross is licensed using Microsoft Public License (Ms-PL)
+// Contributions and inspirations noted in readme.md and license.txt
+// 
+// Project Lead - Stuart Lodge, @slodge, me@slodge.com
+
+using System;
+using Cirrious.MvvmCross.Binding.Bindings.Target;
+using MonoTouch.UIKit;
+
+namespace Cirrious.MvvmCross.Binding.Touch.Target
+{
+    public class MvxUIViewLayerBorderWidthTargetBinding
+        : MvxConvertingTargetBinding
+    {
+        public MvxUIViewLayerBorderWidthTargetBinding(object target) 
+            : base(target)
+        {
+        }
+
+        public override Type TargetType
+        {
+            get { return typeof (float); }
+        }
+
+        protected override void SetValueImpl(object target, object value)
+        {
+            var view = (UIView)target;
+
+            if (view == null || value == null)
+                return;
+
+            if (view.Layer == null)
+                return;
+
+            view.Layer.BorderWidth = (float)value;
+        }
+    }
+}
