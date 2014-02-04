73d90932a499a96a32cb84b863b8ca1934dfb66e
Mayank Gulati <mayank_gulati@hotmail.com>
2014-02-04 11:59:41 -0500

    Fixed FolderExists in FileStore for WindowsStore
    The previous implementation tried to get parent folder and see if the
    folder asked for exists within that... If the folder passed in as
    FolderPath was the LocalStorage folder, then parent will become the root
    which the application does not have access to and throw errors.
    
    Another workaround would be to give the app access to the root folder,
    but that seems to be not the best way to go about it.

--- Plugins/Cirrious/File/Cirrious.MvvmCross.Plugins.File.WindowsStore/MvxWindowsStoreBlockingFileStore.cs
+++ Plugins/Cirrious/File/Cirrious.MvvmCross.Plugins.File.WindowsStore/MvxWindowsStoreBlockingFileStore.cs
@@ -163,12 +163,15 @@ namespace Cirrious.MvvmCross.Plugins.File.WindowsStore
                 folderPath = ToFullPath(folderPath);
                 folderPath = folderPath.TrimEnd('\\');
 
-                var parent = Path.GetDirectoryName(folderPath);
-                var parentFolder = StorageFolder.GetFolderFromPathAsync(parent).Await();
-
-                var leafFolderName = Path.GetFileName(folderPath);
-
-                return parentFolder.GetFoldersAsync().Await().Any(x => x.Name == leafFolderName);
+                try
+                {
+                    var thisFolder = StorageFolder.GetFolderFromPathAsync(folderPath).Await();
+                    return true;
+                }
+                catch (System.UnauthorizedAccessException)
+                {
+                    return false;
+                }
             }
             catch (FileNotFoundException)
             {
