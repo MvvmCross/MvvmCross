b7b1dcd5e043eebec783c52ce3430195a7cdec95
Artur Rybak <artur.rybak@ihs.com>
2014-03-18 13:48:21 +0100

    Added AsyncRequestHandle class, to support request cancel in future.

---
+++ Plugins/Cirrious/Network/Cirrious.MvvmCross.Plugins.Network/Rest/MvxRestRequestAsyncHandle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace Cirrious.MvvmCross.Plugins.Network.Rest
+{
+    public class MvxRestRequestAsyncHandle
+    {
+        public HttpWebRequest WebRequest;
+
+        public MvxRestRequestAsyncHandle()
+        {
+        }
+
+        public MvxRestRequestAsyncHandle(HttpWebRequest webRequest)
+        {
+            WebRequest = webRequest;
+        }
+
+        public void Abort()
+        {
+            if (WebRequest != null)
+                WebRequest.Abort();
+        }
+    }
+}
