c316ba998419743f7ddab67525768ff3800808ef
Anass Bouassaba <anass.bouassaba@digitalpatrioten.com>
2014-04-23 16:11:53 +0200

    Added support for Android FragmentActivity

---
+++ Cirrious/Cirrious.MvvmCross.Droid/Views/MvxFragmentActivity.cs
@@ -0,0 +1,71 @@
+﻿// MvxFragmentActivity.cs
+// (c) Copyright Cirrious Ltd. http://www.cirrious.com
+// MvvmCross is licensed using Microsoft Public License (Ms-PL)
+// Contributions and inspirations noted in readme.md and license.txt
+// 
+// Project Lead - Stuart Lodge, @slodge, me@slodge.com
+//
+// @author: Anass Bouassaba <anass.bouassaba@digitalpatrioten.com>
+
+using System.Collections.Generic;
+using Android.Content;
+using Cirrious.CrossCore.Droid.Views;
+using Cirrious.MvvmCross.Binding.BindingContext;
+using Cirrious.MvvmCross.Binding.Droid.BindingContext;
+using Cirrious.MvvmCross.ViewModels;
+
+namespace Cirrious.MvvmCross.Droid.Views
+{
+	public abstract class MvxFragmentActivity
+		: MvxEventSourceFragmentActivity
+	, IMvxAndroidView
+	, IMvxChildViewModelOwner
+	{
+		private readonly List<int> _ownedSubViewModelIndicies = new List<int>();
+
+		public List<int> OwnedSubViewModelIndicies
+		{
+			get { return _ownedSubViewModelIndicies; }
+		}
+
+		protected MvxFragmentActivity()
+		{
+			BindingContext = new MvxAndroidBindingContext(this, this);
+			this.AddEventListeners();
+		}
+
+		public object DataContext
+		{
+			get { return BindingContext.DataContext; }
+			set { BindingContext.DataContext = value; }
+		}
+
+		public IMvxViewModel ViewModel
+		{
+			get { return DataContext as IMvxViewModel; }
+			set
+			{
+				DataContext = value;
+				OnViewModelSet();
+			}
+		}
+
+		public void MvxInternalStartActivityForResult(Intent intent, int requestCode)
+		{
+			base.StartActivityForResult(intent, requestCode);
+		}
+
+		protected virtual void OnViewModelSet()
+		{
+		}
+
+		public IMvxBindingContext BindingContext { get; set; }
+
+		public override void SetContentView(int layoutResId)
+		{
+			var view = this.BindingInflate(layoutResId, null);
+			SetContentView(view);
+		}
+	}
+}
+
