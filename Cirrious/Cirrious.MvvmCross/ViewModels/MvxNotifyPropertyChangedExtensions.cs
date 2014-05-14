8f3fda2f092ecdd4655664500d7375848bc4b628
Pat Sissons <patricksissons@gmail.com>
2014-05-13 20:51:07 -0600

    adding IMvxNotifyPropertyChanged extension methods to simplify notification property setters
    e.g., this.RaiseAndSetIfChanged(ref _hello, value, () => Hello);
    
    Based on the work of paulcbetts in the reactiveui project
    See https://github.com/reactiveui/ReactiveUI/blob/rxui6-master/ReactiveUI/ReactiveObject.cs

---
+++ Cirrious/Cirrious.MvvmCross/ViewModels/MvxNotifyPropertyChangedExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Cirrious.MvvmCross.ViewModels
+{
+    public static class MvxNotifyPropertyChangedExtensions
+    {
+        private static TReturn RaiseAndSetIfChanged<T, TReturn>(T source, ref TReturn backingField, TReturn newValue, Action raiseAction)
+            where T : IMvxNotifyPropertyChanged
+        {
+            if (EqualityComparer<TReturn>.Default.Equals(backingField, newValue) == false)
+            {
+                backingField = newValue;
+
+                raiseAction();
+            }
+
+            return newValue;
+        }
+
+        public static TReturn RaiseAndSetIfChanged<T, TReturn>(this T source, ref TReturn backingField, TReturn newValue, Expression<Func<TReturn>> propertySelector)
+            where T : IMvxNotifyPropertyChanged
+        {
+            return RaiseAndSetIfChanged(source, ref backingField, newValue, () => source.RaisePropertyChanged(propertySelector));
+        }
+
+        public static TReturn RaiseAndSetIfChanged<T, TReturn>(this T source, ref TReturn backingField, TReturn newValue, string propertyName)
+            where T : IMvxNotifyPropertyChanged
+        {
+            return RaiseAndSetIfChanged(source, ref backingField, newValue, () => source.RaisePropertyChanged(propertyName));
+        }
+
+        public static TReturn RaiseAndSetIfChanged<T, TReturn>(this T source, ref TReturn backingField, TReturn newValue, PropertyChangedEventArgs args)
+            where T : IMvxNotifyPropertyChanged
+        {
+            return RaiseAndSetIfChanged(source, ref backingField, newValue, () => source.RaisePropertyChanged(args));
+        }
+    }
+}
