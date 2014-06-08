2377303928ab83ed9e4851ff0d0e5a2b304ca927
Stuart <lodge.stuart@gmail.com>
2014-06-08 20:20:21 +0100

    Additions for #677

--- Cirrious/File/Cirrious.MvvmCross.Plugins.File/IMvxFileStoreAsync.cs
+++ Cirrious/File/Cirrious.MvvmCross.Plugins.File/IMvxFileStoreAsync.cs
@@ -1,21 +1,21 @@
-﻿// IMvxFileStore.cs
+﻿// IMvxFileStoreAsync.cs
 // (c) Copyright Cirrious Ltd. http://www.cirrious.com
 // MvvmCross is licensed using Microsoft Public License (Ms-PL)
 // Contributions and inspirations noted in readme.md and license.txt
 // 
 // Project Lead - Stuart Lodge, @slodge, me@slodge.com
-using System.Threading.Tasks;
-
-#region using
 
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Threading.Tasks;
 
-#endregion
 
 namespace Cirrious.MvvmCross.Plugins.File
 {
+    /// <summary>
+    /// Warning: this interface not implemented on all platforms currently
+    /// </summary>
 	public interface IMvxFileStoreAsync
     {
 		Task<TryResult<string>> TryReadTextFileAsync(string path);
