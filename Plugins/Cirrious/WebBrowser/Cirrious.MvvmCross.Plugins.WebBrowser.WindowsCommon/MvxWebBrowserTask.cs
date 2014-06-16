93f24a28c983b18a87ac339149da8b32d536d3e8
Steve Dunford <stephen.dunford@sequence.co.uk>
2014-06-16 15:10:23 +0100

    Adding WindowsCommon plugins

---
+++ Plugins/Cirrious/WebBrowser/Cirrious.MvvmCross.Plugins.WebBrowser.WindowsCommon/MvxWebBrowserTask.cs
@@ -0,0 +1,28 @@
+﻿// MvxWebBrowserTask.cs
+// (c) Copyright Cirrious Ltd. http://www.cirrious.com
+// MvvmCross is licensed using Microsoft Public License (Ms-PL)
+// Contributions and inspirations noted in readme.md and license.txt
+// 
+// Project Lead - Stuart Lodge, @slodge, me@slodge.com
+
+#region using
+
+using System;
+
+#endregion
+
+namespace Cirrious.MvvmCross.Plugins.WebBrowser.WindowsCommon
+{
+    public class MvxWebBrowserTask : IMvxWebBrowserTask
+    {
+        #region IMvxWebBrowserTask Members
+
+        public void ShowWebPage(string url)
+        {
+            // note - this call deliberately not awaited - OK to continue with flow
+            Windows.System.Launcher.LaunchUriAsync(new Uri(url));
+        }
+
+        #endregion
+    }
+}
