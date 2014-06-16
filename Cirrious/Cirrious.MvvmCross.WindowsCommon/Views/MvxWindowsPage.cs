6141e64fe272b430c05d6a3ab5a5dd2cb6bf9b1a
Stephen Dunford <stephen.dunford@sequence.co.uk>
2014-06-16 12:11:56 +0100

    Adding WindowsCommon core projects, plus File and ResourceLoader plugins

---
+++ Cirrious/Cirrious.MvvmCross.WindowsCommon/Views/MvxWindowsPage.cs
@@ -0,0 +1,113 @@
+﻿// MvxWinRTPage.cs
+// (c) Copyright Cirrious Ltd. http://www.cirrious.com
+// MvvmCross is licensed using Microsoft Public License (Ms-PL)
+// Contributions and inspirations noted in readme.md and license.txt
+// 
+// Project Lead - Stuart Lodge, @slodge, me@slodge.com
+
+using System;
+using System.Collections.Generic;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using Cirrious.CrossCore;
+using Cirrious.MvvmCross.ViewModels;
+using Cirrious.MvvmCross.Views;
+using Cirrious.MvvmCross.WindowsCommon.Views.Suspension;
+
+namespace Cirrious.MvvmCross.WindowsCommon.Views
+{
+    public abstract class MvxWindowsPage
+        : Page
+          , IMvxWindowsView
+    {
+        private IMvxViewModel _viewModel;
+
+        public IMvxViewModel ViewModel
+        {
+            get { return _viewModel; }
+            set
+            {
+                if (_viewModel == value)
+                    return;
+
+                _viewModel = value;
+                DataContext = ViewModel;
+            }
+        }
+
+        public void ClearBackStack()
+        {
+            throw new NotImplementedException();
+            /*
+            // note - we do *not* use CanGoBack here - as that seems to always returns true!
+            while (NavigationService.BackStack.Any())
+                NavigationService.RemoveBackEntry();
+         */
+        }
+
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            this.OnViewCreate(e.Parameter as MvxViewModelRequest, () => LoadStateBundle(e));
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+
+            var bundle = this.CreateSaveStateBundle();
+            SaveStateBundle(e, bundle);
+            
+            if (e.NavigationMode == NavigationMode.Back)
+                this.OnViewDestroy();
+        }
+
+        private String _pageKey;
+
+        private IMvxSuspensionManager _suspensionManager;
+        protected IMvxSuspensionManager SuspensionManager
+        {
+            get
+            {
+                _suspensionManager = _suspensionManager ?? Mvx.Resolve<IMvxSuspensionManager>();
+                return _suspensionManager;
+            }
+        }
+
+        protected virtual IMvxBundle LoadStateBundle(NavigationEventArgs e)
+        {
+            // nothing loaded by default
+            var frameState = SuspensionManager.SessionStateForFrame(this.Frame);
+            _pageKey = "Page-" + this.Frame.BackStackDepth;
+             IMvxBundle bundle = null;
+
+            if (e.NavigationMode == NavigationMode.New)
+            {
+                // Clear existing state for forward navigation when adding a new page to the
+                // navigation stack
+                var nextPageKey = this._pageKey;
+                int nextPageIndex = this.Frame.BackStackDepth;
+                while (frameState.Remove(nextPageKey))
+                {
+                    nextPageIndex++;
+                    nextPageKey = "Page-" + nextPageIndex;
+                }
+            }
+            else
+            {
+                var dictionary = (IDictionary<string, string>)frameState[this._pageKey];
+                bundle = new MvxBundle(dictionary);
+            }
+
+            return bundle;
+        }
+
+        protected virtual void SaveStateBundle(NavigationEventArgs navigationEventArgs, IMvxBundle bundle)
+        {
+            var frameState = SuspensionManager.SessionStateForFrame(this.Frame);
+            frameState[_pageKey] = bundle.Data;
+        }
+    }
+}
---
+++ Cirrious/Cirrious.MvvmCross.WindowsCommon/Views/MvxWindowsViewDispatcher.cs
@@ -0,0 +1,38 @@
+﻿// MvxStoreViewDispatcher.cs
+// (c) Copyright Cirrious Ltd. http://www.cirrious.com
+// MvvmCross is licensed using Microsoft Public License (Ms-PL)
+// Contributions and inspirations noted in readme.md and license.txt
+// 
+// Project Lead - Stuart Lodge, @slodge, me@slodge.com
+
+using Windows.UI.Xaml.Controls;
+using Cirrious.MvvmCross.ViewModels;
+using Cirrious.MvvmCross.Views;
+
+namespace Cirrious.MvvmCross.WindowsCommon.Views
+{
+    public class MvxWindowsViewDispatcher
+        : MvxWindowsMainThreadDispatcher
+          , IMvxViewDispatcher
+    {
+        private readonly IMvxWindowsViewPresenter _presenter;
+        private readonly Frame _rootFrame;
+
+        public MvxWindowsViewDispatcher(IMvxWindowsViewPresenter presenter, Frame rootFrame)
+            : base(rootFrame.Dispatcher)
+        {
+            _presenter = presenter;
+            _rootFrame = rootFrame;
+        }
+
+        public bool ShowViewModel(MvxViewModelRequest request)
+        {
+            return RequestMainThreadAction(() => _presenter.Show(request));
+        }
+
+        public bool ChangePresentation(MvxPresentationHint hint)
+        {
+            return RequestMainThreadAction(() => _presenter.ChangePresentation(hint));
+        }
+    }
+}
