4a862b79f99338466f91065f9b2218463957bff9
Stephen Dunford <stephen.dunford@sequence.co.uk>
2014-06-18 09:22:52 +0100

    Missing folder

---
+++ nuspec/WCommonContent/FirstView.xaml.cs.pp
@@ -0,0 +1,12 @@
+using Cirrious.MvvmCross.WindowsStore.Views;
+
+namespace $rootnamespace$.Views
+{
+    public sealed partial class FirstView : MvxStorePage
+    {
+        public FirstView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ nuspec/WCommonContent/Setup.cs.pp
@@ -0,0 +1,24 @@
+using Cirrious.CrossCore.Platform;
+using Cirrious.MvvmCross.ViewModels;
+using Cirrious.MvvmCross.WindowsStore.Platform;
+using Windows.UI.Xaml.Controls;
+
+namespace $rootnamespace$
+{
+    public class Setup : MvxStoreSetup
+    {
+        public Setup(Frame rootFrame) : base(rootFrame)
+        {
+        }
+
+        protected override IMvxApplication CreateApp()
+        {
+            return new Core.App();
+        }
+		
+        protected override IMvxTrace CreateDebugTrace()
+        {
+            return new DebugTrace();
+        }
+    }
+}
