47b86118ce270b11d10d55b57bbbec5e8e6e07d8
Alexandre Zollinger Chohfi <azchohfi@hotmail.com>
2014-07-29 19:50:59 -0300

    Fix for not compiling due to ImeAction.Previous being only avaiable for SDK 11+.

--- CrossUI/CrossUI.Droid/Dialog/Elements/EntryElement.cs
+++ CrossUI/CrossUI.Droid/Dialog/Elements/EntryElement.cs
@@ -107,7 +107,9 @@ namespace CrossUI.Droid.Dialog.Elements
             //android can't seem to find the correct NextFocusDown if items are added dynamically, we'll catch the next/previous ourselves
             _entry.EditorAction += (sender, args) =>
                 {
-                    if (args.ActionId == ImeAction.Next || args.ActionId == ImeAction.Previous)
+                    if (args.ActionId == ImeAction.Next ||
+						(Android.OS.Build.VERSION.SdkInt >= Android.OS.BuildVersionCodes.Honeycomb
+						&& args.ActionId == ImeAction.Previous))
                     {
                         ViewGroup group = _entry.Parent as ViewGroup;
                         IViewParent currentLoop = _entry.Parent;
