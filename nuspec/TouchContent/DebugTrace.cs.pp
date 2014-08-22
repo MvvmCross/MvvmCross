fbba27acd450a450295c617a4171eb538ddcea14
vzsg <zsolt.varadi@outlook.com>
2014-08-22 10:11:30 +0200

    Fixed infinite loop of FormatExceptions

--- nuspec/TouchContent/DebugTrace.cs.pp
+++ nuspec/TouchContent/DebugTrace.cs.pp
@@ -24,7 +24,7 @@ namespace $rootnamespace$
             }
             catch (FormatException)
             {
-                Trace(MvxTraceLevel.Error, tag, "Exception during trace of {0} {1} {2}", level, message);
+                Trace(MvxTraceLevel.Error, tag, "Exception during trace of {0} {1}", level, message);
             }
         }
     }
