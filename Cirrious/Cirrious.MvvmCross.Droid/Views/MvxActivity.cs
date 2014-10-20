1b8450e27fe787cf0079125f86a24a050a0dfc21
Stephan van Stekelenburg <stephan.van.stekelenburg@gmail.com>
2014-10-20 21:21:29 +0200

    Generic views for almost all platform specific base views

--- Cirrious/Cirrious.MvvmCross.Droid/Views/MvxActivity.cs
+++ Cirrious/Cirrious.MvvmCross.Droid/Views/MvxActivity.cs
@@ -56,4 +56,15 @@ namespace Cirrious.MvvmCross.Droid.Views
         {
         }
     }
+
+    public abstract class MvxActivity<TViewModel>
+        : MvxActivity
+        , IMvxAndroidView<TViewModel> where TViewModel : class, IMvxViewModel
+    {
+        public new TViewModel ViewModel
+        {
+            get { return (TViewModel)base.ViewModel; }
+            set { base.ViewModel = value; }
+        }
+    }
 }
