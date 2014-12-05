df80f619565ecd0d8ddbee6fec4b7842f68dd413
Kerry W. Lothrop <kwl@zuehlke.com>
2014-12-05 23:49:32 +0100

    Updated to Unified API, removed tabs, reformatted.

--- Cirrious/Cirrious.MvvmCross.Binding.Touch/Target/MvxUISegmentedControlSelectedSegmentTargetBinding.cs
+++ Cirrious/Cirrious.MvvmCross.Binding.Touch/Target/MvxUISegmentedControlSelectedSegmentTargetBinding.cs
@@ -1,67 +1,66 @@
 ﻿using Cirrious.MvvmCross.Binding.Bindings.Target;
-using MonoTouch.UIKit;
-using Cirrious.MvvmCross.Binding;
 using Cirrious.CrossCore.Platform;
+using UIKit;
 using System.Reflection;
 
 namespace Cirrious.MvvmCross.Binding.Touch.Target
 {
-	public class MvxUISegmentedControlSelectedSegmentTargetBinding : MvxPropertyInfoTargetBinding<UISegmentedControl>
-	{
-		private bool _subscribed;
+    public class MvxUISegmentedControlSelectedSegmentTargetBinding : MvxPropertyInfoTargetBinding<UISegmentedControl>
+    {
+        private bool _subscribed;
 
-		public MvxUISegmentedControlSelectedSegmentTargetBinding(object target, PropertyInfo targetPropertyInfo)
-			: base(target, targetPropertyInfo)
-		{
-		}
+        public MvxUISegmentedControlSelectedSegmentTargetBinding(object target, PropertyInfo targetPropertyInfo)
+            : base(target, targetPropertyInfo)
+        {
+        }
 
-		private void HandleValueChanged(object sender, System.EventArgs e)
-		{
-			var view = View;
-			if (view == null)
-				return;
-			FireValueChanged(view.SelectedSegment);
-		}
+        private void HandleValueChanged(object sender, System.EventArgs e)
+        {
+            var view = View;
+            if (view == null)
+                return;
+            FireValueChanged(view.SelectedSegment);
+        }
 
-		public override MvxBindingMode DefaultMode
-		{
-			get { return MvxBindingMode.TwoWay; }
-		}
+        public override MvxBindingMode DefaultMode
+        {
+            get { return MvxBindingMode.TwoWay; }
+        }
 
-		public override void SubscribeToEvents()
-		{
-			var segmentedControl = View;
-			if (segmentedControl == null)
-			{
-				MvxBindingTrace.Trace(MvxTraceLevel.Error, "Error - UISegmentedControll is null in MvxUISegmentedControlSelectedSegmentTargetBinding");
-				return;
-			}
+        public override void SubscribeToEvents()
+        {
+            var segmentedControl = View;
+            if (segmentedControl == null)
+            {
+                MvxBindingTrace.Trace(MvxTraceLevel.Error, "Error - UISegmentedControl is null in MvxUISegmentedControlSelectedSegmentTargetBinding");
+                return;
+            }
 
-			_subscribed = true;
-			segmentedControl.ValueChanged += HandleValueChanged;
-		}
+            _subscribed = true;
+            segmentedControl.ValueChanged += HandleValueChanged;
+        }
 
-		protected override void SetValueImpl(object target, object value)
-		{
-			var view = target as UISegmentedControl;
-			if (view == null)
-				return;
+        protected override void SetValueImpl(object target, object value)
+        {
+            var view = target as UISegmentedControl;
+            if (view == null)
+                return;
 
-			view.SelectedSegment = (int)value;
-		}
+            view.SelectedSegment = (int)value;
+        }
 
-		protected override void Dispose(bool isDisposing)
-		{
-			base.Dispose(isDisposing);
-			if (isDisposing)
-			{
-				var view = View;
-				if (view != null && _subscribed)
-				{
-					view.ValueChanged -= HandleValueChanged;
-					_subscribed = false;
-				}
-			}
-		}
-	}
+        protected override void Dispose(bool isDisposing)
+        {
+            base.Dispose(isDisposing);
+            if (isDisposing)
+            {
+                var view = View;
+                if (view != null && _subscribed)
+                {
+                    view.ValueChanged -= HandleValueChanged;
+                    _subscribed = false;
+                }
+            }
+        }
+    }
 }
