cd3fdd1f6b0c0e70c6b253670315364c59506ce8
David Schwegler <david.schwegler@logos.com>
2015-02-19 14:25:34 -0800

    Adding MvxOwnedViewModelFragment attribute, which is required to enable 3.5 fragments create/save/restore behavior.

---
+++ Cirrious/Cirrious.MvvmCross.Droid.Fragging/MvxOwnedViewModelFragmentAttributeExtensionMethods.cs
@@ -0,0 +1,20 @@
+// MvxConventionAttributeExtensionMethods.cs
+// (c) Copyright Cirrious Ltd. http://www.cirrious.com
+// MvvmCross is licensed using Microsoft Public License (Ms-PL)
+// Contributions and inspirations noted in readme.md and license.txt
+// 
+// Project Lead - Stuart Lodge, @slodge, me@slodge.com
+
+using System;
+
+namespace Cirrious.MvvmCross.Droid.Fragging
+{
+    public static class MvxOwnedViewModelFragmentAttributeExtensionMethods
+    {
+        public static bool IsOwnedViewModelFragment(this Type candidateType)
+        {
+            var attributes = candidateType.GetCustomAttributes(typeof(MvxOwnedViewModelFragmentAttribute), true);
+            return attributes.Length > 0;
+        }
+    }
+}
