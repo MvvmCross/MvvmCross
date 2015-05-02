9f0b7b177aeb37b315d9efcb8084921fc99521a8
Ryan Herman <ryan@incroud.com>
2015-05-02 03:46:57 -0700

    Added Windows Universal Project

---
+++ CrossCore/Cirrious.CrossCore.Universal/Converters/MvxNativeValueConverter.cs
@@ -0,0 +1,76 @@
+// MvxNativeValueConverter.cs
+// (c) Copyright Cirrious Ltd. http://www.cirrious.com
+// MvvmCross is licensed using Microsoft Public License (Ms-PL)
+// Contributions and inspirations noted in readme.md and license.txt
+// 
+// Project Lead - Stuart Lodge, @slodge, me@slodge.com
+
+using System;
+using System.Globalization;
+using Cirrious.CrossCore.Converters;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Cirrious.CrossCore.WindowsStore.Converters
+{
+    public class MvxNativeValueConverter
+        : IValueConverter
+    {
+        private readonly IMvxValueConverter _wrapped;
+
+        protected IMvxValueConverter Wrapped
+        {
+            get { return _wrapped; }
+        }
+
+        public MvxNativeValueConverter(IMvxValueConverter wrapped)
+        {
+            _wrapped = wrapped;
+        }
+
+        public virtual object Convert(object value, Type targetType, object parameter, string language)
+        {
+            // note - Language ignored here!
+            var toReturn = _wrapped.Convert(value, targetType, parameter, CultureInfo.CurrentUICulture);
+            return MapIfSpecialValue(toReturn);
+        }
+
+        public virtual object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            // note - Language ignored here!
+            var toReturn = _wrapped.ConvertBack(value, targetType, parameter, CultureInfo.CurrentUICulture);
+            return MapIfSpecialValue(toReturn);
+        }
+
+        private static object MapIfSpecialValue(object toReturn)
+        {
+            if (toReturn == MvxBindingConstant.DoNothing)
+            {
+                Mvx.Trace("DoNothing does not have an equivalent in WinRT - returning UnsetValue instead");
+                return DependencyProperty.UnsetValue;
+            }
+
+            if (toReturn == MvxBindingConstant.UnsetValue)
+            {
+                return DependencyProperty.UnsetValue;
+            }
+
+            return toReturn;
+        }
+    }
+
+    public class MvxNativeValueConverter<T>
+        : MvxNativeValueConverter
+        where T : IMvxValueConverter, new()
+    {
+        protected new T Wrapped
+        {
+            get { return (T)base.Wrapped; }
+        }
+
+        public MvxNativeValueConverter()
+            : base(new T())
+        {
+        }
+    }
+}
---
+++ CrossCore/Cirrious.CrossCore.Universal/Platform/MvxDesignTimeHelper.cs
@@ -0,0 +1,38 @@
+// MvxDesignTimeHelper.cs
+// (c) Copyright Cirrious Ltd. http://www.cirrious.com
+// MvvmCross is licensed using Microsoft Public License (Ms-PL)
+// Contributions and inspirations noted in readme.md and license.txt
+// 
+// Project Lead - Stuart Lodge, @slodge, me@slodge.com
+
+using Cirrious.CrossCore.Core;
+using Cirrious.CrossCore.IoC;
+
+namespace Cirrious.CrossCore.WindowsStore.Platform
+{
+    public abstract class MvxDesignTimeHelper
+    {
+        protected MvxDesignTimeHelper()
+        {
+            if (!IsInDesignTool)
+                return;
+
+            if (MvxSingleton<IMvxIoCProvider>.Instance == null)
+            {
+                var iocProvider = MvxSimpleIoCContainer.Initialize();
+                Mvx.RegisterSingleton(iocProvider);
+            }
+        }
+
+        private static bool? _isInDesignTime;
+        protected static bool IsInDesignTool
+        {
+            get
+            {
+                if (!_isInDesignTime.HasValue)
+                    _isInDesignTime = Windows.ApplicationModel.DesignMode.DesignModeEnabled;
+                return _isInDesignTime.Value;
+            }
+        }
+    }
+}
