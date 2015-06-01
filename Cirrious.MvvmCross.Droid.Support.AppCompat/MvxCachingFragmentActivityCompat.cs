b0659e5b98e03fa494af0e2a3d4d3725206f234c
Martijn van Dijk <mhvdijk@gmail.com>
2015-06-01 17:30:55 +0200

    Added licenses

--- Cirrious.MvvmCross.Droid.Support.AppCompat/MvxCachingFragmentActivityCompat.cs
+++ Cirrious.MvvmCross.Droid.Support.AppCompat/MvxCachingFragmentActivityCompat.cs
@@ -1,3 +1,10 @@
+// MvxCachingFragmentActivityCompat.cs
+// (c) Copyright Cirrious Ltd. http://www.cirrious.com
+// MvvmCross is licensed using Microsoft Public License (Ms-PL)
+// Contributions and inspirations noted in readme.md and license.txt
+// 
+// Project Lead - Stuart Lodge, @slodge, me@slodge.com
+
 using Android.Content.Res;
 using Android.Graphics;
 using Android.OS;
--- Cirrious.MvvmCross.Droid.Support.RecyclerView/MvxRecyclerAdapter.cs
+++ Cirrious.MvvmCross.Droid.Support.RecyclerView/MvxRecyclerAdapter.cs
@@ -1,3 +1,10 @@
+// MvxRecyclerAdapter.cs
+// (c) Copyright Cirrious Ltd. http://www.cirrious.com
+// MvvmCross is licensed using Microsoft Public License (Ms-PL)
+// Contributions and inspirations noted in readme.md and license.txt
+// 
+// Project Lead - Stuart Lodge, @slodge, me@slodge.com
+
 using System;
 using System.Collections;
 using System.Collections.Specialized;
