3d2b39f512c39471cf0ae4ed6eccd3f4e5b90775
Tomasz Cielecki <tomasz@ostebaronen.dk>
2015-06-01 16:31:17 +0200

    Make SwipeRefreshLayout look more like other bindings

--- Cirrious.MvvmCross.Droid.Support.V4/MvxSwipeRefreshLayout.cs
+++ Cirrious.MvvmCross.Droid.Support.V4/MvxSwipeRefreshLayout.cs
@@ -1,46 +1,56 @@
-﻿using System;
+using System;
 using System.Windows.Input;
 using Android.Content;
 using Android.Runtime;
 using Android.Support.V4.Widget;
 using Android.Util;
 
-namespace Cirrious.MvvmCross.Droid.Support.V4 {
+namespace Cirrious.MvvmCross.Droid.Support.V4 
+{
+	[Register("cirrious.mvvmcross.droid.support.v4.MvxSwipeRefreshLayout")]
+	public class MvxSwipeRefreshLayout : SwipeRefreshLayout 
+	{	 
+		protected MvxSwipeRefreshLayout(IntPtr javaReference, JniHandleOwnership transfer)
+            : base(javaReference, transfer) { }
 
-    [Register("cirrious.mvvmcross.droid.support.v4.MvxSwipeRefreshLayout")]
-	public class MvxSwipeRefreshLayout : SwipeRefreshLayout {
+		public MvxSwipeRefreshLayout(Context p0)
+			: this (p0, null) { }
 
-		/*
-		 * Credits for the implementation go out to James Montemagno
-		 * http://motzcod.es/post/82102717747/xamarin-android-swiperefreshlayout-for-mvvmcross
-		 */ 
+		public MvxSwipeRefreshLayout(Context p0, IAttributeSet p1)
+			: base (p0, p1) { }
 
-		public MvxSwipeRefreshLayout ( Context p0 )
-			: this ( p0, null ) {
-			Init ();
-		}
+		private ICommand _refreshCommand;
+        private bool _refreshOverloaded;
 
-		public MvxSwipeRefreshLayout ( Context p0, IAttributeSet p1 )
-			: base ( p0, p1 ) {
-			Init ();
-		}
+        public ICommand RefreshCommand
+        {
+            get { return _refreshCommand; }
+            set
+            {
+                _refreshCommand = value;
+                if (_refreshCommand != null)
+                    EnsureRefreshCommandOverloaded();
+            }
+        }
+        
+        private void EnsureRefreshCommandOverloaded()
+        {
+            if (_refreshOverloaded)
+                return;
 
-		public ICommand RefreshCommand { get; set; }
+            _refreshOverloaded = true;
+            Refresh += (sender, args) => ExecuteRefreshCommand(RefreshCommand);
+        }
 
-		private void Init() { 
-			//This gets called when we pull down to refresh to trigger command 
-			this.Refresh += (object sender, EventArgs e ) => {
-				var command = RefreshCommand;
+        protected virtual void ExecuteRefreshCommand(ICommand command)
+        {
+            if (command == null)
+                return;
 
-				if ( command == null )
-					return;
+            if (!command.CanExecute(null))
+                return;
 
-				if (!command.CanExecute(null))
-					return;
-				
-				command.Execute ( null );
-			};
-		}
+            command.Execute(null);
+        }
 	}
 }
-
