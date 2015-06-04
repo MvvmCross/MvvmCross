e266b856489c7d1dc5c5e8316004f684092f23f8
Martijn van Dijk <mhvdijk@gmail.com>
2015-06-04 11:41:21 +0200

    Added MvxViewPagerFragmentAdapter

---
+++ Cirrious.MvvmCross.Droid.Support.V4/MvxViewPagerFragmentAdapter.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Android.Content;
+using Android.Runtime;
+using Android.Support.V4.App;
+using Cirrious.MvvmCross.Droid.Fragging.Fragments;
+using Cirrious.MvvmCross.ViewModels;
+using Java.Lang;
+
+namespace Cirrious.MvvmCross.Droid.Support.V4
+{
+    public class MvxViewPagerFragmentAdapter
+        : FragmentPagerAdapter
+    {
+        private readonly Context _context;
+
+        public IEnumerable<FragmentInfo> Fragments { get; private set; }
+
+        public override int Count
+        {
+            get { return Fragments.Count(); }
+        }
+
+        protected MvxViewPagerFragmentAdapter(IntPtr javaReference, JniHandleOwnership transfer)
+            : base(javaReference, transfer)
+        {
+        }
+
+        public MvxViewPagerFragmentAdapter(
+            Context context, FragmentManager fragmentManager, IEnumerable<FragmentInfo> fragments)
+            : base(fragmentManager)
+        {
+            _context = context;
+            Fragments = fragments;
+        }
+
+        public override Fragment GetItem(int position)
+        {
+            var fragmentInfo = Fragments.ElementAt(position);
+            var fragment = Fragment.Instantiate(_context,
+                FragmentJavaName(fragmentInfo.FragmentType));
+            ((MvxFragment)fragment).ViewModel = fragmentInfo.ViewModel;
+            return fragment;
+        }
+
+        protected static string FragmentJavaName(Type fragmentType)
+        {
+            var namespaceText = fragmentType.Namespace ?? "";
+            if (namespaceText.Length > 0)
+                namespaceText = namespaceText.ToLowerInvariant() + ".";
+            return namespaceText + fragmentType.Name;
+        }
+
+        public override ICharSequence GetPageTitleFormatted(int position)
+        {
+            return new Java.Lang.String(Fragments.ElementAt(position).Title);
+        }
+
+        public class FragmentInfo
+        {
+            public string Title { get; set; }
+
+            public Type FragmentType { get; set; }
+
+            public IMvxViewModel ViewModel { get; set; }
+        }
+    }
+}
+
