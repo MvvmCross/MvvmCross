76376d646e3440bd185222630ec1b839f4f86d67
Jeremy Kolb <jkolb@ara.com>
2015-06-16 18:34:33 -0400

    Add MvxAppCompatSpinner: a tint-aware version of MvxSpinner

---
+++ Cirrious.MvvmCross.Droid.Support.AppCompat/Widget/MvxAppCompatSpinner.cs
@@ -0,0 +1,102 @@
+using System;
+using System.Collections;
+using System.Windows.Input;
+using Android.Content;
+using Android.Runtime;
+using Android.Support.V7.Widget;
+using Android.Util;
+using Cirrious.MvvmCross.Binding.Attributes;
+using Cirrious.MvvmCross.Binding.Droid.Views;
+
+namespace Cirrious.MvvmCross.Droid.Support.AppCompat.Widget
+{
+    /// <summary>
+    /// Tint-aware version of MvxSpinner styled properly with AppCompat V22.2+.
+    /// TODO: We may want to figure out a way to delegate to a common class for both.
+    /// </summary>
+    [Register("cirrious.mvvmcross.droid.support.appcompat.widget.MvxAppCompatSpinner")]
+    public class MvxAppCompatSpinner : AppCompatSpinner
+    {
+        public MvxAppCompatSpinner(Context context, IAttributeSet attrs)
+            : this(
+                context, attrs,
+                new MvxAdapter(context)
+                {
+                    SimpleViewLayoutId = global::Android.Resource.Layout.SimpleDropDownItem1Line
+                }) {}
+
+        public MvxAppCompatSpinner(Context context, IAttributeSet attrs, IMvxAdapter adapter)
+            : base(context, attrs)
+        {
+            var itemTemplateId = MvxAttributeHelpers.ReadListItemTemplateId(context, attrs);
+            var dropDownItemTemplateId = MvxAttributeHelpers.ReadDropDownListItemTemplateId(context, attrs);
+            adapter.ItemTemplateId = itemTemplateId;
+            adapter.DropDownItemTemplateId = dropDownItemTemplateId;
+            Adapter = adapter;
+            SetupHandleItemSelected();
+        }
+
+        protected MvxAppCompatSpinner(IntPtr javaReference, JniHandleOwnership transfer)
+            : base(javaReference, transfer) {}
+
+        public new IMvxAdapter Adapter
+        {
+            get { return base.Adapter as IMvxAdapter; }
+            set
+            {
+                var existing = Adapter;
+                if (existing == value)
+                    return;
+
+                if (existing != null && value != null)
+                {
+                    value.ItemsSource = existing.ItemsSource;
+                    value.ItemTemplateId = existing.ItemTemplateId;
+                }
+
+                base.Adapter = value;
+            }
+        }
+
+        [MvxSetToNullAfterBinding]
+        public IEnumerable ItemsSource
+        {
+            get { return Adapter.ItemsSource; }
+            set { Adapter.ItemsSource = value; }
+        }
+
+        public int ItemTemplateId
+        {
+            get { return Adapter.ItemTemplateId; }
+            set { Adapter.ItemTemplateId = value; }
+        }
+
+        public int DropDownItemTemplateId
+        {
+            get { return Adapter.DropDownItemTemplateId; }
+            set { Adapter.DropDownItemTemplateId = value; }
+        }
+
+        public ICommand HandleItemSelected { get; set; }
+
+        private void SetupHandleItemSelected()
+        {
+            base.ItemSelected += (sender, args) =>
+            {
+                var position = args.Position;
+                HandleSelected(position);
+            };
+        }
+
+        protected virtual void HandleSelected(int position)
+        {
+            var item = Adapter.GetRawItem(position);
+            if (this.HandleItemSelected == null
+                || item == null
+                || !this.HandleItemSelected.CanExecute(item))
+                return;
+
+            this.HandleItemSelected.Execute(item);
+        }
+    }
+}
