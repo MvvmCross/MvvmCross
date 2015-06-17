ae1b052b8faaf45631cd0adbeb325bfaa47b116f
Tomasz Cielecki <tomasz@ostebaronen.dk>
2015-06-17 23:19:38 +0200

    Symbols are pushed when pushing nupkgs :)

--- nuspec/pack.bat
+++ nuspec/pack.bat
@@ -14,9 +14,4 @@ nuget push Cirrious.MvvmCross.Droid.Support.AppCompat.1.0.0-alpha1.nupkg
 nuget push Cirrious.MvvmCross.Droid.Support.Fragging.1.0.0-alpha1.nupkg
 nuget push Cirrious.MvvmCross.Droid.Support.V4.1.0.0-alpha1.nupkg
 nuget push Cirrious.MvvmCross.Droid.Support.RecyclerView.1.0.0-alpha1.nupkg
-
-nuget push Cirrious.MvvmCross.Droid.Support.AppCompat.1.0.0-alpha1.symbols.nupkg
-nuget push Cirrious.MvvmCross.Droid.Support.Fragging.1.0.0-alpha1.symbols.nupkg
-nuget push Cirrious.MvvmCross.Droid.Support.V4.1.0.0-alpha1.symbols.nupkg
-nuget push Cirrious.MvvmCross.Droid.Support.RecyclerView.1.0.0-alpha1.symbols.nupkg
 pause
