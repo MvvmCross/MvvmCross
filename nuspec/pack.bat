87bd20a9d16e9e763a89e761e4854d1cfbd43757
Tomasz Cielecki <tomasz@ostebaronen.dk>
2015-06-18 19:25:12 +0200

    Whoa for loop?!

--- nuspec/pack.bat
+++ nuspec/pack.bat
@@ -1,13 +1,10 @@
 del *.nupkg
-nuget setapikey ---
+nuget setapikey 
 
 nuget pack Cirrious.MvvmCross.Droid.Support.AppCompat.nuspec -symbols
 nuget pack Cirrious.MvvmCross.Droid.Support.Fragging.nuspec -Symbols
 nuget pack Cirrious.MvvmCross.Droid.Support.V4.nuspec -Symbols
 nuget pack Cirrious.MvvmCross.Droid.Support.RecyclerView.nuspec -Symbols
 
-nuget push Cirrious.MvvmCross.Droid.Support.AppCompat.1.0.0-alpha1.nupkg
-nuget push Cirrious.MvvmCross.Droid.Support.Fragging.1.0.0-alpha1.nupkg
-nuget push Cirrious.MvvmCross.Droid.Support.V4.1.0.0-alpha1.nupkg
-nuget push Cirrious.MvvmCross.Droid.Support.RecyclerView.1.0.0-alpha1.nupkg
+for /r %%i in (*.nupkg) do nuget push %%i
 pause
