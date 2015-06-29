df5fe2e0eed4cdbabbe14ec216c7e63079933cd1
Erik van Seters <github@dexyon.com>
2015-06-29 07:57:32 +0200

    Make use of the AppCompat Theme

--- Samples/Example.Droid/Views/ListView.cs
+++ Samples/Example.Droid/Views/ListView.cs
@@ -1,11 +1,11 @@
 using Android.App;
 using Android.OS;
-using Cirrious.MvvmCross.Droid.Views;
+using Cirrious.MvvmCross.Droid.Support.AppCompat;
 
 namespace Example.Droid.Views
 {
     [Activity(Label = "View for ListViewModel")]
-    public class ListView : MvxActivity
+    public class ListView : MvxActivityCompat
     {
         protected override void OnCreate(Bundle bundle)
         {
--- Samples/Example.Droid/Views/StartView.cs
+++ Samples/Example.Droid/Views/StartView.cs
@@ -1,11 +1,11 @@
 using Android.App;
 using Android.OS;
-using Cirrious.MvvmCross.Droid.Views;
+using Cirrious.MvvmCross.Droid.Support.AppCompat;
 
 namespace Example.Droid.Views
 {
 	[Activity(Label = "View for StartViewModel")]
-	public class StartView : MvxActivity
+    public class StartView : MvxActivityCompat
     {
         protected override void OnCreate(Bundle bundle)
         {
