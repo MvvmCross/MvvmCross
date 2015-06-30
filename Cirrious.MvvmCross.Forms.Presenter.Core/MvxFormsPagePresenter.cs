397896c4cb66ac57b776a11cf710010070a04bb7
Martijn van Dijk <mhvdijk@gmail.com>
2015-06-30 10:00:18 +0200

    MvvmCross for Xamarin Forms

---
+++ Cirrious.MvvmCross.Forms.Presenter.Core/MvxFormsPagePresenter.cs
@@ -0,0 +1,110 @@
+﻿// MvxFormsPagePresenter.cs
+// 2015 (c) Copyright Cheesebaron. http://ostebaronen.dk
+// Cirrious.MvvmCross.Forms.Presenter is licensed using Microsoft Public License (Ms-PL)
+// Contributions and inspirations noted in readme.md and license.txt
+// 
+// Project Lead - Tomasz Cielecki, @cheesebaron, mvxplugins@ostebaronen.dk
+// Contributor - Marcos Cobeña Morián, @CobenaMarcos, marcoscm@me.com
+﻿
+﻿using Cirrious.CrossCore;
+using Cirrious.MvvmCross.ViewModels;
+using Cirrious.MvvmCross.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Cirrious.MvvmCross.Forms.Presenter.Core
+{
+    public class MvxFormsPagePresenter
+        : IMvxViewPresenter
+    {
+        protected Application _mvxFormsApp;
+
+        public Application MvxFormsApp
+        {
+            get { return _mvxFormsApp; }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentException("MvxFormsApp cannot be null");
+                }
+
+                _mvxFormsApp = value;
+            }
+        }
+
+        public MvxFormsPagePresenter()
+        { }
+
+        public MvxFormsPagePresenter(Application mvxFormsApp)
+        {
+            _mvxFormsApp = mvxFormsApp;
+        }
+
+        public virtual async void ChangePresentation(MvxPresentationHint hint)
+        {
+            if (hint is MvxClosePresentationHint)
+            {
+                var mainPage = _mvxFormsApp.MainPage as NavigationPage;
+
+                if (mainPage == null)
+                {
+                    Mvx.TaggedTrace("MvxFormsPresenter:ChangePresentation()", "Shit, son! Don't know what to do");
+                }
+                else
+                {
+                    // TODO - perhaps we should do more here... also async void is a boo boo
+                    await mainPage.PopAsync();
+                }
+            }
+        }
+
+        public virtual async void Show(MvxViewModelRequest request)
+        {
+            if (await TryShowPage(request))
+                return;
+
+            Mvx.Error("Skipping request for {0}", request.ViewModelType.Name);
+        }
+
+        protected virtual void CustomPlatformInitialization(NavigationPage mainPage)
+        {
+        }
+
+        private async Task<bool> TryShowPage(MvxViewModelRequest request)
+        {
+            var page = MvxPresenterHelpers.CreatePage(request);
+            if (page == null)
+                return false;
+
+            var viewModel = MvxPresenterHelpers.LoadViewModel(request);
+
+            var mainPage = _mvxFormsApp.MainPage as NavigationPage;
+            page.BindingContext = viewModel;
+
+            if (mainPage == null)
+            {
+                _mvxFormsApp.MainPage = new NavigationPage(page);
+                mainPage = _mvxFormsApp.MainPage as NavigationPage;
+                CustomPlatformInitialization(mainPage);
+            }
+            else
+            {
+                try
+                {
+                    await mainPage.PushAsync(page);
+                }
+                catch (Exception e)
+                {
+                    Mvx.Error("Exception pushing {0}: {1}\n{2}", page.GetType(), e.Message, e.StackTrace);
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Cirrious.MvvmCross.Forms.Presenter.Droid/MvxFormsDroidPagePresenter.cs
@@ -0,0 +1,32 @@
+// MvxFormsDroidPagePresenter.cs
+// 2015 (c) Copyright Cheesebaron. http://ostebaronen.dk
+// Cirrious.MvvmCross.Forms.Presenter is licensed using Microsoft Public License (Ms-PL)
+// Contributions and inspirations noted in readme.md and license.txt
+// 
+// Project Lead - Tomasz Cielecki, @cheesebaron, mvxplugins@ostebaronen.dk
+// Contributor - Marcos Cobeña Morián, @CobenaMarcos, marcoscm@me.com
+
+using System.Threading.Tasks;
+using Cirrious.CrossCore;
+using Cirrious.MvvmCross.Droid.Views;
+using Cirrious.MvvmCross.Forms.Presenter.Core;
+using Cirrious.MvvmCross.ViewModels;
+using Cirrious.MvvmCross.Views;
+using Xamarin.Forms;
+
+namespace Cheesebaron.MvxPlugins.FormsPresenters.Droid
+{
+    public class MvxFormsDroidPagePresenter
+        : MvxFormsPagePresenter
+        , IMvxAndroidViewPresenter
+    {
+        public MvxFormsDroidPagePresenter()
+        {
+        }
+
+        public MvxFormsDroidPagePresenter(MvxFormsApp mvxFormsApp)
+            : base(mvxFormsApp)
+        {
+        }
+    }
+}
