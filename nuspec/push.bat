3c9b22f09a9ed91f43d647bf31f1fca150bbb4bc
Tomasz Cielecki <tomasz@ostebaronen.dk>
2015-06-30 17:44:47 +0200

    Updated nuspecs

--- nuspec/push.bat
+++ nuspec/push.bat
@@ -1,4 +1,10 @@
-nuget setapikey ---
+nuget setapikey
 
-for /r %%i in (*1.nupkg) do nuget push %%i
+for /r %%i in (*.nupkg) do (call :pushpackage "%%i")
 pause
+
+:pushpackage
+  set np=%1
+  if "%np%"=="%np:symbols=%" (
+	nuget push %np% 
+  )
