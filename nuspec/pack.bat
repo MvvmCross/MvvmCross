f8fdf75271410c3cddb9bda0e525f4be3c9de834
Tomasz Cielecki <tomasz@ostebaronen.dk>
2015-06-30 10:26:30 +0200

    Fix nuspec name in pack script

--- nuspec/pack.bat
+++ nuspec/pack.bat
@@ -1,12 +1,7 @@
 del *.nupkg
 nuget setapikey ---
 
-nuget pack Cheesebaron.MvxPlugins.AppId.nuspec -symbols
-nuget pack Cheesebaron.MvxPlugins.Sms.nuspec -symbols
-nuget pack Cheesebaron.MvxPlugins.Settings.nuspec -symbols
-nuget pack Cheesebaron.MvxPlugins.SimpleWebToken.nuspec -symbols
-nuget pack Cheesebaron.MvxPlugins.FormsPresenters.nuspec -symbols
-nuget pack Cheesebaron.MvxPlugins.Notifications.nuspec -symbols
+nuget pack Cirrious.MvvmCross.Forms.Presenter.nuspec -symbols
 
 for /r %%i in (*.nupkg) do (call :pushpackage "%%i")
 pause
