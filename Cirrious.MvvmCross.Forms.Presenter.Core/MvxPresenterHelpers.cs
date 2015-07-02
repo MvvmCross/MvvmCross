0e1b6c051ada9b2a5ca01168850eb25816f34a3b
stefandevo <stefan.de.vogelaere@gmail.com>
2015-07-02 08:18:04 +0200

    Allow to find Xamarin.Forms pages in different assembly

--- Cirrious.MvvmCross.Forms.Presenter.Core/MvxPresenterHelpers.cs
+++ Cirrious.MvvmCross.Forms.Presenter.Core/MvxPresenterHelpers.cs
@@ -6,43 +6,33 @@
 // Project Lead - Tomasz Cielecki, @cheesebaron, mvxplugins@ostebaronen.dk
 // Contributor - Marcos Cobeña Morián, @CobenaMarcos, marcoscm@me.com
 ﻿
-﻿using System;
-using System.Linq;
-using System.Reflection;
-using Cirrious.CrossCore;
-using Cirrious.CrossCore.IoC;
+﻿using Cirrious.CrossCore;
 using Cirrious.MvvmCross.ViewModels;
 using Xamarin.Forms;
 
 namespace Cirrious.MvvmCross.Forms.Presenter.Core
 {
-    public static class MvxPresenterHelpers
-    {
-        public static IMvxViewModel LoadViewModel(MvxViewModelRequest request)
-        {
-            var viewModelLoader = Mvx.Resolve<IMvxViewModelLoader>();
-            var viewModel = viewModelLoader.LoadViewModel(request, null);
-            return viewModel;
-        }
+	public static class MvxPresenterHelpers
+	{
+		public static IMvxViewModel LoadViewModel(MvxViewModelRequest request)
+		{
+			var viewModelLoader = Mvx.Resolve<IMvxViewModelLoader>();
+			var viewModel = viewModelLoader.LoadViewModel(request, null);
+			return viewModel;
+		}
 
-        public static Page CreatePage(MvxViewModelRequest request)
-        {
-            var viewModelName = request.ViewModelType.Name;
-            var pageName = viewModelName.Replace("ViewModel", "Page");
-            var pageType = request.ViewModelType.GetTypeInfo().Assembly.CreatableTypes()
-                                  .FirstOrDefault(t => t.Name == pageName);
-            if (pageType == null)
-            {
-                Mvx.Trace("Page not found for {0}", pageName);
-                return null;
-            }
-
-            var page = Activator.CreateInstance(pageType) as Page;
-            if (page == null)
-            {
-                Mvx.Error("Failed to create ContentPage {0}", pageName);
-            }
-            return page;
-        }
-    }
+		public static Page CreatePage(MvxViewModelRequest request)
+		{
+			IMvxFormsPageLoader viewPageLoader;
+			Mvx.TryResolve(out viewPageLoader);
+			if (viewPageLoader == null)
+			{
+				// load default instead
+				viewPageLoader = new MvxFormsPageLoader();
+				Mvx.RegisterSingleton(viewPageLoader);
+			}
+			var page = viewPageLoader.LoadPage(request);
+			return page;
+		}
+	}
 }
