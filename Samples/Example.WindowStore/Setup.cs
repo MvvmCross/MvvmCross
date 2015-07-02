f61f43fcb1a6523b6ef4a7ed788f01fa2cfe784a
Martijn van Dijk <mhvdijk@gmail.com>
2015-07-02 11:54:05 +0200

    Added WindowsStore

---
+++ Samples/Example.WindowStore/Setup.cs
@@ -0,0 +1,40 @@
+using Cirrious.CrossCore;
+using Cirrious.CrossCore.Platform;
+using Cirrious.MvvmCross.Forms.Presenter.Core;
+using Cirrious.MvvmCross.ViewModels;
+using Cirrious.MvvmCross.Views;
+using Cirrious.MvvmCross.WindowsCommon.Platform;
+using Cirrious.MvvmCross.WindowsCommon.Views;
+using Windows.UI.Xaml.Controls;
+using Xamarin.Forms;
+
+namespace Example.WindowStore
+{
+    public class Setup : MvxWindowsSetup
+    {
+        public Setup(Frame rootFrame) : base(rootFrame)
+        {
+        }
+
+        protected override IMvxApplication CreateApp()
+        {
+            return new Example.App();
+        }
+		
+        protected override IMvxTrace CreateDebugTrace()
+        {
+            return new DebugTrace();
+        }
+
+        protected override IMvxWindowsViewPresenter CreateViewPresenter(IMvxWindowsFrame rootFrame)
+        {
+            Forms.Init(null);
+
+            var xamarinFormsApp = new MvxFormsApp();
+            var presenter = new MvxFormsWindowsStorePagePresenter(rootFrame, xamarinFormsApp);
+            Mvx.RegisterSingleton<IMvxViewPresenter>(presenter);
+
+            return presenter;
+        }
+    }
+}
