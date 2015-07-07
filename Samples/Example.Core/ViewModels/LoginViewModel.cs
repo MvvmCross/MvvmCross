e06e0373395744cea8f5bdc77f6b59ce08c4f612
Erik van Seters <github@dexyon.com>
2015-07-07 07:12:42 +0200

    Added a simple LoginViewModel icm with a MvxActivityCompat to the examples.

---
+++ Samples/Example.Core/ViewModels/LoginViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows.Input;
+using Cirrious.MvvmCross.ViewModels;
+
+namespace Example.Core.ViewModels
+{
+    public class LoginViewModel : MvxViewModel
+    {
+        public LoginViewModel () {
+            this.Username = "TestUser";
+            this.Password = "YouCantSeeMe";
+        }
+
+        private string _username;
+        public string Username
+        {
+            get { return _username; }
+            set
+            {
+                _username = value;
+                RaisePropertyChanged(() => Username);
+            }
+        }
+
+        private string _password;
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+                RaisePropertyChanged(() => Password);
+            }
+        }
+
+        public virtual ICommand Login
+        {
+            get
+            {
+                return new MvxCommand( () => ShowViewModel<MainViewModel>() );
+            }
+        }
+    }
+}
+
---
+++ Samples/Example.Droid/Activities/LoginActivity.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Content.PM;
+using Cirrious.MvvmCross.Droid.Support.AppCompat;
+
+namespace Example.Droid.Activities
+{
+    [Activity(
+        Label = "Examples",
+        Theme = "@style/AppTheme",
+        LaunchMode = LaunchMode.SingleTop,
+        ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize,
+        Name = "example.droid.activities.LoginActivity"
+    )]			
+    public class LoginActivity : MvxActivityCompat
+    {
+        protected override void OnCreate (Bundle bundle)
+        {
+            base.OnCreate (bundle);
+            SetContentView ( Resource.Layout.Login );
+        }
+    }
+}
+
