c381a4554db32eca1876b8def89794f31228e7f5
Tomasz Cielecki <tomasz@ostebaronen.dk>
2015-07-15 11:23:06 +0200

    Update pack script

--- nuspec/pack.ps1
+++ nuspec/pack.ps1
@@ -9,42 +9,43 @@ if (-Not(Test-Path ("nuget.exe")))
 Set-Alias nuget $targetNugetExe -Scope Global -Verbose
 
 del *.nupkg
-nuget pack MvvmCross.HotTuna.CrossCore.3.0.1.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.Accelerometer.3.0.1.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.All.3.0.1.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.Bookmarks.3.0.1.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.Color.3.0.1.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.DownloadCache.3.0.1.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.Email.3.0.1.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.FieldBinding.3.0.1.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.File.3.0.1.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.Json.3.0.1.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.JsonLocalisation.3.0.1.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.Location.3.0.1.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.Messenger.3.0.1.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.MethodBinding.3.0.1.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.Network.3.0.1.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.PhoneCall.3.0.1.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.PictureChooser.3.0.1.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.ReflectionEx.3.0.1.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.ResourceLoader.3.0.1.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.Share.3.0.1.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.SoundEffects.3.0.1.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.SQLite.3.0.1.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.ThreadUtils.3.0.1.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.Visibility.3.0.1.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.WebBrowser.3.0.1.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.MvvmCrossLibraries.3.0.1.nuspec -Symbols
+nuget pack MvvmCross.HotTuna.Binding.nuspec -Symbols
+nuget pack MvvmCross.HotTuna.CrossCore.nuspec -Symbols
+nuget pack MvvmCross.HotTuna.MvvmCrossLibraries.nuspec -Symbols
+nuget pack MvvmCross.HotTuna.Plugin.Accelerometer.nuspec -Symbols
+nuget pack MvvmCross.HotTuna.Plugin.All.nuspec -Symbols
+nuget pack MvvmCross.HotTuna.Plugin.Bookmarks.nuspec -Symbols
+nuget pack MvvmCross.HotTuna.Plugin.Color.nuspec -Symbols
+nuget pack MvvmCross.HotTuna.Plugin.DownloadCache.nuspec -Symbols
+nuget pack MvvmCross.HotTuna.Plugin.Email.nuspec -Symbols
+nuget pack MvvmCross.HotTuna.Plugin.FieldBinding.nuspec -Symbols
+nuget pack MvvmCross.HotTuna.Plugin.File.nuspec -Symbols
+nuget pack MvvmCross.HotTuna.Plugin.Json.nuspec -Symbols
+nuget pack MvvmCross.HotTuna.Plugin.JsonLocalisation.nuspec -Symbols
+nuget pack MvvmCross.HotTuna.Plugin.Location.nuspec -Symbols
+nuget pack MvvmCross.HotTuna.Plugin.Messenger.nuspec -Symbols
+nuget pack MvvmCross.HotTuna.Plugin.MethodBinding.nuspec -Symbols
+nuget pack MvvmCross.HotTuna.Plugin.Network.nuspec -Symbols
+nuget pack MvvmCross.HotTuna.Plugin.PhoneCall.nuspec -Symbols
+nuget pack MvvmCross.HotTuna.Plugin.PictureChooser.nuspec -Symbols
+nuget pack MvvmCross.HotTuna.Plugin.ReflectionEx.nuspec -Symbols
+nuget pack MvvmCross.HotTuna.Plugin.ResourceLoader.nuspec -Symbols
+nuget pack MvvmCross.HotTuna.Plugin.Share.nuspec -Symbols
+nuget pack MvvmCross.HotTuna.Plugin.SoundEffects.nuspec -Symbols
+nuget pack MvvmCross.HotTuna.Plugin.SQLite.nuspec -Symbols
+nuget pack MvvmCross.HotTuna.Plugin.ThreadUtils.nuspec -Symbols
+nuget pack MvvmCross.HotTuna.Plugin.Visibility.nuspec -Symbols
+nuget pack MvvmCross.HotTuna.Plugin.WebBrowser.nuspec -Symbols
 # note no -Symbols
-nuget pack MvvmCross.HotTuna.StarterPack.3.0.1.nuspec
-nuget pack MvvmCross.HotTuna.Tests.3.0.1.nuspec -Symbols
+nuget pack MvvmCross.HotTuna.StarterPack.nuspec
+nuget pack MvvmCross.HotTuna.Tests.nuspec -Symbols
 # note no -Symbols
-nuget pack MvvmCross.3.0.1.nuspec
-nuget pack MvvmCross.HotTuna.Touch.Dialog.3.0.1.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Droid.Dialog.3.0.1.nuspec -Symbols
-nuget pack MvvmCross.BindingEx.3.0.1.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.AutoViews.3.0.1.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Touch.AutoViews.3.0.1.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Droid.AutoViews.3.0.1.nuspec -Symbols
+nuget pack MvvmCross.nuspec
+nuget pack MvvmCross.HotTuna.Touch.Dialog.nuspec -Symbols
+nuget pack MvvmCross.HotTuna.Droid.Dialog.nuspec -Symbols
+nuget pack MvvmCross.BindingEx.nuspec -Symbols
+nuget pack MvvmCross.HotTuna.AutoViews.nuspec -Symbols
+nuget pack MvvmCross.HotTuna.Touch.AutoViews.nuspec -Symbols
+nuget pack MvvmCross.HotTuna.Droid.AutoViews.nuspec -Symbols
 
 pause
