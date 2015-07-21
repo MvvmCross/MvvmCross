3947e932c58c259f3af7ddb154909bc42045c796
Peter Burke <peter@burke.ws>
2015-07-21 17:14:16 +1000

    Restructured Samples Directory, one SLN per Sample, Added First example done as a XAML

---
+++ Samples/Example001XAML/Example/ViewModels/FirstViewModel.cs
@@ -0,0 +1,30 @@
+using Cirrious.MvvmCross.ViewModels;
+using System.Windows.Input;
+
+namespace Example.ViewModels
+{
+    public class FirstViewModel 
+		: MvxViewModel
+    {
+		private string _yourNickname = string.Empty;
+        public string YourNickname
+		{ 
+			get { return _yourNickname; }
+			set { _yourNickname = value; RaisePropertyChanged(() => YourNickname); RaisePropertyChanged(() => Hello); }
+		}
+
+        public string Hello
+        {
+            get { return "Hello " + YourNickname; }
+        }
+
+
+        public ICommand ShowAboutPageCommand
+        {
+            get
+            {
+                return new MvxCommand(() => ShowViewModel<AboutViewModel>());
+            }
+        }
+    }
+}
--- nuspec/dev_pack.bat
+++ nuspec/dev_pack.bat
@@ -3,11 +3,9 @@ del *.nupkg
 
 nuget pack Cirrious.MvvmCross.Forms.Presenter.nuspec -symbols
 
-for /r %%i in (*.nupkg) do (call :pushpackage "%%i")
+for /r %%i in (*.symbols.nupkg) do (call :pushpackage "%%i")
 pause
 
 :pushpackage
   set np=%1
-  if "%np%"=="%np:symbols=%" (
-	nuget push -source c:\dev_nupkg\ %np%
-  )
+  nuget push -source c:\dev_nupkg\ %np%
