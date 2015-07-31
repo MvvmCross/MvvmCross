d4350c9223286e2c01af53acb9338d05c6afa5b2
Martijn van Dijk <mhvdijk@gmail.com>
2015-07-31 13:50:07 +0200

    Add some documentation

--- Cirrious.MvvmCross.Droid.Support.Fragging/MvxCachingFragmentActivity.cs
+++ Cirrious.MvvmCross.Droid.Support.Fragging/MvxCachingFragmentActivity.cs
@@ -217,6 +217,7 @@ namespace Cirrious.MvvmCross.Droid.Support.Fragging
         /// <param name="contentId">Where you want to show the Fragment</param>
         /// <param name="bundle">Bundle which usually contains a Serialized MvxViewModelRequest</param>
         /// <param name="addToBackStack">If you want to add the fragment to the backstack so on backbutton it will go back to it</param>
+        /// <param name="forceReplaceFragment">Force replace a fragment with the same viewmodel at the same contentid</param>
         protected void ShowFragment(string tag, int contentId, Bundle bundle = null, bool addToBackStack = false, bool forceReplaceFragment = false)
         {
             FragmentInfo fragInfo;
