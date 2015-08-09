ba5787b41cce2b4280d0544e67bc229e925f8765
Martijn van Dijk <mhvdijk@gmail.com>
2015-08-09 19:02:44 +0200

    Added missing licenses

--- Cirrious.MvvmCross.Droid.Support.V4/MvxFragmentStatePagerAdapter.cs
+++ Cirrious.MvvmCross.Droid.Support.V4/MvxFragmentStatePagerAdapter.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿// MvxFragmentStatePagerAdapter.cs
+// (c) Copyright Cirrious Ltd. http://www.cirrious.com
+// MvvmCross is licensed using Microsoft Public License (Ms-PL)
+// Contributions and inspirations noted in readme.md and license.txt
+// 
+// Project Lead - Stuart Lodge, @slodge, me@slodge.com
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Android.Content;
