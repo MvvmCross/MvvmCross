a73fd81b34f50ce80b3e29a1eb2f377596ec2436
Tomasz Cielecki <tomasz@ostebaronen.dk>
2015-08-17 15:09:31 +0200

    Initial UWP shiz niz

---
+++ Cirrious/Cirrious.MvvmCross.WindowsUWP/Platform/MvxDebugTrace.cs
@@ -0,0 +1,31 @@
+// MvxDebugTrace.cs
+// (c) Copyright Cirrious Ltd. http://www.cirrious.com
+// MvvmCross is licensed using Microsoft Public License (Ms-PL)
+// Contributions and inspirations noted in readme.md and license.txt
+// 
+// Project Lead - Stuart Lodge, @slodge, me@slodge.com
+
+using System;
+using System.Diagnostics;
+using Cirrious.CrossCore.Platform;
+
+namespace Cirrious.MvvmCross.WindowsUWP.Platform
+{
+    public class MvxDebugTrace : IMvxTrace
+    {
+        public void Trace(MvxTraceLevel level, string tag, Func<string> message)
+        {
+            Debug.WriteLine(tag + ":" + level + ":" + message());
+        }
+
+        public void Trace(MvxTraceLevel level, string tag, string message)
+        {
+            Debug.WriteLine(tag + ": " + level + ": " + message);
+        }
+
+        public void Trace(MvxTraceLevel level, string tag, string message, params object[] args)
+        {
+            Debug.WriteLine(tag + ": " + level + ": " + message, args);
+        }
+    }
+}
---
+++ Cirrious/Cirrious.MvvmCross.WindowsUWP/Views/MvxWindowsPage.cs
@@ -0,0 +1,130 @@
+﻿// MvxWinRTPage.cs
+// (c) Copyright Cirrious Ltd. http://www.cirrious.com
+// MvvmCross is licensed using Microsoft Public License (Ms-PL)
+// Contributions and inspirations noted in readme.md and license.txt
+// 
+// Project Lead - Stuart Lodge, @slodge, me@slodge.com
+
+using System;
+using System.Collections.Generic;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using Cirrious.CrossCore;
+using Cirrious.MvvmCross.ViewModels;
+using Cirrious.MvvmCross.Views;
+using Cirrious.MvvmCross.WindowsUWP.Views.Suspension;
+
+namespace Cirrious.MvvmCross.WindowsUWP.Views
+{
+    public class MvxWindowsPage
+        : Page
+        , IMvxWindowsView
+    {
+        private IMvxViewModel _viewModel;
+
+        public IMvxWindowsFrame WrappedFrame => new MvxWrappedFrame(Frame);
+
+        public IMvxViewModel ViewModel
+        {
+            get { return _viewModel; }
+            set
+            {
+                if (_viewModel == value)
+                    return;
+
+                _viewModel = value;
+                DataContext = ViewModel;
+            }
+        }
+
+        public void ClearBackStack()
+        {
+            throw new NotImplementedException();
+            /*
+            // note - we do *not* use CanGoBack here - as that seems to always returns true!
+            while (NavigationService.BackStack.Any())
+                NavigationService.RemoveBackEntry();
+         */
+        }
+
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            var reqData = (string)e.Parameter;
+            var converter = Mvx.Resolve<IMvxNavigationSerializer>();
+            var req = converter.Serializer.DeserializeObject<MvxViewModelRequest>(reqData);
+
+            this.OnViewCreate(req, () => LoadStateBundle(e));
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+
+            var bundle = this.CreateSaveStateBundle();
+            SaveStateBundle(e, bundle);
+            
+            if (e.NavigationMode == NavigationMode.Back)
+                this.OnViewDestroy();
+        }
+
+        private String _pageKey;
+
+        private IMvxSuspensionManager _suspensionManager;
+        protected IMvxSuspensionManager SuspensionManager
+        {
+            get
+            {
+                _suspensionManager = _suspensionManager ?? Mvx.Resolve<IMvxSuspensionManager>();
+                return _suspensionManager;
+            }
+        }
+
+        protected virtual IMvxBundle LoadStateBundle(NavigationEventArgs e)
+        {
+            // nothing loaded by default
+            var frameState = SuspensionManager.SessionStateForFrame(WrappedFrame);
+            _pageKey = "Page-" + Frame.BackStackDepth;
+             IMvxBundle bundle = null;
+
+            if (e.NavigationMode == NavigationMode.New)
+            {
+                // Clear existing state for forward navigation when adding a new page to the
+                // navigation stack
+                var nextPageKey = _pageKey;
+                var nextPageIndex = Frame.BackStackDepth;
+                while (frameState.Remove(nextPageKey))
+                {
+                    nextPageIndex++;
+                    nextPageKey = "Page-" + nextPageIndex;
+                }
+            }
+            else
+            {
+                var dictionary = (IDictionary<string, string>)frameState[_pageKey];
+                bundle = new MvxBundle(dictionary);
+            }
+
+            return bundle;
+        }
+
+        protected virtual void SaveStateBundle(NavigationEventArgs navigationEventArgs, IMvxBundle bundle)
+        {
+            var frameState = SuspensionManager.SessionStateForFrame(WrappedFrame);
+            frameState[_pageKey] = bundle.Data;
+        }
+    }
+
+    public class MvxWindowsPage<TViewModel>
+        : MvxWindowsPage
+        , IMvxWindowsView<TViewModel> where TViewModel : class, IMvxViewModel
+    {
+        public new TViewModel ViewModel
+        {
+            get { return (TViewModel)base.ViewModel; }
+            set { base.ViewModel = value; }
+        }
+    }
+}
