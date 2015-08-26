4d03600a9e74ad15675887574730723cb5b5c95f
Stephan van Stekelenburg <stephan.van.stekelenburg@gmail.com>
2015-08-26 20:58:17 +0200

    Add initial roslyn analyzer
    First analyzer which looks for classes that ultimately derive from
    `IMvxView` and declare a property with a name of `ViewModel` whose
    return type is either derived from `INotifyPropertyChanged` or
    `IMvxViewModel` and informs users to use the generic variant of that
    class.

---
+++ CodeAnalysis/Analyzers/ConsiderUsingGenericBaseViewAnalyzer.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Linq;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using MvvmCross.CodeAnalysis.Core;
+using MvvmCross.CodeAnalysis.Extensions;
+
+namespace MvvmCross.CodeAnalysis.Analyzers
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class ConsiderUsingGenericBaseViewAnalyzer : DiagnosticAnalyzer
+    {
+        public const string ViewModelPropertyName = "ViewModel";
+        internal static readonly LocalizableString Title = "Consider deriving from generic base class with strongly typed ViewModel property";
+        internal static readonly LocalizableString MessageFormat = "Derive from '{0}<T>', which already implements a strongly typed ViewModel property";
+        internal const string Category = Categories.Usage;
+
+        internal static DiagnosticDescriptor Rule = 
+            new DiagnosticDescriptor(DiagnosticIds.UseGenericBaseClassRuleId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, true);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics 
+            => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
+        }
+
+        private static void AnalyzeSymbol(SymbolAnalysisContext context)
+        {
+            var namedSymbol = context.Symbol as INamedTypeSymbol;
+            var iMvxViewType = context.Compilation.GetTypeByMetadataName(typeof(Cirrious.MvvmCross.Views.IMvxView).FullName);
+
+            if (namedSymbol != null &&
+                namedSymbol.ImplementsSymbol(iMvxViewType) &&
+                namedSymbol.GetMembers(ViewModelPropertyName).Any())
+            {
+                var viewModelProperty = namedSymbol.GetMembers(ViewModelPropertyName).FirstOrDefault() as IPropertySymbol;
+                var viewModelReturnType = viewModelProperty?.Type;
+
+                if (!IsViewModelType(context, viewModelReturnType))
+                    return;
+
+                var syntax = namedSymbol.DeclaringSyntaxReferences.First().GetSyntax() as ClassDeclarationSyntax;
+
+                var baseType = syntax?.BaseList.Types.First();
+
+                context.ReportDiagnostic(
+                    Diagnostic.Create(
+                        Rule
+                        , baseType?.GetLocation()
+                        , baseType?.ToString())
+                );
+            }
+        }
+
+        private static bool IsViewModelType(SymbolAnalysisContext context, ITypeSymbol symbol)
+        {
+            var iMvxViewModelType = context.Compilation.GetTypeByMetadataName(typeof(Cirrious.MvvmCross.ViewModels.IMvxViewModel).FullName);
+            var iNotifyPropertyChangedType = context.Compilation.GetTypeByMetadataName(typeof(System.ComponentModel.INotifyPropertyChanged).FullName);
+
+            return symbol.ImplementsSymbol(iMvxViewModelType) ||
+                   symbol.ImplementsSymbol(iNotifyPropertyChangedType);
+        }
+    }
+}
