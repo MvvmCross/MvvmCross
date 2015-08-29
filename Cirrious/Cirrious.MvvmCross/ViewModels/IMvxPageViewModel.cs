c7827fde7cfd555a786a80223f57c2e10e9474c1
Shawn Castrianni <shawncastrianni@Shawns-MacBook-Pro.local>
2015-08-28 23:55:46 -0500

    Add initial support for a MvxPageViewController that dynamically creates UIViewController pages via additional child ViewModels.

---
+++ Cirrious/Cirrious.MvvmCross/ViewModels/IMvxPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Cirrious.MvvmCross.ViewModels;
+
+namespace Cirrious.MvvmCross
+{
+	public interface IMvxPagedViewModel : IMvxViewModel
+	{
+		string PagedViewId { get; }
+	}
+
+	public interface IMvxPageViewModel : IMvxViewModel
+	{
+		IMvxPagedViewModel GetDefaultViewModel ();
+		IMvxPagedViewModel GetNextViewModel(IMvxPagedViewModel currentViewModel);
+		IMvxPagedViewModel GetPreviousViewModel(IMvxPagedViewModel currentViewModel);
+	}
+}
---
+++ Samples/MvxPageDemo.Touch/Views/ThirdPageView.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Cirrious.MvvmCross.Touch.Views;
+using MvxPageDemo.ViewModels;
+using UIKit;
+using Cirrious.MvvmCross.Binding.BindingContext;
+
+namespace MvxPageDemo.Touch.Views
+{
+	public class ThirdPageView : MvxViewController<ThirdPageViewModel>
+	{
+		private UILabel _pageL = null;
+
+		public ThirdPageView()
+		{
+			Console.WriteLine ("Constructing ThirdPageView");
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad ();
+			CreateUI ();
+			var set = this.CreateBindingSet<ThirdPageView, ThirdPageViewModel>();
+			set.Bind (this).For(s => s.Title).To (vm => vm.PageTitle);
+			set.Bind (_pageL).To (vm => vm.PageTitle);
+			set.Apply();
+		}
+
+		public override void ViewDidAppear (bool animated)
+		{
+			base.ViewDidAppear (animated);
+			this.ParentViewController.Title = Title;
+		}
+
+		private void CreateUI()
+		{
+			EdgesForExtendedLayout = UIRectEdge.None;
+			View.BackgroundColor = UIColor.Blue;
+			_pageL = new UILabel ();
+			_pageL.TranslatesAutoresizingMaskIntoConstraints = false;
+			View.AddSubview (_pageL);
+			View.AddConstraint (NSLayoutConstraint.Create (_pageL, NSLayoutAttribute.CenterX, NSLayoutRelation.Equal, View, NSLayoutAttribute.CenterX, 1, 0));
+			View.AddConstraint (NSLayoutConstraint.Create (_pageL, NSLayoutAttribute.CenterY, NSLayoutRelation.Equal, View, NSLayoutAttribute.CenterY, 1, 0));
+		}
+	}
+}
