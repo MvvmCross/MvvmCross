9f632a24f42071e81f7a34778ce549054de25d54
Tomasz Cielecki <tomasz@ostebaronen.dk>
2015-09-10 12:48:47 +0200

    Fix nuspecs

--- nuspec/pack.ps1
+++ nuspec/pack.ps1
@@ -9,9 +9,7 @@ if (-Not(Test-Path ("nuget.exe")))
 Set-Alias nuget $targetNugetExe -Scope Global -Verbose
 
 del *.nupkg
-nuget pack MvvmCross.HotTuna.Binding.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.CrossCore.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.MvvmCrossLibraries.nuspec -Symbols
+
 nuget pack MvvmCross.HotTuna.Plugin.Accelerometer.nuspec -Symbols
 nuget pack MvvmCross.HotTuna.Plugin.All.nuspec -Symbols
 nuget pack MvvmCross.HotTuna.Plugin.Bookmarks.nuspec -Symbols
@@ -32,21 +30,9 @@ nuget pack MvvmCross.HotTuna.Plugin.ReflectionEx.nuspec -Symbols
 nuget pack MvvmCross.HotTuna.Plugin.ResourceLoader.nuspec -Symbols
 nuget pack MvvmCross.HotTuna.Plugin.Share.nuspec -Symbols
 nuget pack MvvmCross.HotTuna.Plugin.SoundEffects.nuspec -Symbols
-# nuget pack MvvmCross.HotTuna.Plugin.SQLite.nuspec -Symbols
+nuget pack MvvmCross.HotTuna.Plugin.SQLitePCL.nuspec -Symbols
 nuget pack MvvmCross.HotTuna.Plugin.ThreadUtils.nuspec -Symbols
 nuget pack MvvmCross.HotTuna.Plugin.Visibility.nuspec -Symbols
 nuget pack MvvmCross.HotTuna.Plugin.WebBrowser.nuspec -Symbols
-# note no -Symbols
-nuget pack MvvmCross.HotTuna.StarterPack.nuspec
-nuget pack MvvmCross.HotTuna.Tests.nuspec -Symbols
-# note no -Symbols
-nuget pack MvvmCross.nuspec
-nuget pack MvvmCross.HotTuna.Touch.Dialog.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Droid.Dialog.nuspec -Symbols
-nuget pack MvvmCross.BindingEx.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.AutoViews.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Touch.AutoViews.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Droid.AutoViews.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Droid.FullFragging.nuspec -Symbols
 
 pause
