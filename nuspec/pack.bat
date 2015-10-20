4f1777bf9040c2c681b807f570f60949fbaa42cc
Tomasz Cielecki <tomasz@ostebaronen.dk>
2015-10-20 19:10:11 +0200

    Add nuspecs to pack script

--- nuspec/pack.bat
+++ nuspec/pack.bat
@@ -5,6 +5,9 @@ nuget pack Cirrious.MvvmCross.Droid.Support.AppCompat.nuspec -symbols
 nuget pack Cirrious.MvvmCross.Droid.Support.Fragging.nuspec -Symbols
 nuget pack Cirrious.MvvmCross.Droid.Support.V4.nuspec -Symbols
 nuget pack Cirrious.MvvmCross.Droid.Support.RecyclerView.nuspec -Symbols
+nuget pack Cirrious.MvvmCross.Droid.Support.Design.nuspec -Symbols
+nuget pack Cirrious.MvvmCross.Droid.Support.Leanback.nuspec -Symbols
+nuget pack Cirrious.MvvmCross.Droid.Support.Preference.nuspec -Symbols
 
 for /r %%i in (*.nupkg) do (call :pushpackage "%%i")
 pause
