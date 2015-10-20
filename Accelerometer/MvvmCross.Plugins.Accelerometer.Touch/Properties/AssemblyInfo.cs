578f85dc0355799fff7cae9e2c85d5c08e12ea13
Tomasz Cielecki <tomasz@ostebaronen.dk>
2015-10-20 18:40:09 +0200

    Update packages, bump Assembly versions and add missing assembly info files.

---
+++ Accelerometer/MvvmCross.Plugins.Accelerometer.Touch/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MvvmCross.Plugins.Accelerometer.Touch")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MvvmCross.Plugins.Accelerometer.Touch")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2d64cb25-c671-4fdd-b4cc-5a745eecf1ca")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
--- Network/MvvmCross.Plugins.Network.Droid/Properties/AssemblyInfo.cs
+++ Network/MvvmCross.Plugins.Network.Droid/Properties/AssemblyInfo.cs
@@ -34,8 +34,8 @@ using Android.App;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
 
 // Add some common permissions, these can be removed if not needed
 
