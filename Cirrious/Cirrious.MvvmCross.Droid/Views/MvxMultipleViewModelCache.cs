45ae5bdcb7f01e8b571198e97fdaa92d29aa65e9
Przemysław Raciborski <przemyslawraciborski@gmail.com>
2015-11-17 12:19:10 +0100

    It is now possible to cache different instances of ViewModel with IMvxMultipleViewModelCache by passing "viewmodel tag".
    Main reason why it was implemented: caching fragments was not possible if activity had multiple instances of same fragment type - cause ViewModel cache worked only for one of those instances.

--- Cirrious/Cirrious.MvvmCross.Droid/Views/MvxMultipleViewModelCache.cs
+++ Cirrious/Cirrious.MvvmCross.Droid/Views/MvxMultipleViewModelCache.cs
@@ -7,38 +7,76 @@ namespace Cirrious.MvvmCross.Droid.Views
     public class MvxMultipleViewModelCache
         : IMvxMultipleViewModelCache
     {
-        private readonly Lazy<ConcurrentDictionary<Type, IMvxViewModel>> _lazyCurrentViewModels;
+        private readonly Lazy<ConcurrentDictionary<CachedViewModelType, IMvxViewModel>> _lazyCurrentViewModels;
 
         public MvxMultipleViewModelCache()
         {
-            _lazyCurrentViewModels = new Lazy<ConcurrentDictionary<Type, IMvxViewModel>>(() => new ConcurrentDictionary<Type, IMvxViewModel>());
+            _lazyCurrentViewModels = new Lazy<ConcurrentDictionary<CachedViewModelType, IMvxViewModel>>(() => new ConcurrentDictionary<CachedViewModelType, IMvxViewModel>());
         }
 
-        private ConcurrentDictionary<Type, IMvxViewModel> CurrentViewModels => _lazyCurrentViewModels.Value;
+        private ConcurrentDictionary<CachedViewModelType, IMvxViewModel> CurrentViewModels => _lazyCurrentViewModels.Value;
 
-        public void Cache(IMvxViewModel toCache)
+        public void Cache(IMvxViewModel toCache, string viewModelTag = "singleInstanceCache")
         {
             if (toCache == null) return;
 
             var type = toCache.GetType();
 
-            if (!CurrentViewModels.ContainsKey(type))
-                CurrentViewModels.TryAdd(type, toCache);
+            var cachedViewModelType = new CachedViewModelType(type, viewModelTag);
+            if (!CurrentViewModels.ContainsKey(cachedViewModelType))
+                CurrentViewModels.TryAdd(cachedViewModelType, toCache);
         }
 
-        public IMvxViewModel GetAndClear(Type viewModelType)
+        public IMvxViewModel GetAndClear(Type viewModelType, string viewModelTag = "singleInstanceCache")
         {
             if (viewModelType == null) return null;
 
             IMvxViewModel vm;
-            CurrentViewModels.TryRemove(viewModelType, out vm);
+            var cachedViewModelType = new CachedViewModelType(viewModelType, viewModelTag);
+            CurrentViewModels.TryRemove(cachedViewModelType, out vm);
 
             return vm;
         }
 
-        public T GetAndClear<T>() where T : IMvxViewModel
+        public T GetAndClear<T>(string viewModelTag = "singleInstanceCache") where T : IMvxViewModel
         {
-            return (T) GetAndClear(typeof (T));
+            return (T) GetAndClear(typeof (T), viewModelTag);
         }
+
+        private class CachedViewModelType
+        {
+            public Type ViewModelType { get; private set; }
+            public string ViewModelTag { get; private set; }
+
+            public CachedViewModelType(Type viewModelType, string viewModelTag)
+            {
+                ViewModelType = viewModelType;
+                ViewModelTag = viewModelTag ?? string.Empty;
+            }
+
+            public override int GetHashCode()
+            {
+                unchecked
+                {
+                    int hashCode = 17;
+                    hashCode = hashCode*23 + ViewModelType.GetHashCode();
+                    hashCode = hashCode*23 + ViewModelTag.GetHashCode();
+                    return hashCode;
+                }
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (ReferenceEquals(obj, this))
+                    return true;
+
+                var other = obj as CachedViewModelType;
+
+                return other != null && 
+                       other.ViewModelTag.Equals(this.ViewModelTag) &&
+                       other.ViewModelType == this.ViewModelType;
+            }
+        }
+
     }
 }
