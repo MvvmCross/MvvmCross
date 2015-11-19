aecf20ffa865721b1a674c7aba9576e919ebd765
Martijn van Dijk <mhvdijk@gmail.com>
2015-11-19 10:25:32 +0100

    Revert "Support Interfaces in MvxTargetBindingFactoryRegistry"
    This reverts commit 3211339978a948c2d980c800496f8b4067bcc0f6.

--- Cirrious/Cirrious.MvvmCross.Binding/Bindings/Target/Construction/MvxTargetBindingFactoryRegistry.cs
+++ Cirrious/Cirrious.MvvmCross.Binding/Bindings/Target/Construction/MvxTargetBindingFactoryRegistry.cs
@@ -16,114 +16,109 @@ namespace Cirrious.MvvmCross.Binding.Bindings.Target.Construction
     public class MvxTargetBindingFactoryRegistry : IMvxTargetBindingFactoryRegistry
     {
         private readonly Dictionary<string, IMvxPluginTargetBindingFactory> _lookups =
-        new Dictionary<string, IMvxPluginTargetBindingFactory>();
+            new Dictionary<string, IMvxPluginTargetBindingFactory>();
 
-		public virtual IMvxTargetBinding CreateBinding(object target, string targetName)
-		{
-			if (target == null)
-				return null;
+        public virtual IMvxTargetBinding CreateBinding(object target, string targetName)
+        {
+            IMvxTargetBinding binding;
+            if (TryCreateSpecificFactoryBinding(target, targetName, out binding))
+                return binding;
 
-			IMvxTargetBinding binding;
-			if (TryCreateSpecificFactoryBinding(target, targetName, out binding))
-				return binding;
+            if (TryCreateReflectionBasedBinding(target, targetName, out binding))
+                return binding;
 
-			if (TryCreateReflectionBasedBinding(target, targetName, out binding))
-				return binding;
+            return null;
+        }
 
-			return null;
-		}
+        protected virtual bool TryCreateReflectionBasedBinding(object target, string targetName,
+                                                               out IMvxTargetBinding binding)
+        {
+            if (string.IsNullOrEmpty(targetName))
+            {
+                MvxBindingTrace.Trace(MvxTraceLevel.Error,
+                                      "Empty binding target passed to MvxTargetBindingFactoryRegistry");
+                binding = null;
+                return false;
+            }
 
-		protected virtual bool TryCreateReflectionBasedBinding(object target, string targetName,
-			out IMvxTargetBinding binding)
-		{
-			if (string.IsNullOrEmpty(targetName))
-			{
-				MvxBindingTrace.Trace(MvxTraceLevel.Error,
-					"Empty binding target passed to MvxTargetBindingFactoryRegistry");
-				binding = null;
-				return false;
-			}
+            if (target == null)
+            {
+                // null passed in so return false - fixes #584
+                binding = null;
+                return false;
+            }
 
-			var targetPropertyInfo = target.GetType().GetProperty(targetName);
-			if (targetPropertyInfo != null
-				&& targetPropertyInfo.CanWrite)
-			{
-				binding = new MvxWithEventPropertyInfoTargetBinding(target, targetPropertyInfo);
-				return true;
-			}
+            var targetPropertyInfo = target.GetType().GetProperty(targetName);
+            if (targetPropertyInfo != null
+                && targetPropertyInfo.CanWrite)
+            {
+                binding = new MvxWithEventPropertyInfoTargetBinding(target, targetPropertyInfo);
+                return true;
+            }
 
-			var targetEventInfo = target.GetType().GetEvent(targetName);
-			if (targetEventInfo != null)
-			{
-				// we only handle EventHandler's here
-				// other event types will need to be handled by custom bindings
-				if (targetEventInfo.EventHandlerType == typeof(EventHandler))
-				{
-					binding = new MvxEventHandlerEventInfoTargetBinding(target, targetEventInfo);
-					return true;
-				}
-			}
+            var targetEventInfo = target.GetType().GetEvent(targetName);
+            if (targetEventInfo != null)
+            {
+                // we only handle EventHandler's here
+                // other event types will need to be handled by custom bindings
+                if (targetEventInfo.EventHandlerType == typeof (EventHandler))
+                {
+                    binding = new MvxEventHandlerEventInfoTargetBinding(target, targetEventInfo);
+                    return true;
+                }
+            }
 
-			binding = null;
-			return false;
-		}
+            binding = null;
+            return false;
+        }
 
-		protected virtual bool TryCreateSpecificFactoryBinding(object target, string targetName,
-			out IMvxTargetBinding binding)
-		{
-			var factory = FindSpecificFactory(target.GetType(), targetName);
-			if (factory != null)
-			{
-				binding = factory.CreateBinding(target, targetName);
-				return true;
-			}
+        protected virtual bool TryCreateSpecificFactoryBinding(object target, string targetName,
+                                                               out IMvxTargetBinding binding)
+        {
+            if (target == null)
+            {
+                // null passed in so return false - fixes #584
+                binding = null;
+                return false;
+            }
 
-			binding = null;
-			return false;
-		}
+            var factory = FindSpecificFactory(target.GetType(), targetName);
+            if (factory != null)
+            {
+                binding = factory.CreateBinding(target, targetName);
+                return true;
+            }
 
-		public void RegisterFactory(IMvxPluginTargetBindingFactory factory)
-		{
-			foreach (var supported in factory.SupportedTypes)
-			{
-				var key = GenerateKey(supported.Type, supported.Name);
-				_lookups[key] = factory;
-			}
-		}
+            binding = null;
+            return false;
+        }
 
-		private string GenerateKey(Type type, string name)
-		{
-			return string.Format("{0}:{1}", type.FullName, name);
-		}
+        public void RegisterFactory(IMvxPluginTargetBindingFactory factory)
+        {
+            foreach (var supported in factory.SupportedTypes)
+            {
+                var key = GenerateKey(supported.Type, supported.Name);
+                _lookups[key] = factory;
+            }
+        }
 
-		private IMvxPluginTargetBindingFactory FindSpecificFactory(Type type, string name)
-		{
-			foreach (var interfaceType in GetPossibleTypes(type))
-			{
-				var key = GenerateKey(interfaceType, name);
-				IMvxPluginTargetBindingFactory factory;
-				if (_lookups.TryGetValue(key, out factory))
-				{
-					return factory;
-				}
-			}
+        private string GenerateKey(Type type, string name)
+        {
+            return string.Format("{0}:{1}", type.FullName, name);
+        }
 
-			return null;
-		}
-
-		private static IEnumerable<Type> GetPossibleTypes(Type type)
-		{
-			var tmpType = type;
-			while (tmpType != null)
-			{
-				yield return tmpType;
-				tmpType = tmpType.BaseType;
-			}
-
-			foreach (var t in type.GetInterfaces())
-			{
-				yield return t;
-			}
-		}
+        private IMvxPluginTargetBindingFactory FindSpecificFactory(Type type, string name)
+        {
+            IMvxPluginTargetBindingFactory factory;
+            var key = GenerateKey(type, name);
+            if (_lookups.TryGetValue(key, out factory))
+            {
+                return factory;
+            }
+            var baseType = type.GetTypeInfo().BaseType;
+            if (baseType != null)
+                return FindSpecificFactory(baseType, name);
+            return null;
+        }
     }
 }
