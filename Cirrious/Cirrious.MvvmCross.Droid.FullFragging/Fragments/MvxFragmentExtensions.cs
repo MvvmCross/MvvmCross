011af1cc3822efe1fec73f70251330fd77bb2a4f
Derek Beattie <derek.beattie@gmail.com>
2015-12-02 14:26:50 -0600

    Fix merge of PR 1186

--- Cirrious/Cirrious.MvvmCross.Droid.FullFragging/Fragments/MvxFragmentExtensions.cs
+++ Cirrious/Cirrious.MvvmCross.Droid.FullFragging/Fragments/MvxFragmentExtensions.cs
@@ -59,10 +59,10 @@ namespace Cirrious.MvvmCross.Droid.FullFragging.Fragments
 
             if (viewModelType == null)
             {
-                if (!type.HasMvxFragmentAttribute())
+                if (!type.IsCacheableFragmentAttribute())
                     throw new InvalidOperationException($"Your fragment is not generic and it does not have {nameof(MvxFragmentAttribute)} attribute set!");
 
-                var cacheableFragmentAttribute = type.GetMvxFragmentAttribute();
+                var cacheableFragmentAttribute = type.GetCacheableFragmentAttribute();
                 if (cacheableFragmentAttribute.ViewModelType == null)
                     throw new InvalidOperationException($"Your fragment is not generic and it does not use {nameof(MvxFragmentAttribute)} with ViewModel Type constructor.");
 
