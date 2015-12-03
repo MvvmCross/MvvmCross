4e79543064d9161a1a31a9140e07b4f5644c63c7
Boris Spinner <bs@infomantis.de>
2015-12-03 19:54:47 +0100

    Added MvxObjectAdapter and Wrapper
    The ObjectWrapper is needed for the ObjectAdapter and Presenters.
    See https://developer.android.com/reference/android/support/v17/leanback/widget/Presenter.html#onBindViewHolder(android.support.v17.leanback.widget.Presenter.ViewHolder,%20java.lang.Object)
    onBindViewHolder recieves Java.Lang.Objects, so you have to wrap your C#-Models with the new ObjectWrapper

---
+++ Cirrious.MvvmCross.Droid.Support.Leanback/Adapters/IMvxObjectAdapter.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using Cirrious.MvvmCross.Binding.Attributes;
+
+namespace Cirrious.MvvmCross.Droid.Support.Leanback.Adapters
+{
+	public interface IMvxObjectAdapter
+	{
+		[MvxSetToNullAfterBinding]
+		IEnumerable ItemsSource { get; set; }
+	}
+}
