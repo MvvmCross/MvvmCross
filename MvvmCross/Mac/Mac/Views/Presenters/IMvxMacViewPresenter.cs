713a0b0260c554fe52faa67f4b034e0ba8cf62e1
Kerry W. Lothrop <kerry@lothrop.de>
2016-01-03 11:12:52 +0100

    More cleanup.

--- MvvmCross/Mac/Mac/Views/Presenters/IMvxMacViewPresenter.cs
+++ MvvmCross/Mac/Mac/Views/Presenters/IMvxMacViewPresenter.cs
@@ -1,4 +1,4 @@
-// IMvxTouchViewPresenter.cs
+// IMvxMacViewPresenter.cs
 
 // MvvmCross is licensed using Microsoft Public License (Ms-PL)
 // Contributions and inspirations noted in readme.md and license.txt
