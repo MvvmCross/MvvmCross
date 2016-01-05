15b7717ef2bb12cf2a19f7a19ed54e58cfa1fe49
Martijn van Dijk <mhvdijk@gmail.com>
2016-01-05 12:04:58 +0100

    Rename files

--- nuspec/pack.ps1
+++ nuspec/pack.ps1
@@ -10,29 +10,29 @@ Set-Alias nuget $targetNugetExe -Scope Global -Verbose
 
 del *.nupkg
 
-nuget pack MvvmCross.HotTuna.Plugin.Accelerometer.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.All.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.Bookmarks.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.Color.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.DownloadCache.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.Email.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.FieldBinding.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.File.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.Json.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.JsonLocalisation.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.Location.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.Messenger.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.MethodBinding.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.Network.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.PhoneCall.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.PictureChooser.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.ReflectionEx.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.ResourceLoader.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.Share.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.SoundEffects.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.SQLitePCL.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.ThreadUtils.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.Visibility.nuspec -Symbols
-nuget pack MvvmCross.HotTuna.Plugin.WebBrowser.nuspec -Symbols
+nuget pack MvvmCross.Plugin.Accelerometer.nuspec -Symbols
+nuget pack MvvmCross.Plugin.All.nuspec -Symbols
+nuget pack MvvmCross.Plugin.Bookmarks.nuspec -Symbols
+nuget pack MvvmCross.Plugin.Color.nuspec -Symbols
+nuget pack MvvmCross.Plugin.DownloadCache.nuspec -Symbols
+nuget pack MvvmCross.Plugin.Email.nuspec -Symbols
+nuget pack MvvmCross.Plugin.FieldBinding.nuspec -Symbols
+nuget pack MvvmCross.Plugin.File.nuspec -Symbols
+nuget pack MvvmCross.Plugin.Json.nuspec -Symbols
+nuget pack MvvmCross.Plugin.JsonLocalisation.nuspec -Symbols
+nuget pack MvvmCross.Plugin.Location.nuspec -Symbols
+nuget pack MvvmCross.Plugin.Messenger.nuspec -Symbols
+nuget pack MvvmCross.Plugin.MethodBinding.nuspec -Symbols
+nuget pack MvvmCross.Plugin.Network.nuspec -Symbols
+nuget pack MvvmCross.Plugin.PhoneCall.nuspec -Symbols
+nuget pack MvvmCross.Plugin.PictureChooser.nuspec -Symbols
+nuget pack MvvmCross.Plugin.ReflectionEx.nuspec -Symbols
+nuget pack MvvmCross.Plugin.ResourceLoader.nuspec -Symbols
+nuget pack MvvmCross.Plugin.Share.nuspec -Symbols
+nuget pack MvvmCross.Plugin.SoundEffects.nuspec -Symbols
+nuget pack MvvmCross.Plugin.SQLitePCL.nuspec -Symbols
+nuget pack MvvmCross.Plugin.ThreadUtils.nuspec -Symbols
+nuget pack MvvmCross.Plugin.Visibility.nuspec -Symbols
+nuget pack MvvmCross.Plugin.WebBrowser.nuspec -Symbols
 
 pause
