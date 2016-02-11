283aaf58cc3f37dc0187cf06efebd4796771fe8d
Tomasz Cielecki <tomasz@ostebaronen.dk>
2016-02-11 10:20:04 +0100

    another one...

--- MvvmCross.Droid.Support.V7.AppCompat/Widget/MvxAppCompatAutoCompleteTextView.cs
+++ MvvmCross.Droid.Support.V7.AppCompat/Widget/MvxAppCompatAutoCompleteTextView.cs
@@ -19,7 +19,7 @@ namespace MvvmCross.Droid.Support.V7.AppCompat.Widget
     using MvvmCross.Binding.Attributes;
     using MvvmCross.Binding.Droid.Views;
 
-    [Register("MvvmCross.Droid.Support.V7.AppCompat.widget.MvxAppCompatAutoCompleteTextView")]
+    [Register("mvvmcross.droid.support.v7.appcompat.widget.MvxAppCompatAutoCompleteTextView")]
     public class MvxAppCompatAutoCompleteTextView
         : AppCompatAutoCompleteTextView
     {
