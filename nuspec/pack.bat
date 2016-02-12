7790730c89a3e1af116fd559803f625069282ead
Kerry W. Lothrop <kerry@lothrop.de>
2016-02-12 16:08:44 +0100

    Removed Cirrious nuspecs (not needed anymore).

--- nuspec/pack.bat
+++ nuspec/pack.bat
@@ -10,17 +10,6 @@ nuget pack MvvmCross.Droid.Support.V7.RecyclerView.nuspec -Symbols
 nuget pack MvvmCross.Droid.Support.V4.nuspec -Symbols
 nuget pack MvvmCross.Droid.Support.V14.Preference.nuspec -Symbols
 
-# Cirrious legacy
-nuget pack deprecated\Cirrious.MvvmCross.Droid.Support.AppCompat.nuspec
-nuget pack deprecated\Cirrious.MvvmCross.Droid.Support.Design.nuspec
-nuget pack deprecated\Cirrious.MvvmCross.Droid.Support.Fragging.nuspec
-nuget pack deprecated\Cirrious.MvvmCross.Droid.Support.Leanback.nuspec
-nuget pack deprecated\Cirrious.MvvmCross.Droid.Support.Preference.nuspec
-nuget pack deprecated\Cirrious.MvvmCross.Droid.Support.RecyclerView.nuspec
-nuget pack deprecated\Cirrious.MvvmCross.Droid.Support.V4.nuspec
-nuget pack deprecated\Cirrious.MvvmCross.Droid.Support.V7.Preference.nuspec
-nuget pack deprecated\Cirrious.MvvmCross.Droid.Support.V14.Preference.nuspec
-
 for /r %%i in (*.nupkg) do (call :pushpackage "%%i")
 pause
 
