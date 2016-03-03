93e98e4e4f14be6590197c4028fe91fa19990516
Alexandre Zollinger Chohfi <azchohfi@hotmail.com>
2016-03-02 23:18:27 -0300

    Added Analyzer to check if a Command with CanExecute never has it's RaiseCanExecuteChanged method called.

--- CodeAnalysis/MvvmCross.CodeAnalysis/Core/DiagnosticIds.cs
+++ CodeAnalysis/MvvmCross.CodeAnalysis/Core/DiagnosticIds.cs
@@ -3,5 +3,6 @@
     public static class DiagnosticIds
     {
         public const string UseGenericBaseClassRuleId = "MVX1001";
+        public const string CommandWithCanExecuteWithoutCanExecuteChangedRuleId = "MVX1002";
     }
 }
