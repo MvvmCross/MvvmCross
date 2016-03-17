f3f0ab6f65792852ba0958a4f8f648f3de16f3e0
Przemysław Raciborski <przemyslawraciborski@gmail.com>
2016-03-17 22:46:07 +0100

    Implement FAB auto-hide on scroll behavior. Implements feature request #92

---
+++ MvvmCross.Droid.Support.Design/Behaviors/ScrollAwareFABBehavior.cs
@@ -0,0 +1,42 @@
+using System;
+using Android.Content;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V4.View;
+using Android.Util;
+using Android.Views;
+
+namespace MvvmCross.Droid.Support.Design.Behaviors
+{
+    public class ScrollAwareFABBehavior : FloatingActionButton.Behavior
+    {
+        public ScrollAwareFABBehavior(IntPtr handle, JniHandleOwnership ownership) : base(handle, ownership)
+        {
+            
+        }
+
+        public ScrollAwareFABBehavior()
+        {
+         
+        }
+
+        public override bool OnStartNestedScroll(CoordinatorLayout coordinatorLayout, Java.Lang.Object child, View directTargetChild, View target, int nestedScrollAxes)
+        {
+            return nestedScrollAxes == ViewCompat.ScrollAxisVertical ||
+                   base.OnStartNestedScroll(coordinatorLayout, child, directTargetChild, target, nestedScrollAxes);
+        }
+
+
+        public override void OnNestedScroll(CoordinatorLayout coordinatorLayout, Java.Lang.Object child, View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed)
+        {
+            base.OnNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
+
+            var floatingActionButtonChild = child.JavaCast<FloatingActionButton>();
+
+            if (dyConsumed > 0 && floatingActionButtonChild.Visibility == ViewStates.Visible)
+                floatingActionButtonChild.Hide();
+            else if (dyConsumed < 0 && floatingActionButtonChild.Visibility != ViewStates.Visible)
+                floatingActionButtonChild.Show();
+        }    
+    }
+}
