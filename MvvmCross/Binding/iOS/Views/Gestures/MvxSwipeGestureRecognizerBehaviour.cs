20dc6e1c2b752e50730dc8e4c18fce6188f59a7f
Simon Schick <simonsimcity@gmail.com>
2016-03-22 10:39:35 +0100

    Added swipe-gesture-recognizer

---
+++ MvvmCross/Binding/iOS/Views/Gestures/MvxSwipeGestureRecognizerBehaviour.cs
@@ -0,0 +1,32 @@
+﻿// MvxSwipeGestureRecognizerBehaviour.cs
+
+// MvvmCross is licensed using Microsoft Public License (Ms-PL)
+// Contributions and inspirations noted in readme.md and license.txt
+//
+// Project Lead - Stuart Lodge, @slodge, me@slodge.com
+
+namespace MvvmCross.Binding.iOS.Views.Gestures
+{
+    using UIKit;
+
+    public class MvxSwipeGestureRecognizerBehaviour
+        : MvxGestureRecognizerBehavior<UISwipeGestureRecognizer>
+    {
+        protected override void HandleGesture(UISwipeGestureRecognizer gesture)
+        {
+            this.FireCommand();
+        }
+
+        public MvxSwipeGestureRecognizerBehaviour(UIView target, UISwipeGestureRecognizerDirection direction,
+                                                uint numberOfTouchesRequired = 1)
+        {
+            var swipe = new UISwipeGestureRecognizer(this.HandleGesture)
+            {
+                Direction = direction,
+                NumberOfTouchesRequired = numberOfTouchesRequired
+            };
+
+            this.AddGestureRecognizer(target, swipe);
+        }
+    }
+}
