21a1a1183e5b7ebf5fc0e42da71507502bdf065f
Przemysław Raciborski <przemyslawraciborski@gmail.com>
2016-03-23 21:39:51 +0100

    Fixed invalid (no menu + recyclerview took whole page) UI bugs in MultiItemTemplate example fragment.

--- Samples/Example.Droid/Fragments/RecyclerViewMultiItemTemplateFragment.cs
+++ Samples/Example.Droid/Fragments/RecyclerViewMultiItemTemplateFragment.cs
@@ -1,22 +1,15 @@
-using Android.OS;
 using Android.Runtime;
-using Android.Views;
 using Example.Core.ViewModels;
-using MvvmCross.Binding.Droid.BindingContext;
 using MvvmCross.Droid.Support.V7.Fragging.Attributes;
-using MvvmCross.Droid.Support.V7.Fragging.Fragments;
 
 namespace Example.Droid.Fragments
 {
     [MvxFragment(typeof(MainViewModel), Resource.Id.content_frame)]
     [Register("example.droid.fragments.RecyclerViewMultiItemTemplateFragment")]
-    public class RecyclerViewMultiItemTemplateFragment : MvxFragment<RecyclerViewMultiItemTemplateViewModel>
+    public class RecyclerViewMultiItemTemplateFragment : BaseFragment<RecyclerViewMultiItemTemplateViewModel>
     {
-        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
-        {
-            base.OnCreateView(inflater, container, savedInstanceState);
-            return this.BindingInflate(Resource.Layout.fragment_recyclerview_multiitemtemplate, null);
-        }
+         
 
+        protected override int FragmentId => Resource.Layout.fragment_recyclerview_multiitemtemplate;
     }
 }
