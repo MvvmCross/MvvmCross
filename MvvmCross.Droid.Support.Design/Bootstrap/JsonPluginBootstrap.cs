9b6f92ead2acc77bcfca68eff38f0a960601ec44
Tom Curran <tom@tomcurran.org>
2016-04-17 17:40:08 +0100

    added MvxBottomSheetDialogFragment to design support library

---
+++ MvvmCross.Droid.Support.Design/Bootstrap/JsonPluginBootstrap.cs
@@ -0,0 +1,9 @@
+using MvvmCross.Platform.Plugins;
+
+namespace MvvmCross.Droid.Support.Design.Bootstrap
+{
+    public class JsonPluginBootstrap
+        : MvxPluginBootstrapAction<MvvmCross.Plugins.Json.PluginLoader>
+    {
+    }
+}
