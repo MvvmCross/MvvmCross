bdd0ea47212f0120139fca4f8e6a82cabb04c416
Seifer <seed-seifer@ya.ru>
2016-04-20 16:08:48 +0300

    Add Fused Location Plugin nuspec and bootstrap

---
+++ nuspec/BootstrapContent/FusedLocationPluginBootstrap.cs.pp
@@ -0,0 +1,9 @@
+using MvvmCross.Platform.Plugins;
+
+namespace $rootnamespace$.Bootstrap
+{
+    public class LocationPluginBootstrap
+        : MvxPluginBootstrapAction<MvvmCross.Plugins.Location.PluginLoader, MvvmCross.Plugins.Location.Fused.Droid.Plugin>
+    {
+    }
+}
