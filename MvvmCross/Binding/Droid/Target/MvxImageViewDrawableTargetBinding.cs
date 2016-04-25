a889e3b5b4eb95f9877333da090e8a630df41728
Christophe Oosterlynck <christophe.oosterlynck@flowpilots.com>
2016-04-25 22:12:54 +0200

    Set image resource directly on the ImageView instead of decoding it ourself

--- MvvmCross/Binding/Droid/Target/MvxImageViewDrawableTargetBinding.cs
+++ MvvmCross/Binding/Droid/Target/MvxImageViewDrawableTargetBinding.cs
@@ -9,42 +9,44 @@ namespace MvvmCross.Binding.Droid.Target
 {
     using System;
 
-    using Android.Graphics;
     using Android.Widget;
 
     using MvvmCross.Platform.Platform;
 
     public class MvxImageViewDrawableTargetBinding
-        : MvxBaseImageViewTargetBinding
+        : MvxAndroidTargetBinding
     {
+        protected ImageView ImageView => (ImageView)Target;
+
         public MvxImageViewDrawableTargetBinding(ImageView imageView)
             : base(imageView)
         {
         }
 
+        public override MvxBindingMode DefaultMode => MvxBindingMode.OneWay;
+
         public override Type TargetType => typeof(int);
 
-        protected override bool GetBitmap(object value, out Bitmap bitmap)
+        protected override void SetValueImpl(object target, object value)
         {
+            var imageView = (ImageView)target;
+
             if (!(value is int))
             {
                 MvxBindingTrace.Trace(MvxTraceLevel.Warning,
                     "Value was not a valid Drawable");
-                bitmap = null;
-                return false;
+                imageView.SetImageDrawable(null);
+                return;
             }
 
             var intValue = (int)value;
 
             if (intValue == 0)
-                bitmap = null;
+                imageView.SetImageDrawable(null);
             else
             {
-                var resources = this.AndroidGlobals.ApplicationContext.Resources;
-                bitmap = BitmapFactory.DecodeResource(resources, intValue, new BitmapFactory.Options() { InPurgeable = true });
+                imageView.SetImageResource(intValue);
             }
-
-            return true;
         }
     }
 }
