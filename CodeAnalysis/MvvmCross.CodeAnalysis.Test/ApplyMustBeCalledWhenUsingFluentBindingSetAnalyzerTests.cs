c73bb53a19b6a6f692b7d942b1f813c19dd24615
Alexandre Zollinger Chohfi <azchohfi@hotmail.com>
2016-04-26 14:17:26 -0400

    Added CodeFix for Apply on Fluent Binding. Still with wrong whitespace.

--- CodeAnalysis/MvvmCross.CodeAnalysis.Test/ApplyMustBeCalledWhenUsingFluentBindingSetAnalyzerTests.cs
+++ CodeAnalysis/MvvmCross.CodeAnalysis.Test/ApplyMustBeCalledWhenUsingFluentBindingSetAnalyzerTests.cs
@@ -1,15 +1,15 @@
 ﻿using Microsoft.CodeAnalysis;
 using MvvmCross.CodeAnalysis.Analyzers;
+using MvvmCross.CodeAnalysis.CodeFixes;
 using MvvmCross.CodeAnalysis.Core;
 using NUnit.Framework;
 
 namespace MvvmCross.CodeAnalysis.Test
 {
-	[TestFixture]
-	public class ApplyMustBeCalledWhenUsingFluentBindingSetAnalyzerTests : DiagnosticVerifier<ApplyMustBeCalledWhenUsingFluentBindingSetAnalyzer> //CodeFixVerifier<ApplyMustBeCalledWhenUsingFluentBindingSetAnalyzer, ApplyMustBeCalledWhenUsingFluentBindingSetCodeFix>
-	{
-		private const string ViewModel = @"
-using System;
+    [TestFixture]
+    public class ApplyMustBeCalledWhenUsingFluentBindingSetAnalyzerTests : CodeFixVerifier<ApplyMustBeCalledWhenUsingFluentBindingSetAnalyzer, ApplyMustBeCalledWhenUsingFluentBindingSetCodeFix>
+    {
+        private const string ViewModel = @"using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,18 +19,18 @@ using MvvmCross.Core.ViewModels;
 
 namespace IOSApp.Core.ViewModels
 {
-	public class FirstViewModel : MvxViewModel
-	{
-		private string _hello = ""Hello MvvmCross"";
-		public string Hello
-		{
-			get { return _hello; }
-			set { SetProperty(ref _hello, value); }
-		}
-	}
+    public class FirstViewModel : MvxViewModel
+    {
+        private string _hello = ""Hello MvvmCross"";
+        public string Hello
+        {
+            get { return _hello; }
+            set { SetProperty(ref _hello, value); }
+        }
+    }
 }";
 
-		private const string GeneratedDesigner = @"// WARNING
+        private const string GeneratedDesigner = @"// WARNING
 //
 // This file has been generated automatically by Xamarin Studio from the outlets and
 // actions declared in your storyboard file.
@@ -43,37 +43,36 @@ using UIKit;
 
 namespace IOSApp.IOS.Views
 {
-	[Register (""FirstView"")]
+    [Register (""FirstView"")]
 
-	partial class FirstView
-		{
-			[Outlet]
-			[GeneratedCode(""iOS Designer"", ""1.0"")]
-			UILabel Label { get; set; }
+    partial class FirstView
+        {
+            [Outlet]
+            [GeneratedCode(""iOS Designer"", ""1.0"")]
+            UILabel Label { get; set; }
 
-			[Outlet]
-			[GeneratedCode(""iOS Designer"", ""1.0"")]
-			UITextField TextField { get; set; }
+            [Outlet]
+            [GeneratedCode(""iOS Designer"", ""1.0"")]
+            UITextField TextField { get; set; }
 
-			void ReleaseDesignerOutlets()
-			{
-				if (Label != null)
-				{
-					Label.Dispose();
-					Label = null;
-				}
-				if (TextField != null)
-				{
-					TextField.Dispose();
-					TextField = null;
-				}
-			}
-		}
-	}
+            void ReleaseDesignerOutlets()
+            {
+                if (Label != null)
+                {
+                    Label.Dispose();
+                    Label = null;
+                }
+                if (TextField != null)
+                {
+                    TextField.Dispose();
+                    TextField = null;
+                }
+            }
+        }
+    }
 ";
 
-		private const string Expected = @"
-using System;
+        private const string Expected = @"using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -85,27 +84,26 @@ using IOSApp.Core.ViewModels;
 
 namespace IOSApp.IOS.Views
 {
-	public partial class FirstView : MvxViewController
-	{
-		public FirstView() : base(""FirstView"", null)
-		{
-		}
+    public partial class FirstView : MvxViewController
+    {
+        public FirstView() : base(""FirstView"", null)
+        {
+        }
 
-		public override void ViewDidLoad()
-		{
-			base.ViewDidLoad();
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
 
-			var set = this.CreateBindingSet<FirstView, Core.ViewModels.FirstViewModel>();
+            var set = this.CreateBindingSet<FirstView, Core.ViewModels.FirstViewModel>();
 
-			set.Bind(Label).To(vm => vm.Hello);
-			set.Bind(TextField).To(vm => vm.Hello);
-			set.Apply();
-		}
-	}
+            set.Bind(Label).To(vm => vm.Hello);
+            set.Bind(TextField).To(vm => vm.Hello);
+            set.Apply();
+        }
+    }
 }";
 
-		private const string Test = @"
-using System;
+        private const string Test = @"using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -115,62 +113,73 @@ using MvvmCross.iOS.Views;
 using MvvmCross.Core.ViewModels;
 using IOSApp.Core.ViewModels;
 
-namespace IOSApp.IOS
+namespace IOSApp.IOS.Views
 {
-	public partial class FirstView : MvxViewController
-	{
-		public FirstView() : base(""FirstView"", null)
-		{
-		}
+    public partial class FirstView : MvxViewController
+    {
+        public FirstView() : base(""FirstView"", null)
+        {
+        }
 
-		public override void ViewDidLoad()
-		{
-			base.ViewDidLoad();
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
 
-			var set = this.CreateBindingSet<FirstView, Core.ViewModels.FirstViewModel>();
+            var set = this.CreateBindingSet<FirstView, Core.ViewModels.FirstViewModel>();
 
-			set.Bind(Label).To(vm => vm.Hello);
-			set.Bind(TextField).To(vm => vm.Hello);
-		}
-	}
+            set.Bind(Label).To(vm => vm.Hello);
+            set.Bind(TextField).To(vm => vm.Hello);
+        }
+    }
 }";
 
-		[Test]
-		public void ApplyMustBeCalledWhenUsingFluentBindingSetShoudShowOneDiagnosticIfApplyIsNotCalled()
-		{
-			var expectedDiagnostic = new DiagnosticResult
-			{
-				Id = DiagnosticIds.ApplyMustBeCalledWhenUsingFluentBindingSetId,
-				Message = "Call set.Apply() method to apply your bindings",
-				Severity = DiagnosticSeverity.Warning,
-				Locations =
-					new[]
-					{
-						new DiagnosticResultLocation("Test0.cs", 24, 19)
-					}
-			};
+        [Test]
+        public void ApplyMustBeCalledWhenUsingFluentBindingSetShoudShowOneDiagnosticIfApplyIsNotCalled()
+        {
+            var expectedDiagnostic = new DiagnosticResult
+            {
+                Id = DiagnosticIds.ApplyMustBeCalledWhenUsingFluentBindingSetId,
+                Message = "Call set.Apply() method to apply your bindings",
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[]
+                    {
+                        new DiagnosticResultLocation("Test0.cs", 23, 17)
+                    }
+            };
 
-			var project = new[]
-			{
-				new MvxTestFileSource(Test, MvxProjType.Ios),
-				new MvxTestFileSource(GeneratedDesigner, MvxProjType.Ios), 
-				new MvxTestFileSource(ViewModel, MvxProjType.Core)
-			};
+            var project = new[]
+            {
+                new MvxTestFileSource(Test, MvxProjType.Ios),
+                new MvxTestFileSource(GeneratedDesigner, MvxProjType.Ios), 
+                new MvxTestFileSource(ViewModel, MvxProjType.Core)
+            };
 
-			VerifyCSharpDiagnostic(project, expectedDiagnostic);
-		}
+            VerifyCSharpDiagnostic(project, expectedDiagnostic);
+        }
 
-		[Test]
-		public void ApplyMustBeCalledWhenUsingFluentBindingSetShoudShowNoDiagnosticIfApplyIsCalled()
-		{
-			var project = new[]
-			{
-				new MvxTestFileSource(Expected, MvxProjType.Ios),
-				new MvxTestFileSource(GeneratedDesigner, MvxProjType.Ios),
-				new MvxTestFileSource(ViewModel, MvxProjType.Core)
-			};
+        [Test]
+        public void ApplyMustBeCalledWhenUsingFluentBindingSetShoudShowNoDiagnosticIfApplyIsCalled()
+        {
+            var project = new[]
+            {
+                new MvxTestFileSource(Expected, MvxProjType.Ios),
+                new MvxTestFileSource(GeneratedDesigner, MvxProjType.Ios),
+                new MvxTestFileSource(ViewModel, MvxProjType.Core)
+            };
 
-			VerifyCSharpDiagnostic(project);
-		}
-	}
+            VerifyCSharpDiagnostic(project);
+        }
+
+        [Test]
+        public void ApplyMustBeCalledWhenUsingFluentBindingSetAnalyzerShouldFixTheCode()
+        {
+            var solution = new[]
+            {
+                new MvxTestFileSource(GeneratedDesigner, MvxProjType.Ios),
+                new MvxTestFileSource(ViewModel, MvxProjType.Core)
+            };
+            VerifyCSharpFix(solution, Test, MvxProjType.Ios, Expected);
+        }
+    }
 }
