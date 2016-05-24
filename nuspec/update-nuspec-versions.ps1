a6454413ec5ec9fbd1d889c14e04695424bc08ee
Tomasz Cielecki <tomasz@ostebaronen.dk>
2016-05-24 21:47:03 +0200

    Update nuspec versions

--- nuspec/update-nuspec-versions.ps1
+++ nuspec/update-nuspec-versions.ps1
@@ -1,7 +1,7 @@
-$newVersion = "4.1.0"
+$newVersion = "4.1.6"
 
-$versionPattern = "(<version)>(\d\.\d\.\d(-alpha|beta\d)?)(</version>)"
-$dependencyPattern = "(<dependency\s+id=`"MvvmCross[^`"]+`"\s+version=)`"(\d\.\d\.\d(-alpha|beta\d)?)(`"\s*/>)"
+$versionPattern = "(<version)>(\d\.\d\.\d)(</version>)"
+$dependencyPattern = "(<dependency\s+id=`"MvvmCross[^`"]+`"\s+version=)`"(\d\.\d\.\d)(`"\s*/>)"
 
 $files = Get-ChildItem ".\" -Filter *.nuspec
 
@@ -9,8 +9,8 @@ for($i=0; $i -lt $files.Count; $i++) {
     $content = Get-Content $files[$i].FullName
     $newContent = ""
     ForEach ($line in $content) {
-        $line = $line -Replace $versionPattern, ('$1>' + $newVersion + '$4')
-        $newContent += ($line -Replace $dependencyPattern, ('$1"' + $newVersion + '$4')) + "`n"
+        $line = $line -Replace $versionPattern, ('$1>' + $newVersion + '$3')
+        $newContent += ($line -Replace $dependencyPattern, ('$1"' + $newVersion + '$3')) + "`n"
     }
     Set-Content -Force ($files[$i].FullName) $newContent
 }
