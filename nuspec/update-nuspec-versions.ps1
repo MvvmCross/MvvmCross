d2b5b5c7af049fabb4834c200b6d9a380ed56916
Tomasz Cielecki <tomasz@ostebaronen.dk>
2016-05-24 21:43:43 +0200

    Bump nuspec versions

--- nuspec/update-nuspec-versions.ps1
+++ nuspec/update-nuspec-versions.ps1
@@ -1,4 +1,4 @@
-$newVersion = "4.1.5"
+$newVersion = "4.1.6"
 
 $versionPattern = "(<version)>(\d\.\d\.\d(-alpha|beta\d)?)(</version>)"
 $dependencyPattern = "(<dependency\s+id=`"MvvmCross[^`"]+`"\s+version=)`"(\d\.\d\.\d(-alpha|beta\d)?)(`"\s*/>)"
