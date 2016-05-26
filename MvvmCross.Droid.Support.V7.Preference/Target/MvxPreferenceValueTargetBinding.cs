c089dd86f958175349243e08636272d87886d007
Jeremy Kolb <jkolb@ara.com>
2016-05-26 16:56:10 -0400

    Bind to Android.Support.V7.Preferences.Preference
    Allows binding to EditTextPreference.Text and TwoStatePreference.Checked as
    well as one-way binding to any other Preference subclass using the generic
    "Value" binding.
    
    Call `MvxPreferenceSetupHelper.FillTargetFactories` from your `Setup.cs` file
    and do things like:
    
        public class SettingsViewFragment : MvxPreferenceFragmentCompat<SettingsViewModel>
        {
            public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
            {
                // Since the view is created in PreferenceFragmentCompat's OnCreateView
                // we don't use BindingInflate like a typical MvxFragment.
                var view = base.OnCreateView(inflater, container, savedInstanceState);
    
                var serverPreference = (EditTextPreference)this.FindPreference("pref_eugs_server");
    
                var bindingSet = this.CreateBindingSet<SettingsViewFragment, SettingsViewModel>();
                bindingSet.Bind(serverPreference)
                    .For(v => v.Text)
                    .To(vm => vm.EugsServerAddress);
                bindingSet.Apply();
    
                return view;
            }
    
            public override void OnCreatePreferences(Bundle savedInstanceState, string rootKey)
            {
                this.AddPreferencesFromResource(Resource.Xml.preferences);
            }
        }

---
+++ MvvmCross.Droid.Support.V7.Preference/Target/MvxPreferenceValueTargetBinding.cs
@@ -0,0 +1,52 @@
+using System;
+using MvvmCross.Binding;
+using MvvmCross.Binding.Droid.Target;
+using MvvmCross.Platform;
+
+namespace MvvmCross.Droid.Support.V7.Preference.Target
+{
+    public class MvxPreferenceValueTargetBinding : MvxAndroidTargetBinding
+    {
+        public MvxPreferenceValueTargetBinding(Android.Support.V7.Preferences.Preference preference)
+            : base(preference)
+        { }
+
+        public Android.Support.V7.Preferences.Preference Preference => Target as Android.Support.V7.Preferences.Preference;
+
+        public override Type TargetType => typeof(Android.Support.V7.Preferences.Preference);
+
+        public override MvxBindingMode DefaultMode => MvxBindingMode.TwoWay;
+
+        public override void SubscribeToEvents()
+        {
+            Preference.PreferenceChange += HandlePreferenceChange;
+        }
+
+        protected void HandlePreferenceChange(object sender, Android.Support.V7.Preferences.Preference.PreferenceChangeEventArgs e)
+        {
+            if (e.Preference == Preference)
+            {
+                this.FireValueChanged(e.NewValue);
+                e.Handled = true;
+            }
+        }
+
+        protected override void Dispose(bool isDisposing)
+        {
+            if (isDisposing)
+            {
+                if (Preference != null)
+                {
+                    Preference.PreferenceChange -= this.HandlePreferenceChange;
+                }
+            }
+
+            base.Dispose(isDisposing);
+        }
+
+        protected override void SetValueImpl(object target, object value)
+        {
+            Mvx.Warning("SetValueImpl called on generic Preference target");
+        }
+    }
+}
---
+++ MvvmCross.Droid.Support.V7.Preference/Target/MvxTwoStatePreferenceValueTargetBinding.cs
@@ -0,0 +1,19 @@
+using Android.Support.V7.Preferences;
+
+namespace MvvmCross.Droid.Support.V7.Preference.Target
+{
+    public class MvxTwoStatePreferenceValueTargetBinding : MvxPreferenceValueTargetBinding
+    {
+        public MvxTwoStatePreferenceValueTargetBinding(TwoStatePreference preference)
+            : base(preference) { }
+
+        protected override void SetValueImpl(object target, object value)
+        {
+            var t = target as TwoStatePreference;
+            if (t != null)
+            {
+                t.Checked = (bool)value;
+            }
+        }
+    }
+}
