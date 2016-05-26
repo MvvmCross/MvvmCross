c5669e41ae232a8d53556b43e6d46b341aa988e7
Jeremy Kolb <jkolb@ara.com>
2016-05-26 17:28:23 -0400

    Allows binding to EditTextPreference.Text and TwoStatePreference.Checked as well as one-way binding to any other Preference subclass using the generic "Value" binding.
    Call MvxPreferenceSetupHelper.FillTargetFactories from your Setup.cs file
    and do things like:
    
    public class SettingsViewFragment : MvxPreferenceFragment<SettingsViewModel>
    {
        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
        {
            // Since the view is created in PreferenceFragmentCompat's OnCreateView
            // we don't use BindingInflate like a typical MvxFragment.
            var view = base.OnCreateView(inflater, container, savedInstanceState);
    
            var serverPreference = (EditTextPreference)this.FindPreference("pref_eugs_server");
    
            var bindingSet = this.CreateBindingSet<SettingsViewFragment, SettingsViewModel>();
            bindingSet.Bind(serverPreference)
                .For(v => v.Text)
                .To(vm => vm.EugsServerAddress);
            bindingSet.Apply();
    
            return view;
        }
    
        public override void OnCreatePreferences(Bundle savedInstanceState, string rootKey)
        {
            this.AddPreferencesFromResource(Resource.Xml.preferences);
        }
    }

---
+++ MvvmCross/Binding/Droid/Target/MvxEditTextPreferenceTextTargetBinding.cs
@@ -0,0 +1,19 @@
+﻿using Android.Preferences;
+
+namespace MvvmCross.Binding.Droid.Target
+{
+    public class MvxEditTextPreferenceTextTargetBinding : MvxPreferenceValueTargetBinding
+    {
+        public MvxEditTextPreferenceTextTargetBinding(EditTextPreference preference)
+            : base(preference) { }
+
+        protected override void SetValueImpl(object target, object value)
+        {
+            var t = target as EditTextPreference;
+            if (t != null)
+            {
+                t.Text = (string)value;
+            }
+        }
+    }
+}
