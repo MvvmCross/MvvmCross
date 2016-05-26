b7ec441806c9376cbbb9d7363bafa5edc42224c4
Jeremy Kolb <jkolb@ara.com>
2016-05-26 17:28:23 -0400

    Allows binding to EditTextPreference.Text and TwoStatePreference.Checked as well as one-way binding to any other Preference subclass using the generic "Value" binding.
    Call MvxPreferenceSetupHelper.FillTargetFactories from your Setup.cs file
    and do things like:
    
    public class SettingsViewFragment : MvxPreferenceFragment<SettingsViewModel>
    {
        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
        {
            // Since the view is created in PreferenceFragmentCompat's OnCreateView
            // we don't use BindingInflate like a typical MvxFragment.
            var view = base.OnCreateView(inflater, container, savedInstanceState);
    
            var serverPreference = (EditTextPreference)this.FindPreference("pref_eugs_server");
    
            var bindingSet = this.CreateBindingSet<SettingsViewFragment, SettingsViewModel>();
            bindingSet.Bind(serverPreference)
                .For(v => v.Text)
                .To(vm => vm.EugsServerAddress);
            bindingSet.Apply();
    
            return view;
        }
    
        public override void OnCreatePreferences(Bundle savedInstanceState, string rootKey)
        {
            this.AddPreferencesFromResource(Resource.Xml.preferences);
        }
    }

---
+++ MvvmCross/Binding/Droid/Target/MvxPreferenceValueTargetBinding.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Android.Preferences;
+using MvvmCross.Platform;
+
+namespace MvvmCross.Binding.Droid.Target
+{
+    public class MvxPreferenceValueTargetBinding : MvxAndroidTargetBinding
+    {
+        public MvxPreferenceValueTargetBinding(Preference preference)
+            : base(preference)
+        { }
+
+        public Preference Preference => Target as Preference;
+
+        public override Type TargetType => typeof(Preference);
+
+        public override MvxBindingMode DefaultMode => MvxBindingMode.TwoWay;
+
+        public override void SubscribeToEvents()
+        {
+            Preference.PreferenceChange += HandlePreferenceChange;
+        }
+
+        protected void HandlePreferenceChange(object sender, Preference.PreferenceChangeEventArgs e)
+        {
+            if (e.Preference == Preference)
+            {
+                this.FireValueChanged(e.NewValue);
+                e.Handled = true;
+            }
+        }
+
+        protected override void Dispose(bool isDisposing)
+        {
+            if (isDisposing)
+            {
+                if (Preference != null)
+                {
+                    Preference.PreferenceChange -= this.HandlePreferenceChange;
+                }
+            }
+
+            base.Dispose(isDisposing);
+        }
+
+        protected override void SetValueImpl(object target, object value)
+        {
+            Mvx.Warning("SetValueImpl called on generic Preference target");
+        }
+    }
+}
