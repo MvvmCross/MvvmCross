2d21b43ee13e9b9d1495a1637901f962767f4673
Martin Nygren <martin@Martin-Macbook-Work.local>
2016-07-12 10:50:15 +0100

    [TASK] Updates the copyright header.

--- MvvmCross.Forms.Presenter.iOS/MvxFormsApplicationDelegate.cs
+++ MvvmCross.Forms.Presenter.iOS/MvxFormsApplicationDelegate.cs
@@ -1,9 +1,16 @@
-﻿using System;
-using Xamarin.Forms.Platform.iOS;
+﻿// MvxFormsIosPagePresenter.cs
+// 2015 (c) Copyright Cheesebaron. http://ostebaronen.dk
+// MvvmCross.Forms.Presenter is licensed using Microsoft Public License (Ms-PL)
+// Contributions and inspirations noted in readme.md and license.txt
+//
+// Project Lead - Tomasz Cielecki, @cheesebaron, mvxplugins@ostebaronen.dk
+// Contributor - Martin Nygren, @zzcgumn, zzcgumn@me.com
 
+using System;
 using MvvmCross.iOS.Platform;
 using MvvmCross.Core.Platform;
 using UIKit;
+using Xamarin.Forms.Platform.iOS;
 
 namespace MvvmCross.Forms.Views.iOS
 {
