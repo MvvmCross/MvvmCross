79770e0a5d763078b6d2eb1969d4ce8a4fc6efe6
Chris Apers <chris.apers@newsweb.fr>
2016-07-21 16:18:30 +0200

    Added support for view model instantiation using segue on iOS

---
+++ MvvmCross/iOS/iOS/Views/MvxSegueExtensionMethods.cs
@@ -0,0 +1,36 @@
+﻿using MvvmCross.Core.ViewModels;
+using MvvmCross.Core.Views;
+using MvvmCross.Platform.iOS.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UIKit;
+
+namespace MvvmCross.iOS.Views {
+    internal static class MvxSegueExtensionMethods {
+
+        internal static Type GetViewModelType(this IMvxView view)
+        {
+            var viewType = view.GetType();
+            var props = viewType.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            var prop = props.Where(p => p.Name == "ViewModel").FirstOrDefault();
+            return prop?.PropertyType;
+        }
+
+        internal static void InstantiateViewModelForSegue(this UIViewController _, UIStoryboardSegue segue)
+        {
+            var view = segue.DestinationViewController as IMvxIosView;
+            if (view != null && view.Request == null)
+            {
+                var type = view.GetViewModelType();
+                if (type != null)
+                {
+                    var by = new MvxRequestedBy(MvxRequestedByType.Other, $"Segue: {segue.Identifier}");
+                    view.Request = new MvxViewModelRequest(type, null, null, by);
+                }
+            }
+        }
+    }
+}
