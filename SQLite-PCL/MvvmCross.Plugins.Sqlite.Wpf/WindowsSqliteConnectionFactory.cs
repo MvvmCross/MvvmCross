25a2e41182fe9de77dbd44140af8fdc0dd0837b5
Tomasz Cielecki <tomasz@ostebaronen.dk>
2016-08-26 20:02:10 +0200

    Switch to sqlite-pcl-net from @praeclarum

--- SQLite-PCL/MvvmCross.Plugins.Sqlite.Wpf/WindowsSqliteConnectionFactory.cs
+++ SQLite-PCL/MvvmCross.Plugins.Sqlite.Wpf/WindowsSqliteConnectionFactory.cs
@@ -1,14 +1,10 @@
-﻿using SQLite.Net.Interop;
-using SQLite.Net.Platform.Generic;
-using System;
+﻿using System;
 using System.IO;
 
 namespace MvvmCross.Plugins.Sqlite.Wpf
 {
     public class WindowsSqliteConnectionFactory : MvxSqliteConnectionFactoryBase
-    {
-		public override ISQLitePlatform CurrentPlattform => new SQLitePlatformGeneric();
-
+    { 
         public override string GetPlattformDatabasePath(string databaseName)
         {
             return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), databaseName);
