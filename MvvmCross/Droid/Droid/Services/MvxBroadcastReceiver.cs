1d18b33aea496813a16586a7ae5637d9714f838b
Przemysław Raciborski <PrzemyslawRaciborski@gmail.com>
2016-09-14 22:13:03 +0200

    Support for MvvmCross initialization inside IntentService/BroadcastReceiver. (#1426)
    Fixes issue #1425

---
+++ MvvmCross/Droid/Droid/Services/MvxBroadcastReceiver.cs
@@ -0,0 +1,25 @@
+using System;
+using Android.Content;
+using Android.Runtime;
+using MvvmCross.Droid.Platform;
+
+namespace MvvmCross.Droid.Services
+{
+    public abstract class MvxBroadcastReceiver : BroadcastReceiver
+    {
+        protected MvxBroadcastReceiver(IntPtr javaReference, JniHandleOwnership transfer)
+            : base(javaReference, transfer)
+        {
+        }
+
+        protected MvxBroadcastReceiver()
+        {
+        }
+
+        public override void OnReceive(Context context, Intent intent)
+        {
+            var setup = MvxAndroidSetupSingleton.EnsureSingletonAvailable(context);
+            setup.EnsureInitialized();
+        }
+    }
+}
