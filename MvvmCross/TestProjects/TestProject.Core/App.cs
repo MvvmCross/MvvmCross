2a26ccd11cfa79718b0ff1cef255078ad0c4ee8c
Marc Bruins <marc@marcbruins.nl>
2016-10-07 08:15:15 +0200

    Add projects

---
+++ MvvmCross/TestProjects/TestProject.Core/App.cs
@@ -0,0 +1,17 @@
+﻿using MvvmCross.Core.ViewModels;
+
+namespace TestProject.Core
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            CreatableTypes()
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+            RegisterAppStart<ViewModels.ContentViewModel>();
+        }
+    }
+}
