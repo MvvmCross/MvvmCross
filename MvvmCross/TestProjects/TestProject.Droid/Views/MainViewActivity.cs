c6f7cb28ff37455c9e032746b9fbed8ed9592eb8
Marc Bruins <marc@marcbruins.nl>
2016-10-07 09:24:51 +0200

    Android sample project now works

--- MvvmCross/TestProjects/TestProject.Droid/Views/MainViewActivity.cs
+++ MvvmCross/TestProjects/TestProject.Droid/Views/MainViewActivity.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using Android.App;
 using MvvmCross.Droid.Views;
 using TestProject.Core.ViewModels;
 
 namespace TestProject.Droid
 {
+    [Activity(Label = "MainView", MainLauncher = true)]
     public class MainViewActivity : MvxActivity<MainViewModel>
     {
         public MainViewActivity()
