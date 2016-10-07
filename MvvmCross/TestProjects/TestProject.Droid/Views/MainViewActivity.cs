964525fbd1ff5cfb8af5ba82e48c881ce701441b
Marc Bruins <marc@marcbruins.nl>
2016-10-07 09:12:51 +0200

    Made android start

---
+++ MvvmCross/TestProjects/TestProject.Droid/Views/MainViewActivity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MvvmCross.Droid.Views;
+using TestProject.Core.ViewModels;
+
+namespace TestProject.Droid
+{
+    public class MainViewActivity : MvxActivity<MainViewModel>
+    {
+        public MainViewActivity()
+        {
+            
+        }
+
+        protected override void OnViewModelSet()
+        {
+            base.OnViewModelSet();
+            SetContentView(Resource.Layout.MainView);
+        }
+    }
+}
