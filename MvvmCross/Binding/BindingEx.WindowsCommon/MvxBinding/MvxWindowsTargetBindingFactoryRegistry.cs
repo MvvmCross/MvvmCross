7f6ebd9c65c80e5c9de264044a1e574e98d66b39
Tomasz Cielecki <tomasz@ostebaronen.dk>
2016-10-30 17:33:08 +0100

    Ressurect BindingEx for WindowsPhone 8.1 and Windows 8.1 as WindowsCommon

---
+++ MvvmCross/Binding/BindingEx.WindowsCommon/MvxBinding/MvxWindowsTargetBindingFactoryRegistry.cs
@@ -0,0 +1,69 @@
+﻿// MvxWindowsTargetBindingFactoryRegistry.cs
+
+// MvvmCross is licensed using Microsoft Public License (Ms-PL)
+// Contributions and inspirations noted in readme.md and license.txt
+//
+// Project Lead - Stuart Lodge, @slodge, me@slodge.com
+
+using MvvmCross.Binding;
+using MvvmCross.Binding.Bindings.Target;
+using MvvmCross.Binding.Bindings.Target.Construction;
+using MvvmCross.Platform.Platform;
+
+#if WINDOWS_COMMON
+using MvvmCross.BindingEx.WindowsCommon.MvxBinding.Target;
+
+namespace MvvmCross.BindingEx.WindowsCommon.MvxBinding
+#endif
+
+#if WINDOWS_WPF
+using MvvmCross.BindingEx.Wpf.MvxBinding.Target;
+
+namespace MvvmCross.BindingEx.Wpf.MvxBinding
+#endif
+{
+    public class MvxWindowsTargetBindingFactoryRegistry : MvxTargetBindingFactoryRegistry
+    {
+        protected override bool TryCreateReflectionBasedBinding(object target, string targetName,
+                                                                out IMvxTargetBinding binding)
+        {
+            if (TryCreatePropertyDependencyBasedBinding(target, targetName, out binding))
+            {
+                return true;
+            }
+
+            return base.TryCreateReflectionBasedBinding(target, targetName, out binding);
+        }
+
+        private static bool TryCreatePropertyDependencyBasedBinding(object target, string targetName,
+                                                             out IMvxTargetBinding binding)
+        {
+            if (target == null)
+            {
+                binding = null;
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(targetName))
+            {
+                MvxBindingTrace.Trace(MvxTraceLevel.Error,
+                                      "Empty binding target passed to MvxWindowsTargetBindingFactoryRegistry");
+                binding = null;
+                return false;
+            }
+
+            var dependencyProperty = target.GetType().FindDependencyProperty(targetName);
+            if (dependencyProperty == null)
+            {
+                binding = null;
+                return false;
+            }
+
+            var actualProperty = target.GetType().FindActualProperty(targetName);
+            var actualPropertyType = actualProperty?.PropertyType ?? typeof(object);
+
+            binding = new MvxDependencyPropertyTargetBinding(target, targetName, dependencyProperty, actualPropertyType);
+            return true;
+        }
+    }
+}
---
+++ MvvmCross/Binding/BindingEx.WindowsCommon/MvxWindowsAssemblyCache.cs
@@ -0,0 +1,44 @@
+﻿// MvxWindowsAssemblyCache.cs
+
+// MvvmCross is licensed using Microsoft Public License (Ms-PL)
+// Contributions and inspirations noted in readme.md and license.txt
+//
+// Project Lead - Stuart Lodge, @slodge, me@slodge.com
+
+using System.Collections.Generic;
+using System.Reflection;
+
+using MvvmCross.Platform.Core;
+using MvvmCross.Platform.Exceptions;
+
+#if WINDOWS_COMMON
+namespace MvvmCross.BindingEx.WindowsCommon
+#endif
+
+#if WINDOWS_WPF
+namespace MvvmCross.BindingEx.Wpf
+#endif
+{
+    public class MvxWindowsAssemblyCache
+        : MvxSingleton<IMvxWindowsAssemblyCache>
+          , IMvxWindowsAssemblyCache
+    {
+        public static void EnsureInitialized()
+        {
+            if (Instance != null)
+                return;
+
+            var instance = new MvxWindowsAssemblyCache();
+
+            if (Instance != instance)
+                throw new MvxException("Error initialising MvxWindowsAssemblyCache");
+        }
+
+        public MvxWindowsAssemblyCache()
+        {
+            Assemblies = new List<Assembly>();
+        }
+
+        public IList<Assembly> Assemblies { get; }
+    }
+}
