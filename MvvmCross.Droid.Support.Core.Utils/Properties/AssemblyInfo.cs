c8e701d8cd551e828c38b1c59a9ef0de6a56f2f7
Tomasz Cielecki <tomasz@ostebaronen.dk>
2016-11-02 00:23:09 +0100

    Utils -> Util

--- MvvmCross.Droid.Support.Core.Utils/Properties/AssemblyInfo.cs
+++ MvvmCross.Droid.Support.Core.Utils/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("MvvmCross.Droid.Support.Core.Utils")]
+[assembly: AssemblyTitle("MvvmCross.Droid.Support.Core.Util")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
