d9889f4e86e4d5aef8676da50f68c9c7c4ca7005
Tomasz Cielecki <tomasz@ostebaronen.dk>
2016-11-02 00:23:16 +0100

    Fix nuspec and no symbols for V4

--- nuspec/pack.bat
+++ nuspec/pack.bat
@@ -1,13 +1,16 @@
 del *.nupkg
 nuget setapikey
 
-nuget pack MvvmCross.Droid.Support.V7.AppCompat.nuspec -symbols
+nuget pack MvvmCross.Droid.Support.Core.UI.nuspec -symbols
+nuget pack MvvmCross.Droid.Support.Core.Util.nuspec -symbols
+nuget pack MvvmCross.Droid.Support.Fragment.nuspec -symbols
 nuget pack MvvmCross.Droid.Support.Design.nuspec -Symbols
+nuget pack MvvmCross.Droid.Support.V14.Preference.nuspec -Symbols
 nuget pack MvvmCross.Droid.Support.V17.Leanback.nuspec -Symbols
+nuget pack MvvmCross.Droid.Support.V4.nuspec
+nuget pack MvvmCross.Droid.Support.V7.AppCompat.nuspec -symbols
 nuget pack MvvmCross.Droid.Support.V7.Preference.nuspec -Symbols
 nuget pack MvvmCross.Droid.Support.V7.RecyclerView.nuspec -Symbols
-nuget pack MvvmCross.Droid.Support.V4.nuspec -Symbols
-nuget pack MvvmCross.Droid.Support.V14.Preference.nuspec -Symbols
 
 for /r %%i in (*.nupkg) do (call :pushpackage "%%i")
 pause
