9e73f79ac8c25255bb4b9bab287bbc9e8d19652b
Martijn van Dijk <mhvdijk@gmail.com>
2016-12-14 23:27:49 +0100

    Cleanup mvx windows projects

--- MvvmCross/Binding/Wpf/Properties/AssemblyInfo.cs
+++ MvvmCross/Binding/Wpf/Properties/AssemblyInfo.cs
@@ -1,10 +1,10 @@
 ﻿using System.Reflection;
-using System.Resources;
+using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MvvmCross.Platform.WindowsCommon")]
+[assembly: AssemblyTitle("MvvmCross.Binding.Wpf")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
@@ -12,7 +12,14 @@ using System.Resources;
 [assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8fcfdb4e-c935-41e4-a46f-e6caa39f9e52")]
 
 // Version information for an assembly consists of the following four values:
 //
