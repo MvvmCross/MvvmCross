85c255e7e424601c5b345de9d419ed13a7c73b8b
Martijn van Dijk <mhvdijk@gmail.com>
2017-02-09 16:26:49 +0100

    Add test project

---
+++ TestProjects/Navigation/RoutingExample.Core/RoutingFacades/RandomRoutingFacade.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MvvmCross.Core.Navigation;
+using MvvmCross.Core.ViewModels;
+using RoutingExample.Core.RoutingFacades;
+using RoutingExample.Core.ViewModels;
+
+[assembly: MvxNavigation(typeof(RandomRoutingFacade), @"mvx://random")]
+
+namespace RoutingExample.Core.RoutingFacades
+{
+    public class RandomRoutingFacade
+        : IMvxNavigationFacade
+    {
+        public Task<MvxViewModelRequest> BuildViewModelRequest(string url, IDictionary<string, string> currentParameters, MvxRequestedBy requestedBy)
+        {
+            // you can also use the values captured by the regex in currentPrameters
+
+            var viewModelType = (new Random().Next() % 2 == 0) ? typeof(TestAViewModel) : typeof(TestBViewModel);
+
+            return Task.FromResult(new MvxViewModelRequest(viewModelType, new MvxBundle(), null, requestedBy));
+        }
+    }
+}
