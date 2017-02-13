a6995bd82fc8c541e154511e952148fe88f464fd
Plac3hold3r <Plac3hold3r+github@outlook.com>
2017-02-13 22:05:26 +0200

    Android core moved binding string literal to constants

--- MvvmCross/Binding/Droid/MvxAndroidBindingBuilder.cs
+++ MvvmCross/Binding/Droid/MvxAndroidBindingBuilder.cs
@@ -5,11 +5,10 @@
 //
 // Project Lead - Stuart Lodge, @slodge, me@slodge.com
 
-using Android.Preferences;
 using Android.Graphics;
+using Android.Preferences;
 using Android.Views;
 using Android.Widget;
-
 using MvvmCross.Binding.BindingContext;
 using MvvmCross.Binding.Bindings.Target.Construction;
 using MvvmCross.Binding.Droid.Binders;
@@ -82,72 +81,124 @@ namespace MvvmCross.Binding.Droid
         {
             base.FillTargetFactories(registry);
 
-            registry.RegisterCustomBindingFactory<View>("Click",
-                                                            view => new MvxViewClickBinding(view));
-            registry.RegisterCustomBindingFactory<TextView>("Text",
-                                                            textView => new MvxTextViewTextTargetBinding(textView));
-            registry.RegisterCustomBindingFactory<TextView>("TextFormatted",
-                                                            textView => new MvxTextViewTextFormattedTargetBinding(textView));
-            registry.RegisterCustomBindingFactory<TextView>("Hint",
-                                                            textView => new MvxTextViewHintTargetBinding(textView));
-            registry.RegisterPropertyInfoBindingFactory((typeof(MvxAutoCompleteTextViewPartialTextTargetBinding)),
-                                                    typeof(MvxAutoCompleteTextView), "PartialText");
+            registry.RegisterCustomBindingFactory<View>(
+                MvxAndroidPropertyBinding.View_Click,
+                view => new MvxViewClickBinding(view));
+
+            registry.RegisterCustomBindingFactory<TextView>(
+                MvxAndroidPropertyBinding.TextView_Text,
+                textView => new MvxTextViewTextTargetBinding(textView));
+
+            registry.RegisterCustomBindingFactory<TextView>(
+                MvxAndroidPropertyBinding.TextView_TextFormatted,
+                textView => new MvxTextViewTextFormattedTargetBinding(textView));
+
+            registry.RegisterCustomBindingFactory<TextView>(
+                MvxAndroidPropertyBinding.TextView_Hint,
+                textView => new MvxTextViewHintTargetBinding(textView));
+
             registry.RegisterPropertyInfoBindingFactory(
-                                                    typeof(MvxAutoCompleteTextViewSelectedObjectTargetBinding),
-                                                    typeof(MvxAutoCompleteTextView),
-                                                    "SelectedObject");
-            registry.RegisterPropertyInfoBindingFactory(typeof(MvxCompoundButtonCheckedTargetBinding),
-                                                    typeof(CompoundButton), "Checked");
-            registry.RegisterPropertyInfoBindingFactory(typeof(MvxSeekBarProgressTargetBinding), typeof(SeekBar),
-                                                    "Progress");
-            registry.RegisterCustomBindingFactory<View>("Visible",
-                                                            view => new MvxViewVisibleBinding(view));
-            registry.RegisterCustomBindingFactory<View>("Hidden",
-                                                            view => new MvxViewHiddenBinding(view));
-            registry.RegisterCustomBindingFactory<ImageView>("Bitmap",
-                                                            imageView => new MvxImageViewBitmapTargetBinding(imageView));
-            registry.RegisterCustomBindingFactory<ImageView>("Drawable",
-                                                            imageView => new MvxImageViewImageDrawableTargetBinding(imageView));
-            registry.RegisterCustomBindingFactory<ImageView>("DrawableId",
-                                                            imageView => new MvxImageViewDrawableTargetBinding(imageView));
-            registry.RegisterCustomBindingFactory<ImageView>("DrawableName",
-                                                            imageView => new MvxImageViewDrawableNameTargetBinding(imageView));
-            registry.RegisterCustomBindingFactory<ImageView>("ResourceName",
-                                                            imageView => new MvxImageViewResourceNameTargetBinding(imageView));
-            registry.RegisterCustomBindingFactory<ImageView>("AssetImagePath",
-                                                             imageView => new MvxImageViewImageTargetBinding(imageView));
-            registry.RegisterCustomBindingFactory<MvxSpinner>("SelectedItem",
-                                                              spinner => new MvxSpinnerSelectedItemBinding(spinner));
-            registry.RegisterCustomBindingFactory<AdapterView>("SelectedItemPosition",
-                                                               adapterView => new MvxAdapterViewSelectedItemPositionTargetBinding(adapterView));
-            registry.RegisterCustomBindingFactory<MvxListView>("SelectedItem",
-                                                               adapterView => new MvxListViewSelectedItemTargetBinding(adapterView));
-            registry.RegisterCustomBindingFactory<MvxExpandableListView>("SelectedItem",
-                                                                         adapterView =>
-                                                                             new MvxExpandableListViewSelectedItemTargetBinding(adapterView));
-            registry.RegisterCustomBindingFactory<RatingBar>("Rating",
-                                                            ratingBar => new MvxRatingBarRatingTargetBinding(ratingBar));
-            registry.RegisterCustomBindingFactory<View>("LongClick",
-                                                            view =>
-                                                            new MvxViewLongClickBinding(view));
+                (typeof(MvxAutoCompleteTextViewPartialTextTargetBinding)),
+                typeof(MvxAutoCompleteTextView),
+                MvxAndroidPropertyBinding.MvxAutoCompleteTextView_PartialText);
+
+            registry.RegisterPropertyInfoBindingFactory(
+                typeof(MvxAutoCompleteTextViewSelectedObjectTargetBinding),
+                typeof(MvxAutoCompleteTextView),
+                MvxAndroidPropertyBinding.MvxAutoCompleteTextView_SelectedObject);
+
+            registry.RegisterPropertyInfoBindingFactory(
+                typeof(MvxCompoundButtonCheckedTargetBinding),
+                typeof(CompoundButton),
+                MvxAndroidPropertyBinding.CompoundButton_Checked);
+
+            registry.RegisterPropertyInfoBindingFactory(
+                typeof(MvxSeekBarProgressTargetBinding), 
+                typeof(SeekBar),
+                MvxAndroidPropertyBinding.SeekBar_Progress);
+
+            registry.RegisterCustomBindingFactory<View>(
+                MvxAndroidPropertyBinding.View_Visible,
+                view => new MvxViewVisibleBinding(view));
+
+            registry.RegisterCustomBindingFactory<View>(
+                MvxAndroidPropertyBinding.View_Hidden,
+                view => new MvxViewHiddenBinding(view));
+
+            registry.RegisterCustomBindingFactory<ImageView>(
+                MvxAndroidPropertyBinding.ImageView_Bitmap,
+                imageView => new MvxImageViewBitmapTargetBinding(imageView));
+
+            registry.RegisterCustomBindingFactory<ImageView>(
+                MvxAndroidPropertyBinding.ImageView_Drawable,
+                imageView => new MvxImageViewImageDrawableTargetBinding(imageView));
+
+            registry.RegisterCustomBindingFactory<ImageView>(
+                MvxAndroidPropertyBinding.ImageView_DrawableId,
+                imageView => new MvxImageViewDrawableTargetBinding(imageView));
+
+            registry.RegisterCustomBindingFactory<ImageView>(
+                MvxAndroidPropertyBinding.ImageView_DrawableName,
+                imageView => new MvxImageViewDrawableNameTargetBinding(imageView));
+
+            registry.RegisterCustomBindingFactory<ImageView>(
+                MvxAndroidPropertyBinding.ImageView_ResourceName,
+                imageView => new MvxImageViewResourceNameTargetBinding(imageView));
+
+            registry.RegisterCustomBindingFactory<ImageView>(
+                MvxAndroidPropertyBinding.ImageView_AssetImagePath,
+                imageView => new MvxImageViewImageTargetBinding(imageView));
+
+            registry.RegisterCustomBindingFactory<MvxSpinner>(
+                MvxAndroidPropertyBinding.MvxSpinner_SelectedItem,
+                spinner => new MvxSpinnerSelectedItemBinding(spinner));
+
+            registry.RegisterCustomBindingFactory<AdapterView>(
+                MvxAndroidPropertyBinding.AdapterView_SelectedItemPosition,
+                adapterView => new MvxAdapterViewSelectedItemPositionTargetBinding(adapterView));
+
+            registry.RegisterCustomBindingFactory<MvxListView>(
+                MvxAndroidPropertyBinding.MvxListView_SelectedItem,
+                adapterView => new MvxListViewSelectedItemTargetBinding(adapterView));
+
+            registry.RegisterCustomBindingFactory<MvxExpandableListView>(
+                MvxAndroidPropertyBinding.MvxExpandableListView_SelectedItem,
+                adapterView => new MvxExpandableListViewSelectedItemTargetBinding(adapterView));
+
+            registry.RegisterCustomBindingFactory<RatingBar>(
+                MvxAndroidPropertyBinding.RatingBar_Rating,
+                ratingBar => new MvxRatingBarRatingTargetBinding(ratingBar));
+
+            registry.RegisterCustomBindingFactory<View>(
+                MvxAndroidPropertyBinding.View_LongClick,
+                view => new MvxViewLongClickBinding(view));
+
             registry.RegisterCustomBindingFactory<MvxRadioGroup>(
-                "SelectedItem",
+                MvxAndroidPropertyBinding.MvxRadioGroup_SelectedItem,
                 radioGroup => new MvxRadioGroupSelectedItemBinding(radioGroup));
-            registry.RegisterCustomBindingFactory("TextFocus", (EditText view) => new MvxTextViewFocusTargetBinding(view));
+
+            registry.RegisterCustomBindingFactory(
+                MvxAndroidPropertyBinding.EditText_TextFocus, 
+                (EditText view) => new MvxTextViewFocusTargetBinding(view));
+
             registry.RegisterCustomBindingFactory<SearchView>(
-                "Query",
+                MvxAndroidPropertyBinding.SearchView_Query,
                 search => new MvxSearchViewQueryTextTargetBinding(search));
+
             registry.RegisterCustomBindingFactory<Preference>(
-                "Value",
+                MvxAndroidPropertyBinding.Preference_Value,
                 preference => new MvxPreferenceValueTargetBinding(preference));
+
             registry.RegisterCustomBindingFactory<EditTextPreference>(
-                "Text",
+                MvxAndroidPropertyBinding.EditTextPreference_Text,
                 preference => new MvxEditTextPreferenceTextTargetBinding(preference));
+
             registry.RegisterCustomBindingFactory<ListPreference>(
-                "Value",
+                MvxAndroidPropertyBinding.ListPreference_Value,
                 preference => new MvxListPreferenceTargetBinding(preference));
+
             registry.RegisterCustomBindingFactory<TwoStatePreference>(
-                "Checked",
+                MvxAndroidPropertyBinding.TwoStatePreference_Checked,
                 preference => new MvxTwoStatePreferenceCheckedTargetBinding(preference));
         }
 
@@ -155,23 +206,23 @@ namespace MvvmCross.Binding.Droid
         {
             base.FillDefaultBindingNames(registry);
 
-            registry.AddOrOverwrite(typeof(Button), "Click");
-            registry.AddOrOverwrite(typeof(CheckBox), "Checked");
-            registry.AddOrOverwrite(typeof(TextView), "Text");
-            registry.AddOrOverwrite(typeof(MvxListView), "ItemsSource");
-            registry.AddOrOverwrite(typeof(MvxLinearLayout), "ItemsSource");
-            registry.AddOrOverwrite(typeof(MvxGridView), "ItemsSource");
-            registry.AddOrOverwrite(typeof(MvxRelativeLayout), "ItemsSource");
-            registry.AddOrOverwrite(typeof(MvxFrameLayout), "ItemsSource");
-            registry.AddOrOverwrite(typeof(MvxTableLayout), "ItemsSource");
-            registry.AddOrOverwrite(typeof(MvxFrameControl), "DataContext");
-            registry.AddOrOverwrite(typeof(MvxImageView), "ImageUrl");
-            registry.AddOrOverwrite(typeof(MvxDatePicker), "Value");
-            registry.AddOrOverwrite(typeof(MvxTimePicker), "Value");
-            registry.AddOrOverwrite(typeof(CompoundButton), "Checked");
-            registry.AddOrOverwrite(typeof(SeekBar), "Progress");
-            registry.AddOrOverwrite(typeof(IMvxImageHelper<Bitmap>), "ImageUrl");
-            registry.AddOrOverwrite(typeof(SearchView), "Query");
+            registry.AddOrOverwrite(typeof(Button), MvxAndroidPropertyBinding.View_Click);
+            registry.AddOrOverwrite(typeof(CheckBox), MvxAndroidPropertyBinding.CompoundButton_Checked);
+            registry.AddOrOverwrite(typeof(TextView), MvxAndroidPropertyBinding.TextView_Text);
+            registry.AddOrOverwrite(typeof(MvxListView), nameof(MvxListView.ItemsSource));
+            registry.AddOrOverwrite(typeof(MvxLinearLayout), nameof(MvxLinearLayout.ItemsSource));
+            registry.AddOrOverwrite(typeof(MvxGridView), nameof(MvxGridView.ItemsSource));
+            registry.AddOrOverwrite(typeof(MvxRelativeLayout), nameof(MvxRelativeLayout.ItemsSource));
+            registry.AddOrOverwrite(typeof(MvxFrameLayout), nameof(MvxFrameLayout.ItemsSource));
+            registry.AddOrOverwrite(typeof(MvxTableLayout), nameof(MvxTableLayout.ItemsSource));
+            registry.AddOrOverwrite(typeof(MvxFrameControl), nameof(MvxFrameControl.DataContext));
+            registry.AddOrOverwrite(typeof(MvxImageView), nameof(MvxImageView.ImageUrl));
+            registry.AddOrOverwrite(typeof(MvxDatePicker), nameof(MvxDatePicker.Value));
+            registry.AddOrOverwrite(typeof(MvxTimePicker), nameof(MvxTimePicker.Value));
+            registry.AddOrOverwrite(typeof(CompoundButton), MvxAndroidPropertyBinding.CompoundButton_Checked);
+            registry.AddOrOverwrite(typeof(SeekBar), MvxAndroidPropertyBinding.SeekBar_Progress);
+            registry.AddOrOverwrite(typeof(IMvxImageHelper<Bitmap>), nameof(IMvxImageHelper<Bitmap>.ImageUrl));
+            registry.AddOrOverwrite(typeof(SearchView), MvxAndroidPropertyBinding.SearchView_Query);
         }
 
         protected override void RegisterPlatformSpecificComponents()
---
+++ MvvmCross/Binding/Droid/MvxAndroidPropertyBinding.cs
@@ -0,0 +1,42 @@
+// MvxAndroidPropertyBinding.cs
+
+// MvvmCross is licensed using Microsoft Public License (Ms-PL)
+// Contributions and inspirations noted in readme.md and license.txt
+//
+// Project Lead - Stuart Lodge, @slodge, me@slodge.com
+
+namespace MvvmCross.Binding.Droid
+{
+    internal static class MvxAndroidPropertyBinding
+    {
+        public const string View_Click = "Click";
+        public const string TextView_Text = "Text";
+        public const string TextView_TextFormatted = "TextFormatted";
+        public const string TextView_Hint = "Hint";
+        public const string MvxAutoCompleteTextView_PartialText = "PartialText";
+        public const string MvxAutoCompleteTextView_SelectedObject = "SelectedObject";
+        public const string CompoundButton_Checked = "Checked";
+        public const string SeekBar_Progress = "Progress";
+        public const string View_Visible = "Visible";
+        public const string View_Hidden = "Hidden";
+        public const string ImageView_Bitmap = "Bitmap";
+        public const string ImageView_Drawable = "Drawable";
+        public const string ImageView_DrawableId = "DrawableId";
+        public const string ImageView_DrawableName = "DrawableName";
+        public const string ImageView_ResourceName = "ResourceName";
+        public const string ImageView_AssetImagePath = "AssetImagePath";
+        public const string MvxSpinner_SelectedItem = "SelectedItem";
+        public const string AdapterView_SelectedItemPosition = "SelectedItemPosition";
+        public const string MvxListView_SelectedItem = "SelectedItem";
+        public const string MvxExpandableListView_SelectedItem = "SelectedItem";
+        public const string RatingBar_Rating = "Rating";
+        public const string View_LongClick = "LongClick";
+        public const string MvxRadioGroup_SelectedItem = "SelectedItem";
+        public const string EditText_TextFocus = "TextFocus";
+        public const string SearchView_Query = "Query";
+        public const string Preference_Value = "Value";
+        public const string EditTextPreference_Text = "Text";
+        public const string ListPreference_Value = "Value";
+        public const string TwoStatePreference_Checked = "Checked";
+    }
+}
