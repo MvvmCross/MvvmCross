c282fc0280fbcfb2bd93871de9b78ff40dfdfb7c
Plac3hold3r <Plac3hold3r+github@outlook.com>
2017-02-14 22:24:28 +0200

    Updated extension methods to rather use 'bind' over 'binding' naming conversion

--- MvvmCross/Binding/iOS/MvxIosPropertyBindingExtensions.cs
+++ MvvmCross/Binding/iOS/MvxIosPropertyBindingExtensions.cs
@@ -11,82 +11,82 @@ namespace MvvmCross.Binding.iOS
 {
     public static class MvxIosPropertyBindingExtensions
     {
-        public static string BindingTouchUpInside(this UIControl uiControl)
+        public static string BindTouchUpInside(this UIControl uiControl)
             => MvxIosPropertyBinding.UIControl_TouchUpInside;
 
-        public static string BindingValueChanged(this UIControl uiControl)
+        public static string BindValueChanged(this UIControl uiControl)
             => MvxIosPropertyBinding.UIControl_ValueChanged;
 
-        public static string BindingVisibility(this UIView uiView)
+        public static string BindVisibility(this UIView uiView)
             => MvxIosPropertyBinding.UIView_Visibility;
 
-        public static string BindingVisible(this UIView uiView)
+        public static string BindVisible(this UIView uiView)
             => MvxIosPropertyBinding.UIView_Visible;
 
-        public static string BindingHidden(this UIActivityIndicatorView uiActivityIndicatorView)
+        public static string BindHidden(this UIActivityIndicatorView uiActivityIndicatorView)
              => MvxIosPropertyBinding.UIActivityIndicatorView_Hidden;
 
-        public static string BindingHidden(this UIView uiView)
+        public static string BindHidden(this UIView uiView)
             => MvxIosPropertyBinding.UIView_Hidden;
 
-        public static string BindingValue(this UISlider uiSlider)
+        public static string BindValue(this UISlider uiSlider)
             => MvxIosPropertyBinding.UISlider_Value;
 
-        public static string BindingValue(this UIStepper uiStepper)
+        public static string BindValue(this UIStepper uiStepper)
             => MvxIosPropertyBinding.UIStepper_Value;
 
-        public static string BindingSelectedSegment(this UISegmentedControl uiSegmentedControl)
+        public static string BindSelectedSegment(this UISegmentedControl uiSegmentedControl)
             => MvxIosPropertyBinding.UISegmentedControl_SelectedSegment;
 
-        public static string BindingDate(this UIDatePicker uiDatePicker)
+        public static string BindDate(this UIDatePicker uiDatePicker)
             => MvxIosPropertyBinding.UIDatePicker_Date;
 
-        public static string BindingShouldReturn(this UITextField uiTextField)
+        public static string BindShouldReturn(this UITextField uiTextField)
             => MvxIosPropertyBinding.UITextField_ShouldReturn;
 
-        public static string BindingTime(this UIDatePicker uiDatePicker)
+        public static string BindTime(this UIDatePicker uiDatePicker)
             => MvxIosPropertyBinding.UIDatePicker_Time;
 
-        public static string BindingText(this UILabel uiLabel)
+        public static string BindText(this UILabel uiLabel)
             => MvxIosPropertyBinding.UILabel_Text;
 
-        public static string BindingText(this UITextField uiTextField)
+        public static string BindText(this UITextField uiTextField)
             => MvxIosPropertyBinding.UITextField_Text;
 
-        public static string BindingText(this UITextView uiTextView)
+        public static string BindText(this UITextView uiTextView)
             => MvxIosPropertyBinding.UITextView_Text;
 
-        public static string BindingLayerBorderWidth(this UIView uiView)
+        public static string BindLayerBorderWidth(this UIView uiView)
             => MvxIosPropertyBinding.UIView_LayerBorderWidth;
 
-        public static string BindingOn(this UISwitch uiSwitch)
+        public static string BindOn(this UISwitch uiSwitch)
             => MvxIosPropertyBinding.UISwitch_On;
 
-        public static string BindingText(this UISearchBar uiSearchBar)
+        public static string BindText(this UISearchBar uiSearchBar)
             => MvxIosPropertyBinding.UISearchBar_Text;
 
-        public static string BindingTitle(this UIButton uiButton)
+        public static string BindTitle(this UIButton uiButton)
             => MvxIosPropertyBinding.UIButton_Title;
 
-        public static string BindingDisabledTitle(this UIButton uiButton)
+        public static string BindDisabledTitle(this UIButton uiButton)
             => MvxIosPropertyBinding.UIButton_DisabledTitle;
 
-        public static string BindingHighlightedTitle(this UIButton uiButton)
+        public static string BindHighlightedTitle(this UIButton uiButton)
             => MvxIosPropertyBinding.UIButton_HighlightedTitle;
 
-        public static string BindingSelectedTitle(this UIButton uiButton)
+        public static string BindSelectedTitle(this UIButton uiButton)
             => MvxIosPropertyBinding.UIButton_SelectedTitle;
 
-        public static string BindingTap(this UIView uiView)
+        public static string BindTap(this UIView uiView)
             => MvxIosPropertyBinding.UIView_Tap;
 
-        public static string BindingDoubleTap(this UIView uiView)
+        public static string BindDoubleTap(this UIView uiView)
             => MvxIosPropertyBinding.UIView_DoubleTap;
 
-        public static string BindingTwoFingerTap(this UIView uiView)
+        public static string BindTwoFingerTap(this UIView uiView)
             => MvxIosPropertyBinding.UIView_TwoFingerTap;
 
-        public static string BindingTextFocusk(this UIView uiView)
+        public static string BindTextFocusk(this UIView uiView)
             => MvxIosPropertyBinding.UITextField_TextFocus;
     }
 }
