a3f4dbbaef832a20bbfd21710a27109f5c9d512a
nmilcoff <nicolasmilcoff@hotmail.com.ar>
2017-02-21 09:42:57 -0300

    Remove unused namespaces

--- MvvmCross-iOSSupport/MvvmCross.iOS.Support/Presenters/MvxTabBarPresenter/IMvxTabBarViewController.cs
+++ MvvmCross-iOSSupport/MvvmCross.iOS.Support/Presenters/MvxTabBarPresenter/IMvxTabBarViewController.cs
@@ -1,17 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using MvvmCross.Core.ViewModels;
+﻿using MvvmCross.Core.ViewModels;
 using UIKit;
 
 namespace MvvmCross.iOS.Support.Presenters
 {
-    public interface IMvxTabBarViewController
-    {
-        void ShowTabView(UIViewController viewController, bool wrapInNavigationController, string tabTitle, string tabIconName, string tabAccessibilityIdentifier);
+	public interface IMvxTabBarViewController
+	{
+		void ShowTabView(UIViewController viewController, bool wrapInNavigationController, string tabTitle, string tabIconName, string tabAccessibilityIdentifier);
 
-        bool ShowChildView(UIViewController viewController);
+		bool ShowChildView(UIViewController viewController);
 
-        bool CloseChildViewModel(IMvxViewModel viewModel);
-    }
+		bool CloseChildViewModel(IMvxViewModel viewModel);
+	}
 }
 
