488708c416987af8056fb6a092afc17da0206087
Julio Ernesto Rodríguez Cabañas <julio.cabanas@screenmedia.co.uk>
2017-03-14 15:50:33 +0000

    Changed tabs for spaces

--- MvvmCross-Plugins/Email/MvvmCross.Plugins.Email.Droid/MvxComposeEmailTask.cs
+++ MvvmCross-Plugins/Email/MvvmCross.Plugins.Email.Droid/MvxComposeEmailTask.cs
@@ -19,11 +19,11 @@ using MvvmCross.Platform.Droid.Views;
 namespace MvvmCross.Plugins.Email.Droid
 {
     [Preserve(AllMembers = true)]
-	public class MvxComposeEmailTask
+    public class MvxComposeEmailTask
         : MvxAndroidTask
         , IMvxComposeEmailTaskEx
     {
-		private List<Java.IO.File> filesToDelete;
+        private List<Java.IO.File> filesToDelete;
 
         public void ComposeEmail(string to, string cc = null, string subject = null, string body = null, bool isHtml = false, string dialogTitle = null)
         {
@@ -77,39 +77,39 @@ namespace MvvmCross.Plugins.Email.Droid
                 DoOnActivity(activity => {
                     filesToDelete = new List<Java.IO.File>();
 
-					foreach (var file in attachments)
-					{
-						// fix for Gmail error
-						using (var memoryStream = new MemoryStream())
-						{
-							var fileName = Path.GetFileNameWithoutExtension(file.FileName);
-							var extension = Path.GetExtension(file.FileName);
+                    foreach (var file in attachments)
+                    {
+                        // fix for Gmail error
+                        using (var memoryStream = new MemoryStream())
+                        {
+                            var fileName = Path.GetFileNameWithoutExtension(file.FileName);
+                            var extension = Path.GetExtension(file.FileName);
 
-							// save file in external cache (required so Gmail app can independently access it, otherwise Gmail won't take the attachment)
-							var newFile = new Java.IO.File(activity.ExternalCacheDir, fileName + extension);
+                            // save file in external cache (required so Gmail app can independently access it, otherwise Gmail won't take the attachment)
+                            var newFile = new Java.IO.File(activity.ExternalCacheDir, fileName + extension);
 
-							file.Content.CopyTo(memoryStream);
-							var bytes = memoryStream.ToArray();
-							using (var localFileStream = new FileOutputStream(newFile))
-							{
-								localFileStream.Write(bytes);
-							}
+                            file.Content.CopyTo(memoryStream);
+                            var bytes = memoryStream.ToArray();
+                            using (var localFileStream = new FileOutputStream(newFile))
+                            {
+                                localFileStream.Write(bytes);
+                            }
 
-							newFile.SetReadable(true, false);
-							newFile.DeleteOnExit();
-							uris.Add(Uri.FromFile(newFile));
+                            newFile.SetReadable(true, false);
+                            newFile.DeleteOnExit();
+                            uris.Add(Uri.FromFile(newFile));
 
-							filesToDelete.Add(newFile);
-						}
-					}
+                            filesToDelete.Add(newFile);
+                        }
+                    }
                 });
 
                 if (uris.Any())
                     emailIntent.PutParcelableArrayListExtra(Intent.ExtraStream, uris);
             }
 
-			emailIntent.AddFlags(ActivityFlags.GrantReadUriPermission);
-			emailIntent.AddFlags(ActivityFlags.GrantWriteUriPermission);
+            emailIntent.AddFlags(ActivityFlags.GrantReadUriPermission);
+            emailIntent.AddFlags(ActivityFlags.GrantWriteUriPermission);
 
             // fix for GMail App 5.x (File not found / permission denied when using "StartActivity")
             StartActivityForResult(0, Intent.CreateChooser(emailIntent, dialogTitle ?? string.Empty));
@@ -119,19 +119,19 @@ namespace MvvmCross.Plugins.Email.Droid
 
         public bool CanSendAttachments => true;
 
-		protected override void ProcessMvxIntentResult(MvxIntentResultEventArgs result)
-		{
-			base.ProcessMvxIntentResult(result);
+        protected override void ProcessMvxIntentResult(MvxIntentResultEventArgs result)
+        {
+            base.ProcessMvxIntentResult(result);
 
-			// on return, delete all attachments from external cache
-			foreach (Java.IO.File file in filesToDelete)
-			{
-				if (file.Exists())
-				{
-					file.Delete();
-				}
-			}
-			filesToDelete.Clear();
-		}
+            // on return, delete all attachments from external cache
+            foreach (Java.IO.File file in filesToDelete)
+            {
+                if (file.Exists())
+                {
+                    file.Delete();
+                }
+            }
+            filesToDelete.Clear();
+        }
     }
 }
