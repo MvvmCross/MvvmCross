94978be70f48ae6e75bc1a84d0f4709693a8351f
Houssem <kadi.houssem.eddine@gmail.com>
2017-03-20 23:12:09 +0100

    adding missing usings

--- MvvmCross-Plugins/Email/MvvmCross.Plugins.Email.Uwp/MvxComposeEmailTask.cs
+++ MvvmCross-Plugins/Email/MvvmCross.Plugins.Email.Uwp/MvxComposeEmailTask.cs
@@ -6,74 +6,78 @@
 // Project Lead - Stuart Lodge, @slodge, me@slodge.com
 
 using System;
-using Windows.System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Email;
+using Windows.Storage;
 
 namespace MvvmCross.Plugins.Email.WindowsCommon
 {
     public class MvxComposeEmailTask
       : IMvxComposeEmailTaskEx
-  {
-    public bool CanSendAttachments => true;
-
-    public bool CanSendEmail => true;
-
-    public void ComposeEmail(string to, string cc = null, string subject = null, string body = null, bool isHtml = false, string dialogTitle = null)
     {
-      var toArray = to == null ? null : new[] { to };
-      var ccArray = cc == null ? null : new[] { cc };
-      ComposeEmail(
-          toArray,
-          ccArray,
-          subject,
-          body,
-          isHtml,
-          null,
-          dialogTitle);
-    }
+        public bool CanSendAttachments => true;
 
-    public async void ComposeEmail(IEnumerable<string> to, IEnumerable<string> cc = null, string subject = null, string body = null, bool isHtml = false, IEnumerable<MvvmCross.Plugins.Email.EmailAttachment> attachments = null, string dialogTitle = null)
-    {
-        //TODO: It is better to have this function as async Task so to avoid exception swallowing
-      EmailMessage email = new EmailMessage();
+        public bool CanSendEmail => true;
 
-      if (to != null)
-        foreach (var item in to)
+        public void ComposeEmail(string to, string cc = null, string subject = null, string body = null, bool isHtml = false, string dialogTitle = null)
         {
-          email.To.Add(new EmailRecipient(item));
+            var toArray = to == null ? null : new[] { to };
+            var ccArray = cc == null ? null : new[] { cc };
+            ComposeEmail(
+                toArray,
+                ccArray,
+                subject,
+                body,
+                isHtml,
+                null,
+                dialogTitle);
         }
 
-      if (cc != null)
-        foreach (var item in cc)
+        public async void ComposeEmail(IEnumerable<string> to, IEnumerable<string> cc = null, string subject = null, string body = null, bool isHtml = false, IEnumerable<MvvmCross.Plugins.Email.EmailAttachment> attachments = null, string dialogTitle = null)
         {
-          email.CC.Add(new EmailRecipient(item));
+            //TODO: It is better to have this function as async Task so to avoid exception swallowing
+            EmailMessage email = new EmailMessage();
+
+            if (to != null)
+                foreach (var item in to)
+                {
+                    email.To.Add(new EmailRecipient(item));
+                }
+
+            if (cc != null)
+                foreach (var item in cc)
+                {
+                    email.CC.Add(new EmailRecipient(item));
+                }
+
+            email.Subject = subject ?? "";
+            email.Body = body ?? "";
+
+            if (attachments != null)
+                foreach (var item in attachments)
+                {
+                    email.Attachments.Add(new Windows.ApplicationModel.Email.EmailAttachment(item.FileName, await GetTextFile(item))
+                    );
+                }
+
+
+            await EmailManager.ShowComposeNewEmailAsync(email);
+
         }
 
-      email.Subject = subject ?? "";
-      email.Body = body ?? "";
 
-      if (attachments != null)
-        foreach (var item in attachments)
+        private static async Task<StorageFile> GetTextFile(MvvmCross.Plugins.Email.EmailAttachment attachement)
         {
-          email.Attachments.Add(new Windows.ApplicationModel.Email.EmailAttachment(item.FileName, await GetTextFile(item))
-          );
+            var localFolder = ApplicationData.Current.LocalFolder;
+            var file = await localFolder.CreateFileAsync(attachement.FileName, CreationCollisionOption.ReplaceExisting);
+
+            using (var reader = new StreamReader(attachement.Content))
+            {
+                await FileIO.WriteTextAsync(file, await reader.ReadToEndAsync());
+            }
+            return file;
         }
-
-
-      await EmailManager.ShowComposeNewEmailAsync(email);
-
     }
-
-
-    private static async Task<StorageFile> GetTextFile(MvvmCross.Plugins.Email.EmailAttachment attachement)
-    {
-      var localFolder = ApplicationData.Current.LocalFolder;
-      var file = await localFolder.CreateFileAsync(attachement.FileName, CreationCollisionOption.ReplaceExisting);
-
-      using (var reader = new StreamReader(attachement.Content))
-      {
-        await FileIO.WriteTextAsync(file, await reader.ReadToEndAsync());
-      }
-      return file;
-    }
-  }
 }
