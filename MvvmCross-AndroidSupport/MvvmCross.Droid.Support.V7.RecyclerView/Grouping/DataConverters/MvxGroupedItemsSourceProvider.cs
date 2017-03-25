d0a518cef621da87fe29dadba77b38e7e14e448d
Przemek Raciborski [thefex] <przemyslaw.raciborski@gmail.com>
2017-03-25 15:11:03 +0100

    Grouped Items Observable Child Collection support #1642, fixes some NullReferenceExceptions

--- MvvmCross-AndroidSupport/MvvmCross.Droid.Support.V7.RecyclerView/Grouping/DataConverters/MvxGroupedItemsSourceProvider.cs
+++ MvvmCross-AndroidSupport/MvvmCross.Droid.Support.V7.RecyclerView/Grouping/DataConverters/MvxGroupedItemsSourceProvider.cs
@@ -11,12 +11,11 @@ namespace MvvmCross.Droid.Support.V7.RecyclerView.Grouping.DataConverters
 {
     internal class MvxGroupedItemsSourceProvider
     {
+        private readonly ObservableCollection<object> _observableItemsSource = new ObservableCollection<object>();
         private readonly IList<IDisposable> _collectionChangedDisposables = new List<IDisposable>();
+        private readonly Dictionary<MvxGroupedData, IDisposable> _groupedDataDisposables = new Dictionary<MvxGroupedData, IDisposable>();
 
-        private readonly Dictionary<MvxGroupedData, IDisposable> _groupedDataDisposables =
-            new Dictionary<MvxGroupedData, IDisposable>();
-
-        public ObservableCollection<object> Source { get; } = new ObservableCollection<object>();
+        public ObservableCollection<object> Source => _observableItemsSource;
 
         public void Initialize(IEnumerable groupedItems, IMvxGroupedDataConverter groupedDataConverter)
         {
@@ -34,7 +33,7 @@ namespace MvvmCross.Droid.Support.V7.RecyclerView.Grouping.DataConverters
                             switch (args.Action)
                             {
                                 case NotifyCollectionChangedAction.Reset:
-                                    Source.Clear();
+                                    _observableItemsSource.Clear();
                                     foreach (var disposables in _groupedDataDisposables.Values)
                                         disposables.Dispose();
 
@@ -44,13 +43,13 @@ namespace MvvmCross.Droid.Support.V7.RecyclerView.Grouping.DataConverters
                                     AddItems(args.NewItems, groupedDataConverter);
                                     break;
                                 case NotifyCollectionChangedAction.Remove:
-                                    foreach (var item in args.OldItems.Cast<object>())
+                                    foreach (var item in Enumerable.Cast<object>(args.OldItems))
                                     {
                                         var mvxGroupedData = groupedDataConverter.ConvertToMvxGroupedData(item);
 
                                         foreach (var childItem in mvxGroupedData.Items)
-                                            Source.Remove(childItem);
-                                        Source.Remove(mvxGroupedData);
+                                            _observableItemsSource.Remove(childItem);
+                                        _observableItemsSource.Remove(mvxGroupedData);
 
                                         if (_groupedDataDisposables.ContainsKey(mvxGroupedData))
                                         {
@@ -69,7 +68,7 @@ namespace MvvmCross.Droid.Support.V7.RecyclerView.Grouping.DataConverters
 
         private void DisposeOldSource()
         {
-            Source.Clear();
+            _observableItemsSource.Clear();
             foreach (var disposables in _collectionChangedDisposables)
                 disposables.Dispose();
             foreach (var disposable in _groupedDataDisposables.Values)
@@ -81,15 +80,16 @@ namespace MvvmCross.Droid.Support.V7.RecyclerView.Grouping.DataConverters
 
         private void AddItems(IEnumerable groupedItems, IMvxGroupedDataConverter groupedDataConverter)
         {
-            foreach (
-                var mvxGroupable in groupedItems.Cast<object>().Select(groupedDataConverter.ConvertToMvxGroupedData))
+            foreach (var mvxGroupable in groupedItems.Cast<object>().Select(groupedDataConverter.ConvertToMvxGroupedData))
             {
-                if (!Source.Contains(mvxGroupable))
-                    Source.Add(mvxGroupable);
+                if (!_observableItemsSource.Contains(mvxGroupable))
+                    _observableItemsSource.Add(mvxGroupable);
 
-                foreach (var child in mvxGroupable.Items)
-                    if (!Source.Contains(child))
-                        Source.Add(child);
+                foreach (var child in mvxGroupable.Items.Cast<object>().ToList())
+                {
+                    if (!_observableItemsSource.Contains(child))
+                        _observableItemsSource.Add(child);
+                }
 
 
                 var childNotifyCollectionChanged = mvxGroupable.Items as INotifyCollectionChanged;
@@ -98,42 +98,43 @@ namespace MvvmCross.Droid.Support.V7.RecyclerView.Grouping.DataConverters
                     continue;
 
                 if (!_groupedDataDisposables.ContainsKey(mvxGroupable))
-                    _groupedDataDisposables.Add(mvxGroupable,
-                        childNotifyCollectionChanged.WeakSubscribe((sender, args) =>
+                {
+                    _groupedDataDisposables.Add(mvxGroupable, childNotifyCollectionChanged.WeakSubscribe((sender, args) =>
+                    {
+                        switch (args.Action)
                         {
-                            switch (args.Action)
-                            {
-                                case NotifyCollectionChangedAction.Add:
-                                    AddChildItems(mvxGroupable, args.NewItems);
-                                    break;
-                                case NotifyCollectionChangedAction.Remove:
-                                    foreach (var itemToRemove in args.OldItems)
-                                        Source.Remove(itemToRemove);
-                                    break;
-                                case NotifyCollectionChangedAction.Reset:
-                                    ResetChildCollection(mvxGroupable);
-                                    break;
-                                default:
-                                    throw new NotImplementedException("No move/replace in Grouped Items yet...");
-                            }
-                        }));
+                            case NotifyCollectionChangedAction.Add:
+                                AddChildItems(mvxGroupable, args.NewItems);
+                                break;
+                            case NotifyCollectionChangedAction.Remove:
+                                foreach (var itemToRemove in args.OldItems)
+                                    _observableItemsSource.Remove(itemToRemove);
+                                break;
+                            case NotifyCollectionChangedAction.Reset:
+                                ResetChildCollection(mvxGroupable);
+                                break;
+                            default:
+                                throw new NotImplementedException("No move/replace in Grouped Items yet...");
+                        }
+                    }));
+                }
             }
         }
 
         private void AddChildItems(MvxGroupedData toGroup, IEnumerable items)
         {
-            var groupIndex = Source.IndexOf(toGroup);
+            var groupIndex = _observableItemsSource.IndexOf(toGroup);
 
             if (groupIndex == -1)
                 return;
 
             foreach (var itemToAdd in items)
-                Source.Insert(toGroup.Items.Count() + groupIndex, itemToAdd);
+                _observableItemsSource.Insert(toGroup.Items.Count() + groupIndex, itemToAdd);
         }
 
         private void ResetChildCollection(MvxGroupedData ofGroupedData)
         {
-            var groupIndex = Source.IndexOf(ofGroupedData);
+            var groupIndex = _observableItemsSource.IndexOf(ofGroupedData);
 
             if (groupIndex == -1)
                 return;
@@ -141,11 +142,11 @@ namespace MvvmCross.Droid.Support.V7.RecyclerView.Grouping.DataConverters
             var listToDelete = new List<object>();
             groupIndex++;
 
-            for (var i = groupIndex; i < Source.Count && Source[i] is MvxGroupedData == false; ++i)
-                listToDelete.Add(Source[groupIndex]);
+            for (int i = groupIndex; i < _observableItemsSource.Count && _observableItemsSource[i] is MvxGroupedData == false; ++i)
+                listToDelete.Add(_observableItemsSource[groupIndex]);
 
             foreach (var itemToRemove in listToDelete)
-                Source.Remove(itemToRemove);
+                _observableItemsSource.Remove(itemToRemove);
         }
     }
 }
