e0c7e033c8983df063d859e76be01852f0cbad65
nmilcoff <nicolasmilcoff@hotmail.com.ar>
2017-04-10 12:37:36 -0300

    Fix existing TestProjects // tabs to spaces in some files

--- TestProjects/Eventhooks/Eventhooks.iOS/Views/FirstView.cs
+++ TestProjects/Eventhooks/Eventhooks.iOS/Views/FirstView.cs
@@ -3,31 +3,33 @@ using Eventhooks.Core.ViewModels;
 using MvvmCross.Binding.BindingContext;
 using MvvmCross.iOS.Views;
 using UIKit;
+using MvvmCross.iOS.Views.Presenters.Attributes;
 
 namespace Eventhooks.iOS
 {
-	public partial class FirstView : MvxViewController<FirstViewModel>
-	{
-		public FirstView() : base("FirstView", null)
-		{
-		}
+    [MvxRootPresentation(WrapInNavigationController = true)]
+    public partial class FirstView : MvxViewController<FirstViewModel>
+    {
+        public FirstView() : base("FirstView", null)
+        {
+        }
 
-		public override void ViewDidLoad()
-		{
-			base.ViewDidLoad();
-			// Perform any additional setup after loading the view, typically from a nib.
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
 
-			var bindingSet = this.CreateBindingSet<FirstView, FirstViewModel>();
-			bindingSet.Bind(SecondViewButton).To(vm => vm.ShowSecondView);
-			bindingSet.Apply();
+            var bindingSet = this.CreateBindingSet<FirstView, FirstViewModel>();
+            bindingSet.Bind(SecondViewButton).To(vm => vm.ShowSecondView);
+            bindingSet.Apply();
 
-		}
+        }
 
-		public override void DidReceiveMemoryWarning()
-		{
-			base.DidReceiveMemoryWarning();
-			// Release any cached data, images, etc that aren't in use.
-		}
-	}
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
 }
 
