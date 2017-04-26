7641e96a17bc448766f99534443a4989c4d8247a
nmilcoff <nicolasmilcoff@hotmail.com.ar>
2017-04-26 09:35:20 -0300

    Playground.Mac creation

---
+++ TestProjects/Playground/Playground.Mac/AppDelegate.cs
@@ -0,0 +1,49 @@
+﻿using AppKit;
+using CoreGraphics;
+using Foundation;
+using MvvmCross.Core.ViewModels;
+using MvvmCross.Mac.Platform;
+using MvvmCross.Mac.Views.Presenters;
+using MvvmCross.Platform;
+
+namespace Playground.Mac
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MvxApplicationDelegate
+    {
+        public NSWindow Window { get; set; }
+
+        public AppDelegate()
+        {
+        }
+
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            Window = new NSWindow(new CGRect(0, 0, 400, 300),
+                      NSWindowStyle.Titled | NSWindowStyle.Resizable | NSWindowStyle.Closable,
+                      NSBackingStore.Buffered, false, NSScreen.MainScreen);
+            Window.Title = "Playground";
+            Window.ContentView = new NSView(new CGRect(0, 0, 400, 300));
+
+            Window.WillClose += (sender, e) =>
+            {
+                NSApplication.SharedApplication.Terminate(this);
+            };
+
+            var presenter = new MvxMacViewPresenter(this, Window);
+
+            var setup = new Setup(this, presenter);
+            setup.Initialize();
+
+            var startup = Mvx.Resolve<IMvxAppStart>();
+            startup.Start();
+
+            Window.MakeKeyAndOrderFront(this);
+        }
+
+        public override void WillTerminate(NSNotification notification)
+        {
+            // Insert code here to tear down your application
+        }
+    }
+}
