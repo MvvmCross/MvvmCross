7fb7fcb2a783d234817473298e74728026b266cb
Sylvain Gravel <sgravel@lerondpoint.net>
2017-04-27 14:45:03 -0400

    Initial bindings working

---
+++ MvvmCross-Forms/MvvmCross.Forms.Presenter.Binding/MvxBindablePropertyExtensionMethods.cs
@@ -0,0 +1,73 @@
+﻿// MvxBindablePropertyExtensionMethods.cs
+
+// MvvmCross is licensed using Microsoft Public License (Ms-PL)
+// Contributions and inspirations noted in readme.md and license.txt
+//
+// Project Lead - Stuart Lodge, @slodge, me@slodge.com
+
+using System;
+using System.Reflection;
+using Xamarin.Forms;
+
+namespace MvvmCross.Forms.Presenter.Binding
+{
+    public static class MvxBindablePropertyExtensionMethods
+    {
+        public static PropertyInfo FindActualProperty(this Type type, string name)
+        {
+            if (string.IsNullOrEmpty(name))
+                return null;
+
+            var property = type.GetRuntimeProperty(name);
+            return property;
+        }
+
+        private static PropertyInfo FindBindablePropertyInfo(Type type, string dependencyPropertyName)
+        {
+            if (string.IsNullOrEmpty(dependencyPropertyName))
+                return null;
+
+            if (!EnsureIsBindablePropertyName(ref dependencyPropertyName))
+                return null;
+
+            var typeInfo = type.GetTypeInfo();
+            while (typeInfo != null)
+            {
+                var propertyInfo = typeInfo.GetDeclaredProperty(dependencyPropertyName);
+                if (propertyInfo != null)
+                {
+                    return propertyInfo;
+                }
+
+                if (typeInfo.BaseType == null)
+                {
+                    return null;
+                }
+
+                typeInfo = typeInfo.BaseType.GetTypeInfo();
+            }
+
+            return null;
+        }
+
+        public static BindableProperty FindBindableProperty(this Type type, string name)
+        {
+            if (string.IsNullOrEmpty(name))
+                return null;
+
+            var propertyInfo = FindBindablePropertyInfo(type, name);
+
+            return propertyInfo?.GetValue(null) as BindableProperty;
+        }
+
+        private static bool EnsureIsBindablePropertyName(ref string bindablePropertyName)
+        {
+            if (string.IsNullOrEmpty(bindablePropertyName))
+                return false;
+
+            if (!bindablePropertyName.EndsWith("Property"))
+                bindablePropertyName += "Property";
+            return true;
+        }
+    }
+}
---
+++ MvvmCross-Forms/MvvmCross.Forms.Presenter.Binding/MvxBinding/Target/MvxBindablePropertyTargetBinding.cs
@@ -0,0 +1,87 @@
+﻿// MvxBindablePropertyTargetBinding.cs
+
+// MvvmCross is licensed using Microsoft Public License (Ms-PL)
+// Contributions and inspirations noted in readme.md and license.txt
+//
+// Project Lead - Stuart Lodge, @slodge, me@slodge.com
+
+using System;
+using MvvmCross.Binding;
+using MvvmCross.Binding.Bindings.Target;
+using MvvmCross.Binding.ExtensionMethods;
+using MvvmCross.Platform.Platform;
+using Xamarin.Forms;
+
+namespace MvvmCross.Forms.Presenter.Binding
+{
+    public class MvxBindablePropertyTargetBinding : MvxConvertingTargetBinding
+    {
+        private readonly string _targetName;
+        private readonly BindableProperty _targetDependencyProperty;
+        private readonly Type _actualPropertyType;
+
+        public MvxBindablePropertyTargetBinding(object target, string targetName, BindableProperty targetBindableProperty, Type actualPropertyType)
+            : base(target)
+        {
+            _targetDependencyProperty = targetBindableProperty;
+            _actualPropertyType = actualPropertyType;
+            _targetName = targetName;
+        }
+
+        public override void SubscribeToEvents()
+        {
+            var frameworkElement = Target as Element;
+            if (frameworkElement == null)
+                return;
+
+            var listenerBinding = new Xamarin.Forms.Binding
+            {
+                Path = _targetName,
+                Source = frameworkElement
+            };
+            var attachedProperty = BindableProperty.CreateAttached("ListenAttached" + _targetName + Guid.NewGuid()
+                                                                                                        .ToString("N"),
+                                                                   typeof(object),
+                                                                   typeof(Element),
+                                                                   null,
+                                                                   BindingMode.OneWay,
+                                                                   null,
+                                                                   (s, o, n) => FireValueChanged(n));
+        }
+
+        public override Type TargetType => _actualPropertyType;
+
+        private MvxBindingMode _defaultBindingMode = MvxBindingMode.TwoWay;
+        public override MvxBindingMode DefaultMode => _defaultBindingMode;
+
+        protected virtual object GetValueByReflection()
+        {
+            var target = Target as Element;
+            if (target == null)
+            {
+                MvxBindingTrace.Warning("Weak Target is null in {0} - skipping Get", GetType().Name);
+                return null;
+            }
+
+            return target.GetValue(_targetDependencyProperty);
+        }
+
+        protected override void SetValueImpl(object target, object value)
+        {
+            MvxBindingTrace.Trace(MvxTraceLevel.Diagnostic, "Receiving setValue to " + (value ?? ""));
+            var frameworkElement = target as Element;
+            if (frameworkElement == null)
+            {
+                MvxBindingTrace.Trace(MvxTraceLevel.Warning, "Weak Target is null in {0} - skipping set", GetType().Name);
+                return;
+            }
+
+            frameworkElement.SetValue(_targetDependencyProperty, value);
+        }
+
+        protected override object MakeSafeValue(object value)
+        {
+            return _actualPropertyType.MakeSafeValue(value);
+        }
+    }
+}
