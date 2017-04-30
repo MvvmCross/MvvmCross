d0234d5bf1f89d86e043ba050a15269dc46a69ee
Tomasz Cielecki <tomasz@ostebaronen.dk>
2017-04-30 11:04:34 +0200

    Move MvxViewModelInstanceRequest to core

--- MvvmCross/Core/Core/ViewModels/MvxViewModelInstanceRequest.cs
+++ MvvmCross/Core/Core/ViewModels/MvxViewModelInstanceRequest.cs
@@ -1,29 +1,20 @@
 ﻿// MvxViewModelInstanceRequest.cs
-
+//
 // MvvmCross is licensed using Microsoft Public License (Ms-PL)
 // Contributions and inspirations noted in readme.md and license.txt
-//
-// Project Lead - Stuart Lodge, @slodge, me@slodge.com
 
-namespace MvvmCross.Mac.Views
+namespace MvvmCross.Core.ViewModels
 {
-    using global::MvvmCross.Core.ViewModels;
-
-#warning Move to shared PCL code
-
     public class MvxViewModelInstanceRequest : MvxViewModelRequest
     {
         private readonly IMvxViewModel _viewModelInstance;
 
-        public IMvxViewModel ViewModelInstance
-        {
-            get { return this._viewModelInstance; }
-        }
+        public IMvxViewModel ViewModelInstance => _viewModelInstance;
 
         public MvxViewModelInstanceRequest(IMvxViewModel viewModelInstance)
             : base(viewModelInstance.GetType(), null, null)
         {
-            this._viewModelInstance = viewModelInstance;
+            _viewModelInstance = viewModelInstance;
         }
     }
 }
