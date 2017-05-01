e98f75e5d9256813753ba00851faa64392505952
Jeremy A. Kolb <jkolb@ara.com>
2017-05-01 16:37:16 -0400

    Revert "Fix null check in Adapter setter (#1733)"
    This reverts commit 851dd9bf6b3e6ef00e85ad2589551e6a04473f96.

--- MvvmCross-AndroidSupport/MvvmCross.Droid.Support.V7.RecyclerView/MvxRecyclerView.cs
+++ MvvmCross-AndroidSupport/MvvmCross.Droid.Support.V7.RecyclerView/MvxRecyclerView.cs
@@ -112,7 +112,7 @@ namespace MvvmCross.Droid.Support.V7.RecyclerView
                     var existingHeaderFooterAdapter = existing as IMvxHeaderFooterRecyclerViewAdapter;
                     var newHeaderFooterAdapter = value as IMvxHeaderFooterRecyclerViewAdapter;
 
-                    if (existingHeaderFooterAdapter != null && newHeaderFooterAdapter != null)
+                    if (existingGroupableAdapter != null && newHeaderFooterAdapter != null)
                     {
                         newHeaderFooterAdapter.FooterClickCommand = existingHeaderFooterAdapter.FooterClickCommand;
                         newHeaderFooterAdapter.HeaderClickCommand = existingHeaderFooterAdapter.HeaderClickCommand;
