5cbff6d071763b9747f4ff5b30d8a26074580e1b
Jeremy A. Kolb <jkolb@ara.com>
2017-05-01 16:42:58 -0400

    Revert "MvxRecyclerAdapter FooterClickCommand was assigned to HeaderClick (#1644)"
    This reverts commit c588f9f7d30b2cfce8f318001be0df98dce0443d.

--- MvvmCross-AndroidSupport/MvvmCross.Droid.Support.V7.RecyclerView/MvxRecyclerAdapter.cs
+++ MvvmCross-AndroidSupport/MvvmCross.Droid.Support.V7.RecyclerView/MvxRecyclerAdapter.cs
@@ -235,7 +235,7 @@ namespace MvvmCross.Droid.Support.V7.RecyclerView
                 Click = ItemClick,
                 LongClick = ItemLongClick,
                 FooterClickCommand = FooterClickCommand,
-                HeaderClickCommand = HeaderClickCommand,
+                HeaderClickCommand = FooterClickCommand,
                 GroupHeaderClickCommand = GroupHeaderClickCommand
             };
 
