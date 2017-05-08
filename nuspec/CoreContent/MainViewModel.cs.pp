6e385cbc8ce3e037e556c8c1e060685e796507b6
Martijn van Dijk <mhvdijk@gmail.com>
2017-05-08 13:44:58 +0200

    Clean new nuspec content a bit

--- nuspec/CoreContent/MainViewModel.cs.pp
+++ nuspec/CoreContent/MainViewModel.cs.pp
@@ -6,31 +6,20 @@ namespace $rootnamespace$.ViewModels
     {
         public MainViewModel()
         {
-            ResetTextCommand = new MvxCommand(ResetTextCommandExecute);
         }
-
-        // Even though there's no Init method on MvxViewModel, this method is used by
-        // the framework to pass information when navigating from one VM to another.
-        // Every ViewModel can have its own Init method, as long as they either:
-        // 1 - Implement MvxViewModel<TInit> 
-        // 2 - Follow the guidelines from this document: https://github.com/MvvmCross/MvvmCross/wiki/view-model-lifecycle
-        // If you choose the former, avoid serializing large objects
-        // If you choose the latter, avoid doing asynchronous operations on Init, prefer doing them on the Start method.
-        // public void Init(int id)
-        // {
-        //     this.id = id
-        // }
-
-        // The start method is called as the last step of the ViewModel initialization.
-        // Even though it is a void method, if you need to do any async work it should be done here.
+        
+        //TODO: For more information see: https://www.mvvmcross.com/documentation/fundamentals/navigation
+        public void Init()
+        {
+        }
+        
         public override void Start()
         {
             //TODO: Add starting logic here
         }
-
-        // Commands are a way to bind events that happen on your View to methods from the ViewModel 
-        public IMvxCommand ResetTextCommand { get; }
-        private void ResetTextCommandExecute()
+        
+        public IMvxCommand ResetTextCommand => new MvxCommand(ResetText);
+        private void ResetText()
         {
             Text = "Hello MvvmCross";
         }
