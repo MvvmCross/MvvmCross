7ea053babcb70bb8acbc3b8a8b6212351f305446
Daniel Stolt <daniel@ideliverable.com>
2017-05-18 08:11:29 +0200

    Added missing calls to view model lifecycle methods. Fixes #1813. (#1816)

--- MvvmCross/iOS/iOS/Views/MvxTableViewController.cs
+++ MvvmCross/iOS/iOS/Views/MvxTableViewController.cs
@@ -1,4 +1,4 @@
-// MvxTableViewController.cs
+﻿// MvxTableViewController.cs
 
 // MvvmCross is licensed using Microsoft Public License (Ms-PL)
 // Contributions and inspirations noted in readme.md and license.txt
@@ -79,6 +79,37 @@ namespace MvvmCross.iOS.Views
         public MvxViewModelRequest Request { get; set; }
 
         public IMvxBindingContext BindingContext { get; set; }
+
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
+            ViewModel?.Appearing();
+        }
+
+        public override void ViewDidAppear(bool animated)
+        {
+            base.ViewDidAppear(animated);
+            ViewModel?.Appeared();
+        }
+
+        public override void ViewWillDisappear(bool animated)
+        {
+            base.ViewWillDisappear(animated);
+            ViewModel?.Disappearing();
+        }
+
+        public override void ViewDidDisappear(bool animated)
+        {
+            base.ViewDidDisappear(animated);
+            ViewModel?.Disappeared();
+        }
+
+        public override void DidMoveToParentViewController(UIViewController parent)
+        {
+            base.DidMoveToParentViewController(parent);
+            if (parent == null)
+                ViewModel?.Destroy();
+        }
 
         public override void PrepareForSegue(UIStoryboardSegue segue, NSObject sender)
         {
