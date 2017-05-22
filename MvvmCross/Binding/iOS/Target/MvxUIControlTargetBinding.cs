4f2d6bfe02b4dac8d41cf76b4991fa52c3abb1a1
Thom Ritterfeld <thom@ritterfeld.com>
2017-05-22 11:56:00 +0200

    Updated documentation UIKit.UIControl

--- MvvmCross/Binding/iOS/Target/MvxUIControlTargetBinding.cs
+++ MvvmCross/Binding/iOS/Target/MvxUIControlTargetBinding.cs
@@ -1,4 +1,4 @@
-// MvxUIControlTouchUpInsideTargetBinding.cs
+// MvxUIControlTargetBinding.cs
 
 // MvvmCross is licensed using Microsoft Public License (Ms-PL)
 // Contributions and inspirations noted in readme.md and license.txt
