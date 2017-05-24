1429d8dc144dcbf928105899bc9770ba75a8be5f
Martijn van Dijk <mhvdijk@gmail.com>
2017-05-24 13:40:59 +0200

    Fix not being able to use NavigationService from the appstart

---
+++ TestProjects/Navigation/RoutingExample.Core/AppStart.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using MvvmCross.Core.Navigation;
+using MvvmCross.Core.ViewModels;
+using RoutingExample.Core.ViewModels;
+
+namespace RoutingExample.Core
+{
+    public class AppStart : IMvxAppStart
+    {
+        private readonly IMvxNavigationService _navigationService;
+
+        public AppStart(IMvxNavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+
+        public void Start(object hint = null)
+        {
+            _navigationService.Navigate<MainViewModel>();
+        }
+    }
+}
