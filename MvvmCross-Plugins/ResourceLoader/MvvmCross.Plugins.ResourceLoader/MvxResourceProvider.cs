46e054c5b623cb5822aff4edca31b49bceb5032d
Sylvain Gravel <sgravel@lerondpoint.net>
2017-06-08 13:52:32 -0400

    Moved the resource name generation to MvxResourceProvider and implemented correct behavior as (un)documented

--- MvvmCross-Plugins/ResourceLoader/MvvmCross.Plugins.ResourceLoader/MvxResourceProvider.cs
+++ MvvmCross-Plugins/ResourceLoader/MvvmCross.Plugins.ResourceLoader/MvxResourceProvider.cs
@@ -5,6 +5,9 @@
 //
 // Project Lead - Stuart Lodge, @slodge, me@slodge.com
 
+using System.Linq;
+using System.Text;
+
 namespace MvvmCross.Plugins.ResourceLoader
 {
     public abstract class MvxResourceProvider
@@ -17,6 +20,43 @@ namespace MvvmCross.Plugins.ResourceLoader
         protected static string MakeLookupKey(string namespaceKey, string typeKey, string name)
         {
             return $"{namespaceKey}|{typeKey}|{name}";
+        }
+
+        protected static string GenerateResourceNameFromPath(string path)
+        {
+            char[] invalidChars = { ' ', '\u00A0', '.', ',', ';',
+                                    '|', '~', '@', '#', '%',
+                                    '^', '&', '*', '+', '-',
+                                    '/', '\\', '<', '>', '?',
+                                    '[', ']', '(', ')', '{',
+                                    '}', '"', '\'', ':', '!' };
+            StringBuilder builder = new StringBuilder();
+
+            if (string.IsNullOrEmpty(path))
+            {
+                builder.Append("_");
+            }
+            else
+            {
+                string[] pathParts = path.Replace('/', '.').Split('.');
+
+                foreach (string part in pathParts)
+                {
+                    if (part.Length > 0)
+                    {
+                        if (part[0] >= '0' && part[0] <= '9')
+                        {
+                            builder.Append('_');
+                        }
+
+                        builder.Append(string.Join("_", part.Split(invalidChars)));
+                        builder.Append('.');
+                    }
+                }
+            }
+
+            builder.Remove(builder.Length - 1, 1);
+            return builder.ToString();
         }
     }
 }
