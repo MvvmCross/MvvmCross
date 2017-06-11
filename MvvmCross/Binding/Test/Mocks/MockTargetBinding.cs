08eac120da39b6dfe0b38284947f9434d35a7e79
Tomasz Cielecki <tomasz@ostebaronen.dk>
2017-06-11 21:58:44 +0200

    Move Source, Target and ValueConverter mocks into own classes

---
+++ MvvmCross/Binding/Test/Mocks/MockTargetBinding.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using MvvmCross.Binding.Bindings.Target;
+
+namespace MvvmCross.Binding.Test.Mocks
+{
+    public class MockTargetBinding : IMvxTargetBinding
+    {
+        public MockTargetBinding()
+        {
+            TargetType = typeof(object);
+        }
+
+        public int DisposeCalled { get; set; }
+
+        public void Dispose()
+        {
+            DisposeCalled++;
+        }
+
+        public int SubscribeToEventsCalled { get; set; }
+
+        public void SubscribeToEvents()
+        {
+            SubscribeToEventsCalled++;
+        }
+
+        public Type TargetType { get; set; }
+        public MvxBindingMode DefaultMode { get; set; }
+
+        public List<object> Values { get; } = new List<object>();
+
+        public void SetValue(object value)
+        {
+            Values.Add(value);
+        }
+
+        public void FireValueChanged(MvxTargetChangedEventArgs args)
+        {
+            ValueChanged?.Invoke(this, args);
+        }
+
+        public event EventHandler<MvxTargetChangedEventArgs> ValueChanged;
+    }
+}
---
+++ MvvmCross/Binding/Test/Mocks/MockValueConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using MvvmCross.Platform.Converters;
+using MvvmCross.Platform.Exceptions;
+
+namespace MvvmCross.Binding.Test.Mocks
+{
+    public class MockValueConverter : IMvxValueConverter
+    {
+        public object ConversionResult { get; set; }
+        public bool ThrowOnConversion { get; set; }
+
+        public List<object> ConversionsRequested { get; } = new List<object>();
+        public List<object> ConversionParameters { get; } = new List<object>();
+        public List<Type> ConversionTypes { get; } = new List<Type>();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            ConversionsRequested.Add(value);
+            ConversionParameters.Add(parameter);
+            ConversionTypes.Add(targetType);
+            if(ThrowOnConversion)
+                throw new MvxException("Conversion throw requested");
+            return ConversionResult;
+        }
+
+        public object ConversionBackResult { get; set; }
+        public bool ThrowOnConversionBack { get; set; }
+        public List<object> ConversionsBackRequested { get; } = new List<object>();
+        public List<object> ConversionBackParameters { get; } = new List<object>();
+        public List<Type> ConversionBackTypes { get; } = new List<Type>();
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            ConversionsBackRequested.Add(value);
+            ConversionBackParameters.Add(parameter);
+            ConversionBackTypes.Add(targetType);
+            if(ThrowOnConversionBack)
+                throw new MvxException("Conversion throw requested");
+            return ConversionBackResult;
+        }
+    }
+}
