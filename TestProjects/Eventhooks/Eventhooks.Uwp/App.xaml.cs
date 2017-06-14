91bfc4b965e8cda62e0961c51c15eac28fa8bbe1
Bowman74 <bowman74@hotmail.com>
2017-06-13 20:15:16 -0400

    #1920 Remove unneeded comment

--- TestProjects/Eventhooks/Eventhooks.Uwp/App.xaml.cs
+++ TestProjects/Eventhooks/Eventhooks.Uwp/App.xaml.cs
@@ -55,11 +55,6 @@ namespace Eventhooks.Uwp
             {
                 if (rootFrame.Content == null)
                 {
-                    // When the navigation stack isn't restored navigate to the first page,
-                    // configuring the new page by passing required information as a navigation
-                    // parameter
-                    //rootFrame.Navigate(typeof(MainPage), e.Arguments);
-
                     var setup = new Setup(rootFrame);
                     setup.Initialize();
 
