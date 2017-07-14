80ef9b903acec0e541259ae058744892876cd405
Martijn van Dijk <mhvdijk@gmail.com>
2017-07-14 11:51:05 +0200

    Use ViewModelLocator to load view models

--- MvvmCross/Core/Core/ViewModels/MvxViewModelInstanceRequest.cs
+++ MvvmCross/Core/Core/ViewModels/MvxViewModelInstanceRequest.cs
@@ -3,18 +3,23 @@
 // MvvmCross is licensed using Microsoft Public License (Ms-PL)
 // Contributions and inspirations noted in readme.md and license.txt
 
+using System;
+
 namespace MvvmCross.Core.ViewModels
 {
     public class MvxViewModelInstanceRequest : MvxViewModelRequest
     {
-        private readonly IMvxViewModel _viewModelInstance;
+        public IMvxViewModel ViewModelInstance { get; set;  }
 
-        public IMvxViewModel ViewModelInstance => _viewModelInstance;
+        public MvxViewModelInstanceRequest(Type viewModelType)
+            : base(viewModelType)
+        {
+        }
 
         public MvxViewModelInstanceRequest(IMvxViewModel viewModelInstance)
             : base(viewModelInstance.GetType(), null, null)
         {
-            _viewModelInstance = viewModelInstance;
+            ViewModelInstance = viewModelInstance;
         }
     }
 }
