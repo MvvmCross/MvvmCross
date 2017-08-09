cdd26d02b12fe3953ef4674ef2801020ce00e87d
Martijn van Dijk <mhvdijk@gmail.com>
2017-08-09 16:39:50 +0200

    Use fragment transitions for dialogs

--- MvvmCross-AndroidSupport/MvvmCross.Droid.Support.V7.AppCompat/MvxAppCompatViewPresenter.cs
+++ MvvmCross-AndroidSupport/MvvmCross.Droid.Support.V7.AppCompat/MvxAppCompatViewPresenter.cs
@@ -201,7 +201,32 @@ namespace MvvmCross.Droid.Support.V7.AppCompat
             dialog.Cancelable = attribute.Cancelable;
 
             Dialogs.Add(viewModel, dialog);
-            dialog.Show(CurrentFragmentManager, fragmentName);
+
+            var ft = CurrentFragmentManager.BeginTransaction();
+            if (attribute.SharedElements != null)
+            {
+                foreach (var item in attribute.SharedElements)
+                {
+                    string name = item.Key;
+                    if (string.IsNullOrEmpty(name))
+                        name = ViewCompat.GetTransitionName(item.Value);
+                    ft.AddSharedElement(item.Value, name);
+                }
+            }
+            if (!attribute.EnterAnimation.Equals(int.MinValue) && !attribute.ExitAnimation.Equals(int.MinValue))
+            {
+                if (!attribute.PopEnterAnimation.Equals(int.MinValue) && !attribute.PopExitAnimation.Equals(int.MinValue))
+                    ft.SetCustomAnimations(attribute.EnterAnimation, attribute.ExitAnimation, attribute.PopEnterAnimation, attribute.PopExitAnimation);
+                else
+                    ft.SetCustomAnimations(attribute.EnterAnimation, attribute.ExitAnimation);
+            }
+            if (attribute.TransitionStyle != int.MinValue)
+                ft.SetTransitionStyle(attribute.TransitionStyle);
+
+            if (attribute.AddToBackStack == true)
+                ft.AddToBackStack(fragmentName);
+
+            dialog.Show(ft, fragmentName);
         }
 
         protected virtual void ShowTab(
--- MvvmCross/Droid/Droid/Views/Attributes/MvxDialogAttribute.cs
+++ MvvmCross/Droid/Droid/Views/Attributes/MvxDialogAttribute.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using Android.Views;
 using MvvmCross.Core.Views;
 
 namespace MvvmCross.Droid.Views.Attributes
@@ -7,5 +9,25 @@ namespace MvvmCross.Droid.Views.Attributes
     public class MvxDialogAttribute : MvxBasePresentationAttribute
     {
         public bool Cancelable { get; set; } = true;
+
+        /// <summary>
+        /// Indicates if the fragment can be cached. False by default.
+        /// </summary>
+        public bool AddToBackStack { get; set; } = false;
+
+        public int EnterAnimation { get; set; } = int.MinValue;
+
+        public int ExitAnimation { get; set; } = int.MinValue;
+
+        public int PopEnterAnimation { get; set; } = int.MinValue;
+
+        public int PopExitAnimation { get; set; } = int.MinValue;
+
+        public int TransitionStyle { get; set; } = int.MinValue;
+
+        /// <summary>
+        /// SharedElements that will be added to the transition. String may be left empty when using AppCompat
+        /// </summary>
+        public IDictionary<string, View> SharedElements { get; set; }
     }
 }
