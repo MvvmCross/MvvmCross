0d472c74ca2db31f4eb4803e5b1b7226917474fe
jz5 <jz5@hotmail.com>
2017-08-18 21:00:10 +0900

    Add WPF playground

---
+++ TestProjects/Playground/Playground.Wpf/Setup.cs
@@ -0,0 +1,19 @@
+﻿using MvvmCross.Core.ViewModels;
+using MvvmCross.Wpf.Platform;
+using MvvmCross.Wpf.Views.Presenters;
+using System.Windows.Threading;
+
+namespace Playground.Wpf
+{
+    public class Setup : MvxWpfSetup
+    {
+        public Setup(Dispatcher uiThreadDispatcher, IMvxWpfViewPresenter presenter) : base(uiThreadDispatcher, presenter)
+        {
+        }
+
+        protected override IMvxApplication CreateApp()
+        {
+            return new Core.App();
+        }
+    }
+}
