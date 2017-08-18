6ee8c096f297c9a72be3ab47cae0f5c31108eae6
jz5 <jz5@hotmail.com>
2017-08-18 21:00:10 +0900

    Add WPF playground

---
+++ TestProjects/Playground/Playground.Wpf/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using MvvmCross.Core.ViewModels;
+using MvvmCross.Platform;
+using MvvmCross.Wpf.Views.Presenters;
+using System;
+using System.Windows;
+
+namespace Playground.Wpf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        bool _setupComplete = false;
+
+        void DoSetup()
+        {
+            var presenter = new MvxWpfViewPresenter(MainWindow);
+
+            var setup = new Setup(Dispatcher, presenter);
+            setup.Initialize();
+
+            var start = Mvx.Resolve<IMvxAppStart>();
+            start.Start();
+
+            _setupComplete = true;
+        }
+
+        protected override void OnActivated(EventArgs e)
+        {
+            if (!_setupComplete)
+                DoSetup();
+
+            base.OnActivated(e);
+        }
+    }
+}
