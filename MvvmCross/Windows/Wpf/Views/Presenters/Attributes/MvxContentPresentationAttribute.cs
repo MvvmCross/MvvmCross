ab1b28dc541e3bc41e4709061544d4a2a329d766
jz5 <jz5@hotmail.com>
2017-08-23 17:43:09 +0900

    Update WPF presenter

--- MvvmCross/Windows/Wpf/Views/Presenters/Attributes/MvxContentPresentationAttribute.cs
+++ MvvmCross/Windows/Wpf/Views/Presenters/Attributes/MvxContentPresentationAttribute.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using MvvmCross.Core.Views;
+﻿using MvvmCross.Core.Views;
 
 namespace MvvmCross.Wpf.Views.Presenters.Attributes
 {
     public class MvxContentPresentationAttribute : MvxBasePresentationAttribute
     {
         public string WindowIdentifier { get; set; }
+        public bool StackNavigation { get; set; } = true;
     }
 }
