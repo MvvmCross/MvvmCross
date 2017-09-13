9d52c4caf60c0f9f7513f2a7b7725da3d275107f
Tim Uy <tim@loqu8.com>
2017-09-13 12:02:31 -0700

    add MvxStoryboardWindowPresentation for Mac

---
+++ MvvmCross/Mac/Mac/Views/Presenters/Attributes/MvxStoryboardWindowPresentationAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using AppKit;
+using MvvmCross.Core.Views;
+
+namespace MvvmCross.Mac.Views.Presenters.Attributes
+{
+    public class MvxStoryboardWindowPresentationAttribute : MvxBasePresentationAttribute
+    {
+        public MvxStoryboardWindowPresentationAttribute(string windowControllerName, string storyboardName)
+        {
+            this.WindowControllerName = windowControllerName;
+            this.StoryboardName = storyboardName;
+        }
+
+        public string StoryboardName { get; set; }
+
+        public string WindowControllerName { get; set; }
+
+        public string Identifier { get; set; }
+    }
+}
