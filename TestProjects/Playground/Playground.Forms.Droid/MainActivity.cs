efb73ea5e56dc0588e4a7927f677e1331f51bf39
Martijn van Dijk <mhvdijk@gmail.com>
2017-09-15 15:28:20 +0200

    Add Playground projects for Forms

---
+++ TestProjects/Playground/Playground.Forms.Droid/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using MvvmCross.Droid.Views;
+using MvvmCross.Forms.Droid;
+using MvvmCross.Forms.Presenters;
+using MvvmCross.Platform;
+
+namespace Playground.Forms.Droid
+{
+    [Activity(Label = "Playground.Forms.Droid.Droid", Icon = "@drawable/icon", Theme = "@style/MainTheme", 
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : MvxFormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            //TabLayoutResource = Resource.Layout.Tabbar;
+            //ToolbarResource = Resource.Layout.Toolbar;
+        }
+    }
+}
