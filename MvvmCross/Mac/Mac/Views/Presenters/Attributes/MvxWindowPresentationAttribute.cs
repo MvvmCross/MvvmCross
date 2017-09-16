0426281b869aed1bc5f98884e671d050f10de2d0
Tim Uy <tim@loqu8.com>
2017-09-16 08:11:54 -0700

    demo window attribute default values, restrict changes to initial value

--- MvvmCross/Mac/Mac/Views/Presenters/Attributes/MvxWindowPresentationAttribute.cs
+++ MvvmCross/Mac/Mac/Views/Presenters/Attributes/MvxWindowPresentationAttribute.cs
@@ -6,23 +6,23 @@ namespace MvvmCross.Mac.Views.Presenters.Attributes
 {
     public class MvxWindowPresentationAttribute : MvxBasePresentationAttribute
     {
-        public static float DefaultPositionX = 200;
-        public static float DefaultPositionY = 200;
-        public static float DefaultWidth = 600;
-        public static float DefaultHeight = 400;
-        public static NSWindowStyle DefaultWindowStyle = NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Titled;
-        public static NSBackingStore DefaultBufferingType = NSBackingStore.Buffered;
-        public static NSWindowTitleVisibility DefaultTitleVisibility = NSWindowTitleVisibility.Visible;
-        public static bool DefaultShouldCascadeWindows = true;
+        public static float DefaultPositionX { get; set; } = 200;
+        public static float DefaultPositionY { get; set; } = 200;
+        public static float DefaultWidth { get; set; } = 600;
+        public static float DefaultHeight { get; set; } = 400;
+        public static NSWindowStyle DefaultWindowStyle { get; set; } = NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Titled;
+        public static NSBackingStore DefaultBufferingType { get; set; } = NSBackingStore.Buffered;
+        public static NSWindowTitleVisibility DefaultTitleVisibility { get; set; } = NSWindowTitleVisibility.Visible;
+        public static bool DefaultShouldCascadeWindows { get; set; } = true;
 
-        public static float InitialPositionX = float.MinValue;
-        public static float InitialPositionY = float.MinValue;
-        public static float InitialWidth = float.MinValue;
-        public static float InitialHeight = float.MinValue;
-        public static NSWindowStyle? InitialWindowStyle = null;
-        public static NSBackingStore? InitialBufferingType = null;
-        public static NSWindowTitleVisibility? InitialTitleVisibilty = null;
-        public static bool? InitialShouldCascadeWindows = null;
+        public static float InitialPositionX { get; } = float.MinValue;
+        public static float InitialPositionY { get; } = float.MinValue;
+        public static float InitialWidth { get; } = float.MinValue;
+        public static float InitialHeight { get; } = float.MinValue;
+        public static NSWindowStyle? InitialWindowStyle { get; } = null;
+        public static NSBackingStore? InitialBufferingType { get; } = null;
+        public static NSWindowTitleVisibility? InitialTitleVisibilty { get; } = null;
+        public static bool? InitialShouldCascadeWindows { get; } = null;
 
         public MvxWindowPresentationAttribute(string windowControllerName = null, string storyboardName = null)
         {
