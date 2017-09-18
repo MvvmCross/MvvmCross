3631caaed4f64e2699d2575c2650c307be1b9c01
Martijn van Dijk <mhvdijk@gmail.com>
2017-09-18 11:36:20 +0200

    Fix example projects

--- TestProjects/Playground/Playground.Forms.iOS/AppDelegate.cs
+++ TestProjects/Playground/Playground.Forms.iOS/AppDelegate.cs
@@ -1,59 +1,31 @@
-﻿using Foundation;
+using Foundation;
+using MvvmCross.Core.ViewModels;
+using MvvmCross.Forms.iOS;
+using MvvmCross.Platform;
 using UIKit;
 
 namespace Playground.Forms.iOS
 {
-    // The UIApplicationDelegate for the application. This class is responsible for launching the
-    // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
     [Register("AppDelegate")]
-    public class AppDelegate : UIApplicationDelegate
+    public partial class AppDelegate : MvxFormsApplicationDelegate
     {
-        // class-level declarations
+        public override UIWindow Window { get; set; }
 
-        public override UIWindow Window
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
-            get;
-            set;
-        }
+            Window = new UIWindow(UIScreen.MainScreen.Bounds);
 
-        public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
-        {
-            // Override point for customization after application launch.
-            // If not required for your application you can safely delete this method
+            var setup = new Setup(this, Window);
+            setup.Initialize();
+
+            var startup = Mvx.Resolve<IMvxAppStart>();
+            startup.Start();
+
+            LoadApplication(setup.FormsApplication);
+
+            Window.MakeKeyAndVisible();
 
             return true;
         }
-
-        public override void OnResignActivation(UIApplication application)
-        {
-            // Invoked when the application is about to move from active to inactive state.
-            // This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) 
-            // or when the user quits the application and it begins the transition to the background state.
-            // Games should use this method to pause the game.
-        }
-
-        public override void DidEnterBackground(UIApplication application)
-        {
-            // Use this method to release shared resources, save user data, invalidate timers and store the application state.
-            // If your application supports background exection this method is called instead of WillTerminate when the user quits.
-        }
-
-        public override void WillEnterForeground(UIApplication application)
-        {
-            // Called as part of the transiton from background to active state.
-            // Here you can undo many of the changes made on entering the background.
-        }
-
-        public override void OnActivated(UIApplication application)
-        {
-            // Restart any tasks that were paused (or not yet started) while the application was inactive. 
-            // If the application was previously in the background, optionally refresh the user interface.
-        }
-
-        public override void WillTerminate(UIApplication application)
-        {
-            // Called when the application is about to terminate. Save data, if needed. See also DidEnterBackground.
-        }
     }
 }
-
