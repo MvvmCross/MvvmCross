c8ed0694616637e80bfe037e12a261235cb23918
Marc Bruins <marc@marcbruins.nl>
2017-09-21 09:53:52 +0200

    mvxformstliview with "ItemClick command"

---
+++ MvvmCross-Forms/MvvmCross.Forms/Views/MvxFormsListView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MvvmCross.Forms.Views
+{
+    public class MvxFormsListView : ListView
+    {
+        public ICommand ItemClick
+        {
+            get;
+            set;
+        } = new Command(() => { });
+
+        public MvxFormsListView(ListViewCachingStrategy strategy) : base(strategy)
+        {
+
+        }
+
+        public MvxFormsListView() : base(ListViewCachingStrategy.RetainElement)
+        {
+
+        }
+    }
+}
