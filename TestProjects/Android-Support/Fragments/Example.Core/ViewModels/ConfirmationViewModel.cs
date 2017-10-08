8956e33bff4f2c23db93db9fa71b99aff2b31012
nmilcoff <nicolasmilcoff@hotmail.com.ar>
2017-10-08 20:43:46 -0300

    Update Android-Support example: Add dialog fragment to test NavigationService close

---
+++ TestProjects/Android-Support/Fragments/Example.Core/ViewModels/ConfirmationViewModel.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Threading.Tasks;
+using MvvmCross.Core.Navigation;
+using MvvmCross.Core.ViewModels;
+
+namespace Example.Core.ViewModels
+{
+    public class ConfirmationViewModel : MvxViewModel<ConfirmationConfiguration, bool?>
+    {
+        private readonly IMvxNavigationService _mvxNavigationService;
+
+        public ConfirmationViewModel(IMvxNavigationService mvxNavigationService)
+        {
+            _mvxNavigationService = mvxNavigationService;
+        }
+
+        public override void Prepare(ConfirmationConfiguration parameter)
+        {
+            if (parameter == null)
+                throw new ArgumentNullException(nameof(parameter));
+
+            Title = parameter.Title;
+            Body = parameter.Body;
+            PositiveCommandText = parameter.PositiveCommandText;
+            NegativeCommandText = parameter.NegativeCommandText;
+        }
+
+        private string _title;
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+
+        private string _body;
+        public string Body
+        {
+            get => _body;
+            set => SetProperty(ref _body, value);
+        }
+
+        private string _positiveCommandText;
+        public string PositiveCommandText
+        {
+            get => _positiveCommandText;
+            set => SetProperty(ref _positiveCommandText, value);
+        }
+
+        private string _negativeCommandText;
+        public string NegativeCommandText
+        {
+            get => _negativeCommandText;
+            set => SetProperty(ref _negativeCommandText, value);
+        }
+
+        private IMvxAsyncCommand _yesCommand;
+        public IMvxAsyncCommand PositiveCommand => _yesCommand ?? (_yesCommand = new MvxAsyncCommand(OnPositiveCommandAsync));
+
+        private Task OnPositiveCommandAsync()
+        {
+            return _mvxNavigationService.Close(this, true);
+        }
+
+        private IMvxAsyncCommand _noCommand;
+        public IMvxAsyncCommand NegativeCommand => _noCommand ?? (_noCommand = new MvxAsyncCommand(OnNegativeCommandAsync));
+
+        private Task OnNegativeCommandAsync()
+        {
+            return _mvxNavigationService.Close(this, false);
+        }
+    }
+
+    public class ConfirmationConfiguration
+    {
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string PositiveCommandText { get; set; }
+        public string NegativeCommandText { get; set; }
+    }
+
+
+}
