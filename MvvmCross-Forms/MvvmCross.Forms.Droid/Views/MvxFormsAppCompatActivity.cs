dcaa9a9159c66efc695da7f3523ad3822fbc84f4
Martijn van Dijk <mhvdijk@gmail.com>
2017-10-19 15:04:41 +0200

    Add generic Forms activities

--- MvvmCross-Forms/MvvmCross.Forms.Droid/Views/MvxFormsAppCompatActivity.cs
+++ MvvmCross-Forms/MvvmCross.Forms.Droid/Views/MvxFormsAppCompatActivity.cs
@@ -136,4 +136,15 @@ namespace MvvmCross.Forms.Droid.Views
             LifetimeListener.OnRestart(this);
         }
     }
+
+    public class MvxFormsAppCompatActivity<TViewModel>
+        : MvxFormsAppCompatActivity
+    , IMvxAndroidView<TViewModel> where TViewModel : class, IMvxViewModel
+    {
+        public new TViewModel ViewModel
+        {
+            get { return (TViewModel)base.ViewModel; }
+            set { base.ViewModel = value; }
+        }
+    }
 }
