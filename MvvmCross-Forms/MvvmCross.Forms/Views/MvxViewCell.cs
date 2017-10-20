e2abb650c5d6ba15ba182b195fbe34bb1a0aa22c
Martijn van Dijk <mhvdijk@gmail.com>
2017-10-20 15:24:38 +0200

    Add cell types

--- MvvmCross-Forms/MvvmCross.Forms/Views/MvxViewCell.cs
+++ MvvmCross-Forms/MvvmCross.Forms/Views/MvxViewCell.cs
@@ -5,7 +5,7 @@ using Xamarin.Forms;
 
 namespace MvvmCross.Forms.Views
 {
-    public class MvxBoxView : BoxView, IMvxElement
+    public class MvxViewCell : ViewCell, IMvxElement
     {
         public object DataContext
         {
@@ -53,8 +53,8 @@ namespace MvvmCross.Forms.Views
         }
     }
 
-    public class MvxBoxView<TViewModel>
-        : MvxBoxView
+    public class MvxViewCell<TViewModel>
+        : MvxViewCell
     , IMvxElement<TViewModel> where TViewModel : class, IMvxViewModel
     {
         public new TViewModel ViewModel
