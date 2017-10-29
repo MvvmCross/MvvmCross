9dfea683fdc1b88b5a8f3b981156f11463286c49
Martijn van Dijk <mhvdijk@gmail.com>
2017-10-29 12:18:52 +0100

    Add design time checker to Forms bindings

---
+++ MvvmCross-Forms/MvvmCross.Forms/Bindings/MvxDesignTimeChecker.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using MvvmCross.Binding;
+using MvvmCross.Binding.Parse.Binding;
+using MvvmCross.Platform;
+using MvvmCross.Platform.Core;
+using MvvmCross.Platform.IoC;
+using Xamarin.Forms;
+
+namespace MvvmCross.Forms.Bindings
+{
+    public static class MvxDesignTimeChecker
+    {
+        private static bool _checked;
+
+        public static void Check()
+        {
+            if (_checked)
+                return;
+
+            _checked = true;
+
+            // if Application.Current == null Forms is in design mode
+            if (Application.Current != null)
+                return;
+
+            if (MvxSingleton<IMvxIoCProvider>.Instance == null)
+            {
+                var iocProvider = MvxSimpleIoCContainer.Initialize();
+                Mvx.RegisterSingleton(iocProvider);
+            }
+
+            if (!Mvx.CanResolve<IMvxBindingParser>())
+            {
+                //We might want to look into returning the platform specific Forms binder
+                var builder = new MvxBindingBuilder();
+                builder.DoRegistration();
+            }
+        }
+    }
+}
