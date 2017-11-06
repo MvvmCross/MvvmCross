a6f5954fdfc334eb66e62b53ae215508f2f0ba42
Martijn van Dijk <mhvdijk@gmail.com>
2017-11-06 20:05:30 +0100

    Add event source for Forms views (#2357)
    * Add event source for pages
    
    * Add adapter for cells
    
    * Try to load viewmodel for element
    
    * Remove empty line ends of files
    
    * Remove whiteline

--- MvvmCross-Forms/MvvmCross.Forms/Views/MvxPage.cs
+++ MvvmCross-Forms/MvvmCross.Forms/Views/MvxPage.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using MvvmCross.Binding.BindingContext;
 using MvvmCross.Core.ViewModels;
+using MvvmCross.Forms.Views.EventSource;
 using Xamarin.Forms;
 
 namespace MvvmCross.Forms.Views
 {
-    public class MvxPage : Page, IMvxPage
+    public class MvxPage : MvxEventSourcePage, IMvxPage
     {
+        public MvxPage()
+        {
+            this.AdaptForBinding();
+        }
+
         public object DataContext
         {
             get
