fcd0a010c844562f8738baba1f01c9f8d844ff86
MichaelBTR <michael@builttoroam.com>
2017-11-08 15:44:21 +1100

    Updates for UWP support (#3)
    * Adding missing EmptyVoidLog files
    
    * updates to playground UWP app to include splitview and updates to presentation attributes and handling them in UWP
    
    * Starting the refactoring of presenters into a common base attribute presenter
    
    * refactor UWP view presenter and clean up playground sample XAML
    
    * remove unused code in UWP view presenter and SplitDetailView XAML
    
    * fix android view presenter
    
    * Adding missing EmptyVoidLog files
    
    * updates to playground UWP app to include splitview and updates to presentation attributes and handling them in UWP
    
    * Starting the refactoring of presenters into a common base attribute presenter
    
    * refactor UWP view presenter and clean up playground sample XAML
    
    * remove unused code in UWP view presenter and SplitDetailView XAML
    
    * fix android view presenter
    
    * remove coderush files
    
    * revert ios csproject changes
    
    * revert file encoding change for MvxViewPresenter

--- TestProjects/Playground/Playground.Forms.Uwp/MainPage.xaml.cs
+++ TestProjects/Playground/Playground.Forms.Uwp/MainPage.xaml.cs
@@ -2,7 +2,6 @@ using MvvmCross.Core.ViewModels;
 using MvvmCross.Core.Views;
 using MvvmCross.Forms.Uwp.Presenters;
 using MvvmCross.Platform;
-using Xamarin.Forms.Platform.UWP;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -11,12 +10,12 @@ namespace Playground.Forms.Uwp
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : WindowsPage
+    public sealed partial class MainPage
     {
         public MainPage()
         {
             this.InitializeComponent();
-           
+
             InitializeComponent();
 
             var start = Mvx.Resolve<IMvxAppStart>();
