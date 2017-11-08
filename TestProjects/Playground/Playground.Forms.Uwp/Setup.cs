167a34ae02cf21bf97e60eb668637b2ffeea1eca
Nick Randolph <nick@builttoroam.com>
2017-11-08 22:10:14 +1100

    Fixing codefactor issues

--- TestProjects/Playground/Playground.Forms.Uwp/Setup.cs
+++ TestProjects/Playground/Playground.Forms.Uwp/Setup.cs
@@ -1,6 +1,10 @@
 using MvvmCross.Core.ViewModels;
 using MvvmCross.Forms.Platform;
 using MvvmCross.Forms.Uwp;
+using MvvmCross.Forms.Uwp.Presenters;
+using MvvmCross.Platform;
+using MvvmCross.Uwp.Views;
+using XamlControls = Windows.UI.Xaml.Controls;
 using MvvmCross.Platform.Logging;
 using Windows.ApplicationModel.Activation;
 using XamlControls = Windows.UI.Xaml.Controls;
