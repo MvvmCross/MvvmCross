e06272db7cd00030afb41e8011f63191324509a5
Nick Randolph <nick@builttoroam.com>
2017-11-17 17:16:13 +1100

    Removing RegionId

--- TestProjects/Playground/Playground.Forms.UI/Pages/MixedNavTab1Page.xaml.cs
+++ TestProjects/Playground/Playground.Forms.UI/Pages/MixedNavTab1Page.xaml.cs
@@ -1,18 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using MvvmCross.Forms.Views;
 using MvvmCross.Forms.Views.Attributes;
 using Playground.Core.ViewModels;
-using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Playground.Forms.Pages
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    [MvxTabbedPagePresentation(WrapInNavigationPage = false, Title = "Tab1", RegionId ="tab1")]
+    [MvxTabbedPagePresentation(WrapInNavigationPage = false, Title = "Tab1")]
 
     public partial class MixedNavTab1Page : MvxContentPage<MixedNavTab1ViewModel>
     {
