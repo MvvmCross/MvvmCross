c22f8e0ba7e3094b74e6dba60d02e0f84e3ec06d
Aaron LaBeau <alabeau@gmail.com>
2017-12-06 00:26:31 -0500

    changes to add split view support

---
+++ MvvmCross/tvOS/tvOS/Views/IMvxSplitViewController.cs
@@ -0,0 +1,14 @@
+﻿using UIKit;
+using MvvmCross.Core.ViewModels;
+
+namespace MvvmCross.tvOS.Views
+{
+    public interface IMvxSplitViewController
+    {
+        void ShowMasterView(UIViewController viewController, bool wrapInNavigationController);
+
+        void ShowDetailView(UIViewController viewController, bool wrapInNavigationController);
+
+        bool CloseChildViewModel(IMvxViewModel viewModel);
+    }
+}
