c2fb25da7d10cfd4341905459557e0343d1c93c4
Aaron LaBeau <alabeau@gmail.com>
2017-12-06 20:49:04 -0500

    updated master view in playground to properly navigate

--- TestProjects/Playground/Playground.TvOS/Views/SplitMasterView.designer.cs
+++ TestProjects/Playground/Playground.TvOS/Views/SplitMasterView.designer.cs
@@ -1,20 +1,42 @@
 // WARNING
 //
-// This file has been generated automatically by Visual Studio from the outlets and
-// actions declared in your storyboard file.
-// Manual changes to this file will not be maintained.
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
 //
 using Foundation;
-using System;
 using System.CodeDom.Compiler;
 
 namespace Playground.TvOS
 {
-    [Register ("SplitMasterView")]
-    partial class SplitMasterView
-    {
-        void ReleaseDesignerOutlets ()
-        {
-        }
-    }
+	[Register ("SplitMasterView")]
+	partial class SplitMasterView
+	{
+		[Outlet]
+		UIKit.UIButton btnDetail { get; set; }
+
+		[Outlet]
+		UIKit.UIButton btnDetailNav { get; set; }
+
+		[Outlet]
+		UIKit.UIButton btnStackNav { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (btnDetailNav != null) {
+				btnDetailNav.Dispose ();
+				btnDetailNav = null;
+			}
+
+			if (btnStackNav != null) {
+				btnStackNav.Dispose ();
+				btnStackNav = null;
+			}
+
+			if (btnDetail != null) {
+				btnDetail.Dispose ();
+				btnDetail = null;
+			}
+		}
+	}
 }
