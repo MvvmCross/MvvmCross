702425b7446efc1b8eb9909fcd103a1e4f79de73
Aaron LaBeau <alabeau@gmail.com>
2017-12-09 09:26:16 -0500

    changes requested by martijn00 to clean up the presentation framework for SplitView (Master/Detail)

--- MvvmCross/tvOS/tvOS/Views/Presenters/Attributes/MvxMasterSplitViewPresentationAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using MvvmCross.Core.Views;
-namespace MvvmCross.tvOS.Views.Presenters.Attributes
-{
-    public class MvxMasterSplitViewPresentationAttribute : MvxBasePresentationAttribute
-    {
-        public static bool DefaultWrapInNavigationController = true;
-        public bool WrapInNavigationController { get; set; } = DefaultWrapInNavigationController;
-    }
-}
