18761829d45763172dad541c8c0514a33d3feb91
Aaron LaBeau <alabeau@gmail.com>
2017-12-09 00:22:02 -0500

    nuget package updates for the starter pack

---
+++ nuspec/tvOSContent/AppDelegate.cs.txt.pp
@@ -0,0 +1,33 @@
+using MvvmCross.Core.ViewModels;
+using MvvmCross.iOS.Platform;
+using MvvmCross.Platform;
+using Foundation;
+using UIKit;
+
+using MvvmCross.Core.ViewModels;
+using MvvmCross.Platform;
+using MvvmCross.tvOS.Platform;
+
+namespace $rootnamespace$
+{
+    [Register("AppDelegate")]
+    public partial class AppDelegate : MvxApplicationDelegate
+    {
+        public override UIWindow Window { get; set; }
+
+        public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+        {
+            Window = new UIWindow(UIScreen.MainScreen.Bounds);
+
+            var setup = new Setup(this, Window);
+            setup.Initialize();
+
+            var startup = Mvx.Resolve<IMvxAppStart>();
+            startup.Start();
+
+            Window.MakeKeyAndVisible();
+
+            return true;
+        }
+    }
+}
---
+++ nuspec/tvOSContent/DebugTrace.cs.pp
@@ -0,0 +1,31 @@
+using System;
+using System.Diagnostics;
+using MvvmCross.Platform.Platform;
+
+namespace $rootnamespace$
+{
+    public class DebugTrace : IMvxTrace
+    {
+        public void Trace(MvxTraceLevel level, string tag, Func<string> message)
+        {
+            Debug.WriteLine(tag + ":" + level + ":" + message());
+        }
+
+        public void Trace(MvxTraceLevel level, string tag, string message)
+        {
+            Debug.WriteLine(tag + ":" + level + ":" + message);
+        }
+
+        public void Trace(MvxTraceLevel level, string tag, string message, params object[] args)
+        {
+            try
+            {
+                Debug.WriteLine(tag + ":" + level + ":" + message, args);
+            }
+            catch (FormatException)
+            {
+                Trace(MvxTraceLevel.Error, tag, "Exception during trace of {0} {1}", level, message);
+            }
+        }
+    }
+}
