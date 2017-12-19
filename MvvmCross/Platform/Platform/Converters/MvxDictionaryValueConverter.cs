52fbf2d984e200355325200c380e151ae44c4849
Plac3hold3r <Plac3hold3r+github@outlook.com>
2017-12-19 16:13:44 +0200

    Updated MvxDictionaryValueConverter to make fallback optional

--- MvvmCross/Platform/Platform/Converters/MvxDictionaryValueConverter.cs
+++ MvvmCross/Platform/Platform/Converters/MvxDictionaryValueConverter.cs
@@ -10,18 +10,22 @@ namespace MvvmCross.Platform.Converters
         {
             try
             {
-                var typedParameters = (Tuple<IDictionary<TKey, TValue>, TValue>)parameter;
+                var typedParameters = (Tuple<IDictionary<TKey, TValue>, TValue, bool>)parameter;
 
                 if (typedParameters.Item1.ContainsKey(value))
                 {
                     return typedParameters.Item1[value];
                 }
+                else if (typedParameters.Item3)
+                {
+                    return typedParameters.Item2;
+                }
 
-                return typedParameters.Item2;
+                throw new KeyNotFoundException($"Could not find key {value?.ToString()} for {typeof(MvxDictionaryValueConverter<TKey, TValue>)}.");
             }
             catch (InvalidCastException ex)
             {
-                throw new ArgumentException($"Dictionary Converter expected a parameter of type \"{typeof(Tuple<IDictionary<TKey, TValue>, TValue>)}\" but received type \"{parameter.GetType()}\"", nameof(parameter), ex);
+                throw new ArgumentException($"Dictionary Converter expected a parameter of type \"{typeof(Tuple<IDictionary<TKey, TValue>, TValue, bool>)}\" but received type \"{parameter.GetType()}\"", nameof(parameter), ex);
             }
         }
     }
