b4cea943a0b87a6ae6384fa5db5b4c62c1c52d41
nmilcoff <nicolasmilcoff@hotmail.com.ar>
2017-12-21 15:30:59 -0300

    Added UIDatePicker binding error reproduction scenario to TestProjects/CustomBinding

--- TestProjects/CustomBinding/Core/ViewModels/FirstViewModel.cs
+++ TestProjects/CustomBinding/Core/ViewModels/FirstViewModel.cs
@@ -1,22 +1,30 @@
+using System;
 using MvvmCross.Core.ViewModels;
 
 namespace MvvmCross.TestProjects.CustomBinding.Core.ViewModels
 {
-    public class FirstViewModel 
-		: MvxViewModel
+    public class FirstViewModel
+        : MvxViewModel
     {
-		private string _hello = "Hello MvvmCross";
+        private string _hello = "Hello MvvmCross";
         public string Hello
-		{ 
-			get { return _hello; }
+        {
+            get { return _hello; }
             set { SetProperty(ref _hello, value); }
-		}
+        }
 
         private int _counter = 2;
         public int Counter
         {
             get { return _counter; }
             set { SetProperty(ref _counter, value); }
+        }
+
+        private DateTime _date = DateTime.Now;
+        public DateTime Date
+        {
+            get { return _date; }
+            set { SetProperty(ref _date, value); }
         }
     }
 }
