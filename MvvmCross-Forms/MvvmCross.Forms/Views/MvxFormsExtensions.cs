bd0fcd0e51f3696ca434aa63e523cb6e2d1aaa79
Nick Randolph <nick@builttoroam.com>
2017-12-21 15:36:24 +1100

    Extracting helper method to appropriate class, MvxFormsExtensions

---
+++ MvvmCross-Forms/MvvmCross.Forms/Views/MvxFormsExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace MvvmCross.Forms.Views
+{
+    public static class MvxFormsExtensions
+    {
+        public static bool PageMatchesViewModel(this Page page, Type viewModelType)
+        {
+            return (page as IMvxPage)?.ViewModel.GetType() == viewModelType;
+        }
+    }
+}
--- MvvmCross-Forms/MvvmCross.Forms/Views/MvxNavigationHierarchyHelper.cs
+++ MvvmCross-Forms/MvvmCross.Forms/Views/MvxNavigationHierarchyHelper.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Linq;
 using Xamarin.Forms;
 using System.Text;
@@ -7,11 +6,6 @@ namespace MvvmCross.Forms.Views
 {
     public static class MvxNavigationHierarchyHelper
     {
-        public static bool PageMatchesViewModel(this Page page, Type viewModelType)
-        {
-            return (page as IMvxPage)?.ViewModel.GetType() == viewModelType;
-        }
-
         public static string Hierarchy(this Application application)
         {
             var sb = new StringBuilder();
