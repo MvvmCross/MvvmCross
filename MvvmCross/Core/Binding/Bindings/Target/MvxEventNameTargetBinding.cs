0c85c6d6c07331b54f43a61d743ebf8d306b1b45
Taras Shevchuk <schew4uk@gmail.com>
2017-12-23 15:00:21 +0200

    Created new event binding (uses name of the event instead of EventInfo. Also can pass EventArgs as command parameter)

---
+++ MvvmCross/Core/Binding/Bindings/Target/MvxEventNameTargetBinding.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Reflection;
+using System.Windows.Input;
+
+namespace MvvmCross.Binding.Bindings.Target
+{
+    public class MvxEventNameTargetBinding : MvxTargetBinding
+    {
+        private readonly EventInfo _targetEventInfo;
+        private readonly bool _useEventArgsAsCommandParameter;
+
+        private ICommand _currentCommand;
+
+        public MvxEventNameTargetBinding(object target, string targetEventName, bool useEventArgsAsCommandParameter = true) : base(target)
+        {
+            _targetEventInfo = target.GetType().GetTypeInfo().GetDeclaredEvent(targetEventName);
+            _useEventArgsAsCommandParameter = useEventArgsAsCommandParameter;
+
+            //  addMethod is used because of error:
+            // "Attempting to JIT compile method '(wrapper delegate-invoke) <Module>:invoke_void__this___UIControl_EventHandler (UIKit.UIControl,System.EventHandler)' while running with --aot-only."
+            // see https://bugzilla.xamarin.com/show_bug.cgi?id=3682
+            var addMethod = _targetEventInfo.AddMethod;
+            addMethod.Invoke(target, new object[] { new EventHandler(HandleEvent) });
+        }
+
+        public override Type TargetType { get; } = typeof(ICommand);
+
+        public override MvxBindingMode DefaultMode { get; } = MvxBindingMode.OneWay;
+
+        protected override void Dispose(bool isDisposing)
+        {
+            if (isDisposing)
+            {
+                var target = Target;
+                if (target != null)
+                {
+                    var removeMethod = _targetEventInfo.RemoveMethod;
+                    removeMethod.Invoke(target, new object[] { new EventHandler(HandleEvent) });
+                }
+            }
+
+            base.Dispose(isDisposing);
+        }
+
+        private void HandleEvent(object sender, EventArgs parameter)
+        {
+            var commandParameter = _useEventArgsAsCommandParameter ? (object)parameter : null;
+
+            if (_currentCommand != null && _currentCommand.CanExecute(commandParameter))
+                _currentCommand.Execute(commandParameter);
+        }
+
+        public override void SetValue(object value)
+        {
+            var command = value as ICommand;
+            _currentCommand = command;
+        }
+    }
+
+    public class MvxEventNameTargetBinding<TEventArgs> : MvxTargetBinding
+    {
+        private readonly EventInfo _targetEventInfo;
+        private readonly bool _useEventArgsAsCommandParameter;
+
+        private ICommand _currentCommand;
+
+        public MvxEventNameTargetBinding(object target, string targetEventName, bool useEventArgsAsCommandParameter = true) : base(target)
+        {
+            _targetEventInfo = target.GetType().GetTypeInfo().GetDeclaredEvent(targetEventName);
+            _useEventArgsAsCommandParameter = useEventArgsAsCommandParameter;
+
+            //  addMethod is used because of error:
+            // "Attempting to JIT compile method '(wrapper delegate-invoke) <Module>:invoke_void__this___UIControl_EventHandler (UIKit.UIControl,System.EventHandler)' while running with --aot-only."
+            // see https://bugzilla.xamarin.com/show_bug.cgi?id=3682
+            var addMethod = _targetEventInfo.AddMethod;
+            addMethod.Invoke(target, new object[] { new EventHandler<TEventArgs>(HandleEvent) });
+        }
+
+        public override Type TargetType { get; } = typeof(ICommand);
+
+        public override MvxBindingMode DefaultMode { get; } = MvxBindingMode.OneWay;
+
+        protected override void Dispose(bool isDisposing)
+        {
+            if (isDisposing)
+            {
+                var target = Target;
+                if (target != null)
+                {
+                    var removeMethod = _targetEventInfo.RemoveMethod;
+                    removeMethod.Invoke(target, new object[] { new EventHandler<TEventArgs>(HandleEvent) });
+                }
+            }
+
+            base.Dispose(isDisposing);
+        }
+
+        private void HandleEvent(object sender, TEventArgs parameter)
+        {
+            var commandParameter = _useEventArgsAsCommandParameter ? (object)parameter : null;
+
+            if (_currentCommand != null && _currentCommand.CanExecute(commandParameter))
+                _currentCommand.Execute(commandParameter);
+        }
+
+        public override void SetValue(object value)
+        {
+            var command = value as ICommand;
+            _currentCommand = command;
+        }
+    }
+}
