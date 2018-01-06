3e96ddb6f62d37fbe1e9b65e324ff336a287b9f2
nmilcoff <nicolasmilcoff@hotmail.com.ar>
2018-01-06 20:47:26 -0300

    Added separate implementations for MvxAndroidCurrentTopActivity Simplify responsibilities for MvxAndroidLifeTimeMonitor

---
+++ MvvmCross/Droid/Droid/Views/MvxLifecycleCallbacksBasedAndroidCurrentTopActivity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace MvvmCross.Droid.Views
+{
+    public class MvxLifecycleCallbacksBasedAndroidCurrentTopActivity
+    {
+        public MvxLifecycleCallbacksBasedAndroidCurrentTopActivity()
+        {
+        }
+    }
+}
