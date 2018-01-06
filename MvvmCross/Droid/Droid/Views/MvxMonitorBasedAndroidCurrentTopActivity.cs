ae969559a4785685a88fd6c56aeb04a4f0423493
nmilcoff <nicolasmilcoff@hotmail.com.ar>
2018-01-06 20:47:26 -0300

    Added separate implementations for MvxAndroidCurrentTopActivity Simplify responsibilities for MvxAndroidLifeTimeMonitor

---
+++ MvvmCross/Droid/Droid/Views/MvxMonitorBasedAndroidCurrentTopActivity.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Android.App;
+using MvvmCross.Droid.Platform;
+using MvvmCross.Platform.Droid.Platform;
+
+namespace MvvmCross.Droid.Views
+{
+    // Note that we set Activity = activity in multiple places
+    // basically we just want to intercept the activity as early as possible
+    // regardless of whether the activity has come from an app switch or a new start or...
+    public class MvxMonitorBasedAndroidCurrentTopActivity : IMvxAndroidCurrentTopActivity
+    {
+        public MvxMonitorBasedAndroidCurrentTopActivity(IMvxAndroidActivityLifetimeListener listener)
+        {
+            listener.ActivityChanged += Listener_ActivityChanged;
+        }
+
+        public Activity Activity { get; private set; }
+
+        private void Listener_ActivityChanged(object sender, Views.MvxActivityEventArgs e)
+        {
+            switch (e.ActivityState)
+            {
+                case MvxActivityState.OnCreate:
+                case MvxActivityState.OnStart:
+                case MvxActivityState.OnRestart:
+                case MvxActivityState.OnResume:
+                case MvxActivityState.OnNewIntent:
+                case MvxActivityState.OnSaveInstanceState:
+
+                    Activity = e.Activity;
+                    break;
+
+                case MvxActivityState.OnDestroy:
+
+                    if (e.Activity == Activity)
+                        Activity = null;
+                    break;
+            }
+        }
+    }
+}
