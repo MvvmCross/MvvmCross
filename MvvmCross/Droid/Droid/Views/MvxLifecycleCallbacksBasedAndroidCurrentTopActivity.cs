fe098b9a69803f02531e437e85dc047f3c6c03b4
nmilcoff <nicolasmilcoff@hotmail.com.ar>
2018-01-06 21:01:20 -0300

    Implemented MvxLifecycleCallbacksBasedAndroidCurrentTopActivity Simplify MvxAndroidApplication Edited MvxAndroidSetup: Fix register monitor

--- MvvmCross/Droid/Droid/Views/MvxLifecycleCallbacksBasedAndroidCurrentTopActivity.cs
+++ MvvmCross/Droid/Droid/Views/MvxLifecycleCallbacksBasedAndroidCurrentTopActivity.cs
@@ -1,10 +1,108 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using Android.App;
+using Android.OS;
+using MvvmCross.Platform.Droid.Platform;
+
 namespace MvvmCross.Droid.Views
 {
-    public class MvxLifecycleCallbacksBasedAndroidCurrentTopActivity
+    public class MvxLifecycleCallbacksBasedAndroidCurrentTopActivity : Java.Lang.Object, Application.IActivityLifecycleCallbacks, IMvxAndroidCurrentTopActivity
     {
-        public MvxLifecycleCallbacksBasedAndroidCurrentTopActivity()
+        private ConcurrentDictionary<string, ActivityInfo> _Activities = new ConcurrentDictionary<string, ActivityInfo>();
+
+        public Activity Activity => GetCurrentActivity();
+
+        public void OnActivityCreated(Activity activity, Bundle savedInstanceState)
         {
+            var activityName = GetActivityName(activity);
+            _Activities.GetOrAdd(activityName, new ActivityInfo { Activity = activity, IsCurrent = true });
+        }
+
+        public void OnActivityDestroyed(Activity activity)
+        {
+            var activityName = GetActivityName(activity);
+            _Activities.TryRemove(activityName, out ActivityInfo removed);
+        }
+
+        public void OnActivityPaused(Activity activity)
+        {
+            ActivityInfo toAdd = new ActivityInfo { Activity = activity, IsCurrent = false };
+            var activityName = GetActivityName(activity);
+            _Activities.AddOrUpdate(activityName, toAdd, (key, existing) =>
+            {
+                existing.Activity = activity;
+                existing.IsCurrent = false;
+                return existing;
+            });
+        }
+
+        public void OnActivityResumed(Activity activity)
+        {
+            ActivityInfo toAdd = new ActivityInfo { Activity = activity, IsCurrent = true };
+            var activityName = GetActivityName(activity);
+            _Activities.AddOrUpdate(activityName, toAdd, (key, existing) =>
+            {
+                existing.Activity = activity;
+                existing.IsCurrent = true;
+                return existing;
+            });
+        }
+
+        public void OnActivitySaveInstanceState(Activity activity, Bundle outState)
+        {
+        }
+
+        public void OnActivityStarted(Activity activity)
+        {
+            ActivityInfo toAdd = new ActivityInfo { Activity = activity, IsCurrent = true };
+            var activityName = GetActivityName(activity);
+            _Activities.AddOrUpdate(activityName, toAdd, (key, existing) =>
+            {
+                existing.Activity = activity;
+                existing.IsCurrent = true;
+                return existing;
+            });
+        }
+
+        public void OnActivityStopped(Activity activity)
+        {
+            ActivityInfo toAdd = new ActivityInfo { Activity = activity, IsCurrent = false };
+            var activityName = GetActivityName(activity);
+            _Activities.AddOrUpdate(activityName, toAdd, (key, existing) =>
+            {
+                existing.Activity = activity;
+                existing.IsCurrent = false;
+                return existing;
+            });
+        }
+
+        private Activity GetCurrentActivity()
+        {
+            if (_Activities.Count > 0)
+            {
+                var e = _Activities.GetEnumerator();
+                while (e.MoveNext())
+                {
+                    var current = e.Current;
+                    if (current.Value.IsCurrent)
+                    {
+                        return current.Value.Activity;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        protected string GetActivityName(Activity activity) => activity.Class.SimpleName;
+
+        /// <summary>
+        /// Used to store additional info along with an activity.
+        /// </summary>
+        private class ActivityInfo
+        {
+            public bool IsCurrent { get; set; }
+            public Activity Activity { get; set; }
         }
     }
 }
