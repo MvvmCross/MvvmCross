40accbc2a133fc5112d5f6931f48fdd0a62b86b2
Nick Randolph <nick@builttoroam.com>
2018-01-10 16:01:32 +1100

    Fixing UWP and UWP Forms build errors in playground sample

--- TestProjects/Playground/Playground.Uwp/EmptyVoidLog.cs
+++ TestProjects/Playground/Playground.Uwp/EmptyVoidLog.cs
@@ -5,6 +5,11 @@ namespace Playground.Uwp
 {
     public class EmptyVoidLog : IMvxLog
     {
+        public bool IsLogLevelEnabled(MvxLogLevel logLevel)
+        {
+            return true;
+        }
+
         public bool Log(MvxLogLevel logLevel, Func<string> messageFunc, Exception exception = null, params object[] formatParameters)
         {
             return true;
