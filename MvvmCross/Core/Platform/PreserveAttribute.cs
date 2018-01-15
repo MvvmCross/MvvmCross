1249d9193457f020127582195841ddd53e27a512
Tomasz Cielecki <Tomasz.Cielecki@bksv.com>
2018-01-15 16:10:12 +0100

    Move PreserveAttribute to MvvmCross.Platform namespace

--- MvvmCross/Core/Platform/PreserveAttribute.cs
+++ MvvmCross/Core/Platform/PreserveAttribute.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace MvvmCross.Core.Platform
+namespace MvvmCross.Platform
 {
     [AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum | AttributeTargets.Constructor | AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Event | AttributeTargets.Interface | AttributeTargets.Delegate)]
     public sealed class PreserveAttribute : Attribute
