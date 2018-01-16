72c81df1db987aa240986a13a02bc13828af7fc2
William Barbosa <william.sb@hotmail.com>
2018-01-16 12:39:52 +0100

    Remove reflection from console log provider

--- MvvmCross/Core/Core/Platform/LogProviders/ConsoleLogProvider.cs
+++ MvvmCross/Core/Core/Platform/LogProviders/ConsoleLogProvider.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Linq.Expressions;
-using System.Reflection;
 using System.Text;
 using MvvmCross.Platform.Logging;
 
@@ -10,53 +8,19 @@ namespace MvvmCross.Core.Platform.LogProviders
 {
     internal sealed class ConsoleLogProvider : MvxBaseLogProvider
     {
-        private static readonly Type ConsoleType;
-        private static readonly Type ConsoleColorType;
-        private static readonly Action<string> ConsoleWriteLine;
-        private static readonly Func<int> GetConsoleForeground;
-        private static readonly Action<int> SetConsoleForeground;
-        private static readonly IDictionary<MvxLogLevel, int> Colors;
-
-        static ConsoleLogProvider()
+        private static readonly IDictionary<MvxLogLevel, ConsoleColor> Colors = new Dictionary<MvxLogLevel, ConsoleColor>
         {
-            ConsoleType = Type.GetType("System.Console");
-            ConsoleColorType = ConsoleColorValues.Type;
+            { MvxLogLevel.Fatal, ConsoleColor.Red },
+            { MvxLogLevel.Error, ConsoleColor.Yellow },
+            { MvxLogLevel.Warn, ConsoleColor.Magenta },
+            { MvxLogLevel.Info, ConsoleColor.White },
+            { MvxLogLevel.Debug, ConsoleColor.Gray },
+            { MvxLogLevel.Trace, ConsoleColor.DarkGray }
+        };
 
-            if (!IsLoggerAvailable())
-            {
-                throw new InvalidOperationException("System.Console or System.ConsoleColor type not found");
-            }
+        protected override Logger GetLogger(string name) => new ColouredConsoleLogger(name).Log;
 
-            MessageFormatter = DefaultMessageFormatter;
-            Colors = new Dictionary<MvxLogLevel, int>
-            {
-                {MvxLogLevel.Fatal, ConsoleColorValues.Red},
-                {MvxLogLevel.Error, ConsoleColorValues.Yellow},
-                {MvxLogLevel.Warn, ConsoleColorValues.Magenta},
-                {MvxLogLevel.Info, ConsoleColorValues.White},
-                {MvxLogLevel.Debug, ConsoleColorValues.Gray},
-                {MvxLogLevel.Trace, ConsoleColorValues.DarkGray},
-            };
-            ConsoleWriteLine = GetConsoleWrite();
-            GetConsoleForeground = GetGetConsoleForeground();
-            SetConsoleForeground = GetSetConsoleForeground();
-        }
-
-        internal static bool IsLoggerAvailable()
-            => ConsoleType != null && ConsoleColorType != null;
-
-        protected override Logger GetLogger(string name)
-            => new ColouredConsoleLogger(name, ConsoleWriteLine, GetConsoleForeground, SetConsoleForeground).Log;
-
-        internal delegate string MessageFormatterDelegate(
-            string loggerName,
-            MvxLogLevel level,
-            object message,
-            Exception e);
-
-        internal static MessageFormatterDelegate MessageFormatter { get; set; }
-
-        private static string DefaultMessageFormatter(string loggerName, MvxLogLevel level, object message, Exception e)
+        private static string MessageFormatter(string loggerName, MvxLogLevel level, object message, Exception e)
         {
             var stringBuilder = new StringBuilder();
             stringBuilder.Append(DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss", CultureInfo.InvariantCulture));
@@ -80,60 +44,19 @@ namespace MvvmCross.Core.Platform.LogProviders
             return stringBuilder.ToString();
         }
 
-        private static Action<string> GetConsoleWrite()
-        {
-            var messageParameter = Expression.Parameter(typeof(string), "message");
-
-            MethodInfo writeMethod = ConsoleType.GetMethodPortable("WriteLine", typeof(string));
-            var writeExpression = Expression.Call(writeMethod, messageParameter);
-
-            return Expression.Lambda<Action<string>>(
-                writeExpression, messageParameter).Compile();
-        }
-
-        private static Func<int> GetGetConsoleForeground()
-        {
-            MethodInfo getForeground = ConsoleType.GetPropertyPortable("ForegroundColor").GetGetMethod();
-            var getForegroundExpression = Expression.Convert(Expression.Call(getForeground), typeof(int));
-
-            return Expression.Lambda<Func<int>>(getForegroundExpression).Compile();
-        }
-
-        private static Action<int> GetSetConsoleForeground()
-        {
-            var colorParameter = Expression.Parameter(typeof(int), "color");
-
-            MethodInfo setForeground = ConsoleType.GetPropertyPortable("ForegroundColor").GetSetMethod();
-            var setForegroundExpression = Expression.Call(setForeground,
-                Expression.Convert(colorParameter, ConsoleColorType));
-
-            return Expression.Lambda<Action<int>>(
-                setForegroundExpression, colorParameter).Compile();
-        }
-
         public class ColouredConsoleLogger
         {
             private readonly string _name;
-            private readonly Action<string> _write;
-            private readonly Func<int> _getForeground;
-            private readonly Action<int> _setForeground;
 
-            public ColouredConsoleLogger(string name, Action<string> write,
-                Func<int> getForeground, Action<int> setForeground)
+            public ColouredConsoleLogger(string name)
             {
                 _name = name;
-                _write = write;
-                _getForeground = getForeground;
-                _setForeground = setForeground;
             }
 
             public bool Log(MvxLogLevel logLevel, Func<string> messageFunc, Exception exception,
                 params object[] formatParameters)
             {
-                if (messageFunc == null)
-                {
-                    return true;
-                }
+                if (messageFunc == null) return true;
 
                 messageFunc = LogMessageFormatter.SimulateStructuredLogging(messageFunc, formatParameters);
 
@@ -144,49 +67,24 @@ namespace MvvmCross.Core.Platform.LogProviders
             protected void Write(MvxLogLevel logLevel, string message, Exception e = null)
             {
                 var formattedMessage = MessageFormatter(_name, logLevel, message, e);
-                int color;
 
-                if (Colors.TryGetValue(logLevel, out color))
+                if (Colors.TryGetValue(logLevel, out var color))
                 {
-                    var originalColor = _getForeground();
+                    var originalColor = Console.ForegroundColor;
                     try
                     {
-                        _setForeground(color);
-                        _write(formattedMessage);
+                        Console.ForegroundColor = color;
+                        Console.WriteLine(formattedMessage);
                     }
                     finally
                     {
-                        _setForeground(originalColor);
+                        Console.ForegroundColor = originalColor;
                     }
                 }
                 else
                 {
-                    _write(formattedMessage);
+                    Console.WriteLine(formattedMessage);
                 }
-            }
-        }
-
-        private static class ConsoleColorValues
-        {
-            internal static readonly Type Type;
-            internal static readonly int Red;
-            internal static readonly int Yellow;
-            internal static readonly int Magenta;
-            internal static readonly int White;
-            internal static readonly int Gray;
-            internal static readonly int DarkGray;
-
-            static ConsoleColorValues()
-            {
-                Type = Type.GetType("System.ConsoleColor") ?? Type.GetType("System.ConsoleColor, System.Console");
-
-                if (Type == null) return;
-                Red = (int)Enum.Parse(Type, "Red", false);
-                Yellow = (int)Enum.Parse(Type, "Yellow", false);
-                Magenta = (int)Enum.Parse(Type, "Magenta", false);
-                White = (int)Enum.Parse(Type, "White", false);
-                Gray = (int)Enum.Parse(Type, "Gray", false);
-                DarkGray = (int)Enum.Parse(Type, "DarkGray", false);
             }
         }
     }
