891fa818fe8328d7ff5f11cd8da0a6dc5a5d9633
Martijn van Dijk <mhvdijk@gmail.com>
2018-01-26 10:17:47 +0100

    Remove ShowViewModel in favor for MvxNavigationService (#2559)

--- MvvmCross/Core/Core/ViewModels/MvxViewModel.cs
+++ MvvmCross/Core/Core/ViewModels/MvxViewModel.cs
@@ -15,7 +15,7 @@ using MvvmCross.Platform.Platform;
 namespace MvvmCross.Core.ViewModels
 {
     public abstract class MvxViewModel
-        : MvxNavigatingObject, IMvxViewModel
+        : MvxNotifyPropertyChanged, IMvxViewModel
     {
         protected MvxViewModel()
         {
@@ -95,22 +95,6 @@ namespace MvvmCross.Core.ViewModels
 
     public abstract class MvxViewModel<TParameter> : MvxViewModel, IMvxViewModel<TParameter>
     {
-        public async Task Init(string parameter)
-        {
-            if (!string.IsNullOrEmpty(parameter))
-            {
-                IMvxJsonConverter serializer;
-                if (!Mvx.TryResolve(out serializer))
-                {
-                    throw new MvxIoCResolveException("There is no implementation of IMvxJsonConverter registered. You need to use the MvvmCross Json plugin or create your own implementation of IMvxJsonConverter.");
-                }
-
-                var deserialized = serializer.DeserializeObject<TParameter>(parameter);
-                Prepare(deserialized);
-                await Initialize();
-            }
-        }
-
         public abstract void Prepare(TParameter parameter);
     }
 
