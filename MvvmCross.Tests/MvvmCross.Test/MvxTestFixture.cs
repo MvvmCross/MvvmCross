9691258eaabf28b63f10a7eb116e668174f37502
Tomasz Cielecki <tomasz@ostebaronen.dk>
2018-01-29 22:15:30 +0100

    Create TestFixture

---
+++ MvvmCross.Tests/MvvmCross.Test/MvxTestFixture.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MvvmCross.Test
+{
+    public class MvxTestFixture : MvxIoCSupportingTest, IDisposable
+    {
+        public MvxTestFixture()
+        {
+            Setup();
+        }
+
+        ~MvxTestFixture()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing) 
+            {
+                Reset();
+            }
+        }
+    }
+}
