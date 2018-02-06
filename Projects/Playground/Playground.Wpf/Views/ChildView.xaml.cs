72c158c26464294a0b290da8a514518ccc634921
Nick Randolph <nick@builttoroam.com>
2018-02-06 18:00:59 +1100

    Fixing project references - Droid

--- Projects/Playground/Playground.Wpf/Views/ChildView.xaml.cs
+++ Projects/Playground/Playground.Wpf/Views/ChildView.xaml.cs
@@ -1,14 +1,9 @@
-﻿using MvvmCross.Wpf.Views;
-using MvvmCross.Wpf.Views.Presenters.Attributes;
-using Playground.Core.ViewModels;
+﻿using MvvmCross.Wpf.Views.Presenters.Attributes;
 
 namespace Playground.Wpf.Views
 {
-    /// <summary>
-    /// Interaction logic for ChildView.xaml
-    /// </summary>
     [MvxContentPresentation]
-    public partial class ChildView : MvxWpfView<ChildViewModel>
+    public partial class ChildView
     {
         public ChildView()
         {
