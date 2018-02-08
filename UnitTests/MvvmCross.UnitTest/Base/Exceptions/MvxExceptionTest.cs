9e2289bfdb04a7f4edaf3cf30d15a49e17d9e652
Tomasz Cielecki <tomasz@ostebaronen.dk>
2018-02-08 20:59:26 +0100

    Add exceptions tests

---
+++ UnitTests/MvvmCross.UnitTest/Base/Exceptions/MvxExceptionTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MvvmCross.Platform.Exceptions;
+using Xunit;
+
+namespace MvvmCross.UnitTest.Base.Exceptions
+{
+    public class MvxExceptionTest
+    {
+        [Fact]
+        public void MvxWrap_Is_MvxException()
+        {
+            var ex = new Exception("hello");
+            var wrapped = ex.MvxWrap();
+
+            Assert.IsType<MvxException>(wrapped);
+        }
+
+        [Fact]
+        public void MvxWrap_Is_Thrown_As_MvxException()
+        {
+            var ex = new Exception("hello");
+            var wrapped = ex.MvxWrap();
+
+            Assert.Throws<MvxException>(() => Throw(wrapped));
+
+            void Throw(Exception e)
+            {
+                throw e;
+            }
+        }
+
+        [Fact]
+        public void MvxWrap_Has_InnerException()
+        {
+            var ex = new Exception("hello");
+            var wrapped = ex.MvxWrap();
+
+            Assert.NotNull(wrapped.InnerException);
+            Assert.Equal(ex, wrapped.InnerException);
+        }
+
+        [Fact]
+        public void MvxWrap_Wraps_Only_One_Level()
+        {
+            var ex = new Exception("hello");
+            var wrapped = ex.MvxWrap();
+
+            for (var i = 0; i < 10; i++)
+                wrapped = wrapped.MvxWrap();
+
+            Assert.Equal(ex, wrapped.InnerException);
+        }
+    }
+}
