94b66cb301f4a556af914afeabe60be78a2f4952
William Barbosa <william.sb@hotmail.com>
2018-02-11 00:27:38 -0200

    Change plugin architecture to rely on attribute lookup

---
+++ MvvmCross/Plugin/IMvxPluginManager.cs
@@ -0,0 +1,25 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MS-PL license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+
+namespace MvvmCross.Plugin
+{
+    public interface IMvxPluginManager
+    {
+        Func<Type, IMvxPluginConfiguration> ConfigurationSource { get; }
+
+        bool IsPluginLoaded(Type type);
+
+        bool IsPluginLoaded<TPlugin>() where TPlugin : IMvxPlugin;
+
+        void EnsurePluginLoaded(Type type);
+        
+        void EnsurePluginLoaded<TPlugin>() where TPlugin : IMvxPlugin;
+
+        bool TryEnsurePluginLoaded(Type type);
+        
+        bool TryEnsurePluginLoaded<TPlugin>() where TPlugin : IMvxPlugin;
+     }
+}
