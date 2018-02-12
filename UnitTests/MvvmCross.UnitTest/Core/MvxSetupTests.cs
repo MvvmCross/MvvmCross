0f7aa35f5055b9113d7782855210bdd847b69cfe
William Barbosa <william.sb@hotmail.com>
2018-02-12 17:31:56 -0200

    Add Unit Tests

---
+++ UnitTests/MvvmCross.UnitTest/Core/MvxSetupTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Moq;
+using MvvmCross.Core;
+using MvvmCross.Plugin;
+using Xunit;
+
+namespace MvvmCross.UnitTest.Core
+{
+    public class MvxSetupTests
+    {
+        public abstract class MvxSetupPluginTest
+        {
+            public Mock<IMvxPluginManager> PluginManager { get; } = new Mock<IMvxPluginManager>();
+
+            protected Mock<MvxSetup> SetupUnderTest { get; }
+
+            protected MvxSetupPluginTest()
+            {
+                SetupUnderTest  = new Mock<MvxSetup> { CallBase = true };
+            }
+        }
+
+        public class TheGetPluginsAssemblyMethod : MvxSetupPluginTest
+        {
+            [Fact]
+            public void ReturnsAListOfAllAssembliesThatReferenceMvvmCross()
+            {
+                var assemblies = SetupUnderTest.Object.GetPluginAssemblies();
+
+                Assert.Equal(assemblies.Count(), 1);
+            }
+        }
+
+        public class TheLoadPluginsMethod : MvxSetupPluginTest
+        {
+            [Fact]
+            public void LoadssEveryTypeAnnotatedWithTheMvxSetupAttibute()
+            {
+                SetupUnderTest.Object.LoadPlugins(PluginManager.Object);
+
+                PluginManager
+                    .Verify(manager => manager.EnsurePluginLoaded(It.IsAny<Type>()), Times.Exactly(3));
+            }
+        }
+    }
+}
---
+++ UnitTests/MvvmCross.UnitTest/Mocks/Plugin/PluginMocks.cs
@@ -0,0 +1,28 @@
+﻿using MvvmCross.Plugin;
+
+namespace MvvmCross.UnitTest.Mocks.Plugin
+{
+    [MvxPlugin]
+    public class PluginMock1 : IMvxPlugin
+    {
+        public void Load()
+        {
+        }
+    }
+
+    [MvxPlugin]
+    public class PluginMock2 : IMvxPlugin
+    {
+        public void Load()
+        {
+        }
+    }
+
+    [MvxPlugin]
+    public class PluginMock3 : IMvxPlugin
+    {
+        public void Load()
+        {
+        }
+    }
+}
