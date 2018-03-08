51a41ca7d8ef2e9a089fdf2dd5192a951bd44761
Martijn van Dijk <mhvdijk@gmail.com>
2018-03-08 16:04:13 +0100

    Start off with Tizen implementations

---
+++ MvvmCross/Platform/Tizen/Presenters/MvxTizenViewPresenter.cs
@@ -0,0 +1,23 @@
+﻿
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MS-PL license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using MvvmCross.Presenters;
+
+namespace MvvmCross.Platform.Tizen.Presenters
+{
+    public class MvxTizenViewPresenter : MvxAttributeViewPresenter, IMvxTizenViewPresenter
+    {
+        public override MvxBasePresentationAttribute CreatePresentationAttribute(Type viewModelType, Type viewType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RegisterAttributeTypes()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
