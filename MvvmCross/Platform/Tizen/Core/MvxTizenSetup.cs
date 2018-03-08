173b530c1eab2cf8a8727ae9e736820e65b42997
Martijn van Dijk <mhvdijk@gmail.com>
2018-03-08 16:04:13 +0100

    Start off with Tizen implementations

---
+++ MvvmCross/Platform/Tizen/Core/MvxTizenSetup.cs
@@ -0,0 +1,74 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MS-PL license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using MvvmCross.Binding.Binders;
+using MvvmCross.Binding.Bindings.Target.Construction;
+using MvvmCross.Converters;
+using MvvmCross.Core;
+using MvvmCross.Platform.Tizen.Presenters;
+using MvvmCross.Presenters;
+using MvvmCross.ViewModels;
+using MvvmCross.Views;
+
+namespace MvvmCross.Platform.Tizen.Core
+{
+    public abstract class MvxTizenSetup
+        : MvxSetup
+    {
+        private IMvxTizenViewPresenter _presenter;
+
+        protected IMvxTizenViewPresenter Presenter
+        {
+            get
+            {
+                _presenter = _presenter ?? CreateViewPresenter();
+                return _presenter;
+            }
+        }
+
+        protected virtual IMvxTizenViewPresenter CreateViewPresenter()
+        {
+            return new MvxTizenViewPresenter();
+        }
+
+        protected virtual void RegisterPresenter()
+        {
+            var presenter = Presenter;
+            Mvx.RegisterSingleton(presenter);
+            Mvx.RegisterSingleton<IMvxViewPresenter>(presenter);
+        }
+
+        protected virtual void RegisterBindingBuilderCallbacks()
+        {
+            Mvx.CallbackWhenRegistered<IMvxValueConverterRegistry>(FillValueConverters);
+        }
+
+        protected virtual void FillValueConverters(IMvxValueConverterRegistry registry)
+        {
+            registry.Fill(ValueConverterAssemblies);
+            registry.Fill(ValueConverterHolders);
+        }
+
+        protected virtual IEnumerable<Type> ValueConverterHolders => new List<Type>();
+
+        protected virtual IEnumerable<Assembly> ValueConverterAssemblies
+        {
+            get
+            {
+                var toReturn = new List<Assembly>();
+                toReturn.AddRange(GetViewModelAssemblies());
+                toReturn.AddRange(GetViewAssemblies());
+                return toReturn;
+            }
+        }
+
+        protected override IMvxNameMapping CreateViewToViewModelNaming()
+        {
+            return new MvxPostfixAwareViewToViewModelNameMapping("View");
+        }
+    }
+}
