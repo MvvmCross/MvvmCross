03e57d04ae3d485248c75d5782222e5ec6d2ce6f
Martijn van Dijk <mhvdijk@gmail.com>
2018-03-08 18:28:04 +0100

    Fix a couple of headers

--- MvvmCross/Platform/Ios/Views/MvxViewControllerAdaptingExtensions.cs
+++ MvvmCross/Platform/Ios/Views/MvxViewControllerAdaptingExtensions.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
 
-
 using MvvmCross.Platform.Ios.Views.Base;
 
 namespace MvvmCross.Platform.Ios.Views
--- UnitTests/MvvmCross.UnitTest/Navigation/NavigationServiceTests.cs
+++ UnitTests/MvvmCross.UnitTest/Navigation/NavigationServiceTests.cs
@@ -2,7 +2,7 @@
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
 
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Moq;
  using MvvmCross.Core;
