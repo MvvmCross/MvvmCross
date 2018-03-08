9019bb4da58cfb0a3a5263532c423d722ccf1fc8
Martijn van Dijk <mhvdijk@gmail.com>
2018-03-08 19:16:33 +0100

    Add back plugin file

---
+++ MvvmCross.Plugins/Color/Platform/Wpf/Plugin.cs
@@ -0,0 +1,17 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MS-PL license.
+// See the LICENSE file in the project root for more information.
+
+using MvvmCross.UI;
+
+namespace MvvmCross.Plugin.Color.Platform.Wpf
+{
+    [MvxPlugin]
+    public class Plugin : IMvxPlugin
+    {
+        public void Load()
+        {
+            Mvx.RegisterSingleton<IMvxNativeColor>(new MvxWpfColor());
+        }
+    }
+}
