060ee3e95f53cace0062a5965a71786cb67aabdb
Martijn van Dijk <mhvdijk@gmail.com>
2018-03-08 18:28:04 +0100

    Fix a couple of headers

--- UnitTests/MvvmCross.UnitTest/Binding/Parse/Binding/MvxBindingTest.cs
+++ UnitTests/MvvmCross.UnitTest/Binding/Parse/Binding/MvxBindingTest.cs
@@ -2,7 +2,7 @@
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
 
-﻿using MvvmCross.Binding.Parse.Binding;
+using MvvmCross.Binding.Parse.Binding;
 using MvvmCross.Tests;
 using Xunit;
 
