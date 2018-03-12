324a31e5c71fbc339e86babff740bc4c1aabca5c
tbalcom <trevorbalcom@me.com>
2018-03-12 19:05:56 -0400

    Replaces MvxWakefulBroadcastReceiver with MvxJobService.

---
+++ MvvmCross/Platform/Android/Core/MvxJobService.cs
@@ -0,0 +1,30 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MS-PL license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using Android.App.Job;
+using Android.Runtime;
+
+namespace MvvmCross.Platform.Android.Core
+{
+    [Register("mvvmcross.platform.android.core.MvxJobService")]
+    public abstract class MvxJobService : JobService
+    {
+        protected MvxJobService(IntPtr javaReference, JniHandleOwnership transfer)
+            : base(javaReference, transfer)
+        {
+        }
+
+        protected MvxJobService()
+        {
+        }
+
+        public override bool OnStartJob(JobParameters @params)
+        {
+            var setup = MvxAndroidSetupSingleton.EnsureSingletonAvailable(this);
+            setup.EnsureInitialized();
+            return true;
+        }
+    }
+}
