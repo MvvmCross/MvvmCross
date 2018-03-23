5d5d3323ec57bff6099ccd5ac9d1aa8900a8c568
Jacob Duijzer <jacob.duijzer@redhotminute.com>
2018-03-23 14:49:59 +0100

    Added helpers + documentation for testing RaiseCanExecuteChanged on MvxCommand

---
+++ MvvmCross.Tests/MvxUnitTestCommandHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using MvvmCross.Commands;
+
+namespace MvvmCross.Tests
+{
+    public class MvxUnitTestCommandHelper : IMvxCommandHelper
+    {
+        public event EventHandler CanExecuteChanged;
+
+        private Dictionary<object, int> items = new Dictionary<object, int>();
+
+        public void WillCallRaisePropertyChangedFor(object item)
+        {
+            items.Add(item, 0);
+        }
+
+        public bool HasCalledRaisePropertyChangedFor(object item)
+        {
+            try
+            {
+                return items[item] > 0;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public void RaiseCanExecuteChanged(object sender)
+        {
+            try
+            {
+                items[sender] += 1;
+            }
+            catch (Exception)
+            {
+            }
+
+            if (CanExecuteChanged != null)
+            {
+                CanExecuteChanged.Invoke(sender, new System.EventArgs());
+            }
+        }
+    }
+}
