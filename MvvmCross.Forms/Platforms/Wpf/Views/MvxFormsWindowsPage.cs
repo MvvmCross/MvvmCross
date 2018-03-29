ee96b9c90ce6d59f2bde2305ad64086d0a39813c
Nick Randolph <nick@builttoroam.com>
2018-03-30 09:31:12 +1100

    Attempt at getting XF v3 to work Adding WPF XF

---
+++ MvvmCross.Forms/Platforms/Wpf/Views/MvxFormsWindowsPage.cs
@@ -0,0 +1,49 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MS-PL license.
+// See the LICENSE file in the project root for more information.
+
+using System.Threading.Tasks;
+using MvvmCross.Forms.Platforms.Wpf.Presenters;
+using MvvmCross.Forms.Presenters;
+using MvvmCross.ViewModels;
+using Xamarin.Forms.Platform.WPF;
+
+namespace MvvmCross.Forms.Platforms.Wpf.Views
+{
+    public class MvxFormsWindowsPage : FormsApplicationPage
+    {
+        public MvxFormsWindowsPage()
+        {
+            // Wait for page to load to kick off setup and loading forms 
+            // This is required for when setup becomes async aware
+            Loaded += MvxFormsWindowsPage_Loaded;
+        }
+
+        private void MvxFormsWindowsPage_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Loaded -= MvxFormsWindowsPage_Loaded;
+
+            RunAppStart(e);
+        }
+
+        protected virtual void RunAppStart(object hint = null)
+        {
+            var startup = Mvx.Resolve<IMvxAppStart>();
+            if(!startup.IsStarted)
+                startup.Start(GetAppStartHint(hint));
+
+            LoadFormsApplication();
+        }
+
+        protected virtual object GetAppStartHint(object hint = null)
+        {
+            return null;
+        }
+
+        protected virtual void LoadFormsApplication()
+        {
+            var presenter = Mvx.Resolve<IMvxFormsViewPresenter>() as MvxFormsWpfViewPresenter;
+            LoadApplication(presenter.FormsApplication);
+        }
+    }
+}
---
+++ MvvmCross.Forms/Platforms/Wpf/Views/MvxPageRenderer.cs
@@ -0,0 +1,67 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MS-PL license.
+// See the LICENSE file in the project root for more information.
+
+using MvvmCross.Binding.BindingContext;
+using MvvmCross.ViewModels;
+using Xamarin.Forms.Platform.WPF;
+
+namespace MvvmCross.Forms.Platforms.Wpf.Views
+{
+    public class MvxPageRenderer : PageRenderer, IMvxBindingContextOwner
+    {
+        public new object DataContext
+        {
+            get
+            {
+                return BindingContext.DataContext;
+            }
+            set
+            {
+                BindingContext.DataContext = value;
+            }
+        }
+
+        private IMvxBindingContext _bindingContext;
+        public IMvxBindingContext BindingContext
+        {
+            get
+            {
+                if (_bindingContext == null)
+                    BindingContext = new MvxBindingContext();
+                return _bindingContext;
+            }
+            set
+            {
+                _bindingContext = value;
+            }
+        }
+
+        public virtual IMvxViewModel ViewModel
+        {
+            get
+            {
+                return DataContext as IMvxViewModel;
+            }
+            set
+            {
+                DataContext = value;
+                OnViewModelSet();
+            }
+        }
+
+        protected virtual void OnViewModelSet()
+        {
+        }
+    }
+
+    public class MvxPageRenderer<TViewModel>
+        : MvxPageRenderer where TViewModel : class, IMvxViewModel
+    {
+        public new TViewModel ViewModel
+        {
+            get { return (TViewModel)base.ViewModel; }
+            set { base.ViewModel = value; }
+        }
+    }
+}
