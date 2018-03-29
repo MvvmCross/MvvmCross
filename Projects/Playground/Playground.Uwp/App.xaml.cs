66523631d21ab4716df5a7a596c395c6cf0850df
Nick Randolph <nick@builttoroam.com>
2018-03-30 09:43:25 +1100

    Fixing use of generic base class in xaml

--- Projects/Playground/Playground.Uwp/App.xaml.cs
+++ Projects/Playground/Playground.Uwp/App.xaml.cs
@@ -4,11 +4,16 @@ using MvvmCross.Platforms.Uap.Views;
 
 namespace Playground.Uwp
 {
-    public sealed partial class App : MvxApplication<MvxWindowsSetup<Core.App>, Core.App>
+    public sealed partial class App : PlaygroundApp
     {
         public App()
         {
             InitializeComponent();
         }
+    }
+
+    public class PlaygroundApp : MvxApplication<MvxWindowsSetup<Core.App>, Core.App>
+    {
+
     }
 }
