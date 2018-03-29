b748ce2863ef6177d5480d25d9d6b8996a3acbe2
Martijn van Dijk <mhvdijk@gmail.com>
2018-03-29 13:21:11 +0200

    Fix linker problem

--- ContentFiles/Android/LinkerPleaseInclude.cs
+++ ContentFiles/Android/LinkerPleaseInclude.cs
@@ -5,25 +5,26 @@ using Android.App;
 using Android.Views;
 using Android.Widget;
 using MvvmCross.Binding.BindingContext;
-using MvvmCross.Core.Navigation;
-using MvvmCross.Core.ViewModels;
+using MvvmCross.Navigation;
+using MvvmCross.ViewModels;
 
-namespace $rootnamespace$
+namespace $YourNameSpace$
 {
     // This class is never actually executed, but when Xamarin linking is enabled it does how to ensure types and properties
     // are preserved in the deployed app
+    [Android.Runtime.Preserve(AllMembers = true)]
     public class LinkerPleaseInclude
     {
         public void Include(Button button)
         {
-            button.Click += (s,e) => button.Text = button.Text + "";
+            button.Click += (s, e) => button.Text = button.Text + "";
         }
 
         public void Include(CheckBox checkBox)
         {
             checkBox.CheckedChange += (sender, args) => checkBox.Checked = !checkBox.Checked;
         }
-        
+
         public void Include(Switch @switch)
         {
             @switch.CheckedChange += (sender, args) => @switch.Checked = !@switch.Checked;
@@ -39,7 +40,7 @@ namespace $rootnamespace$
             text.AfterTextChanged += (sender, args) => text.Text = "" + text.Text;
             text.Hint = "" + text.Hint;
         }
-        
+
         public void Include(CheckedTextView text)
         {
             text.AfterTextChanged += (sender, args) => text.Text = "" + text.Text;
@@ -65,8 +66,8 @@ namespace $rootnamespace$
         {
             radioButton.CheckedChange += (sender, args) => radioButton.Checked = args.IsChecked;
         }
-		
-		public void Include(RatingBar ratingBar)
+
+        public void Include(RatingBar ratingBar)
         {
             ratingBar.RatingBarChange += (sender, args) => ratingBar.Rating = 0 + ratingBar.Rating;
         }
@@ -78,26 +79,27 @@ namespace $rootnamespace$
 
         public void Include(INotifyCollectionChanged changed)
         {
-            changed.CollectionChanged += (s,e) => { var test = $"{e.Action}{e.NewItems}{e.NewStartingIndex}{e.OldItems}{e.OldStartingIndex}"; };
+            changed.CollectionChanged += (s, e) => { var test = $"{e.Action}{e.NewItems}{e.NewStartingIndex}{e.OldItems}{e.OldStartingIndex}"; };
         }
 
         public void Include(ICommand command)
         {
             command.CanExecuteChanged += (s, e) => { if (command.CanExecute(null)) command.Execute(null); };
         }
-        
-        public void Include(MvvmCross.Platform.IoC.MvxPropertyInjector injector)
+
+        public void Include(MvvmCross.IoC.MvxPropertyInjector injector)
         {
-            injector = new MvvmCross.Platform.IoC.MvxPropertyInjector ();
-        } 
+            injector = new MvvmCross.IoC.MvxPropertyInjector();
+        }
 
         public void Include(System.ComponentModel.INotifyPropertyChanged changed)
         {
-            changed.PropertyChanged += (sender, e) =>  {
+            changed.PropertyChanged += (sender, e) =>
+            {
                 var test = e.PropertyName;
             };
         }
-        
+
         public void Include(MvxTaskBasedBindingContext context)
         {
             context.Dispose();
