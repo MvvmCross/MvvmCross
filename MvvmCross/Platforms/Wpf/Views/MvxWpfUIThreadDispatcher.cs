491b6d95211ca85d00757a0da409d482ef0f1007
Nick Randolph <nick@builttoroam.com>
2018-03-31 12:02:11 +1100

    Tidying up exception masking to reduce duplicated code

--- MvvmCross/Platforms/Wpf/Views/MvxWpfUIThreadDispatcher.cs
+++ MvvmCross/Platforms/Wpf/Views/MvxWpfUIThreadDispatcher.cs
@@ -1,4 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
 
@@ -26,12 +26,9 @@ namespace MvvmCross.Platforms.Wpf.Views
             }
             else
             {
-                _dispatcher.Invoke(() => 
+                _dispatcher.Invoke(() =>
                 {
-                    if (maskExceptions)
-                        ExceptionMaskedAction(action);
-                    else
-                        action();
+                    ExceptionMaskedAction(action, maskExceptions);
                 });
             }
 
