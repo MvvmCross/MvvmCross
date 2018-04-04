ace2b507d3de8204b293ef703740a93ed398e150
Nick Randolph <nick@builttoroam.com>
2018-04-05 08:25:29 +1000

    Adding Application Startup method to be invoked on UI thread at end of setup

--- MvvmCross/ViewModels/IMvxApplication.cs
+++ MvvmCross/ViewModels/IMvxApplication.cs
@@ -1,4 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
 
@@ -11,5 +11,9 @@ namespace MvvmCross.ViewModels
         void LoadPlugins(IMvxPluginManager pluginManager);
 
         void Initialize();
+
+        void Startup(object hint);
+
+        void Reset();
     }
 }
