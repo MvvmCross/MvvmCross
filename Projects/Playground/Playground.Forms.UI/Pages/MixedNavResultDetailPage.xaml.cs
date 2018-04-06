5d58afec2c27d0e39b50ee7728cd65da8f488874
Nick Randolph <nick@builttoroam.com>
2018-04-06 16:35:17 +1000

    Adding example of navigation with result Fixing issue where navigation back from masterdetail fails due to invalid cast

---
+++ Projects/Playground/Playground.Forms.UI/Pages/MixedNavResultDetailPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using MvvmCross.Forms.Presenters.Attributes;
+using MvvmCross.Forms.Views;
+using Playground.Core.ViewModels;
+using Xamarin.Forms.Xaml;
+
+namespace Playground.Forms.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    [MvxMasterDetailPagePresentation(Position = MasterDetailPosition.Detail, WrapInNavigationPage = true)]
+    public partial class MixedNavResultDetailPage : MvxContentPage<MixedNavResultDetailViewModel>
+    {
+        public MixedNavResultDetailPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
