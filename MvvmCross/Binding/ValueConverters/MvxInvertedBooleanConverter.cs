4d43a9e318f03f267abda96f94aac8798f97805a
Plac3hold3r <Plac3hold3r+github@outlook.com>
2018-04-17 22:09:04 +0200

    Add inverted bool converter

---
+++ MvvmCross/Binding/ValueConverters/MvxInvertedBooleanConverter.cs
@@ -0,0 +1,23 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MS-PL license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Globalization;
+using MvvmCross.Converters;
+
+namespace MvvmCross.Binding.ValueConverters
+{
+    public class MvxInvertedBooleanConverter : MvxValueConverter<bool, bool>
+    {
+        protected override bool Convert(bool value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !value;
+        }
+
+        protected override bool ConvertBack(bool value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !value;
+        }
+    }
+}
