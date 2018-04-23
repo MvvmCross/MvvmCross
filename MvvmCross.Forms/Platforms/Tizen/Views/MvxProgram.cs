505b5a1cf6f2898010e91c4173c55075fa63cd33
Martijn van Dijk <mhvdijk@gmail.com>
2018-04-23 14:44:15 +0200

    Add the basic infrastructure for Tizen and Forms

---
+++ MvvmCross.Forms/Platforms/Tizen/Views/MvxProgram.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MvvmCross.Forms.Presenters;
+using MvvmCross.Platforms.Tizen.Core;
+using Xamarin.Forms;
+
+namespace MvvmCross.Forms.Platforms.Tizen.Views
+{
+    public class MvxProgram : global::Xamarin.Forms.Platform.Tizen.FormsApplication
+    {
+        private Application _formsApplication;
+        protected Application FormsApplication
+        {
+            get
+            {
+                if (_formsApplication == null)
+                {
+                    var formsPresenter = Mvx.Resolve<IMvxFormsViewPresenter>();
+                    _formsApplication = formsPresenter.FormsApplication;
+                }
+
+                return _formsApplication;
+            }
+        }
+
+        private static global::Xamarin.Forms.Platform.Tizen.FormsApplication _application;
+        protected static global::Xamarin.Forms.Platform.Tizen.FormsApplication Application
+        {
+            get
+            {
+                if (_application == null)
+                {
+                    _application = new MvxProgram();
+                }
+
+                return _application;
+            }
+        }
+
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+
+            var setup = MvxTizenSetupSingleton.EnsureSingletonAvailable(Application);
+            setup.EnsureInitialized();
+
+            LoadApplication(FormsApplication);
+        }
+
+        static void Main(string[] args)
+        {
+            global::Xamarin.Forms.Platform.Tizen.Forms.Init(Application);
+            Application.Run(args);
+        }
+    }
+}
