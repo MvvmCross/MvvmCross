b40d64285084612b93215a6b031d85a7f3c4c93c
Jon Geller <borbmizzet@gmail.com>
2018-04-23 11:31:15 -0500

    Demonstrate Issue 2812

--- Projects/Playground/Playground.Forms.Droid/MainActivity.cs
+++ Projects/Playground/Playground.Forms.Droid/MainActivity.cs
@@ -5,14 +5,11 @@
 using Android.App;
 using Android.Content.PM;
 using Android.OS;
-using MvvmCross;
 using MvvmCross.Core;
 using MvvmCross.Forms.Platforms.Android.Core;
 using MvvmCross.Forms.Platforms.Android.Views;
-using MvvmCross.Forms.Presenters;
 using Playground.Core.ViewModels;
 using Playground.Forms.UI;
-using Xamarin.Forms;
 
 namespace Playground.Forms.Droid
 {
@@ -23,20 +20,15 @@ namespace Playground.Forms.Droid
         // MainLauncher = true, // No Splash Screen: Uncomment this lines if removing splash screen
         ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation,
         LaunchMode = LaunchMode.SingleTask)]
-    public class MainActivity : MvxFormsAppCompatActivity
+    public class MainActivity : MvxFormsAppCompatActivity <RootViewModel>
     // No Splash Screen: use this base instead
-    // MvxFormsAppCompatActivity<Setup, Core.App, FormsApp>
+    // MvxFormsAppCompatActivity<Setup, Core.App, FormsApp, MainViewModel>
     {
         protected override void OnCreate(Bundle bundle)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
             base.OnCreate(bundle);
-        }
-
-        protected override void OnDestroy()
-        {
-            base.OnDestroy();
         }
     }
 }
