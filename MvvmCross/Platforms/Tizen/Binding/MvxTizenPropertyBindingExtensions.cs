ec87a87e32f2140133310c354aec564bd38bd569
Martijn van Dijk <mhvdijk@gmail.com>
2018-04-23 14:44:15 +0200

    Add the basic infrastructure for Tizen and Forms

---
+++ MvvmCross/Platforms/Tizen/Binding/MvxTizenPropertyBindingExtensions.cs
@@ -0,0 +1,11 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MS-PL license.
+// See the LICENSE file in the project root for more information.
+
+namespace MvvmCross.Platforms.Tizen.Binding
+{
+    public static class MvxTizenPropertyBindingExtensions
+    {
+
+    }
+}
