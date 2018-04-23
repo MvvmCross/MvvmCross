1c952a5e08136b508ee4120fd3c581c5dc3fc7cc
Martijn van Dijk <mhvdijk@gmail.com>
2018-04-23 14:44:15 +0200

    Add the basic infrastructure for Tizen and Forms

---
+++ MvvmCross/Platforms/Tizen/Binding/MvxTizenBindingBuilder.cs
@@ -0,0 +1,66 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MS-PL license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using MvvmCross.Base;
+using MvvmCross.Converters;
+using MvvmCross.Binding;
+using MvvmCross.Binding.Binders;
+using MvvmCross.Binding.BindingContext;
+using MvvmCross.Binding.Bindings.Target.Construction;
+
+namespace MvvmCross.Platforms.Tizen.Binding
+{
+    public class MvxTizenBindingBuilder
+        : MvxBindingBuilder
+    {
+        private readonly Action<IMvxTargetBindingFactoryRegistry> _fillRegistryAction;
+        private readonly Action<IMvxValueConverterRegistry> _fillValueConvertersAction;
+        private readonly Action<IMvxAutoValueConverters> _fillAutoValueConvertersAction;
+        private readonly Action<IMvxBindingNameRegistry> _fillBindingNamesAction;
+
+        public MvxTizenBindingBuilder(Action<IMvxTargetBindingFactoryRegistry> fillRegistryAction = null,
+                                    Action<IMvxValueConverterRegistry> fillValueConvertersAction = null,
+                                    Action<IMvxAutoValueConverters> fillAutoValueConvertersAction = null,
+                                    Action<IMvxBindingNameRegistry> fillBindingNamesAction = null)
+        {
+            _fillRegistryAction = fillRegistryAction;
+            _fillValueConvertersAction = fillValueConvertersAction;
+            _fillAutoValueConvertersAction = fillAutoValueConvertersAction;
+            _fillBindingNamesAction = fillBindingNamesAction;
+        }
+
+        protected override void FillTargetFactories(IMvxTargetBindingFactoryRegistry registry)
+        {
+            base.FillTargetFactories(registry);
+
+            //TODO: Add targets
+
+            _fillRegistryAction?.Invoke(registry);
+        }
+
+        protected override void FillValueConverters(IMvxValueConverterRegistry registry)
+        {
+            base.FillValueConverters(registry);
+
+            _fillValueConvertersAction?.Invoke(registry);
+        }
+
+        protected override void FillAutoValueConverters(IMvxAutoValueConverters autoValueConverters)
+        {
+            base.FillAutoValueConverters(autoValueConverters);
+
+            _fillAutoValueConvertersAction?.Invoke(autoValueConverters);
+        }
+
+        protected override void FillDefaultBindingNames(IMvxBindingNameRegistry registry)
+        {
+            base.FillDefaultBindingNames(registry);
+
+            //TODO: Add default names
+
+            _fillBindingNamesAction?.Invoke(registry);
+        }
+    }
+}
--- MvvmCross/Platforms/Tizen/Views/IMvxTizenViewsContainer.cs
+++ MvvmCross/Platforms/Tizen/Views/IMvxTizenViewsContainer.cs
@@ -2,12 +2,12 @@
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
 
-using MvvmCross.Presenters;
+using MvvmCross.Views;
 
-namespace MvvmCross.Platform.Tizen.Presenters
+namespace MvvmCross.Platforms.Tizen.Views
 {
-    public interface IMvxTizenViewPresenter
-        : IMvxViewPresenter
+    public interface IMvxTizenViewsContainer
+        : IMvxViewsContainer
     {
     }
 }
