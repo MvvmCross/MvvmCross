01368a8d58a073ca0eaa2f620ecdeeb1228fc86c
tbalcom <trevorbalcom@me.com>
2018-05-23 10:58:26 -0400

    Fixes compiler error: Cannot implicitly convert type 'bool?' to 'bool'.

--- MvvmCross/Platforms/Uap/Views/MvxWindowsPage.cs
+++ MvvmCross/Platforms/Uap/Views/MvxWindowsPage.cs
@@ -72,7 +72,7 @@ namespace MvvmCross.Platforms.Uap.Views
         {
             var backStack = base.Frame?.BackStack;
 
-            while (backStack?.Any())
+            while (backStack?.Any() == true)
             {
                 backStack.RemoveAt(0);
             }
