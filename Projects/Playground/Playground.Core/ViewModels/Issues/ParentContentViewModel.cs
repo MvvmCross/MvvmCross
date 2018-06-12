84bb1e2e80268223cddcfdac6b94386eda2b2ae9
Jon Geller <1184827+borbmizzet@users.noreply.github.com>
2018-06-12 17:06:57 -0500

    demonstrates that xaml binding a child view's BindingContextProperty no longer works

--- Projects/Playground/Playground.Core/ViewModels/Issues/ParentContentViewModel.cs
+++ Projects/Playground/Playground.Core/ViewModels/Issues/ParentContentViewModel.cs
@@ -6,13 +6,22 @@ namespace Playground.Core.ViewModels
 {
     public class ParentContentViewModel : MvxViewModel
     {
-        private ChildContentViewModel _childViewModel;
+        private ChildContentViewModel _childViewModel1;
         public ChildContentViewModel ChildViewModel1
         {
-            get => _childViewModel;
+            get => _childViewModel1;
             set
             {
-                SetProperty(ref _childViewModel, value);
+                SetProperty(ref _childViewModel1, value);
+            }
+        }
+        private ChildContentViewModel _childBindingContext2;
+        public ChildContentViewModel ChildBindingContext2
+        {
+            get => _childBindingContext2;
+            set
+            {
+                SetProperty(ref _childBindingContext2, value);
             }
         }
         private bool _childViewModelEnabled;
@@ -24,13 +33,19 @@ namespace Playground.Core.ViewModels
                 SetProperty(ref _childViewModelEnabled, value);
             }
         }
-        public IMvxCommand ChangeButtonCmd => new MvxCommand(() => ChildViewModel1.Test = (ChildViewModel1.Test == "Bound Text 1" ? "Bound Text 2" : "Bound Text 1"));
-        public IMvxCommand ToggleChildEnabledCmd => new MvxCommand(() => ChildViewModelEnabled = !ChildViewModelEnabled);
+        public IMvxCommand ChangeButtonCmd1 => new MvxCommand(() => ChildViewModel1.Test = (ChildViewModel1.Test == "Bound Text 1" ? "Bound Text 2" : "Bound Text 1"));
+        public IMvxCommand ToggleChild1EnabledCmd => new MvxCommand(() => ChildViewModelEnabled = !ChildViewModelEnabled);
+
+        public IMvxCommand ChangeButtonCmd2 => new MvxCommand(() => ChildBindingContext2.Test = (ChildBindingContext2.Test == "Bound Text 1" ? "Bound Text 2" : "Bound Text 1"));
 
         public override void Prepare()
         {
             var vm = Mvx.Resolve<IMvxViewModelLoader>().LoadViewModel(MvxViewModelRequest<ChildContentViewModel>.GetDefaultRequest(), null) as ChildContentViewModel;
             ChildViewModel1 = vm;
+            var bc = Mvx.Resolve<IMvxViewModelLoader>()
+                    .LoadViewModel(MvxViewModelRequest<ChildContentViewModel>.GetDefaultRequest(), null) as
+                ChildContentViewModel;
+            ChildBindingContext2 = bc;
         }
     }
 }
