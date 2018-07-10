6937b4ae776768237440a96a6b569d0ba86eca9a
Johnny <johnnydavidwebb@gmail.com>
2018-07-09 20:23:05 -0400

    return task instead of completedtask

--- UnitTests/MvvmCross.UnitTest/ViewModels/MvxObservableCollectionTest.cs
+++ UnitTests/MvvmCross.UnitTest/ViewModels/MvxObservableCollectionTest.cs
@@ -36,8 +36,7 @@ namespace MvvmCross.UnitTest.ViewModels
 
             public Task ExecuteOnMainThreadAsync(Func<Task> action, bool maskExceptions = true)
             {
-                action?.Invoke();
-                return Task.CompletedTask;
+                return action?.Invoke();
             }
         }
 
