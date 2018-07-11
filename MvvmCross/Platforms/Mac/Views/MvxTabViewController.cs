32b6868e84740e6b7c35a56822736702989b325e
Martijn van Dijk <mhvdijk@gmail.com>
2018-07-11 18:10:47 +0200

    Fix up Forms and other views to handle destroy

--- MvvmCross/Platforms/Mac/Views/MvxTabViewController.cs
+++ MvvmCross/Platforms/Mac/Views/MvxTabViewController.cs
@@ -1,4 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
 
@@ -115,6 +115,12 @@ namespace MvvmCross.Platforms.Mac.Views
         {
             base.PrepareForSegue(segue, sender);
             this.ViewModelRequestForSegue(segue, sender);
+        }
+
+        public override void RemoveFromParentViewController()
+        {
+            base.RemoveFromParentViewController();
+            ViewModel?.ViewDestroy();
         }
     }
 
