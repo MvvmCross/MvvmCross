379ca5d9840603b50311034e3b091b10e62564c5
Martin Zikmund <martinzikmund@sphereline.com>
2018-08-02 10:12:18 +0200

    Xamarin.Forms Converters sample page with both XF and Native converter

---
+++ Projects/Playground/Playground.Forms.UI/Pages/ConvertersPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MvvmCross.Forms.Views;
+using Playground.Core.ViewModels.Samples;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Playground.Forms.UI.Pages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ConvertersPage : MvxContentPage<ConvertersViewModel>
+	{
+		public ConvertersPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
