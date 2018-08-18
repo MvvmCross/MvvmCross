f3af2d7b46db191ff803d2a99978f98cb970524a
Plac3hold3r <Plac3hold3r+github@outlook.com>
2018-08-18 22:50:06 +0200

    Add demo to playground

---
+++ Projects/Playground/Playground.Core/ViewModels/Bindings/FluentBindingViewModel.cs
@@ -0,0 +1,35 @@
+﻿using MvvmCross.Commands;
+using MvvmCross.Logging;
+using MvvmCross.Navigation;
+using MvvmCross.ViewModels;
+
+namespace Playground.Core.ViewModels.Bindings
+{
+    public class FluentBindingViewModel : BaseViewModel
+    {
+        bool _bindingsEnabled = true;
+
+        public FluentBindingViewModel(IMvxLogProvider logProvider, IMvxNavigationService navigationService)
+            : base(logProvider, navigationService)
+        {
+            ClearBindingsCommand = new MvxCommand(ClearBindings);
+        }
+
+        public IMvxCommand ClearBindingsCommand { get; }
+
+        public MvxInteraction<bool> ClearBindingInteraction { get; } = new MvxInteraction<bool>();
+
+        string _name;
+        public string TextValue
+        {
+            get => _name;
+            set => SetProperty(ref _name, value);
+        }
+
+        void ClearBindings()
+        {
+            _bindingsEnabled = !_bindingsEnabled;
+            ClearBindingInteraction.Raise(_bindingsEnabled);
+        }
+    }
+}
