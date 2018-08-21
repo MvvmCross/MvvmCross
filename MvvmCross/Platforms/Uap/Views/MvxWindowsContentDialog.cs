7f0a0f742211a25b3829d8890ed52da88fa94c9e
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2018-08-21 11:52:44 +0200

    Recomposition ModalView for Uap

--- MvvmCross/Platforms/Uap/Views/MvxWindowsContentDialog.cs
+++ MvvmCross/Platforms/Uap/Views/MvxWindowsContentDialog.cs
@@ -2,23 +2,15 @@
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using MvvmCross.Platforms.Uap.Views.Suspension;
 using MvvmCross.ViewModels;
 using MvvmCross.Views;
-using Windows.UI.Core;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Navigation;
 
 namespace MvvmCross.Platforms.Uap.Views
 {
     public class MvxWindowsContentDialog
         : ContentDialog
         , IMvxView
-        , IDisposable
     {
         public MvxWindowsContentDialog()
         {
@@ -28,10 +20,7 @@ namespace MvvmCross.Platforms.Uap.Views
 
         public IMvxViewModel ViewModel
         {
-            get
-            {
-                return _viewModel;
-            }
+            get => _viewModel;
             set
             {
                 if (_viewModel == value)
@@ -39,24 +28,6 @@ namespace MvvmCross.Platforms.Uap.Views
 
                 _viewModel = value;
                 DataContext = ViewModel;
-            }
-        }
-
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-
-        ~MvxWindowsContentDialog()
-        {
-            Dispose(false);
-        }
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
             }
         }
     }
@@ -67,8 +38,8 @@ namespace MvvmCross.Platforms.Uap.Views
     {
         public new TViewModel ViewModel
         {
-            get { return (TViewModel)base.ViewModel; }
-            set { base.ViewModel = value; }
+            get => (TViewModel)base.ViewModel;
+            set => base.ViewModel = value;
         }
     }
 }
