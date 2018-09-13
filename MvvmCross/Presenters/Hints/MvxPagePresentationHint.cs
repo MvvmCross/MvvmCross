9b03bc505f55a7732210bd63a1029bdeb0e264e0
Tomasz Cielecki <tomasz@ostebaronen.dk>
2018-09-13 18:19:29 +0200

    Not only for Forms anymore

--- MvvmCross/Presenters/Hints/MvxPagePresentationHint.cs
+++ MvvmCross/Presenters/Hints/MvxPagePresentationHint.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
 
@@ -7,7 +7,6 @@ using MvvmCross.ViewModels;
 
 namespace MvvmCross.Presenters.Hints
 {
-    //Only available on Xamarin.Forms
     public class MvxPagePresentationHint
         : MvxPresentationHint
     {
