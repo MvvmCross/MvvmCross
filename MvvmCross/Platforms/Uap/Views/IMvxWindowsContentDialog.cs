42659c2b4663a6a82c4504274f10ad31f6bf4ace
Martijn van Dijk <mhvdijk@gmail.com>
2018-10-03 13:39:33 +0200

    Make interface simpler

--- MvvmCross/Platforms/Uap/Views/IMvxWindowsContentDialog.cs
+++ MvvmCross/Platforms/Uap/Views/IMvxWindowsContentDialog.cs
@@ -2,15 +2,19 @@
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
 
+using MvvmCross.ViewModels;
 using MvvmCross.Views;
-using Windows.Foundation;
-using Windows.UI.Xaml.Controls;
 
 namespace MvvmCross.Platforms.Uap.Views
 {
     public interface IMvxWindowsContentDialog
         : IMvxView
     {
-        IAsyncOperation<ContentDialogResult> ShowAsync(ContentDialogPlacement placement);
+    }
+
+    public interface IMvxWindowsContentDialog<TViewModel>
+        : IMvxWindowsContentDialog
+        , IMvxView<TViewModel> where TViewModel : class, IMvxViewModel
+    {
     }
 }
