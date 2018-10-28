39d1afb106aa4226b7713a23d7ea52c00fd5e3bd
Nick Randolph <nick@builttoroam.com>
2018-10-28 21:33:34 +1100

    Moved interface to separate file

---
+++ MvvmCross/Presenters/Attributes/IMvxPresentationAttribute.cs
@@ -0,0 +1,21 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MS-PL license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+
+namespace MvvmCross.Presenters.Attributes
+{
+    public interface IMvxPresentationAttribute
+    {
+        /// <summary>
+        /// That shall be used only if you are using non generic views.
+        /// </summary>
+        Type ViewModelType { get; set; }
+
+        /// <summary>
+        /// Type of the view
+        /// </summary>
+        Type ViewType { get; set; }
+    }
+}
