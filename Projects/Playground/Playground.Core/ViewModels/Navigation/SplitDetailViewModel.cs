d91d545c10db7e6ca3208a3e4520ed089e713cb0
Kollmann, Michael <Michael.Kollmann@presens.de>
2018-10-30 12:25:13 +0100

    The newly introduced command no longer breaks compatibility with some other non-forms UI

--- Projects/Playground/Playground.Core/ViewModels/Navigation/SplitDetailViewModel.cs
+++ Projects/Playground/Playground.Core/ViewModels/Navigation/SplitDetailViewModel.cs
@@ -14,11 +14,13 @@ namespace Playground.Core.ViewModels
         public SplitDetailViewModel(IMvxLogProvider logProvider, IMvxNavigationService navigationService) : base(logProvider, navigationService)
         {
             ShowChildCommand = new MvxAsyncCommand(async () => await NavigationService.Navigate<SplitDetailNavViewModel>());
-            ShowTabsCommand = new MvxAsyncCommand(async () => await NavigationService.Navigate<TabsRootViewModel>());
+            ShowTabsCommand = new MvxAsyncCommand(async () => await NavigationService.Navigate<TabsRootBViewModel>());
+            ShowTabbedChildCommand = new MvxAsyncCommand(async () => await NavigationService.Navigate<TabsRootViewModel>());
         }
 
         public IMvxAsyncCommand ShowChildCommand { get; private set; }
         public IMvxAsyncCommand ShowTabsCommand { get; private set; }
+        public IMvxAsyncCommand ShowTabbedChildCommand { get; private set; }
 
         public string ContentText => "Text for the Content Area";
 
