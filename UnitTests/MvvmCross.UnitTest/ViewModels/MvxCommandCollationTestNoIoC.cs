0bddbdbd9f5a4ecae5016272bcea53a6e802ffd9
Tomasz Cielecki <tomasz@ostebaronen.dk>
2018-11-12 22:07:48 +0100

    Add test cases for when no IoC present and using MvxCommand

---
+++ UnitTests/MvvmCross.UnitTest/ViewModels/MvxCommandCollationTestNoIoC.cs
@@ -0,0 +1,43 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MS-PL license.
+// See the LICENSE file in the project root for more information.
+
+using MvvmCross.Commands;
+using Xunit;
+
+namespace MvvmCross.UnitTest.ViewModels
+{
+    [Collection("MvxTest")]
+    public class MvxCommandCollationTestNoIoC
+    {
+        private NavigationTestFixture _fixture;
+
+        public MvxCommandCollationTestNoIoC(NavigationTestFixture fixture)
+        {
+            _fixture = fixture;
+            // ensure singletons are cleared, no IoC please :)
+            fixture.Reset();
+        }
+
+        [Fact]
+        public void CanConstructCommand()
+        {
+            var command = new MvxCommand(() => {/* nothing to see here */});
+
+            Assert.NotNull(command);
+        }
+
+        [Fact]
+        public void CanExecuteCommand()
+        {
+            var i = 0;
+            var command = new MvxCommand(() => { ++i; });
+
+            Assert.NotNull(command);
+
+            command.Execute();
+
+            Assert.Equal(1, i);
+        }
+    }
+}
