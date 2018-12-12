889ba8d3b6c197a36835cee30b317bfd89c6ecf8
Tomasz Cielecki <tomasz@ostebaronen.dk>
2018-12-12 20:04:11 +0100

    Make SignleThreadSynchronizationContext internal such that we can check against its type

--- MvvmCross/Base/MvxAsyncExtensions.cs
+++ MvvmCross/Base/MvxAsyncExtensions.cs
@@ -1,4 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
 
@@ -57,7 +57,7 @@ namespace MvvmCross.Base
         /// <summary>
         /// Provides a SynchronizationContext that is single-threaded.
         /// </summary>
-        private sealed class SingleThreadSynchronizationContext : SynchronizationContext
+        internal sealed class SingleThreadSynchronizationContext : SynchronizationContext
         {
             private readonly BlockingCollection<Tuple<SendOrPostCallback, object>> _queue;
 
