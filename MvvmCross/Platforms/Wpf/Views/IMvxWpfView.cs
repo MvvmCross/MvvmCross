c6e4ea6d2b322b7758eb4276e1e2d07736d118f6
Raymond Sun <uniquesyp@126.com>
2018-12-29 18:03:57 +0800

    Fix wpf fluent binding,

--- MvvmCross/Platforms/Wpf/Views/IMvxWpfView.cs
+++ MvvmCross/Platforms/Wpf/Views/IMvxWpfView.cs
@@ -1,14 +1,15 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
 
+using MvvmCross.Binding.BindingContext;
 using MvvmCross.ViewModels;
 using MvvmCross.Views;
 
 namespace MvvmCross.Platforms.Wpf.Views
 {
     public interface IMvxWpfView
-        : IMvxView
+        : IMvxView, IMvxBindingContextOwner
     {
     }
 
