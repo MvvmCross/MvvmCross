45ed2fc49876529be5cb1fdf4ccb6adeee2f46e1
abdelrahman <AbdelrahmanGIT@users.noreply.github.com>
2019-02-18 14:09:50 +0300

    Return the task to properly await showing view model in TvOs

--- MvvmCross/Platforms/Tvos/Views/MvxTvosViewDispatcher.cs
+++ MvvmCross/Platforms/Tvos/Views/MvxTvosViewDispatcher.cs
@@ -23,10 +23,10 @@ namespace MvvmCross.Platforms.Tvos.Views
 
         public async Task<bool> ShowViewModel(MvxViewModelRequest request)
         {
-            Action action = () =>
+            Func<Task> action = () =>
                 {
                     MvxLog.Instance.Trace("tvOSNavigation", "Navigate requested");
-                    _presenter.Show(request);
+                    return _presenter.Show(request);
                 };
             await ExecuteOnMainThreadAsync(action);
             return true;
