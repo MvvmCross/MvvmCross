b954bde6cfebc78ae65758be0ad63c8493c65dfc
Martijn van Dijk <mhvdijk@gmail.com>
2019-03-22 16:44:06 +0100

    Move attributes

--- MvvmCross.Forms/MvxFormsLog.cs
+++ MvvmCross.Forms/MvxFormsLog.cs
@@ -4,8 +4,12 @@
 
 using System.Runtime.CompilerServices;
 using MvvmCross.Logging;
+using Xamarin.Forms;
 
 [assembly: InternalsVisibleTo("MvvmCross.Forms.Platforms.Wpf")]
+[assembly: XmlnsDefinition("http://mvvmcross.com/bind", nameof(MvvmCross.Forms.Bindings))]
+[assembly: XmlnsDefinition("http://mvvmcross.com/bind", nameof(MvvmCross.Forms.Converters))]
+[assembly: XmlnsDefinition("http://mvvmcross.com/bind", nameof(MvvmCross.Forms.Views))]
 namespace MvvmCross.Forms
 {
     internal static class MvxFormsLog
