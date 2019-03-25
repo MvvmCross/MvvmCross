ca4eb000417308234a59c5ce7221ce3a606e9ca4
Martijn van Dijk <mhvdijk@gmail.com>
2019-03-25 14:39:23 +0100

    Change file

--- MvvmCross.Forms/MvxFormsLog.cs
+++ MvvmCross.Forms/MvxFormsLog.cs
@@ -4,12 +4,8 @@
 
 using System.Runtime.CompilerServices;
 using MvvmCross.Logging;
-using Xamarin.Forms;
 
 [assembly: InternalsVisibleTo("MvvmCross.Forms.Platforms.Wpf")]
-[assembly: XmlnsDefinition("http://mvvmcross.com/bind", nameof(MvvmCross.Forms.Bindings))]
-[assembly: XmlnsDefinition("http://mvvmcross.com/bind", nameof(MvvmCross.Forms.Converters))]
-[assembly: XmlnsDefinition("http://mvvmcross.com/bind", nameof(MvvmCross.Forms.Views))]
 namespace MvvmCross.Forms
 {
     internal static class MvxFormsLog
