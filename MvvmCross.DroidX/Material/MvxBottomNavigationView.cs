f513b43eab13db7db7155576e5a6a0f5c7ca2968
Paul <paul@pepper-nuts.nl>
2019-03-26 10:07:37 +0100

    Added the MvxBottomNavigationView, Added the MvxBottomNavigationViewPresentationAttribute, Updated the MvxAppCompatViewPresenter so it works with the bottom navigation view attribute (and MvxPagePresentationHint)

---
+++ MvvmCross.DroidX/Material/MvxBottomNavigationView.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Input;
+using Android.Content;
+using Android.Runtime;
+using Android.Widget;
+using Android.Util;
+using Android.Views;
+using Android.Support.Design.Widget;
+
+namespace MvvmCross.Droid.Support.Design
+{
+    [Register("mvvmcross.droid.support.design.MvxBottomNavigationView")]
+    public class MvxBottomNavigationView : BottomNavigationView, BottomNavigationView.IOnNavigationItemSelectedListener
+    {
+        public List<MvxMenuItem> ItemsSource = new List<MvxMenuItem>();
+
+        public MvxBottomNavigationView(Context context) : base(context)
+        {
+            this.SetOnNavigationItemSelectedListener(this);
+        }
+
+        public MvxBottomNavigationView(Context context, IAttributeSet attrs) : base(context, attrs)
+        {
+            this.SetOnNavigationItemSelectedListener(this);
+        }
+
+        public MvxBottomNavigationView(Context context, IAttributeSet attrs, int defStyleAttr) : base(context, attrs, defStyleAttr)
+        {
+            this.SetOnNavigationItemSelectedListener(this);
+        }
+
+        protected MvxBottomNavigationView(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+        {
+            this.SetOnNavigationItemSelectedListener(this);
+        }
+
+        public void AddItem(IMenuItem item, Type viewModel)
+        {
+            ItemsSource.Add(new MvxMenuItem(item, viewModel));
+
+            // The first item is autoselected
+            if (ItemsSource.Count == 1)
+            {
+                OnNavigationItemSelected(item);
+            }
+        }
+
+        public bool OnNavigationItemSelected(IMenuItem item)
+        {
+            var mvxMenuItem = this.FindItemByMenuItem(item);
+
+            if (mvxMenuItem != null && HandleNavigate.CanExecute(mvxMenuItem.ViewModel))
+            {
+                HandleNavigate.Execute(mvxMenuItem.ViewModel);
+                return true;
+            }
+
+            return false;
+        }
+
+        public MvxMenuItem FindItemByViewModel(Type viewModel)
+        {
+            return ItemsSource.Find(i => i.ViewModel == viewModel);
+        }
+
+        public int FindPositionByViewModel(Type viewModel)
+        {
+            return ItemsSource.FindIndex(i => i.ViewModel == viewModel);
+        }
+
+        public MvxMenuItem FindItemByMenuItem(IMenuItem item)
+        {
+            return ItemsSource.Find(i => i.MenuItem == item);
+        }
+
+        public ICommand HandleNavigate { get; set; }
+    }
+
+    public class MvxMenuItem
+    {
+        public IMenuItem MenuItem { get; set; }
+        public Type ViewModel { get; set; }
+
+        public MvxMenuItem(IMenuItem menuItem, Type viewModel)
+        {
+            this.MenuItem = menuItem;
+            this.ViewModel = viewModel;
+        }
+    }
+}
---
+++ MvvmCross/Platforms/Android/Presenters/Attributes/MvxBottomNavigationViewPresentationAttribute.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using MvvmCross;
+using MvvmCross.Platforms.Android;
+using MvvmCross.Platforms.Android.Presenters.Attributes;
+using MvvmCross.Presenters.Attributes;
+
+namespace MvvmCross.Platforms.Android.Presenters.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public class MvxBottomNavigationViewPresentationAttribute : MvxFragmentPresentationAttribute
+    {
+        public MvxBottomNavigationViewPresentationAttribute()
+        {
+        }
+
+        public MvxBottomNavigationViewPresentationAttribute(int bottomNavigationResourceId, string title, int drawableItemId = int.MinValue,
+            Type activityHostViewModelType = null, int fragmentContentId = int.MinValue, bool addToBackStack = false, Type fragmentHostViewType = null,
+            bool isCacheableFragment = false, string tag = null)
+            : base(activityHostViewModelType, fragmentContentId, addToBackStack,
+                  int.MinValue, int.MinValue, int.MinValue, int.MinValue, int.MinValue, fragmentHostViewType,
+                  isCacheableFragment, tag)
+        {
+            BottomNavigationResourceId = bottomNavigationResourceId;
+            DrawableItemId = drawableItemId;
+            Title = title;
+        }
+
+        public MvxBottomNavigationViewPresentationAttribute(string bottomNavigationResourceName, string title, int drawableItemId = int.MinValue,
+            Type activityHostViewModelType = null, bool addToBackStack = false, Type fragmentHostViewType = null,
+            bool isCacheableFragment = false, string tag = null)
+            : base(activityHostViewModelType, null, addToBackStack, null, null, null,
+                null, null, fragmentHostViewType, isCacheableFragment, tag)
+        {
+            var context = Mvx.IoCProvider.Resolve<IMvxAndroidGlobals>().ApplicationContext;
+
+            BottomNavigationResourceId = !string.IsNullOrEmpty(bottomNavigationResourceName)
+                ? context.Resources.GetIdentifier(bottomNavigationResourceName, "id", context.PackageName)
+                : global::Android.Resource.Id.Content;
+            DrawableItemId = drawableItemId;
+            Title = title;
+        }
+
+        /// <summary>
+        ///     The resource used to get the BottomNavigation from the view
+        /// </summary>
+        public int BottomNavigationResourceId { get; set; }
+
+        /// <summary>
+        ///     The resource used to get the menu item from the view
+        /// </summary>
+        public int DrawableItemId { get; set; }
+
+        /// <summary>
+        ///     The title for the bottom navigation menu item
+        /// </summary>
+        public string Title { get; set; }
+    }
+}
