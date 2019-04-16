c139ad204827ba4810ef3266ca77b255cad24634
Martijn van Dijk <mhvdijk@gmail.com>
2019-04-16 23:35:07 +0200

    Add xml attribute to optimize importing of classes on Forms (#3331)
    * Add xml attribute to optimize importing of classes, controls and effects into XAML code
    
    * Move attributes
    
    * Change file
    
    * Rename file

---
+++ MvvmCross.Forms/AssemblyInfoAttributes.cs
@@ -0,0 +1,5 @@
+﻿using Xamarin.Forms;
+
+[assembly: XmlnsDefinition("http://mvvmcross.com/bind", nameof(MvvmCross.Forms.Bindings))]
+[assembly: XmlnsDefinition("http://mvvmcross.com/bind", nameof(MvvmCross.Forms.Converters))]
+[assembly: XmlnsDefinition("http://mvvmcross.com/bind", nameof(MvvmCross.Forms.Views))]
