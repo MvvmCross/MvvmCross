c9d97e598708d04a9b321f106a74fbc0b04da766
Robert Till <rob.till@outlook.com>
2019-07-04 06:48:55 -0400

    Removed MvxColor and replaced with System.Drawing.Color.

--- MvvmCross.Plugins/Color/Platforms/Ios/MvxIosColor.cs
+++ MvvmCross.Plugins/Color/Platforms/Ios/MvxIosColor.cs
@@ -1,4 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
 
@@ -10,14 +10,14 @@ namespace MvvmCross.Plugin.Color.Platforms.Ios
     [Preserve(AllMembers = true)]
     public class MvxIosColor : IMvxNativeColor
     {
-        public object ToNative(MvxColor mvxColor)
+        public object ToNative(System.Drawing.Color color)
         {
-            return ToUIColor(mvxColor);
+            return ToUIColor(color);
         }
 
-        public static UIColor ToUIColor(MvxColor mvxColor)
+        public static UIColor ToUIColor(System.Drawing.Color color)
         {
-            return new UIColor(mvxColor.R / 255.0f, mvxColor.G / 255.0f, mvxColor.B / 255.0f, mvxColor.A / 255.0f);
+            return new UIColor(color.R / 255.0f, color.G / 255.0f, color.B / 255.0f, color.A / 255.0f);
         }
     }
 }
