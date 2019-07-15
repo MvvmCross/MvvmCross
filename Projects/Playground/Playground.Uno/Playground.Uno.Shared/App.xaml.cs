24d00f35cddd25cd18fe9b9d5b4a82629bae506c
Nick Randolph <nick@builttoroam.com>
2019-07-16 09:48:25 +1000

    Fixing sample and startup to prevent blocking

--- Projects/Playground/Playground.Uno/Playground.Uno.Shared/App.xaml.cs
+++ Projects/Playground/Playground.Uno/Playground.Uno.Shared/App.xaml.cs
@@ -1,5 +1,8 @@
-﻿using MvvmCross.Platforms.Uap.Core;
+﻿using System.Diagnostics;
+using Microsoft.Extensions.Logging;
+using MvvmCross.Platforms.Uap.Core;
 using MvvmCross.Platforms.Uap.Views;
+using Uno.Extensions;
 
 namespace Playground.Uwp
 {
@@ -7,7 +10,59 @@ namespace Playground.Uwp
     {
         public App()
         {
+            ConfigureFilters(LogExtensionPoint.AmbientLoggerFactory);
             InitializeComponent();
+
+            UnhandledException += App_UnhandledException;
+        }
+
+        private void App_UnhandledException(object sender, Windows.UI.Xaml.UnhandledExceptionEventArgs e)
+        {
+            Debug.WriteLine("Unhandled exception:" + e.Message + "|" + e.Exception?.Message);
+            Debug.WriteLine("Stack:" + e.Exception?.StackTrace);
+            Debug.WriteLine("Inner exception:" + e.Exception?.InnerException?.Message);
+            Debug.WriteLine("Inner stack:" + e.Exception?.InnerException?.StackTrace);
+        }
+
+        /// <summary>
+        /// Configures global logging
+        /// </summary>
+        /// <param name="factory"></param>
+        static void ConfigureFilters(ILoggerFactory factory)
+        {
+            factory
+                .WithFilter(new FilterLoggerSettings
+                    {
+                        { "Uno", LogLevel.Warning },
+                        { "Windows", LogLevel.Warning },
+
+                        // Debug JS interop
+                        // { "Uno.Foundation.WebAssemblyRuntime", LogLevel.Debug },
+
+                        // Generic Xaml events
+                        // { "Windows.UI.Xaml", LogLevel.Debug },
+                        // { "Windows.UI.Xaml.VisualStateGroup", LogLevel.Debug },
+                        // { "Windows.UI.Xaml.StateTriggerBase", LogLevel.Debug },
+                        // { "Windows.UI.Xaml.UIElement", LogLevel.Debug },
+
+                        // Layouter specific messages
+                        // { "Windows.UI.Xaml.Controls", LogLevel.Debug },
+                        // { "Windows.UI.Xaml.Controls.Layouter", LogLevel.Debug },
+                        // { "Windows.UI.Xaml.Controls.Panel", LogLevel.Debug },
+                        // { "Windows.Storage", LogLevel.Debug },
+
+                        // Binding related messages
+                        // { "Windows.UI.Xaml.Data", LogLevel.Debug },
+
+                        // DependencyObject memory references tracking
+                        // { "ReferenceHolder", LogLevel.Debug },
+                    }
+                )
+#if DEBUG
+                .AddConsole(LogLevel.Debug);
+#else
+				.AddConsole(LogLevel.Information);
+#endif
         }
     }
 
