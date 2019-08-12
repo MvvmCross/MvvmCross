8829d7d7d5b1b404ffcd2a328eec745277b3b5a0
Robert Till <rob.till@outlook.com>
2019-08-12 14:55:09 -0400

    Removed MvxColor and replaced with System.Drawing.Color. (#3456)

--- MvvmCross.Plugins/Color/Platforms/Android/MvxAndroidColor.cs
+++ MvvmCross.Plugins/Color/Platforms/Android/MvxAndroidColor.cs
@@ -1,4 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
 
@@ -9,14 +9,14 @@ namespace MvvmCross.Plugin.Color.Platforms.Android
     [Preserve(AllMembers = true)]
     public class MvxAndroidColor : IMvxNativeColor
     {
-        public object ToNative(MvxColor mvxColor)
+        public object ToNative(System.Drawing.Color color)
         {
-            return ToNativeColor(mvxColor);
+            return ToNativeColor(color);
         }
 
-        public global::Android.Graphics.Color ToNativeColor(MvxColor mvxColor)
+        public global::Android.Graphics.Color ToNativeColor(System.Drawing.Color color)
         {
-            return new global::Android.Graphics.Color(mvxColor.R, mvxColor.G, mvxColor.B, mvxColor.A);
+            return new global::Android.Graphics.Color(color.R, color.G, color.B, color.A);
         }
     }
 }
