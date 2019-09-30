9920fdf64dffd475a4972162c98b3cd008dac55b
Tomasz Cielecki <tci@trackman.com>
2019-09-30 13:04:53 +0200

    Add location sample to playground

---
+++ Projects/Playground/Playground.Droid/Fragments/LocationView.cs
@@ -0,0 +1,23 @@
+﻿using Android.OS;
+using Android.Views;
+using MvvmCross.Droid.Support.V4;
+using MvvmCross.Platforms.Android.Binding.BindingContext;
+using MvvmCross.Platforms.Android.Presenters.Attributes;
+using Playground.Core.ViewModels;
+using Playground.Core.ViewModels.Location;
+
+namespace Playground.Droid.Fragments
+{
+    [MvxFragmentPresentation(typeof(RootViewModel), Resource.Id.content_frame)]
+    public class LocationView : MvxFragment<LocationViewModel>
+    {
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            _ = base.OnCreateView(inflater, container, savedInstanceState);
+
+            var view = this.BindingInflate(Resource.Layout.location, container, false);
+
+            return view;
+        }
+    }
+}
