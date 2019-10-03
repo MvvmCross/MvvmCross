294bdffefe1e882ca6c0c169862ba3c79f5abe62
Bohdan Benetskyi (PGS Software) <bbenetskyi@pgs-soft.com>
2019-10-03 09:45:17 +0200

    Correct Binding to Model

--- Projects/Playground/Playground.Core/Models/SampleModel.cs
+++ Projects/Playground/Playground.Core/Models/SampleModel.cs
@@ -2,25 +2,12 @@
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
 
-using MvvmCross.ViewModels;
-
 namespace Playground.Core.Models
 {
-    public class SampleModel : MvxNotifyPropertyChanged
+    public class SampleModel
     {
-        private string _message;
-        private decimal _value;
+        public string Message { get; set; }
 
-        public string Message
-        {
-            get => _message;
-            set => SetProperty(ref _message, value);
-        }
-
-        public decimal Value
-        {
-            get => _value;
-            set => SetProperty(ref _value, value);
-        }
+        public decimal Value { get; set; }
     }
 }
--- Projects/Playground/Playground.Core/ViewModels/Navigation/ChangeTabBehaviorViewModel.cs
+++ Projects/Playground/Playground.Core/ViewModels/Navigation/ChangeTabBehaviorViewModel.cs
@@ -42,38 +42,67 @@ namespace Playground.Core.ViewModels.Navigation
     public class FirstTabForBehaviorViewModel : MvxViewModel, IMvxChangeTabAware<SampleModel>
     {
         private SampleModel _sampleModel;
-        public SampleModel SampleModel
+        public string Message
         {
-            get => _sampleModel;
-            set => SetProperty(ref _sampleModel, value);
+            get => _sampleModel.Message;
+            set
+            {
+                _sampleModel.Message = value;
+                RaisePropertyChanged();
+            }
+        }
+        public decimal Value
+        {
+            get => _sampleModel.Value;
+            set
+            {
+                _sampleModel.Value = value;
+                RaisePropertyChanged();
+            }
         }
 
         public FirstTabForBehaviorViewModel()
         {
-            SampleModel = new SampleModel();
+            _sampleModel = new SampleModel();
         }
 
-        public SampleModel OnNavigatedFrom() => SampleModel;
+        public SampleModel OnNavigatedFrom() => _sampleModel;
 
         public void OnNavigatedTo(SampleModel parameter)
         {
             if (parameter != null)
-                SampleModel = parameter;
+            {
+                _sampleModel = parameter;
+                RaiseAllPropertiesChanged();
+            }
         }
     }
 
     public class SecondTabForBehaviorViewModel : MvxViewModel, IMvxChangeTabAware<SampleModel>
     {
         private SampleModel _sampleModel;
-        public SampleModel SampleModel
+        public string Message
         {
-            get => _sampleModel;
-            set => SetProperty(ref _sampleModel, value);
+            get => _sampleModel.Message;
+            set
+            {
+                _sampleModel.Message = value;
+                RaisePropertyChanged();
+            }
+        }
+        public decimal Value
+        {
+            get => _sampleModel.Value;
+            set
+            {
+                _sampleModel.Value = value;
+                RaisePropertyChanged();
+            }
         }
 
         public SecondTabForBehaviorViewModel()
         {
-            SampleModel = new SampleModel();
+            _sampleModel = new SampleModel();
         }
 
         public SampleModel OnNavigatedFrom() => new SampleModel
@@ -85,7 +114,10 @@ namespace Playground.Core.ViewModels.Navigation
         public void OnNavigatedTo(SampleModel parameter)
         {
             if (parameter != null)
-                SampleModel = parameter;
+            {
+                _sampleModel = parameter;
+                RaiseAllPropertiesChanged();
+            }
         }
     }
 }
