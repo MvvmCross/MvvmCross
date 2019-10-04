f4da6ff3cb2da794e1b4b713c345acbc8462fa58
Martin Zikmund <martinzikmund@live.com>
2019-10-04 10:24:31 +0200

    Getting plugin types safely (#3590)
    * Getting plugin types safely
    
    * Removing whitespace

--- MvvmCross/Core/MvxSetup.cs
+++ MvvmCross/Core/MvxSetup.cs
@@ -389,7 +389,7 @@ namespace MvvmCross.Core
 
             var pluginTypes =
                 GetPluginAssemblies()
-                    .SelectMany(assembly => assembly.GetTypes())
+                    .SelectMany(assembly => assembly.ExceptionSafeGetTypes())
                     .Where(TypeContainsPluginAttribute);
 
             foreach (var pluginType in pluginTypes)
