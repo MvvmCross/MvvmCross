1ad188286503bebaf6f2630676db80ba630bc97e
Merdan Gochmuradov <merdan@gochmuradov.com>
2019-10-09 02:48:09 +0500

    Add netcoreapp3.0 support (#3596)
    * Added netcoreapp3.0 support
    
    * Added Playground project for WPF on netcoreapp3.0
    
    * Added netcoreapp3.0 support to Forms projects
    
    * Added Forms Playground project for WPF on netcoreapp3.0
    
    * Removed playground WPFCore projects from release configuration
    
    * Remove PackageTargetFallback
    
    * Don't build WPFCore Playgrounds in Release|Mixed
    
    * Ensure .NET Core 3.0.100 is installed
    
    * Bump unit tests target framework

---
+++ Projects/Playground/Playground.Forms.WpfCore/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using MvvmCross.Forms.Platforms.Wpf.Views;
+
+namespace Playground.Forms.WpfCore
+{
+    public partial class MainWindow : MvxFormsWindowsPage
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
