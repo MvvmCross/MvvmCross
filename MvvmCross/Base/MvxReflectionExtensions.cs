b81d0e2dfc4794ef574790ad1f24442a17be9196
Tomasz Cielecki <tomasz@ostebaronen.dk>
2021-01-02 17:30:57 +0100

    Enable nullable in MvxReflectionExtensions

--- MvvmCross/Base/MvxReflectionExtensions.cs
+++ MvvmCross/Base/MvxReflectionExtensions.cs
@@ -8,6 +8,7 @@ using System.Reflection;
 
 namespace MvvmCross.Base
 {
+#nullable enable
     public static class MvxReflectionExtensions
     {
         public static Attribute[] GetCustomAttributes(this Type type, Type attributeType, bool inherit)
@@ -17,7 +18,7 @@ namespace MvvmCross.Base
 
         public static bool IsInstanceOfType(this Type type, object obj)
         {
-            return obj != null && (type.IsAssignableFrom(obj.GetType()) || obj.IsMarshalByRefObject());
+            return obj != null && (type?.IsAssignableFrom(obj.GetType()) == true || obj.IsMarshalByRefObject());
         }
 
         private static bool IsMarshalByRefObject(this object obj)
@@ -25,31 +26,32 @@ namespace MvvmCross.Base
             return obj != null && obj.GetType().FullName == "System.MarshalByRefObject";
         }
 
-        public static MethodInfo GetAddMethod(this EventInfo eventInfo, bool nonPublic = false)
+        public static MethodInfo? GetAddMethod(this EventInfo eventInfo, bool nonPublic = false)
         {
-            if (eventInfo.AddMethod == null || (!nonPublic && !eventInfo.AddMethod.IsPublic)) {
+            if (eventInfo?.AddMethod == null || (!nonPublic && !eventInfo.AddMethod.IsPublic)) {
                 return null;
             }
 
             return eventInfo.AddMethod;
         }
 
-        public static MethodInfo GetRemoveMethod(this EventInfo eventInfo, bool nonPublic = false)
+        public static MethodInfo? GetRemoveMethod(this EventInfo eventInfo, bool nonPublic = false)
         {
-            if (eventInfo.RemoveMethod == null || (!nonPublic && !eventInfo.RemoveMethod.IsPublic)) {
+            if (eventInfo?.RemoveMethod == null || (!nonPublic && !eventInfo.RemoveMethod.IsPublic)) {
                 return null;
             }
 
             return eventInfo.RemoveMethod;
         }
 
-        public static MethodInfo GetGetMethod(this PropertyInfo property, bool nonPublic = false)
+        public static MethodInfo? GetGetMethod(this PropertyInfo property, bool nonPublic = false)
         {
-            if (property.GetMethod == null || (!nonPublic && !property.GetMethod.IsPublic)) {
+            if (property?.GetMethod == null || (!nonPublic && !property.GetMethod.IsPublic)) {
                 return null;
             }
 
             return property.GetMethod;
         }
     }
+#nullable restore
 }
