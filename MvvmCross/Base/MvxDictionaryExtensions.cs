ef29c0c49ad9848e265278576114b9fd237f8241
Tomasz Cielecki <tomasz@ostebaronen.dk>
2021-01-02 17:22:00 +0100

    Clean up LINQ query in DictionaryExtensions

--- MvvmCross/Base/MvxDictionaryExtensions.cs
+++ MvvmCross/Base/MvxDictionaryExtensions.cs
@@ -5,27 +5,27 @@ using System.Reflection;
 namespace MvvmCross.Base
 {
 	public static class MvxDictionaryExtensions
+    public static class MvxDictionaryExtensions
     {
-		public static IDictionary<string, object> ToPropertyDictionary(this object input)
-		{
-			if (input == null)
-				return new Dictionary<string, object>();
+        public static IDictionary<string, object> ToPropertyDictionary(this object input)
+        {
+            if (input == null)
+                return new Dictionary<string, object>();
 
-			if (input is IDictionary<string, object>)
-				return (IDictionary<string, object>)input;
+            if (input is IDictionary<string, object> dict)
+                return dict;
 
-			var propertyInfos = from property in input.GetType()
-													  .GetProperties(BindingFlags.Instance | BindingFlags.Public |
-																	 BindingFlags.FlattenHierarchy)
-								where property.CanRead
-								select property;
+            var propertyInfos =
+                input.GetType().GetProperties(
+                    BindingFlags.Instance | BindingFlags.Public | BindingFlags.FlattenHierarchy)
+                .Where(p => p.CanRead);
 
-			var dictionary = new Dictionary<string, object>();
-			foreach (var propertyInfo in propertyInfos)
-			{
-				dictionary[propertyInfo.Name] = propertyInfo.GetValue(input);
-			}
-			return dictionary;
-		}
-	}
+            var dictionary = new Dictionary<string, object>();
+            foreach (var propertyInfo in propertyInfos)
+            {
+                dictionary[propertyInfo.Name] = propertyInfo.GetValue(input);
+            }
+            return dictionary;
+        }
+    }
 }
