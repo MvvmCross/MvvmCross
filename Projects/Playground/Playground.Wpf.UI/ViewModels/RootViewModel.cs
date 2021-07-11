f39978a253a9e78d3d9cd1f0af71d6b88cddea76
tuke307 <tonymeissner70@gmail.com>
2021-07-11 13:50:04 +0200

    issue #4221

---
+++ Projects/Playground/Playground.Wpf.UI/ViewModels/RootViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using MvvmCross;
+using MvvmCross.Commands;
+using MvvmCross.Localization;
+using MvvmCross.Logging;
+using MvvmCross.Navigation;
+using MvvmCross.Plugin.Messenger;
+using MvvmCross.ViewModels;
+using Playground.Core.Models;
+using Playground.Core.Services;
+using Playground.Core.ViewModels;
+using Playground.Core.ViewModels.Bindings;
+using Playground.Core.ViewModels.Location;
+using Playground.Core.ViewModels.Navigation;
+using Playground.Core.ViewModels.Samples;
+
+namespace Playground.Wpf.UI.ViewModels
+{
+    public class RootViewModel : Core.ViewModels.RootViewModel
+    {
+        public RootViewModel(ILoggerFactory logProvider, IMvxNavigationService navigationService, IMvxViewModelLoader mvxViewModelLoader)
+               : base(logProvider, navigationService, mvxViewModelLoader)
+        { }
+    }
+}
--- Projects/Playground/Playground.Wpf.UI/Views/RootView.xaml.cs
+++ Projects/Playground/Playground.Wpf.UI/Views/RootView.xaml.cs
@@ -5,7 +5,7 @@ using Playground.Core.ViewModels;
 namespace Playground.Wpf.UI.Views
 {
     [MvxContentPresentation]
-    [MvxViewFor(typeof(RootViewModel))]
+    [MvxViewFor(typeof(Playground.Wpf.UI.ViewModels.RootViewModel))]
     public partial class RootView
     {
         public RootView()
