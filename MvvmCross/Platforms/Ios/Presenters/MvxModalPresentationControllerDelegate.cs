e26591c0aa8ec07af8a411a6c857808536ede204
Brandon Butler <brandon.butler@dornerworks.com>
2021-07-25 08:11:36 -0400

    fix iOS closing non fullscreen modals not closing view model (#4040)

---
+++ MvvmCross/Platforms/Ios/Presenters/MvxModalPresentationControllerDelegate.cs
@@ -0,0 +1,28 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MS-PL license.
+// See the LICENSE file in the project root for more information.
+
+using MvvmCross.Platforms.Ios.Presenters.Attributes;
+using UIKit;
+
+namespace MvvmCross.Platforms.Ios.Presenters
+{
+    public class MvxModalPresentationControllerDelegate : UIAdaptivePresentationControllerDelegate
+    {
+        private readonly IMvxIosViewPresenter _presenter;
+        private readonly UIViewController _viewController;
+        private readonly MvxModalPresentationAttribute _attribute;
+
+        public MvxModalPresentationControllerDelegate(IMvxIosViewPresenter presenter, UIViewController viewController, MvxModalPresentationAttribute attribute)
+        {
+            _presenter = presenter;
+            _viewController = viewController;
+            _attribute = attribute;
+        }
+
+        public override void DidDismiss(UIPresentationController presentationController)
+        {
+            _presenter.ClosedModalViewController(_viewController, _attribute);
+        }
+    }
+}
