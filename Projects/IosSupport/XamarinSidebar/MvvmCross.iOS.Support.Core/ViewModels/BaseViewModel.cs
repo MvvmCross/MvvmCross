27998f9e01f15a805a94f30bedbad5cc369cec84
Tomasz Cielecki <tomasz@ostebaronen.dk>
2021-10-14 23:08:55 +0200

    Remove old unused play projects

--- Projects/IosSupport/XamarinSidebar/MvvmCross.iOS.Support.Core/ViewModels/BaseViewModel.cs
+++
@@ -1,9 +0,0 @@
-using MvvmCross.Core.ViewModels;
-
-namespace MvvmCross.iOS.Support.XamarinSidebarSample.Core.ViewModels
-{
-    public class BaseViewModel : MvxViewModel
-    {
-        public string ExampleValue { get; set; }
-    }
-}
