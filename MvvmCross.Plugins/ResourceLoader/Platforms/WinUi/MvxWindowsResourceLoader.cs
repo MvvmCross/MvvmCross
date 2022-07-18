59de9bcd8d587fceabd00c162332f4c102a2a6be
David Hovey <david@hoveytech.com>
2022-07-18 13:16:31 -0700

    Using .NET 6 for WinUI 3

---
+++ MvvmCross.Plugins/ResourceLoader/Platforms/WinUi/MvxWindowsResourceLoader.cs
@@ -0,0 +1,21 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MS-PL license.
+// See the LICENSE file in the project root for more information.
+
+using Windows.ApplicationModel;
+
+namespace MvvmCross.Plugin.ResourceLoader.Platforms.WinUi
+{
+    public class MvxStoreResourceLoader : MvxResourceLoader
+    {
+        public override void GetResourceStream(string resourcePath, Action<Stream> streamAction)
+        {
+            // we needed to replace the "/" with "\\" - see https://github.com/slodge/MvvmCross/issues/332
+            resourcePath = resourcePath.Replace("/", "\\");
+            var file = Package.Current.InstalledLocation.GetFileAsync(resourcePath).GetResults();
+            var streamWithContent = file.OpenReadAsync().GetResults();
+            var stream = streamWithContent.AsStreamForRead();
+            streamAction?.Invoke(stream);
+        }
+    }
+}
--- MvvmCross/Platforms/WinUi/Binding/MvxBinding/Target/MvxDependencyPropertyTargetBinding.cs
+++ MvvmCross/Platforms/WinUi/Binding/MvxBinding/Target/MvxDependencyPropertyTargetBinding.cs
@@ -1,4 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
 
@@ -55,8 +55,8 @@ namespace MvvmCross.Platforms.WinUi.Binding.MvxBinding.Target
             frameworkElement.SetBinding(attachedProperty, listenerBinding);
         }
 
-        public override Type TargetType => _actualPropertyType;
-
+        public override Type TargetValueType => _actualPropertyType;
+        
         public override MvxBindingMode DefaultMode { get; } = MvxBindingMode.TwoWay;
 
         protected virtual object GetValueByReflection()
