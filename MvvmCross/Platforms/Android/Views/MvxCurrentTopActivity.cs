0eb04546115dbd1dc9549446e56543d6805994b1
evgenyvalavin <35497422+evgenyvalavin@users.noreply.github.com>
2022-10-31 19:48:46 +0500

    Set Null if activity is destroyed

--- MvvmCross/Platforms/Android/Views/MvxCurrentTopActivity.cs
+++ MvvmCross/Platforms/Android/Views/MvxCurrentTopActivity.cs
@@ -2,66 +2,63 @@
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
 
-using System;
-using Android.App;
-using Android.OS;
 using Android.Runtime;
 
-namespace MvvmCross.Platforms.Android.Views
-{
+namespace MvvmCross.Platforms.Android.Views;
+
 #nullable enable
-    [Register("mvvmcross.platforms.android.MvxCurrentTopActivity")]
-    public class MvxCurrentTopActivity
-        : Java.Lang.Object, Application.IActivityLifecycleCallbacks, IMvxAndroidCurrentTopActivity
+[Register("mvvmcross.platforms.android.MvxCurrentTopActivity")]
+public class MvxCurrentTopActivity
+    : Java.Lang.Object, Application.IActivityLifecycleCallbacks, IMvxAndroidCurrentTopActivity
+{
+    private readonly WeakReference<Activity?> _lastSeenActivity = new(null);
+
+    public Activity? Activity
     {
-        private readonly WeakReference<Activity?> _lastSeenActivity = new WeakReference<Activity?>(null);
-
-        public Activity? Activity
+        get
         {
-            get
-            {
-                if (_lastSeenActivity?.TryGetTarget(out var activity) ?? false)
-                    return activity;
-                return null;
-            }
-        }
-
-        public static bool Initialized { get; set; }
-
-        public void OnActivityCreated(Activity activity, Bundle? savedInstanceState)
-        {
-            _lastSeenActivity.SetTarget(activity);
-        }
-
-        public void OnActivityPaused(Activity activity)
-        {
-            _lastSeenActivity.SetTarget(activity);
-        }
-
-        public void OnActivityResumed(Activity activity)
-        {
-            _lastSeenActivity.SetTarget(activity);
-        }
-
-        public void OnActivityDestroyed(Activity activity)
-        {
-            // not interested in this
-        }
-
-        public void OnActivitySaveInstanceState(Activity activity, Bundle outState)
-        {
-            // not interested in this
-        }
-
-        public void OnActivityStarted(Activity activity)
-        {
-            // not interested in this
-        }
-
-        public void OnActivityStopped(Activity activity)
-        {
-            // not interested in this
+            if (_lastSeenActivity?.TryGetTarget(out var activity) ?? false)
+                return activity;
+            return null;
         }
     }
-#nullable restore
+
+    public static bool Initialized { get; set; }
+
+    public void OnActivityCreated(Activity activity, Bundle? savedInstanceState)
+    {
+        _lastSeenActivity.SetTarget(activity);
+    }
+
+    public void OnActivityPaused(Activity activity)
+    {
+        _lastSeenActivity.SetTarget(activity);
+    }
+
+    public void OnActivityResumed(Activity activity)
+    {
+        _lastSeenActivity.SetTarget(activity);
+    }
+
+    public void OnActivityDestroyed(Activity activity)
+    {
+        if (Activity == activity)
+            _lastSeenActivity.SetTarget(default);
+    }
+
+    public void OnActivitySaveInstanceState(Activity activity, Bundle outState)
+    {
+        // not interested in this
+    }
+
+    public void OnActivityStarted(Activity activity)
+    {
+        // not interested in this
+    }
+
+    public void OnActivityStopped(Activity activity)
+    {
+        // not interested in this
+    }
 }
+#nullable restore
