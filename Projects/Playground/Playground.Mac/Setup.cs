8fd2514ecb450c916d6393426499bd30c07d47fa
Sergey Nechaev <6499856+snechaev@users.noreply.github.com>
2023-02-14 13:38:33 +0100

    add playground.Mac back into the solution (as it was before f76f09fa8b7b379d43195bfa56818a7500ff7948)

---
+++ Projects/Playground/Playground.Mac/Setup.cs
@@ -0,0 +1,33 @@
+﻿using MvvmCross.Platforms.Mac.Core;
+using MvvmCross.Platforms.Mac.Presenters.Attributes;
+using MvvmCross.ViewModels;
+using Playground.Core;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Extensions.Logging;
+
+namespace Playground.Mac
+{
+    public class Setup : MvxMacSetup<App>
+    {
+        public Setup()
+        {
+            MvxWindowPresentationAttribute.DefaultWidth = 250;
+            MvxWindowPresentationAttribute.DefaultHeight = 250;
+        }
+
+        protected override ILoggerProvider CreateLogProvider()
+        {
+            return new SerilogLoggerProvider();
+        }
+
+        protected override ILoggerFactory CreateLogFactory()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .CreateLogger();
+
+            return new SerilogLoggerFactory();
+        }
+    }
+}
