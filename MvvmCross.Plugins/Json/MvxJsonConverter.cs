b474c545286774a5b9d6707ff0979a6b3f1b96ef
Tomasz Cielecki <tomasz@ostebaronen.dk>
2023-07-12 09:13:10 +0200

    Drop dependency on Newtonsoft.Json (#4628)

--- MvvmCross.Plugins/Json/MvxJsonConverter.cs
+++ MvvmCross.Plugins/Json/MvxJsonConverter.cs
@@ -1,12 +1,9 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
-
-using System;
-using System.Collections.Generic;
-using System.IO;
+#nullable enable
 using MvvmCross.Base;
-using Newtonsoft.Json;
+using System.Text.Json;
 
 namespace MvvmCross.Plugin.Json
 {
@@ -14,45 +11,34 @@ namespace MvvmCross.Plugin.Json
     public class MvxJsonConverter
         : IMvxJsonConverter
     {
-        private static readonly JsonSerializerSettings Settings;
+        public JsonSerializerOptions Settings { get; set; }
 
-        static MvxJsonConverter()
+        public MvxJsonConverter()
         {
-            Settings = new JsonSerializerSettings
+            Settings = new JsonSerializerOptions
             {
-                ReferenceLoopHandling = ReferenceLoopHandling.Serialize,
-                Converters = new List<JsonConverter>
-                {
-                    new MvxEnumJsonConverter()
-                },
-                DateFormatHandling = DateFormatHandling.IsoDateFormat
+                WriteIndented = false
             };
         }
 
-        public T DeserializeObject<T>(string inputText)
+        public T? DeserializeObject<T>(string inputText)
         {
-            return JsonConvert.DeserializeObject<T>(inputText, Settings);
+            return JsonSerializer.Deserialize<T>(inputText, Settings);
+        }
+
+        public object? DeserializeObject(Type type, string inputText)
+        {
+            return JsonSerializer.Deserialize(inputText, type, Settings);
+        }
+
+        public T? DeserializeObject<T>(Stream stream)
+        {
+            return JsonSerializer.Deserialize<T>(stream, Settings);
         }
 
         public string SerializeObject(object toSerialise)
         {
-            return JsonConvert.SerializeObject(toSerialise, Formatting.None, Settings);
-        }
-
-        public object DeserializeObject(Type type, string inputText)
-        {
-            return JsonConvert.DeserializeObject(inputText, type, Settings);
-        }
-
-        public T DeserializeObject<T>(Stream stream)
-        {
-            var serializer = JsonSerializer.Create(Settings);
-
-            using (var sr = new StreamReader(stream))
-            using (var jsonTextReader = new JsonTextReader(sr))
-            {
-                return serializer.Deserialize<T>(jsonTextReader);
-            }
+            return JsonSerializer.Serialize(toSerialise, Settings);
         }
     }
 }
--- MvvmCross/Base/IMvxTextSerializer.cs
+++ MvvmCross/Base/IMvxTextSerializer.cs
@@ -1,19 +1,16 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
-
-using System;
+#nullable enable
 
 namespace MvvmCross.Base
 {
-#nullable enable
     public interface IMvxTextSerializer
     {
-        T DeserializeObject<T>(string inputText);
+        T? DeserializeObject<T>(string inputText);
 
-        object DeserializeObject(Type type, string inputText);
+        object? DeserializeObject(Type type, string inputText);
 
         string SerializeObject(object toSerialise);
     }
-#nullable restore
 }
