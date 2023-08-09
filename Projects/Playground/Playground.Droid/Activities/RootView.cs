8b18928264ee6324c757d1b62ff8d14e24af5888
Tomasz Cielecki <tomasz@ostebaronen.dk>
2023-08-09 23:02:27 +0200

    Update to .NET 7 (#4673)
    * Pin to dotnet 7 sdk
    
    * Update TFMs to NET7
    
    * Remove MAUI Essentials, it is broken with Central Package Management
    
    * Update to .NET SDK 7.0.306
    
    * Match dotnet version in format workflow
    
    * Fix Sonar complaining about JDK 11

--- Projects/Playground/Playground.Droid/Activities/RootView.cs
+++ Projects/Playground/Playground.Droid/Activities/RootView.cs
@@ -2,9 +2,7 @@
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
 
-using Android.Runtime;
 using Android.Views;
-using Microsoft.Maui.ApplicationModel;
 using MvvmCross.Platforms.Android.Presenters.Attributes;
 using MvvmCross.Platforms.Android.Views;
 using Playground.Core.ViewModels;
@@ -21,12 +19,6 @@ namespace Playground.Droid.Activities
             base.OnCreate(bundle);
 
             SetContentView(Resource.Layout.RootView);
-        }
-
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
-            Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
