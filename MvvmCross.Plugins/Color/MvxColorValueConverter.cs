393e1e6f44563151a9edbc12fc187028a82a8a39
Tomasz Cielecki <tomasz@ostebaronen.dk>
2024-02-03 09:33:32 +0100

    Fix MvxColorValueConverter not working (#4807)

--- MvvmCross.Plugins/Color/MvxColorValueConverter.cs
+++ MvvmCross.Plugins/Color/MvxColorValueConverter.cs
@@ -1,44 +1,35 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
-
-using System;
+#nullable enable
 using System.Globalization;
 using MvvmCross.Converters;
 using MvvmCross.UI;
 
-namespace MvvmCross.Plugin.Color
+namespace MvvmCross.Plugin.Color;
+
+public abstract class MvxColorValueConverter : MvxValueConverter
 {
-#nullable enable
-    public abstract class MvxColorValueConverter : MvxValueConverter
+    private readonly Lazy<IMvxNativeColor?> _nativeColor = new(() => Mvx.IoCProvider?.Resolve<IMvxNativeColor>());
+
+    protected abstract System.Drawing.Color Convert(object value, object? parameter, CultureInfo? culture);
+
+    public sealed override object Convert(object value, Type? targetType, object? parameter,
+        CultureInfo? culture)
     {
-        private readonly IMvxNativeColor? _nativeColor;
+        return _nativeColor.Value?.ToNative(Convert(value, parameter, culture)) ?? MvxBindingConstant.UnsetValue;
+    }
+}
 
-        protected MvxColorValueConverter()
-        {
-            Mvx.IoCProvider?.TryResolve<IMvxNativeColor>(out _nativeColor);
-        }
+public abstract class MvxColorValueConverter<T> : MvxColorValueConverter
+{
+    protected sealed override System.Drawing.Color Convert(object value, object? parameter, CultureInfo? culture)
+    {
+        if (value is T t)
+            return Convert(t, parameter, culture);
 
-        protected abstract System.Drawing.Color Convert(object value, object? parameter, CultureInfo? culture);
-
-        public sealed override object Convert(object value, Type? targetType, object? parameter,
-                                       CultureInfo? culture)
-        {
-            return _nativeColor?.ToNative(Convert(value, parameter, culture)) ?? MvxBindingConstant.UnsetValue;
-        }
+        return default;
     }
 
-    public abstract class MvxColorValueConverter<T> : MvxColorValueConverter
-    {
-        protected sealed override System.Drawing.Color Convert(object value, object? parameter, CultureInfo? culture)
-        {
-            if (value is T t)
-                return Convert(t, parameter, culture);
-
-            return default;
-        }
-
-        protected abstract System.Drawing.Color Convert(T value, object? parameter, CultureInfo? culture);
-    }
-#nullable restore
+    protected abstract System.Drawing.Color Convert(T value, object? parameter, CultureInfo? culture);
 }
