3c316879a3f83934155d43757a67b9dc60758a28
Tomasz Cielecki <tci@trackman.com>
2024-02-22 21:35:46 +0100

    Enable nullables on MvxAppCompatSearchViewQueryTextTargetBinding

--- MvvmCross/Platforms/Android/Binding/Target/MvxAppCompatSearchViewQueryTextTargetBinding.cs
+++ MvvmCross/Platforms/Android/Binding/Target/MvxAppCompatSearchViewQueryTextTargetBinding.cs
@@ -1,60 +1,62 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
-
+#nullable enable
+using System.Diagnostics.CodeAnalysis;
 using MvvmCross.Binding;
 using MvvmCross.Platforms.Android.WeakSubscription;
 using SearchView = AndroidX.AppCompat.Widget.SearchView;
 
-namespace MvvmCross.Platforms.Android.Binding.Target
+namespace MvvmCross.Platforms.Android.Binding.Target;
+
+public class MvxAppCompatSearchViewQueryTextTargetBinding
+    : MvxAndroidTargetBinding
 {
-    public class MvxAppCompatSearchViewQueryTextTargetBinding
-        : MvxAndroidTargetBinding
+    private IDisposable? _subscription;
+
+    public MvxAppCompatSearchViewQueryTextTargetBinding(
+            [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicEvents)]
+            object target)
+        : base(target)
     {
-        private IDisposable _subscription;
+    }
 
-        public MvxAppCompatSearchViewQueryTextTargetBinding(object target)
-            : base(target)
+    public override Type TargetValueType => typeof(string);
+
+    public override MvxBindingMode DefaultMode => MvxBindingMode.TwoWay;
+
+    protected SearchView? SearchView => (SearchView?)Target;
+
+    public override void SubscribeToEvents()
+    {
+        _subscription = SearchView?.WeakSubscribe<SearchView, SearchView.QueryTextChangeEventArgs>(
+            nameof(SearchView.QueryTextChange),
+            HandleQueryTextChanged);
+    }
+
+    protected override void SetValueImpl(object target, object? value)
+    {
+        if (target is SearchView searchView)
+            searchView.SetQuery((string?)value, true);
+    }
+    
+    private void HandleQueryTextChanged(object? sender, SearchView.QueryTextChangeEventArgs e)
+    {
+        if (Target is not SearchView searchView)
+            return;
+
+        var value = searchView.Query;
+        FireValueChanged(value);
+    }
+
+    protected override void Dispose(bool isDisposing)
+    {
+        if (isDisposing)
         {
+            _subscription?.Dispose();
+            _subscription = null;
         }
 
-        public override Type TargetValueType => typeof(string);
-
-        public override MvxBindingMode DefaultMode => MvxBindingMode.TwoWay;
-
-        protected SearchView SearchView => (SearchView)Target;
-
-        public override void SubscribeToEvents()
-        {
-            _subscription = SearchView.WeakSubscribe<SearchView, SearchView.QueryTextChangeEventArgs>(
-                nameof(SearchView.QueryTextChange),
-                HandleQueryTextChanged);
-        }
-
-        protected override void SetValueImpl(object target, object value)
-        {
-            if (target is SearchView searchView)
-                searchView.SetQuery((string)value, true);
-        }
-
-        protected override void Dispose(bool isDisposing)
-        {
-            if (isDisposing)
-            {
-                _subscription?.Dispose();
-                _subscription = null;
-            }
-
-            base.Dispose(isDisposing);
-        }
-
-        private void HandleQueryTextChanged(object sender, SearchView.QueryTextChangeEventArgs e)
-        {
-            if (Target is SearchView searchView)
-            {
-                var value = searchView.Query;
-                FireValueChanged(value);
-            }
-        }
+        base.Dispose(isDisposing);
     }
 }
