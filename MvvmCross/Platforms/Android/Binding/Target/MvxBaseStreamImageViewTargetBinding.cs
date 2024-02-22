d2c68a7e16cc571ed674459960bdf8b355e0b8cc
Tomasz Cielecki <tci@trackman.com>
2024-02-22 21:55:18 +0100

    Enable nullable on MvxBaseStreamImageViewTargetBinding

--- MvvmCross/Platforms/Android/Binding/Target/MvxBaseStreamImageViewTargetBinding.cs
+++ MvvmCross/Platforms/Android/Binding/Target/MvxBaseStreamImageViewTargetBinding.cs
@@ -1,43 +1,35 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
-
-using System.IO;
+#nullable enable
 using Android.Content.Res;
 using Android.Graphics;
 using Android.Graphics.Drawables;
-using Android.Widget;
 
-namespace MvvmCross.Platforms.Android.Binding.Target
+namespace MvvmCross.Platforms.Android.Binding.Target;
+
+public abstract class MvxBaseStreamImageViewTargetBinding(ImageView imageView)
+    : MvxBaseImageViewTargetBinding(imageView)
 {
-    public abstract class MvxBaseStreamImageViewTargetBinding
-        : MvxBaseImageViewTargetBinding
+    protected override bool GetBitmap(object? value, out Bitmap? bitmap)
     {
-        protected MvxBaseStreamImageViewTargetBinding(ImageView imageView)
-            : base(imageView)
+        using var assetStream = GetStream(value);
+        if (assetStream == null)
         {
+            bitmap = null;
+            return false;
         }
 
-        protected override bool GetBitmap(object value, out Bitmap bitmap)
-        {
-            var assetStream = GetStream(value);
-            if (assetStream == null)
-            {
-                bitmap = null;
-                return false;
-            }
-
-            var options = new BitmapFactory.Options { InPurgeable = true };
-            bitmap = BitmapFactory.DecodeStream(assetStream, null, options);
-            return true;
-        }
-
-        protected override void SetImageBitmap(ImageView imageView, Bitmap bitmap)
-        {
-            var drawable = new BitmapDrawable(Resources.System, bitmap);
-            imageView.SetImageDrawable(drawable);
-        }
-
-        protected abstract Stream GetStream(object value);
+        var options = new BitmapFactory.Options { InPurgeable = true };
+        bitmap = BitmapFactory.DecodeStream(assetStream, null, options);
+        return true;
     }
+
+    protected override void SetImageBitmap(ImageView imageView, Bitmap? bitmap)
+    {
+        var drawable = new BitmapDrawable(Resources.System, bitmap);
+        imageView.SetImageDrawable(drawable);
+    }
+
+    protected abstract Stream? GetStream(object? value);
 }
