f3f1b73703768c739c2fd91f0d380c33987eb975
Tomasz Cielecki <tci@trackman.com>
2024-02-22 21:47:41 +0100

    Enable nullable on MvxAutoCompleteTextViewSelectedObjectTargetBinding

--- MvvmCross/Platforms/Android/Binding/Target/MvxAutoCompleteTextViewSelectedObjectTargetBinding.cs
+++ MvvmCross/Platforms/Android/Binding/Target/MvxAutoCompleteTextViewSelectedObjectTargetBinding.cs
@@ -1,59 +1,61 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
-
-using System;
+#nullable enable
+using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
+using Microsoft.Extensions.Logging;
 using MvvmCross.Binding;
 using MvvmCross.Platforms.Android.Binding.Views;
 using MvvmCross.Platforms.Android.WeakSubscription;
 
-namespace MvvmCross.Platforms.Android.Binding.Target
+namespace MvvmCross.Platforms.Android.Binding.Target;
+
+public class MvxAutoCompleteTextViewSelectedObjectTargetBinding
+    : MvxAndroidPropertyInfoTargetBinding<MvxAutoCompleteTextView>
 {
-    public class MvxAutoCompleteTextViewSelectedObjectTargetBinding
-        : MvxAndroidPropertyInfoTargetBinding<MvxAutoCompleteTextView>
+    private IDisposable? _subscription;
+
+    public MvxAutoCompleteTextViewSelectedObjectTargetBinding(
+            [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicEvents)]
+            object target,
+            PropertyInfo targetPropertyInfo)
+        : base(target, targetPropertyInfo)
     {
-        private IDisposable _subscription;
-
-        public MvxAutoCompleteTextViewSelectedObjectTargetBinding(object target, PropertyInfo targetPropertyInfo)
-            : base(target, targetPropertyInfo)
+        var autoComplete = View;
+        if (autoComplete == null)
         {
-            var autoComplete = View;
-            if (autoComplete == null)
-            {
-                MvxBindingLog.Error(
-                                      "Error - autoComplete is null in MvxAutoCompleteTextViewSelectedObjectTargetBinding");
-            }
+            MvxBindingLog.Instance?.LogError(
+                "autoComplete is null in {TypeName}", nameof(MvxAutoCompleteTextViewSelectedObjectTargetBinding));
+        }
+    }
+
+    private void AutoCompleteOnSelectedObjectChanged(object? sender, EventArgs eventArgs)
+    {
+        FireValueChanged(View?.SelectedObject);
+    }
+
+    public override MvxBindingMode DefaultMode => MvxBindingMode.OneWayToSource;
+
+    public override void SubscribeToEvents()
+    {
+        var autoComplete = View;
+        if (autoComplete == null)
+            return;
+
+        _subscription = autoComplete.WeakSubscribe(
+            nameof(autoComplete.SelectedObjectChanged),
+            AutoCompleteOnSelectedObjectChanged);
+    }
+
+    protected override void Dispose(bool isDisposing)
+    {
+        if (isDisposing)
+        {
+            _subscription?.Dispose();
+            _subscription = null;
         }
 
-        private void AutoCompleteOnSelectedObjectChanged(object sender, EventArgs eventArgs)
-        {
-            FireValueChanged(View.SelectedObject);
-        }
-
-        public override MvxBindingMode DefaultMode => MvxBindingMode.OneWayToSource;
-
-        public override void SubscribeToEvents()
-        {
-            var autoComplete = View;
-
-            if (autoComplete == null)
-                return;
-
-            _subscription = autoComplete.WeakSubscribe(
-                nameof(autoComplete.SelectedObjectChanged),
-                AutoCompleteOnSelectedObjectChanged);
-        }
-
-        protected override void Dispose(bool isDisposing)
-        {
-            if (isDisposing)
-            {
-                _subscription?.Dispose();
-                _subscription = null;
-            }
-
-            base.Dispose(isDisposing);
-        }
+        base.Dispose(isDisposing);
     }
 }
