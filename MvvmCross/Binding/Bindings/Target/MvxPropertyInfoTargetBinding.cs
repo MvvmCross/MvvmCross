3b4910d833ac0616211c49ba176aa14da6751384
Tomasz Cielecki <tci@trackman.com>
2024-02-22 20:58:53 +0100

    Enable nullable in MvxPropertyInfoTargetBinding and make it abstract

--- MvvmCross/Binding/Bindings/Target/MvxPropertyInfoTargetBinding.cs
+++ MvvmCross/Binding/Bindings/Target/MvxPropertyInfoTargetBinding.cs
@@ -1,58 +1,53 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
-
-using System;
+#nullable enable
 using System.Reflection;
 using MvvmCross.Binding.Attributes;
 
-namespace MvvmCross.Binding.Bindings.Target
+namespace MvvmCross.Binding.Bindings.Target;
+
+public abstract class MvxPropertyInfoTargetBinding : MvxConvertingTargetBinding
 {
-    public class MvxPropertyInfoTargetBinding : MvxConvertingTargetBinding
+    private readonly PropertyInfo _targetPropertyInfo;
+
+    protected MvxPropertyInfoTargetBinding(object target, PropertyInfo targetPropertyInfo)
+        : base(target)
     {
-        private readonly PropertyInfo _targetPropertyInfo;
+        _targetPropertyInfo = targetPropertyInfo;
+    }
 
-        public MvxPropertyInfoTargetBinding(object target, PropertyInfo targetPropertyInfo)
-            : base(target)
+    protected override void Dispose(bool isDisposing)
+    {
+        if (isDisposing)
         {
-            _targetPropertyInfo = targetPropertyInfo;
-        }
-
-        protected override void Dispose(bool isDisposing)
-        {
-            if (isDisposing)
+            // if the target property should be set to NULL on dispose then we clear it here
+            // this is a fix for the possible memory leaks discussion started https://github.com/slodge/MvvmCross/issues/17#issuecomment-8527392
+            var setToNullAttribute = TargetPropertyInfo.GetCustomAttribute<MvxSetToNullAfterBindingAttribute>(true);
+            if (setToNullAttribute != null)
             {
-                // if the target property should be set to NULL on dispose then we clear it here
-                // this is a fix for the possible memory leaks discussion started https://github.com/slodge/MvvmCross/issues/17#issuecomment-8527392
-                var setToNullAttribute = TargetPropertyInfo.GetCustomAttribute<MvxSetToNullAfterBindingAttribute>(true);
-                if (setToNullAttribute != null)
-                {
-                    SetValue(null);
-                }
+                SetValue(null);
             }
-
-            base.Dispose(isDisposing);
         }
 
-        public override Type TargetValueType => TargetPropertyInfo.PropertyType;
-
-        protected PropertyInfo TargetPropertyInfo => _targetPropertyInfo;
-
-        protected override void SetValueImpl(object target, object value)
-        {
-            var setMethod = TargetPropertyInfo.GetSetMethod();
-            setMethod.Invoke(target, new object[] { value });
-        }
+        base.Dispose(isDisposing);
     }
 
-    public class MvxPropertyInfoTargetBinding<T> : MvxPropertyInfoTargetBinding
-        where T : class
+    public override Type TargetValueType => TargetPropertyInfo.PropertyType;
+
+    protected PropertyInfo TargetPropertyInfo => _targetPropertyInfo;
+
+    protected override void SetValueImpl(object target, object? value)
     {
-        public MvxPropertyInfoTargetBinding(object target, PropertyInfo targetPropertyInfo)
-            : base(target, targetPropertyInfo)
-        {
-        }
-
-        protected T View => Target as T;
+        var setMethod = TargetPropertyInfo.GetSetMethod();
+        setMethod?.Invoke(target, [value]);
     }
+}
+
+public abstract class MvxPropertyInfoTargetBinding<T>(
+        object target, PropertyInfo targetPropertyInfo)
+    : MvxPropertyInfoTargetBinding(target, targetPropertyInfo)
+    where T : class
+{
+    protected T? View => Target as T;
 }
