ac37fba190fbbb56744d866f573078e27eb4b5e2
Tomasz Cielecki <tci@trackman.com>
2024-02-22 20:40:52 +0100

    Enable nullable in MvxAdapterViewSelectedItemPositionTargetBinding

--- MvvmCross/Platforms/Android/Binding/Target/MvxAdapterViewSelectedItemPositionTargetBinding.cs
+++ MvvmCross/Platforms/Android/Binding/Target/MvxAdapterViewSelectedItemPositionTargetBinding.cs
@@ -1,60 +1,58 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
-
-using System;
-using Android.Widget;
+#nullable enable
 using MvvmCross.Binding;
 using MvvmCross.Platforms.Android.WeakSubscription;
 
-namespace MvvmCross.Platforms.Android.Binding.Target
+namespace MvvmCross.Platforms.Android.Binding.Target;
+
+public class MvxAdapterViewSelectedItemPositionTargetBinding
+    : MvxAndroidTargetBinding
 {
-    public class MvxAdapterViewSelectedItemPositionTargetBinding
-        : MvxAndroidTargetBinding
+    private IDisposable? _subscription;
+        
+    private AdapterView? AdapterView => (AdapterView?)Target;
+
+    public MvxAdapterViewSelectedItemPositionTargetBinding(AdapterView adapterView)
+        : base(adapterView)
     {
-        protected AdapterView AdapterView => (AdapterView)Target;
+    }
 
-        private IDisposable _subscription;
+    protected override void SetValueImpl(object target, object? value)
+    {
+        if (value != null)
+            AdapterView?.SetSelection((int)value);
+    }
 
-        public MvxAdapterViewSelectedItemPositionTargetBinding(AdapterView adapterView)
-            : base(adapterView)
+    private void AdapterViewOnItemSelected(object? sender, AdapterView.ItemSelectedEventArgs itemSelectedEventArgs)
+    {
+        FireValueChanged(itemSelectedEventArgs.Position);
+    }
+
+    public override MvxBindingMode DefaultMode => MvxBindingMode.TwoWay;
+
+    public override void SubscribeToEvents()
+    {
+        var adapterView = AdapterView;
+
+        if (adapterView == null)
+            return;
+
+        _subscription = adapterView.WeakSubscribe<AdapterView, AdapterView.ItemSelectedEventArgs>(
+            nameof(adapterView.ItemSelected), AdapterViewOnItemSelected);
+    }
+
+    public override Type TargetValueType => typeof(int);
+
+    protected override void Dispose(bool isDisposing)
+    {
+        if (isDisposing)
         {
+            _subscription?.Dispose();
+            _subscription = null;
         }
 
-        protected override void SetValueImpl(object target, object value)
-        {
-            ((AdapterView)target).SetSelection((int)value);
-        }
-
-        private void AdapterViewOnItemSelected(object sender, AdapterView.ItemSelectedEventArgs itemSelectedEventArgs)
-        {
-            FireValueChanged(itemSelectedEventArgs.Position);
-        }
-
-        public override MvxBindingMode DefaultMode => MvxBindingMode.TwoWay;
-
-        public override void SubscribeToEvents()
-        {
-            var adapterView = AdapterView;
-
-            if (adapterView == null)
-                return;
-
-            _subscription = adapterView.WeakSubscribe<AdapterView, AdapterView.ItemSelectedEventArgs>(
-                nameof(adapterView.ItemSelected), AdapterViewOnItemSelected);
-        }
-
-        public override Type TargetValueType => typeof(int);
-
-        protected override void Dispose(bool isDisposing)
-        {
-            if (isDisposing)
-            {
-                _subscription?.Dispose();
-                _subscription = null;
-            }
-
-            base.Dispose(isDisposing);
-        }
+        base.Dispose(isDisposing);
     }
 }
