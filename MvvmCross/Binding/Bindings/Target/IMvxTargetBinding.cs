9b645ee8d18061b0cd692c5ba23e8814d5f0a8e5
Tomasz Cielecki <tci@trackman.com>
2024-02-22 21:14:44 +0100

    Enable nullable on IMvxTargetBinding

--- MvvmCross/Binding/Bindings/Target/IMvxTargetBinding.cs
+++ MvvmCross/Binding/Bindings/Target/IMvxTargetBinding.cs
@@ -1,20 +1,17 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
+#nullable enable
+namespace MvvmCross.Binding.Bindings.Target;
 
-using System;
-
-namespace MvvmCross.Binding.Bindings.Target
+public interface IMvxTargetBinding : IMvxBinding
 {
-    public interface IMvxTargetBinding : IMvxBinding
-    {
-        Type TargetValueType { get; }
-        MvxBindingMode DefaultMode { get; }
+    event EventHandler<MvxTargetChangedEventArgs>? ValueChanged;
 
-        void SetValue(object value);
+    Type TargetValueType { get; }
+    MvxBindingMode DefaultMode { get; }
 
-        event EventHandler<MvxTargetChangedEventArgs> ValueChanged;
+    void SetValue(object? value);
 
-        void SubscribeToEvents();
-    }
+    void SubscribeToEvents();
 }
