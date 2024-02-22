48e18cc0eff70e508b3f5b3060859390c8877025
Tomasz Cielecki <tci@trackman.com>
2024-02-22 20:36:42 +0100

    Enable nullable in MvxTargetBinding

--- MvvmCross/Binding/Bindings/Target/MvxTargetBinding.cs
+++ MvvmCross/Binding/Bindings/Target/MvxTargetBinding.cs
@@ -1,83 +1,89 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
+#nullable enable
 
-using System;
+using Microsoft.Extensions.Logging;
 
-namespace MvvmCross.Binding.Bindings.Target
+namespace MvvmCross.Binding.Bindings.Target;
+
+public abstract class MvxTargetBinding : MvxBinding, IMvxTargetBinding
 {
-    public abstract class MvxTargetBinding : MvxBinding, IMvxTargetBinding
+    public event EventHandler<MvxTargetChangedEventArgs>? ValueChanged;
+
+    private readonly WeakReference _target;
+
+    protected MvxTargetBinding(object target)
     {
-        private readonly WeakReference _target;
-
-        protected MvxTargetBinding(object target)
-        {
-            _target = new WeakReference(target);
-        }
-
-        protected object Target => _target.Target;
-
-        public virtual void SubscribeToEvents()
-        {
-            // do nothing by default
-        }
-
-        protected virtual void FireValueChanged(object newValue)
-        {
-            ValueChanged?.Invoke(this, new MvxTargetChangedEventArgs(newValue));
-        }
-
-        public abstract Type TargetValueType { get; }
-
-        public abstract void SetValue(object value);
-
-        public event EventHandler<MvxTargetChangedEventArgs> ValueChanged;
-
-        public abstract MvxBindingMode DefaultMode { get; }
+        _target = new WeakReference(target);
     }
 
-    public abstract class MvxTargetBinding<TTarget, TValue> : MvxBinding, IMvxTargetBinding
-        where TTarget : class
+    protected object? Target => _target.Target;
+
+    public virtual void SubscribeToEvents()
     {
-        private readonly WeakReference<TTarget> _target;
+        // do nothing by default
+    }
 
-        protected MvxTargetBinding(TTarget target)
+    protected virtual void FireValueChanged(object? newValue)
+    {
+        ValueChanged?.Invoke(this, new MvxTargetChangedEventArgs(newValue));
+    }
+
+    public abstract Type TargetValueType { get; }
+
+    public abstract void SetValue(object? value);
+
+    public abstract MvxBindingMode DefaultMode { get; }
+}
+
+public abstract class MvxTargetBinding<TTarget, TValue> : MvxBinding, IMvxTargetBinding
+    where TTarget : class
+{
+    public event EventHandler<MvxTargetChangedEventArgs>? ValueChanged;
+
+    private readonly WeakReference<TTarget> _target;
+
+    protected MvxTargetBinding(TTarget target)
+    {
+        _target = new WeakReference<TTarget>(target);
+    }
+
+    protected TTarget? Target
+    {
+        get
         {
-            _target = new WeakReference<TTarget>(target);
+            _target.TryGetTarget(out var target);
+            return target;
+        }
+    }
+
+    public virtual void SubscribeToEvents()
+    {
+        // do nothing by default
+    }
+
+    protected virtual void FireValueChanged(TValue? newValue)
+    {
+        ValueChanged?.Invoke(this, new MvxTargetChangedEventArgs(newValue));
+    }
+
+    public abstract MvxBindingMode DefaultMode { get; }
+
+    public Type TargetValueType => typeof(TValue);
+
+    protected abstract void SetValue(TValue? value);
+
+    public void SetValue(object? value)
+    {
+        if (value != null && value is not TValue)
+        {
+            MvxBindingLog.Instance?.LogError(
+                "Invalid value type for target binding {TypeName}: received {ValueTypeName} but expects {ExpectedTypeName}, and cast failed",
+                GetType().Name, value.GetType().Name, typeof(TValue).Name);
+            return;
         }
 
-        protected TTarget Target
-        {
-            get
-            {
-                _target.TryGetTarget(out var target);
-                return target;
-            }
-        }
-
-        public virtual void SubscribeToEvents()
-        {
-            // do nothing by default
-        }
-
-        protected virtual void FireValueChanged(TValue newValue)
-        {
-            ValueChanged?.Invoke(this, new MvxTargetChangedEventArgs(newValue));
-        }
-
-        public abstract MvxBindingMode DefaultMode { get; }
-
-        public Type TargetValueType => typeof(TValue);
-
-        public event EventHandler<MvxTargetChangedEventArgs> ValueChanged;
-
-        protected abstract void SetValue(TValue value);
-
-        public void SetValue(object value)
-        {
-            if (value != null && !(value is TValue))
-                MvxBindingLog.Error($"Invalid value type for target binding {GetType().Name}: received {value.GetType().Name} but expects {typeof(TValue).Name}. And cast failed.");
-            SetValue(value == null ? default(TValue) : (TValue)value);
-        }
+        SetValue(value == null ? default : (TValue)value);
     }
 }
