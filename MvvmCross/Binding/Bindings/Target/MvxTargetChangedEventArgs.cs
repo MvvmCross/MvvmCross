0786789f2fc2033cc66dfd91f687f9653e8475e0
Tomasz Cielecki <tci@trackman.com>
2024-02-22 21:16:57 +0100

    Enable nullable on MvxTargetChangedEventArgs

--- MvvmCross/Binding/Bindings/Target/MvxTargetChangedEventArgs.cs
+++ MvvmCross/Binding/Bindings/Target/MvxTargetChangedEventArgs.cs
@@ -1,19 +1,10 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
+#nullable enable
+namespace MvvmCross.Binding.Bindings.Target;
 
-using System;
-
-namespace MvvmCross.Binding.Bindings.Target
+public class MvxTargetChangedEventArgs(object? value) : EventArgs
 {
-    public class MvxTargetChangedEventArgs
-        : EventArgs
-    {
-        public MvxTargetChangedEventArgs(object value)
-        {
-            Value = value;
-        }
-
-        public object Value { get; private set; }
-    }
+    public object? Value { get; private set; } = value;
 }
