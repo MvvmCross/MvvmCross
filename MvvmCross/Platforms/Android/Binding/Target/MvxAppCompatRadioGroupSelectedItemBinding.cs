84b959c9b3271a37bb5c237966781b96671d8014
Tomasz Cielecki <tci@trackman.com>
2024-02-22 20:45:37 +0100

    Enable nullable in MvxAppCompatRadioGroupSelectedItemBinding

--- MvvmCross/Platforms/Android/Binding/Target/MvxAppCompatRadioGroupSelectedItemBinding.cs
+++ MvvmCross/Platforms/Android/Binding/Target/MvxAppCompatRadioGroupSelectedItemBinding.cs
@@ -1,124 +1,122 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
-
-using System;
+#nullable enable
 using Android.Views;
-using Android.Widget;
 using AndroidX.AppCompat.Widget;
 using MvvmCross.Binding;
 using MvvmCross.Platforms.Android.Binding.Views;
 using MvvmCross.WeakSubscription;
 
-namespace MvvmCross.Platforms.Android.Binding.Target
+namespace MvvmCross.Platforms.Android.Binding.Target;
+
+public class MvxAppCompatRadioGroupSelectedItemBinding
+    : MvxAndroidTargetBinding
 {
-    public class MvxAppCompatRadioGroupSelectedItemBinding
-        : MvxAndroidTargetBinding
+    private object? _currentValue;
+    private IDisposable? _subscription;
+
+    public MvxAppCompatRadioGroupSelectedItemBinding(MvxAppCompatRadioGroup radioGroup)
+        : base(radioGroup)
     {
-        private object _currentValue;
-        private IDisposable _subscription;
+        _subscription = radioGroup.WeakSubscribe<RadioGroup, RadioGroup.CheckedChangeEventArgs>(
+            nameof(RadioGroup.CheckedChange),
+            RadioGroupCheckedChanged);
+    }
 
-        public MvxAppCompatRadioGroupSelectedItemBinding(MvxAppCompatRadioGroup radioGroup)
-            : base(radioGroup)
+    private bool CheckValueChanged(object? newValue)
+    {
+        bool changed;
+        if (newValue == null)
         {
-            _subscription = radioGroup.WeakSubscribe<RadioGroup, RadioGroup.CheckedChangeEventArgs>(
-                nameof(RadioGroup.CheckedChange),
-                RadioGroupCheckedChanged);
+            changed = _currentValue != null;
+        }
+        else
+        {
+            changed = !newValue.Equals(_currentValue);
+        }
+        return changed;
+    }
+
+    private void RadioGroupCheckedChanged(object? sender, RadioGroup.CheckedChangeEventArgs args)
+    {
+        var radioGroup = (MvxAppCompatRadioGroup?)Target;
+        if (radioGroup == null)
+            return;
+
+        object? newValue = null;
+
+        var r = radioGroup.FindViewById<AppCompatRadioButton>(args.CheckedId);
+        if (r != null)
+        {
+            var index = radioGroup.IndexOfChild(r);
+            newValue = radioGroup.Adapter.GetRawItem(index);
         }
 
-        private bool CheckValueChanged(object newValue)
+        bool changed = CheckValueChanged(newValue);
+        if (!changed)
+            return;
+
+        _currentValue = newValue;
+        FireValueChanged(newValue);
+    }
+
+    protected override void SetValueImpl(object target, object? value)
+    {
+        var radioGroup = (MvxAppCompatRadioGroup?)target;
+        if (radioGroup == null)
+            return;
+
+        bool changed = CheckValueChanged(value);
+        if (!changed)
+            return;
+
+        // find the radio button associated with the new value
+        var checkid = GetRadioButtonId(value, radioGroup);
+
+        if (checkid == View.NoId)
         {
-            bool changed;
-            if (newValue == null)
-            {
-                changed = _currentValue != null;
-            }
-            else
-            {
-                changed = !newValue.Equals(_currentValue);
-            }
-            return changed;
+            radioGroup.ClearCheck();
         }
-
-        private void RadioGroupCheckedChanged(object sender, RadioGroup.CheckedChangeEventArgs args)
+        else
         {
-            var radioGroup = (MvxAppCompatRadioGroup)Target;
-            if (radioGroup == null)
-                return;
-
-            object newValue = null;
-
-            var r = radioGroup.FindViewById<AppCompatRadioButton>(args.CheckedId);
-            if (r != null)
-            {
-                var index = radioGroup.IndexOfChild(r);
-                newValue = radioGroup.Adapter.GetRawItem(index);
-            }
-
-            bool changed = CheckValueChanged(newValue);
-            if (!changed)
-                return;
-
-            _currentValue = newValue;
-            FireValueChanged(newValue);
+            radioGroup.Check(checkid);
         }
+    }
 
-        protected override void SetValueImpl(object target, object newValue)
+    private static int GetRadioButtonId(object? value, MvxAppCompatRadioGroup radioGroup)
+    {
+        var checkid = View.NoId;
+
+        
+        if (value == null)
+            return checkid;
+
+        for (int i = 0; i < radioGroup.ChildCount; i++)
         {
-            var radioGroup = (MvxAppCompatRadioGroup)target;
-            if (radioGroup == null)
-                return;
-
-            bool changed = CheckValueChanged(newValue);
-            if (!changed)
-                return;
-
-            int checkid = View.NoId;
-
-            // find the radio button associated with the new value
-            if (newValue != null)
+            var li = radioGroup.GetChildAt(i);
+            var data = radioGroup.Adapter.GetRawItem(i);
+            if (value.Equals(data) && li is AppCompatRadioButton radioButton)
             {
-                for (int i = 0; i < radioGroup.ChildCount; i++)
-                {
-                    var li = radioGroup.GetChildAt(i);
-                    var data = radioGroup.Adapter.GetRawItem(i);
-                    if (li != null)
-                    {
-                        if (newValue.Equals(data))
-                        {
-                            var radioButton = li as AppCompatRadioButton;
-                            if (radioButton != null)
-                            {
-                                checkid = radioButton.Id;
-                                break;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (checkid == View.NoId)
-            {
-                radioGroup.ClearCheck();
-            }
-            else
-            {
-                radioGroup.Check(checkid);
+                checkid = radioButton.Id;
+                break;
             }
         }
 
-        public override MvxBindingMode DefaultMode => MvxBindingMode.TwoWay;
+        return checkid;
+    }
 
-        public override Type TargetValueType => typeof(object);
+    public override MvxBindingMode DefaultMode => MvxBindingMode.TwoWay;
 
-        protected override void Dispose(bool isDisposing)
+    public override Type TargetValueType => typeof(object);
+
+    protected override void Dispose(bool isDisposing)
+    {
+        if (isDisposing)
         {
-            if (isDisposing)
-            {
-                _subscription?.Dispose();
-                _subscription = null;
-            }
-            base.Dispose(isDisposing);
+            _subscription?.Dispose();
+            _subscription = null;
         }
+        base.Dispose(isDisposing);
     }
 }
