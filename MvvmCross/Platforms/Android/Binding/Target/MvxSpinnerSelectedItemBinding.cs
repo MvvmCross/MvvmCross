b2d443225ce732a8d10e477b318bd857dfd76303
Tomasz Cielecki <tci@trackman.com>
2024-02-23 14:41:30 +0100

    Enable nullable on MvxSpinnerSelectedItemBinding

--- MvvmCross/Platforms/Android/Binding/Target/MvxSpinnerSelectedItemBinding.cs
+++ MvvmCross/Platforms/Android/Binding/Target/MvxSpinnerSelectedItemBinding.cs
@@ -2,100 +2,102 @@
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
 
-using System;
-using Android.Widget;
+#nullable enable
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.Logging;
 using MvvmCross.Binding;
 using MvvmCross.Platforms.Android.Binding.Views;
 using MvvmCross.Platforms.Android.WeakSubscription;
 
-namespace MvvmCross.Platforms.Android.Binding.Target
+namespace MvvmCross.Platforms.Android.Binding.Target;
+
+public class MvxSpinnerSelectedItemBinding
+    : MvxAndroidTargetBinding
 {
-    public class MvxSpinnerSelectedItemBinding
-        : MvxAndroidTargetBinding
+    protected MvxSpinner? Spinner => (MvxSpinner?)Target;
+
+    private object? _currentValue;
+    private IDisposable? _subscription;
+
+    public MvxSpinnerSelectedItemBinding(
+            [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicEvents)]
+            MvxSpinner spinner)
+        : base(spinner)
     {
-        protected MvxSpinner Spinner => (MvxSpinner)Target;
+    }
 
-        private object _currentValue;
-        private IDisposable _subscription;
+    private void SpinnerItemSelected(object? sender, AdapterView.ItemSelectedEventArgs e)
+    {
+        var spinner = Spinner;
+        if (spinner == null)
+            return;
 
-        public MvxSpinnerSelectedItemBinding(MvxSpinner spinner)
-            : base(spinner)
+        var newValue = spinner.Adapter.GetRawItem(e.Position);
+
+        bool changed;
+        if (newValue == null)
         {
+            changed = _currentValue != null;
+        }
+        else
+        {
+            changed = !newValue.Equals(_currentValue);
         }
 
-        private void SpinnerItemSelected(object sender, AdapterView.ItemSelectedEventArgs e)
+        if (!changed)
         {
-            var spinner = Spinner;
-            if (spinner == null)
+            return;
+        }
+
+        _currentValue = newValue;
+        FireValueChanged(newValue);
+    }
+
+    protected override void SetValueImpl(object target, object? value)
+    {
+        var spinner = (MvxSpinner)target;
+
+        if (value == null)
+        {
+            MvxBindingLog.Warning("Null values not permitted in spinner SelectedItem binding currently");
+            return;
+        }
+
+        if (!value.Equals(_currentValue))
+        {
+            var index = spinner.Adapter.GetPosition(value);
+            if (index < 0)
+            {
+                MvxBindingLog.Instance?.LogWarning("Value not found for spinner {Value}", value);
                 return;
-
-            var newValue = spinner.Adapter.GetRawItem(e.Position);
-
-            bool changed;
-            if (newValue == null)
-            {
-                changed = _currentValue != null;
             }
-            else
-            {
-                changed = !newValue.Equals(_currentValue);
-            }
-
-            if (!changed)
-            {
-                return;
-            }
-
-            _currentValue = newValue;
-            FireValueChanged(newValue);
+            _currentValue = value;
+            spinner.SetSelection(index);
         }
+    }
 
-        protected override void SetValueImpl(object target, object value)
+    public override MvxBindingMode DefaultMode => MvxBindingMode.TwoWay;
+
+    public override void SubscribeToEvents()
+    {
+        var spinner = Spinner;
+        if (spinner == null)
+            return;
+
+        _subscription = spinner.WeakSubscribe<AdapterView, AdapterView.ItemSelectedEventArgs>(
+            nameof(spinner.ItemSelected),
+            SpinnerItemSelected);
+    }
+
+    public override Type TargetValueType => typeof(object);
+
+    protected override void Dispose(bool isDisposing)
+    {
+        if (isDisposing)
         {
-            var spinner = (MvxSpinner)target;
-
-            if (value == null)
-            {
-                MvxBindingLog.Warning("Null values not permitted in spinner SelectedItem binding currently");
-                return;
-            }
-
-            if (!value.Equals(_currentValue))
-            {
-                var index = spinner.Adapter.GetPosition(value);
-                if (index < 0)
-                {
-                    MvxBindingLog.Warning("Value not found for spinner {0}", value.ToString());
-                    return;
-                }
-                _currentValue = value;
-                spinner.SetSelection(index);
-            }
+            _subscription?.Dispose();
+            _subscription = null;
         }
-
-        public override MvxBindingMode DefaultMode => MvxBindingMode.TwoWay;
-
-        public override void SubscribeToEvents()
-        {
-            var spinner = Spinner;
-            if (spinner == null)
-                return;
-
-            _subscription = spinner.WeakSubscribe<AdapterView, AdapterView.ItemSelectedEventArgs>(
-                nameof(spinner.ItemSelected),
-                SpinnerItemSelected);
-        }
-
-        public override Type TargetValueType => typeof(object);
-
-        protected override void Dispose(bool isDisposing)
-        {
-            if (isDisposing)
-            {
-                _subscription?.Dispose();
-                _subscription = null;
-            }
-            base.Dispose(isDisposing);
-        }
+        base.Dispose(isDisposing);
     }
 }
