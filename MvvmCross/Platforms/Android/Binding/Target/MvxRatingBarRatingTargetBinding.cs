9c101a8b3fb17156689d4ce8cef9bccb8671dcc0
Tomasz Cielecki <tci@trackman.com>
2024-02-23 14:28:15 +0100

    Enable nullables on MvxRatingBarRatingTargetBinding

--- MvvmCross/Platforms/Android/Binding/Target/MvxRatingBarRatingTargetBinding.cs
+++ MvvmCross/Platforms/Android/Binding/Target/MvxRatingBarRatingTargetBinding.cs
@@ -2,61 +2,61 @@
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
 
-using System;
-using Android.Widget;
+#nullable enable
+using System.Diagnostics.CodeAnalysis;
 using MvvmCross.Binding;
 using MvvmCross.Platforms.Android.WeakSubscription;
 
-namespace MvvmCross.Platforms.Android.Binding.Target
+namespace MvvmCross.Platforms.Android.Binding.Target;
+
+public class MvxRatingBarRatingTargetBinding
+    : MvxAndroidTargetBinding
 {
-    public class MvxRatingBarRatingTargetBinding
-        : MvxAndroidTargetBinding
+    private IDisposable? _subscription;
+
+    protected RatingBar? RatingBar => (RatingBar?)Target;
+
+    public MvxRatingBarRatingTargetBinding(
+        [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicEvents)]
+        RatingBar target)
+        : base(target)
     {
-        protected RatingBar RatingBar => (RatingBar)Target;
+    }
 
-        private IDisposable _subscription;
+    public override void SubscribeToEvents()
+    {
+        _subscription = RatingBar?.WeakSubscribe<RatingBar, RatingBar.RatingBarChangeEventArgs>(
+            nameof(RatingBar.RatingBarChange),
+            RatingBar_RatingBarChange);
+    }
 
-        public MvxRatingBarRatingTargetBinding(RatingBar target)
-            : base(target)
-        {
-        }
+    private void RatingBar_RatingBarChange(object? sender, RatingBar.RatingBarChangeEventArgs e)
+    {
+        if (Target is not RatingBar target)
+            return;
 
-        public override void SubscribeToEvents()
-        {
-            _subscription = RatingBar.WeakSubscribe<RatingBar, RatingBar.RatingBarChangeEventArgs>(
-                nameof(RatingBar.RatingBarChange),
-                RatingBar_RatingBarChange);
-        }
+        var value = target.Rating;
+        FireValueChanged(value);
+    }
 
-        private void RatingBar_RatingBarChange(object sender, RatingBar.RatingBarChangeEventArgs e)
-        {
-            var target = Target as RatingBar;
-
-            if (target == null)
-                return;
-
-            var value = target.Rating;
-            FireValueChanged(value);
-        }
-
-        protected override void SetValueImpl(object target, object value)
-        {
-            var ratingBar = (RatingBar)target;
+    protected override void SetValueImpl(object target, object? value)
+    {
+        var ratingBar = (RatingBar)target;
+        if (value != null)
             ratingBar.Rating = (float)value;
-        }
+    }
 
-        public override Type TargetValueType => typeof(float);
+    public override Type TargetValueType => typeof(float);
 
-        public override MvxBindingMode DefaultMode => MvxBindingMode.TwoWay;
+    public override MvxBindingMode DefaultMode => MvxBindingMode.TwoWay;
 
-        protected override void Dispose(bool isDisposing)
+    protected override void Dispose(bool isDisposing)
+    {
+        if (isDisposing)
         {
-            if (isDisposing)
-            {
-                _subscription?.Dispose();
-                _subscription = null;
-            }
-            base.Dispose(isDisposing);
+            _subscription?.Dispose();
+            _subscription = null;
         }
+        base.Dispose(isDisposing);
     }
 }
