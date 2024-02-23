0c74eab9d6d4bd67492bd0e55328a6fa8286df7d
Tomasz Cielecki <tci@trackman.com>
2024-02-23 14:45:48 +0100

    Enable nullable on MvxTextViewTextFormattedTargetBinding

--- MvvmCross/Platforms/Android/Binding/Target/MvxTextViewTextFormattedTargetBinding.cs
+++ MvvmCross/Platforms/Android/Binding/Target/MvxTextViewTextFormattedTargetBinding.cs
@@ -2,85 +2,74 @@
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
 
-using System;
+#nullable enable
+using System.Diagnostics.CodeAnalysis;
 using Android.Text;
-using Android.Widget;
 using MvvmCross.Binding;
 using MvvmCross.Binding.Extensions;
 using MvvmCross.Platforms.Android.WeakSubscription;
 
-namespace MvvmCross.Platforms.Android.Binding.Target
+namespace MvvmCross.Platforms.Android.Binding.Target;
+
+public class MvxTextViewTextFormattedTargetBinding(
+        [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicEvents)]
+        TextView target)
+    : MvxAndroidTargetBinding(target), IMvxEditableTextView
 {
-    public class MvxTextViewTextFormattedTargetBinding
-        : MvxAndroidTargetBinding, IMvxEditableTextView
+    private readonly bool _isEditTextBinding = target is EditText;
+    private IDisposable? _subscription;
+
+    protected TextView? TextView => Target as TextView;
+
+    public override Type TargetValueType => typeof(ISpanned);
+
+    protected override bool ShouldSkipSetValueForViewSpecificReasons(object target, object? value)
     {
-        private readonly bool _isEditTextBinding;
-        private IDisposable _subscription;
+        if (!_isEditTextBinding)
+            return false;
 
-        protected TextView TextView => Target as TextView;
+        return this.ShouldSkipSetValueAsHaveNearlyIdenticalNumericText(target, value);
+    }
 
-        public MvxTextViewTextFormattedTargetBinding(TextView target)
-            : base(target)
+    protected override void SetValueImpl(object target, object? value)
+    {
+        ((TextView)target).TextFormatted = (ISpanned?)value;
+    }
+
+    public override MvxBindingMode DefaultMode => _isEditTextBinding ? MvxBindingMode.TwoWay : MvxBindingMode.OneWay;
+
+    public override void SubscribeToEvents()
+    {
+        var view = TextView;
+        if (view == null)
+            return;
+
+        _subscription = view.WeakSubscribe<TextView, AfterTextChangedEventArgs>(
+            nameof(view.AfterTextChanged),
+            EditTextOnAfterTextChanged);
+    }
+
+    private void EditTextOnAfterTextChanged(object? sender, AfterTextChangedEventArgs afterTextChangedEventArgs)
+    {
+        FireValueChanged(TextView?.TextFormatted);
+    }
+
+    protected override void Dispose(bool isDisposing)
+    {
+        if (isDisposing)
         {
-            if (target == null)
-            {
-                MvxBindingLog.Error("Error - TextView is null in MvxTextViewTextFormattedTargetBinding");
-                return;
-            }
-
-            _isEditTextBinding = target is EditText;
+            _subscription?.Dispose();
+            _subscription = null;
         }
+        base.Dispose(isDisposing);
+    }
 
-        public override Type TargetValueType => typeof(ISpanned);
-
-        protected override bool ShouldSkipSetValueForViewSpecificReasons(object target, object value)
-        {
-            if (!_isEditTextBinding)
-                return false;
-
-            return this.ShouldSkipSetValueAsHaveNearlyIdenticalNumericText(target, value);
-        }
-
-        protected override void SetValueImpl(object target, object toSet)
-        {
-            ((TextView)target).TextFormatted = (ISpanned)toSet;
-        }
-
-        public override MvxBindingMode DefaultMode => _isEditTextBinding ? MvxBindingMode.TwoWay : MvxBindingMode.OneWay;
-
-        public override void SubscribeToEvents()
+    public string? CurrentText
+    {
+        get
         {
             var view = TextView;
-            if (view == null)
-                return;
-
-            _subscription = view.WeakSubscribe<TextView, AfterTextChangedEventArgs>(
-                nameof(view.AfterTextChanged),
-                EditTextOnAfterTextChanged);
-        }
-
-        private void EditTextOnAfterTextChanged(object sender, AfterTextChangedEventArgs afterTextChangedEventArgs)
-        {
-            FireValueChanged(TextView.TextFormatted);
-        }
-
-        protected override void Dispose(bool isDisposing)
-        {
-            if (isDisposing)
-            {
-                _subscription?.Dispose();
-                _subscription = null;
-            }
-            base.Dispose(isDisposing);
-        }
-
-        public string CurrentText
-        {
-            get
-            {
-                var view = TextView;
-                return view?.TextFormatted.ToString();
-            }
+            return view?.TextFormatted?.ToString();
         }
     }
 }
