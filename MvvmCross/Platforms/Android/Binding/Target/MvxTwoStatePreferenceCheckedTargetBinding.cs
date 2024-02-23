c6a99089b00acee4c2fadb530efe54ffb7ce79f3
Tomasz Cielecki <tci@trackman.com>
2024-02-23 14:47:31 +0100

    Enable nullable on MvxTwoStatePreferenceCheckedTargetBinding

--- MvvmCross/Platforms/Android/Binding/Target/MvxTwoStatePreferenceCheckedTargetBinding.cs
+++ MvvmCross/Platforms/Android/Binding/Target/MvxTwoStatePreferenceCheckedTargetBinding.cs
@@ -2,24 +2,19 @@
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
 
+#nullable enable
 using AndroidX.Preference;
 
-namespace MvvmCross.Platforms.Android.Binding.Target
-{
-    public class MvxTwoStatePreferenceCheckedTargetBinding : MvxPreferenceValueTargetBinding
-    {
-        public MvxTwoStatePreferenceCheckedTargetBinding(TwoStatePreference preference)
-            : base(preference)
-        {
-        }
+namespace MvvmCross.Platforms.Android.Binding.Target;
 
-        protected override void SetValueImpl(object target, object value)
+public class MvxTwoStatePreferenceCheckedTargetBinding(TwoStatePreference preference)
+    : MvxPreferenceValueTargetBinding(preference)
+{
+    protected override void SetValueImpl(object target, object? value)
+    {
+        if (target is TwoStatePreference t && value != null)
         {
-            var t = target as TwoStatePreference;
-            if (t != null)
-            {
-                t.Checked = (bool)value;
-            }
+            t.Checked = (bool)value;
         }
     }
 }
