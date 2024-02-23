03e805576b05c05a30fff2e6a2ec07d9304f51a6
Tomasz Cielecki <tci@trackman.com>
2024-02-23 14:53:56 +0100

    Enable nullable on MvxViewLongClickBinding

--- MvvmCross/Platforms/Android/Binding/Target/MvxViewLongClickBinding.cs
+++ MvvmCross/Platforms/Android/Binding/Target/MvxViewLongClickBinding.cs
@@ -2,58 +2,60 @@
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
 
-using System;
+#nullable enable
+using System.Diagnostics.CodeAnalysis;
 using System.Windows.Input;
 using Android.Views;
 using MvvmCross.Binding;
 using MvvmCross.Platforms.Android.WeakSubscription;
 
-namespace MvvmCross.Platforms.Android.Binding.Target
+namespace MvvmCross.Platforms.Android.Binding.Target;
+
+public class MvxViewLongClickBinding
+    : MvxAndroidTargetBinding
 {
-    public class MvxViewLongClickBinding
-        : MvxAndroidTargetBinding
+    private ICommand? _command;
+    private IDisposable? _subscription;
+
+    protected View? View => (View?)Target;
+
+    public MvxViewLongClickBinding(
+            [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicEvents)]
+            View view)
+        : base(view)
     {
-        private ICommand _command;
-        private IDisposable _subscription;
+        _subscription = view.WeakSubscribe<View, View.LongClickEventArgs>(nameof(view.LongClick), ViewOnLongClick);
+    }
 
-        protected View View => (View)Target;
+    private void ViewOnLongClick(object? sender, View.LongClickEventArgs longClickEventArgs)
+    {
+        if (_command == null)
+            return;
 
-        public MvxViewLongClickBinding(View view)
-            : base(view)
+        if (!_command.CanExecute(null))
+            return;
+
+        _command.Execute(null);
+    }
+
+    protected override void SetValueImpl(object target, object? value)
+    {
+        _command = value as ICommand;
+    }
+
+    public override MvxBindingMode DefaultMode => MvxBindingMode.OneWay;
+
+    public override Type TargetValueType => typeof(ICommand);
+
+    protected override void Dispose(bool isDisposing)
+    {
+        if (isDisposing)
         {
-            _subscription = view.WeakSubscribe<View, View.LongClickEventArgs>(nameof(view.LongClick), ViewOnLongClick);
+            _subscription?.Dispose();
+            _subscription = null;
+
+            _command = null;
         }
-
-        private void ViewOnLongClick(object sender, View.LongClickEventArgs longClickEventArgs)
-        {
-            if (_command == null)
-                return;
-
-            if (!_command.CanExecute(null))
-                return;
-
-            _command.Execute(null);
-        }
-
-        protected override void SetValueImpl(object target, object value)
-        {
-            _command = value as ICommand;
-        }
-
-        public override MvxBindingMode DefaultMode => MvxBindingMode.OneWay;
-
-        public override Type TargetValueType => typeof(ICommand);
-
-        protected override void Dispose(bool isDisposing)
-        {
-            if (isDisposing)
-            {
-                _subscription?.Dispose();
-                _subscription = null;
-
-                _command = null;
-            }
-            base.Dispose(isDisposing);
-        }
+        base.Dispose(isDisposing);
     }
 }
