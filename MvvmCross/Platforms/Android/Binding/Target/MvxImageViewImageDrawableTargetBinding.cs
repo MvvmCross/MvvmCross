1803c1a9fa20fecab3019601dc5d71cd82663b3e
Tomasz Cielecki <tci@trackman.com>
2024-02-23 12:52:32 +0100

    Enable nullable on MvxImageViewImageDrawableTargetBinding

--- MvvmCross/Platforms/Android/Binding/Target/MvxImageViewImageDrawableTargetBinding.cs
+++ MvvmCross/Platforms/Android/Binding/Target/MvxImageViewImageDrawableTargetBinding.cs
@@ -1,31 +1,23 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
-
-using System;
+#nullable enable
 using Android.Graphics.Drawables;
-using Android.Widget;
 using MvvmCross.Binding;
 
-namespace MvvmCross.Platforms.Android.Binding.Target
+namespace MvvmCross.Platforms.Android.Binding.Target;
+
+public class MvxImageViewImageDrawableTargetBinding(ImageView target)
+    : MvxAndroidTargetBinding(target)
 {
-    public class MvxImageViewImageDrawableTargetBinding : MvxAndroidTargetBinding
+    public override Type TargetValueType => typeof(ImageView);
+
+    public override MvxBindingMode DefaultMode => MvxBindingMode.OneWay;
+
+    protected override void SetValueImpl(object target, object? value)
     {
-        public MvxImageViewImageDrawableTargetBinding(ImageView target)
-            : base(target)
-        {
-        }
-
-        public override Type TargetValueType => typeof(ImageView);
-
-        public override MvxBindingMode DefaultMode => MvxBindingMode.OneWay;
-
-        protected override void SetValueImpl(object target, object value)
-        {
-            var view = (ImageView)target;
-            var drawable = value as Drawable;
-
-            view.SetImageDrawable(drawable);
-        }
+        var view = (ImageView)target;
+        var drawable = value as Drawable;
+        view.SetImageDrawable(drawable);
     }
 }
