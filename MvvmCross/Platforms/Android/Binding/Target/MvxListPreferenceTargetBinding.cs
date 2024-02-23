55f541ff69f678f0119ed44c59d6ab0fcf809298
Tomasz Cielecki <tci@trackman.com>
2024-02-23 12:55:30 +0100

    Enable nullable on MvxListPreferenceTargetBinding

--- MvvmCross/Platforms/Android/Binding/Target/MvxListPreferenceTargetBinding.cs
+++ MvvmCross/Platforms/Android/Binding/Target/MvxListPreferenceTargetBinding.cs
@@ -1,23 +1,17 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
-
+#nullable enable
 using AndroidX.Preference;
 
-namespace MvvmCross.Platforms.Android.Binding.Target
-{
-    public class MvxListPreferenceTargetBinding : MvxPreferenceValueTargetBinding
-    {
-        public MvxListPreferenceTargetBinding(ListPreference preference)
-            : base(preference)
-        {
-        }
+namespace MvvmCross.Platforms.Android.Binding.Target;
 
-        protected override void SetValueImpl(object target, object value)
-        {
-            var pref = target as ListPreference;
-            if (pref != null)
-                pref.Value = (string)value;
-        }
+public class MvxListPreferenceTargetBinding(ListPreference preference)
+    : MvxPreferenceValueTargetBinding(preference)
+{
+    protected override void SetValueImpl(object target, object? value)
+    {
+        if (target is ListPreference pref)
+            pref.Value = (string?)value;
     }
 }
