687cbbf83d165d1679428ef188933d69e4cc6975
Tomasz Cielecki <tci@trackman.com>
2024-02-23 15:51:55 +0100

    Use readonly auto prop

--- MvvmCross/Binding/Bindings/Target/MvxTargetChangedEventArgs.cs
+++ MvvmCross/Binding/Bindings/Target/MvxTargetChangedEventArgs.cs
@@ -1,10 +1,11 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
+
 #nullable enable
 namespace MvvmCross.Binding.Bindings.Target;
 
 public class MvxTargetChangedEventArgs(object? value) : EventArgs
 {
-    public object? Value { get; private set; } = value;
+    public object? Value { get; } = value;
 }
