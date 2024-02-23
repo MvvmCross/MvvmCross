2dc3bba4e6aa9c9f9849a099397a97ca321cee24
Tomasz Cielecki <tci@trackman.com>
2024-02-23 12:54:24 +0100

    Enable nullable on MvxImageViewImageTargetBinding

--- MvvmCross/Platforms/Android/Binding/Target/MvxImageViewImageTargetBinding.cs
+++ MvvmCross/Platforms/Android/Binding/Target/MvxImageViewImageTargetBinding.cs
@@ -1,48 +1,40 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
-
-using System;
-using System.IO;
-using Android.Widget;
+#nullable enable
+using Microsoft.Extensions.Logging;
 using MvvmCross.Binding;
 
-namespace MvvmCross.Platforms.Android.Binding.Target
+namespace MvvmCross.Platforms.Android.Binding.Target;
+
+public class MvxImageViewImageTargetBinding(ImageView imageView)
+    : MvxBaseStreamImageViewTargetBinding(imageView)
 {
-    public class MvxImageViewImageTargetBinding
-        : MvxBaseStreamImageViewTargetBinding
+    public override Type TargetValueType => typeof(string);
+
+    protected override Stream? GetStream(object? value)
     {
-        public MvxImageViewImageTargetBinding(ImageView imageView)
-            : base(imageView)
+        if (value == null)
         {
+            MvxBindingLog.Instance?.LogWarning("Null value passed to ImageView binding");
+            return null;
         }
 
-        public override Type TargetValueType => typeof(string);
-
-        protected override Stream GetStream(object value)
+        var stringValue = value as string;
+        if (string.IsNullOrWhiteSpace(stringValue))
         {
-            if (value == null)
-            {
-                MvxBindingLog.Warning("Null value passed to ImageView binding");
-                return null;
-            }
-
-            var stringValue = value as string;
-            if (string.IsNullOrWhiteSpace(stringValue))
-            {
-                MvxBindingLog.Warning("Empty value passed to ImageView binding");
-                return null;
-            }
-
-            var drawableResourceName = GetImageAssetName(stringValue);
-            var assetStream = AndroidGlobals.ApplicationContext.Assets.Open(drawableResourceName);
-
-            return assetStream;
+            MvxBindingLog.Instance?.LogWarning("Empty value passed to ImageView binding");
+            return null;
         }
 
-        private static string GetImageAssetName(string rawImage)
-        {
-            return rawImage.TrimStart('/');
-        }
+        var drawableResourceName = GetImageAssetName(stringValue);
+        var assetStream = Application.Context.Assets?.Open(drawableResourceName);
+
+        return assetStream;
+    }
+
+    private static string GetImageAssetName(string rawImage)
+    {
+        return rawImage.TrimStart('/');
     }
 }
