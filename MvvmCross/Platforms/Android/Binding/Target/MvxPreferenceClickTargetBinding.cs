4e80dc91437ffc6a2a7d1a1a23516344a8ef5821
Tomasz Cielecki <tci@trackman.com>
2024-02-23 14:24:32 +0100

    Enable nullable on MvxPreferenceClickTargetBinding

--- MvvmCross/Platforms/Android/Binding/Target/MvxPreferenceClickTargetBinding.cs
+++ MvvmCross/Platforms/Android/Binding/Target/MvxPreferenceClickTargetBinding.cs
@@ -1,83 +1,88 @@
-using System;
+#nullable enable
+using System.Diagnostics.CodeAnalysis;
 using System.Windows.Input;
 using AndroidX.Preference;
 using MvvmCross.Binding;
 using MvvmCross.Platforms.Android.WeakSubscription;
 
-namespace MvvmCross.Platforms.Android.Binding.Target
+namespace MvvmCross.Platforms.Android.Binding.Target;
+
+public class MvxPreferenceClickTargetBinding
+    : MvxAndroidTargetBinding
 {
-    public class MvxPreferenceClickTargetBinding : MvxAndroidTargetBinding
+    private readonly EventHandler<EventArgs> _canExecuteEventHandler;
+    private ICommand? _command;
+    private IDisposable? _clickSubscription;
+    private IDisposable? _canExecuteSubscription;
+
+    protected Preference? Preference => (Preference?)Target;
+
+    public MvxPreferenceClickTargetBinding(
+        [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicEvents)]
+        Preference view)
+        : base(view)
     {
-        private ICommand _command;
-        private IDisposable _clickSubscription;
-        private IDisposable _canExecuteSubscription;
-        private readonly EventHandler<EventArgs> _canExecuteEventHandler;
+        _canExecuteEventHandler = OnCanExecuteChanged;
 
-        protected Preference Preference => (Preference)Target;
+        _clickSubscription = view.WeakSubscribe<Preference, Preference.PreferenceClickEventArgs>(
+            nameof(Preference.PreferenceClick),
+            ViewOnPreferenceClick);
+    }
 
-        public MvxPreferenceClickTargetBinding(Preference view)
-            : base(view)
+    private void ViewOnPreferenceClick(object? sender, Preference.PreferenceClickEventArgs args)
+    {
+        if (_command == null)
+            return;
+
+        if (!_command.CanExecute(null))
+            return;
+
+        _command.Execute(null);
+    }
+
+    protected override void SetValueImpl(object target, object? value)
+    {
+        _canExecuteSubscription?.Dispose();
+        _canExecuteSubscription = null;
+
+        _command = value as ICommand;
+        if (_command != null)
         {
-            _canExecuteEventHandler = OnCanExecuteChanged;
-
-            _clickSubscription = Preference.WeakSubscribe<Preference, Preference.PreferenceClickEventArgs>(
-                nameof(Preference.PreferenceClick),
-                ViewOnPreferenceClick);
+            _canExecuteSubscription =
+                MvvmCross.WeakSubscription.MvxWeakSubscriptionExtensions.WeakSubscribe(_command,
+                    _canExecuteEventHandler);
         }
+        RefreshEnabledState();
+    }
 
-        private void ViewOnPreferenceClick(object sender, Preference.PreferenceClickEventArgs args)
+    private void RefreshEnabledState()
+    {
+        var view = Preference;
+        if (view == null)
+            return;
+
+        view.Enabled = _command?.CanExecute(null) ?? false;
+    }
+
+    private void OnCanExecuteChanged(object? sender, EventArgs e)
+    {
+        RefreshEnabledState();
+    }
+
+    public override MvxBindingMode DefaultMode => MvxBindingMode.OneWay;
+
+    public override Type TargetValueType => typeof(ICommand);
+
+    protected override void Dispose(bool isDisposing)
+    {
+        if (isDisposing)
         {
-            if (_command == null)
-                return;
+            _clickSubscription?.Dispose();
+            _clickSubscription = null;
 
-            if (!_command.CanExecute(null))
-                return;
-
-            _command.Execute(null);
-        }
-
-        protected override void SetValueImpl(object target, object value)
-        {
             _canExecuteSubscription?.Dispose();
             _canExecuteSubscription = null;
-
-            _command = value as ICommand;
-            if (_command != null)
-            {
-                _canExecuteSubscription = MvvmCross.WeakSubscription.MvxWeakSubscriptionExtensions.WeakSubscribe(_command, _canExecuteEventHandler);
-            }
-            RefreshEnabledState();
         }
-
-        private void RefreshEnabledState()
-        {
-            var view = Preference;
-            if (view == null)
-                return;
-
-            view.Enabled = _command?.CanExecute(null) ?? false;
-        }
-
-        private void OnCanExecuteChanged(object sender, EventArgs e)
-        {
-            RefreshEnabledState();
-        }
-
-        public override MvxBindingMode DefaultMode => MvxBindingMode.OneWay;
-
-        public override Type TargetValueType => typeof(ICommand);
-
-        protected override void Dispose(bool isDisposing)
-        {
-            if (isDisposing)
-            {
-                _clickSubscription?.Dispose();
-                _clickSubscription = null;
-
-                _canExecuteSubscription?.Dispose();
-                _canExecuteSubscription = null;
-            }
-            base.Dispose(isDisposing);
-        }
+        base.Dispose(isDisposing);
     }
 }
