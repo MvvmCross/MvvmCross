ad85d3b103c3b936791a0611372c9f96c5a54950
Tomasz Cielecki <tci@trackman.com>
2024-03-17 16:11:18 -0700

    Fix warnings in MvxLanguageBinder

--- MvvmCross/Localization/IMvxLanguageBinder.cs
+++ MvvmCross/Localization/IMvxLanguageBinder.cs
@@ -1,15 +1,13 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
-
-namespace MvvmCross.Localization
-{
 #nullable enable
-    public interface IMvxLanguageBinder
-    {
-        string? GetText(string entryKey);
 
-        string? GetText(string entryKey, params object[] args);
-    }
-#nullable restore
+namespace MvvmCross.Localization;
+
+public interface IMvxLanguageBinder
+{
+    string? GetText(string entryKey);
+
+    string? GetText(string entryKey, params object[] args);
 }
