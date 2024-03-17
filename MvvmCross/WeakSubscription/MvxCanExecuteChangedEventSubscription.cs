3f3ae1b4aeeb4ef2ab2e076bc37b5b72b2f0ec84
Tomasz Cielecki <tci@trackman.com>
2024-03-17 16:27:00 -0700

    Fix nullable warning in MvxCanExecuteChangedEventSubscription

--- MvvmCross/WeakSubscription/MvxCanExecuteChangedEventSubscription.cs
+++ MvvmCross/WeakSubscription/MvxCanExecuteChangedEventSubscription.cs
@@ -1,29 +1,19 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
+#nullable enable
 
-using System;
 using System.Reflection;
 using System.Windows.Input;
 
-namespace MvvmCross.WeakSubscription
+namespace MvvmCross.WeakSubscription;
+
+public class MvxCanExecuteChangedEventSubscription(
+        ICommand source,
+        EventHandler<EventArgs> eventHandler)
+    : MvxWeakEventSubscription<ICommand, EventArgs>(source, CanExecuteChangedEventInfo, eventHandler)
 {
-#nullable enable
-    public class MvxCanExecuteChangedEventSubscription
-        : MvxWeakEventSubscription<ICommand, EventArgs>
-    {
-        private static readonly EventInfo CanExecuteChangedEventInfo = typeof(ICommand).GetEvent("CanExecuteChanged");
+    private static readonly EventInfo CanExecuteChangedEventInfo = typeof(ICommand).GetEvent("CanExecuteChanged")!;
 
-        public MvxCanExecuteChangedEventSubscription(
-            ICommand source, EventHandler<EventArgs> eventHandler)
-            : base(source, CanExecuteChangedEventInfo, eventHandler)
-        {
-        }
-
-        protected override Delegate CreateEventHandler()
-        {
-            return new EventHandler(OnSourceEvent);
-        }
-    }
-#nullable restore
+    protected override Delegate CreateEventHandler() => new EventHandler(OnSourceEvent);
 }
