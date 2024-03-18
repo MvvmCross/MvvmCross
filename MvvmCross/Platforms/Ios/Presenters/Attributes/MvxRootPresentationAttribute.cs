810642f1fd200c45e2246ef83de946d5054c57e4
Tomasz Cielecki <tci@trackman.com>
2024-03-17 20:39:03 -0700

    Fix warnings in iOS presentation attributes

--- MvvmCross/Platforms/Ios/Presenters/Attributes/MvxRootPresentationAttribute.cs
+++ MvvmCross/Platforms/Ios/Presenters/Attributes/MvxRootPresentationAttribute.cs
@@ -1,24 +1,23 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
+#nullable enable
 
 using MvvmCross.Presenters.Attributes;
-using UIKit;
 
-namespace MvvmCross.Platforms.Ios.Presenters.Attributes
+namespace MvvmCross.Platforms.Ios.Presenters.Attributes;
+
+public class MvxRootPresentationAttribute : MvxBasePresentationAttribute
 {
-    public class MvxRootPresentationAttribute : MvxBasePresentationAttribute
-    {
-        public static float DefaultAnimationDuration = 1.0f;
+    public static readonly float DefaultAnimationDuration = 1.0f;
 
-        public static bool DefaultWrapInNavigationController = false;
+    public static readonly bool DefaultWrapInNavigationController = false;
 
-        public static UIViewAnimationOptions DefaultAnimationOptions = UIViewAnimationOptions.TransitionNone;
+    public static readonly UIViewAnimationOptions DefaultAnimationOptions = UIViewAnimationOptions.TransitionNone;
 
-        public float AnimationDuration { get; set; } = DefaultAnimationDuration;
+    public float AnimationDuration { get; set; } = DefaultAnimationDuration;
 
-        public UIViewAnimationOptions AnimationOptions { get; set; } = DefaultAnimationOptions;
+    public UIViewAnimationOptions AnimationOptions { get; set; } = DefaultAnimationOptions;
 
-        public bool WrapInNavigationController { get; set; } = DefaultWrapInNavigationController;
-    }
+    public bool WrapInNavigationController { get; set; } = DefaultWrapInNavigationController;
 }
