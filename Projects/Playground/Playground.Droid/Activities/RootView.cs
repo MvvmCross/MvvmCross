1d84184c7d2f6c7ce4195ecc25c538e21df988ed
Tomasz Cielecki <249719+Cheesebaron@users.noreply.github.com>
2025-07-18 11:49:35 +0200

    Apply window insets to support Android edge-to-edge

--- Projects/Playground/Playground.Droid/Activities/RootView.cs
+++ Projects/Playground/Playground.Droid/Activities/RootView.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using Android.Views;
+using AndroidX.Core.View;
 using MvvmCross.Platforms.Android.Presenters.Attributes;
 using MvvmCross.Platforms.Android.Views;
 using Playground.Core.ViewModels;
@@ -12,13 +13,28 @@ namespace Playground.Droid.Activities
     [MvxActivityPresentation]
     [Activity(Theme = "@style/AppTheme",
         WindowSoftInputMode = SoftInput.AdjustPan)]
-    public class RootView : MvxActivity<RootViewModel>
+    public class RootView : MvxActivity<RootViewModel>, IOnApplyWindowInsetsListener
     {
-        protected override void OnCreate(Bundle bundle)
+        protected override void OnCreate(Bundle savedInstanceState)
         {
-            base.OnCreate(bundle);
+            base.OnCreate(savedInstanceState);
 
             SetContentView(Resource.Layout.RootView);
+
+            ViewCompat.SetOnApplyWindowInsetsListener(FindViewById(Resource.Id.main_frame), this);
+        }
+
+        public WindowInsetsCompat OnApplyWindowInsets(View v, WindowInsetsCompat insets)
+        {
+            var inset = insets.GetInsets(WindowInsetsCompat.Type.SystemBars());
+
+            (v.LayoutParameters as FrameLayout.LayoutParams).SetMargins(
+                inset.Left,
+                inset.Top,
+                inset.Right,
+                inset.Bottom);
+
+            return WindowInsetsCompat.Consumed;
         }
     }
 }
