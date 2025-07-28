823914dd421b50d9812f97bb2a658ce72591a616
Tomasz Cielecki <249719+Cheesebaron@users.noreply.github.com>
2025-07-28 09:22:32 +0200

    breaking: Annotate and split MvxViewModelInstanceRequest

--- MvvmCross/ViewModels/MvxViewModelInstanceRequest.cs
+++ MvvmCross/ViewModels/MvxViewModelInstanceRequest.cs
@@ -1,26 +1,33 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
+#nullable enable
 
-using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace MvvmCross.ViewModels
 {
-#nullable enable
-    public class MvxViewModelInstanceRequest : MvxViewModelRequest
+    public class MvxViewModelInstanceRequest(
+        [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] Type viewModelType)
+            : MvxViewModelRequest(viewModelType)
     {
         public IMvxViewModel? ViewModelInstance { get; set; }
+    }
 
-        public MvxViewModelInstanceRequest(Type viewModelType)
-            : base(viewModelType)
+    public class MvxViewModelInstanceRequest<
+        [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] TViewModel>
+            : MvxViewModelInstanceRequest
+                where TViewModel : IMvxViewModel
+    {
+        public MvxViewModelInstanceRequest()
+            : base(typeof(TViewModel))
         {
         }
 
         public MvxViewModelInstanceRequest(IMvxViewModel viewModelInstance)
-            : base(viewModelInstance.GetType(), null, null)
+            : base(typeof(TViewModel))
         {
             ViewModelInstance = viewModelInstance;
         }
     }
-#nullable restore
 }
