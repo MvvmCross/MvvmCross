4dab43cc16b08bc0e5b4672c1ecfd2c194550ff4
Sergey Nechaev <6499856+snechaev@users.noreply.github.com>
2025-10-28 20:41:57 +0100

    [Mac] Allow to specify window controller type in addition to name for the MvxWindowPresentationAttribute
    Fixes #5003

--- MvvmCross/Platforms/Mac/Presenters/MvxMacViewPresenter.cs
+++ MvvmCross/Platforms/Mac/Presenters/MvxMacViewPresenter.cs
@@ -221,7 +221,12 @@ namespace MvvmCross.Platforms.Mac.Presenters
             else
             {
                 // Instantiate using Reflection - failure is possible if blank constructor is missing
-                windowController = (MvxWindowController)Activator.CreateInstance(Type.GetType(attribute.WindowControllerName));
+                var controllerType = attribute.WindowControllerType ?? Type.GetType(attribute.WindowControllerName)
+                    ?? throw new MvxException($"Could not determine window controller type for the {attribute.ViewModelType?.Name ?? "<unknown vm>"} view model. " +
+                                              $"Please specify either in the {nameof(MvxWindowPresentationAttribute.WindowControllerType)} or " +
+                                              $"{nameof(MvxWindowPresentationAttribute.WindowControllerName)} property of the {nameof(MvxWindowPresentationAttribute)} " +
+                                              $"for the corresponding view model.");
+                windowController = (MvxWindowController)Activator.CreateInstance(controllerType);
             }
             windowController.ShouldCascadeWindows = attribute.ShouldCascadeWindows;
             return windowController;
