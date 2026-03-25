f70828e0433f969459372e2a971a5042ea2f57b7
Tomasz Cielecki <249719+Cheesebaron@users.noreply.github.com>
2026-03-25 10:47:53 +0100

    Fix logging and add custom binding

--- Projects/Playground/Playground.iOS/AppDelegate.cs
+++ Projects/Playground/Playground.iOS/AppDelegate.cs
@@ -1,20 +1,8 @@
 using MvvmCross.Platforms.Ios.Core;
-using Serilog;
 
 namespace Playground.iOS;
 
 // The UIApplicationDelegate for the application. This class is responsible for launching the
 // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
 [Register("AppDelegate")]
-public class AppDelegate : MvxSceneApplicationDelegate
-{
-    public override void FinishedLaunching(UIApplication application)
-    {
-        Log.Logger = new LoggerConfiguration()
-            .MinimumLevel.Verbose()
-            .WriteTo.Async(a => a.NSLog())
-            .WriteTo.Async(a => a.Trace())
-            .CreateLogger();
-        base.FinishedLaunching(application);
-    }
-}
+public class AppDelegate : MvxSceneApplicationDelegate;
--- Projects/Playground/Playground.iOS/SceneDelegate.cs
+++ Projects/Playground/Playground.iOS/SceneDelegate.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics.CodeAnalysis;
 using Microsoft.Extensions.DependencyInjection;
+using MvvmCross.Binding.Bindings.Target.Construction;
 using MvvmCross.DependencyInjection;
 using MvvmCross.Platforms.Ios.Core;
 using MvvmCross.Platforms.Ios.Hosting;
@@ -10,6 +11,8 @@ using MvvmCross.Plugin.Messenger;
 using MvvmCross.Plugin.Visibility.Platforms.Ios;
 using Playground.Core;
 using Playground.Core.ViewModels;
+using Playground.iOS.Bindings;
+using Playground.iOS.Controls;
 using Serilog;
 
 namespace Playground.iOS;
@@ -19,8 +22,18 @@ public class SceneDelegate : MvxSceneDelegate
 {
     public override void WillConnect(UIScene scene, UISceneSession session, UISceneConnectionOptions connectionOptions)
     {
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Verbose()
+            .WriteTo.Async(a => a.NSLog())
+            .WriteTo.Async(a => a.Trace())
+            .CreateLogger();
+
         Window = new UIWindow((UIWindowScene)scene);
         MvxIosHostBuilder.CreateBuilder(Window)
+            .ConfigureTargetBindings(registry =>
+                registry.RegisterCustomBindingFactory<BinaryEdit>(
+                    "MyCount",
+                    view => new BinaryEditTargetBinding(view)))
             .ConfigureServices(services =>
             {
                 services.AddLogging(l => l.AddSerilog());
