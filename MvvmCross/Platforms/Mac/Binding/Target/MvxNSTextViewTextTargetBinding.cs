5392a35eeacb58ed524c2b25ec9412e2e98727ca
Sergey Nechaev <6499856+snechaev@users.noreply.github.com>
2026-05-15 15:47:31 +0200

    Use NSTextView.Value property to bind text. Improvements towards #4909
    We now use a String-based API instead of an NSAttributedString-based API. As a result, we no longer modify any visual aspects of the text display (font color, etc) and the control can handle all of these things in the standard way. In particular, switching to the Dark Mode now works properly (previously, the text was displayed in black on a dark background)
    Also use weak event subscription.

--- MvvmCross/Platforms/Mac/Binding/Target/MvxNSTextViewTextTargetBinding.cs
+++ MvvmCross/Platforms/Mac/Binding/Target/MvxNSTextViewTextTargetBinding.cs
@@ -1,16 +1,19 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
-
+#nullable enable
 using System.Reflection;
 using Microsoft.Extensions.Logging;
 using MvvmCross.Binding;
 using MvvmCross.Binding.Bindings.Target;
+using MvvmCross.WeakSubscription;
 
 namespace MvvmCross.Platforms.Mac.Binding.Target
 {
     public class MvxNSTextViewTextTargetBinding : MvxPropertyInfoTargetBinding<NSTextView>
     {
+        private readonly IDisposable? _subscription;
+
         public MvxNSTextViewTextTargetBinding(NSTextView target, PropertyInfo targetPropertyInfo)
             : base(target, targetPropertyInfo)
         {
@@ -23,16 +26,16 @@ namespace MvvmCross.Platforms.Mac.Binding.Target
             else
             {
                 // Todo: Perhaps we want to trigger on editing complete rather than didChange
-                editText.TextDidChange += EditTextDidChange;
+                _subscription = editText.WeakSubscribe(nameof(NSTextView.TextDidChange), EditTextDidChange);
             }
         }
 
-        private void EditTextDidChange(object sender, EventArgs eventArgs)
+        private void EditTextDidChange(object? sender, EventArgs eventArgs)
         {
             var view = View;
             if (view == null)
                 return;
-            FireValueChanged(view.TextStorage.ToString());
+            FireValueChanged(view.Value);
         }
 
         public override MvxBindingMode DefaultMode
@@ -40,21 +43,18 @@ namespace MvvmCross.Platforms.Mac.Binding.Target
             get { return MvxBindingMode.TwoWay; }
         }
 
-        protected override void SetValueImpl(object target, object value)
+        protected override void SetValueImpl(object target, object? value)
         {
             base.SetValueImpl(target, value ?? "");
         }
 
+        [System.Diagnostics.CodeAnalysis.RequiresUnreferencedCode("Binding functionality accesses members dynamically through reflection.")]
         protected override void Dispose(bool isDisposing)
         {
             base.Dispose(isDisposing);
             if (isDisposing)
             {
-                var editText = View;
-                if (editText != null)
-                {
-                    editText.TextDidChange -= EditTextDidChange;
-                }
+                _subscription?.Dispose();
             }
         }
     }
